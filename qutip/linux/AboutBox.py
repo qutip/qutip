0572eb05255eb68956c8f748da9c06e730bf09cc
Paul Nation <nonhermitian@gmail.com>
2011-04-21 07:31:31 +0000

    updated AboutBox for new google code website

--- qutip/linux/AboutBox.py
+++ qutip/linux/AboutBox.py
@@ -12,7 +12,7 @@ class AboutBox(gtk.Window):
         about.set_copyright("Copyright (c) 2011")
         about.set_authors(['Paul D. Nation','Robert J. Johansson'])
         about.set_comments("The Quantum Toolbox in Python\nNumpy Version:      "+str(numpy.__version__)+"\nScipy Version:         "+str(scipy.__version__)+"\nMatplotlib Version:  "+str(matplotlib.__version__))
-        about.set_website("http://dml.riken.jp")
+        about.set_website("http://code.google.com/p/qutip/")
         about.set_logo(gtk.gdk.pixbuf_new_from_file(str(CD_BASE)+'/logo.png'))
         about.run()
         about.destroy()
