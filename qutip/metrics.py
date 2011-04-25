b8c8175bde9149bdc3fc999bcb036a5ef0b50a63
Paul Nation <nonhermitian@gmail.com>
2011-04-25 04:00:19 +0000

    added trace distance function

--- qutip/metrics.py
+++ qutip/metrics.py
@@ -16,27 +16,49 @@
 from Qobj import *
 import scipy.linalg as la
 from scipy import real,trace
-
-def fidelity(A,B):
+from numpy import vectorize
+def scalar_fidelity(A,B):
     """
-    calculates the fidelity (pseudo-metric) between two density matricies
+    Calculates the fidelity (pseudo-metric) between two density matricies.
     
     @param A: density matrix
     @param B: density matrix with same dimensions as A
+    
+    See: Nielsen & Chuang, "Quantum Computation and Quantum Information"
     """
     if A.dims!=B.dims:
         raise TypeError('Density matricies do not have same dimensions.')
     else:
-        a,ae=la.sqrtm(A.full(),disp=False)
-        b=B.full()
-        return real(trace(la.sqrtm(dot(a,dot(b,a)))))
+        A=A.sqrtm()
+        A*(B*A)
+        return real(trace((A*(B*A)).sqrtm().full()))
+
+fidelity=vectorize(scalar_fidelity)
+
+
+def scalar_trace_dist(A,B):
+    """
+    Calculates the trace distance between two density matricies.
+    
+    @param A: density matrix
+    @param B: density matrix with same dimensions as A
+    
+    See: Nielsen & Chuang, "Quantum Computation and Quantum Information"
+    """
+    diff=A-B
+    diff=diff.dag()*diff
+    out=diff.sqrtm().full()
+    return real(0.5*trace(out))
+
+trace_dist=vectorize(scalar_trace_dist)
 
 
 
 if __name__ == "__main__":
     from states import *
     x=coherent(10,3j)
-    y=coherent(10,1+3j)
+    y=coherent(10,1+1.5j)
     r1=x*x.dag()
     r2=y*y.dag()
-    print fidelity(r1,r2)
+    print fidelity([r1,r2],r2)
+    print trace_dist([r1,r2],r2)
