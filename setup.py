02fd3e1065b64f2292618583ebfaac14b3d6bece
Paul Nation <nonhermitian@gmail.com>
2011-05-10 04:32:58 +0000

    setup.py now installs all non-python files as well (i.e. installs logo and such)

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ from setuptools import setup, find_packages
  
 execfile('qutip/_version.py')
 setup(
-    name = "QuTIP",
+    name = "QuTiP",
     version = __version__,
     packages = find_packages(),
     author = "Paul D. Nation, Robert J. Johansson",
     author_email = "pnation@riken.jp, robert@riken.jp",
     license = "GPL3",
-    url = "http://dml.riken.jp"
+    description = ("A framework desgined to solve for the dynamics of open-quantum systems."),
+    keywords = "quantum physics dynamics",
+    url = "http://code.google.com/p/qutip/",
+    classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Topic :: Science",
+            "License :: GNU GPL3 License",
+        ],
+    include_package_data=True
+    
     
 )
