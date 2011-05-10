42e060c497dfc8e7e2d12476680de3066faa9c24
Robert Johansson <jrjohansson@gmail.com>
2011-05-10 04:16:31 +0000

    added support for time dependent hamiltonians in ode solvers. added an example that show rabi oscillations due to a time-depentent driving field

---
+++ examples/ex_driven_qubit_rabi.py
@@ -0,0 +1,85 @@
+#
+# Textbook example: Rabi oscillations in a driven two-level system. This
+# example show how to solve a problem with a arbitrarily time-dependent
+# Hamiltonian. 
+#
+from qutip import *
+import time
+
+def hamiltonian_t(t, args):
+    #
+    # evaluate the hamiltonian at time t. 
+    #
+    H0 = args[0]
+    H1 = args[1]
+    w  = args[2]
+
+    return H0 + H1 * sin(w * t)
+
+def sd_qubit_integrate(delta, eps0, A, w, gamma1, gamma2, psi0, tlist):
+
+    # Hamiltonian
+    sx = sigmax()
+    sz = sigmaz()
+    sm = destroy(2)
+
+    H0 = - delta/2.0 * sx - eps0/2.0 * sz
+    H1 = - A * sx
+        
+    H_args = (H0, H1, w)
+
+    # collapse operators
+    c_op_list = []
+
+    n_th = 0.0 # zero temperature
+
+    # relaxation
+    rate = gamma1 * (1 + n_th)
+    if rate > 0.0:
+        c_op_list.append(sqrt(rate) * sm)
+
+    # excitation
+    rate = gamma1 * n_th
+    if rate > 0.0:
+        c_op_list.append(sqrt(rate) * sm.dag())
+
+    # dephasing 
+    rate = gamma2
+    if rate > 0.0:
+        c_op_list.append(sqrt(rate) * sz)
+
+
+    # evolve and calculate expectation values
+    expt_list = me_ode_solve(hamiltonian_t, psi0, tlist, c_op_list, [sm.dag() * sm], H_args)  
+    #expt_list = wf_ode_solve(hamiltonian_t, psi0, tlist, [sm.dag() * sm], H_args)  
+
+    return expt_list[0]
+    
+#
+# set up the calculation
+#
+delta = 0.0  * 2 * pi   # qubit sigma_x coefficient
+eps0  = 1.0  * 2 * pi   # qubit sigma_z coefficient
+A     = 0.05 * 2 * pi   # driving amplitude (sigma_x coupled)
+w     = 1.0  * 2 * pi   # driving frequency
+
+gamma1 = 0.025          # relaxation rate
+gamma2 = 0.0            # dephasing  rate
+
+# intial state
+psi0 = basis(2,0)
+
+tlist = linspace(0,50,500)
+
+start_time = time.time()
+p_ex = sd_qubit_integrate(delta, eps0, A, w, gamma1, gamma2, psi0, tlist)
+print 'time elapsed = ' + str(time.time() - start_time) 
+
+plot(tlist, real(p_ex))
+xlabel('Time')
+ylabel('P_ex')
+title('Excitation probabilty of qubit')
+#savefig("qubit_rabi.png")
+show()
+
+
--- qutip/ode_solve.py
+++ qutip/ode_solve.py
@@ -15,7 +15,7 @@
 ###########################################################################
 
 import random
-
+from types import *
 from scipy.integrate import *
 
 from Qobj import *
@@ -26,10 +26,13 @@ from expect import *
 # ------------------------------------------------------------------------------
 # Wave function evolution using a ODE solver (unitary quantum evolution)
 # 
-def wf_ode_solve(H, psi0, tlist, expt_op_list):
+def wf_ode_solve(H, psi0, tlist, expt_op_list, H_args=None):
     """!
     @brief Evolve the wave function using an ODE solver
     """
+
+    if isinstance(H, FunctionType):
+        return wf_ode_solve_td(H, psi0, tlist, expt_op_list, H_args)
 
     n_expt_op = len(expt_op_list)
     n_tsteps  = len(tlist)
@@ -79,15 +82,81 @@ def wf_ode_solve(H, psi0, tlist, expt_op_list):
 def psi_ode_func(t, psi, H):
     return -1j * (H * psi)
 
+# ------------------------------------------------------------------------------
+# Wave function evolution using a ODE solver (unitary quantum evolution), for
+# time dependent hamiltonians
+# 
+def wf_ode_solve_td(H_func, psi0, tlist, expt_op_list, H_args):
+    """!
+    @brief Evolve the wave function using an ODE solver
+    """
+
+    n_expt_op = len(expt_op_list)
+    n_tsteps  = len(tlist)
+    dt        = tlist[1]-tlist[0]
+
+    if n_expt_op == 0:
+        result_list = [Qobj() for k in xrange(n_tsteps)]
+    else:
+        result_list = zeros([n_expt_op, n_tsteps], dtype=complex)
+
+    if not isket(psi0):
+        raise TypeError("psi0 must be a ket")
+
+    #
+    # setup integrator
+    #
+    H_func_and_args = (H_func, H_args)
+    initial_vector = psi0.full()
+    r = scipy.integrate.ode(psi_ode_func_td).set_integrator('zvode').set_initial_value(initial_vector, tlist[0]).set_f_params(H_func_and_args)
+
+    #
+    # start evolution
+    #
+    psi = Qobj(psi0)
+
+    t_idx = 0
+    for t in tlist:
+        if not r.successful():
+            break;
+
+        psi.data = r.y
+
+        # calculate all the expectation values, or output psi if no operators where given
+        if n_expt_op == 0:
+            result_list[t_idx] = Qobj(psi) # copy rho
+        else:
+            for m in range(0, n_expt_op):
+                result_list[m,t_idx] = expect(expt_op_list[m], psi)
+
+        r.integrate(r.t + dt)
+        t_idx += 1
+          
+    return result_list
+
+#
+# evaluate dpsi(t)/dt for time-dependent hamiltonian
+#
+def psi_ode_func_td(t, psi, H_func_and_args):
+    H_func = H_func_and_args[0]
+    H_args = H_func_and_args[1]
+
+    H = H_func(t, H_args)
+
+    return -1j * (H.data * psi)
+
 
 # ------------------------------------------------------------------------------
 # Master equation solver
 # 
-def me_ode_solve(H, rho0, tlist, c_op_list, expt_op_list):
+def me_ode_solve(H, rho0, tlist, c_op_list, expt_op_list, H_args=None):
     """!
     @brief Evolve the density matrix using an ODE solver
     """
     n_op      = len(c_op_list)
+
+    if isinstance(H, FunctionType):
+        return me_ode_solve_td(H, rho0, tlist, c_op_list, expt_op_list, H_args)
 
     #
     # check initial state
@@ -160,7 +229,94 @@ def rho_ode_func(t, rho, L):
 
 
 
+# ------------------------------------------------------------------------------
+# Master equation solver
+# 
+def me_ode_solve_td(H_func, rho0, tlist, c_op_list, expt_op_list, H_args):
+    """!
+    @brief Evolve the density matrix using an ODE solver with time dependent
+    Hamiltonian.
+    """
+    n_op      = len(c_op_list)
 
+    #
+    # check initial state
+    #
+    if isket(rho0):
+        # if initial state is a ket and no collapse operator where given,
+        # fallback on the unitary schrodinger equation solver
+        if n_op == 0:
+            return wf_ode_solve_td(H_func, rho0, tlist, expt_op_list, H_args)
+
+        # Got a wave function as initial state: convert to density matrix.
+        rho0 = rho0 * rho0.dag()
+
+    #
+    # prepare output array
+    # 
+    n_expt_op = len(expt_op_list)
+    n_tsteps  = len(tlist)
+    dt        = tlist[1]-tlist[0]
+
+    if n_expt_op == 0:
+        result_list = [Qobj() for k in xrange(n_tsteps)]
+    else:
+        result_list = zeros([n_expt_op, n_tsteps], dtype=complex)
+
+    #
+    # construct liouvillian
+    #
+    L = 0 #-1j*(spre(H) - spost(H))
+    for m in range(0, n_op):
+        cdc = c_op_list[m].dag() * c_op_list[m]
+        L += spre(c_op_list[m])*spost(c_op_list[m].dag())-0.5*spre(cdc)-0.5*spost(cdc)
+
+    #
+    # setup integrator
+    #
+    H_func_and_args = (H_func, H_args, L)
+    initial_vector = rho0.full().reshape(prod(rho0.shape),1)
+    r = scipy.integrate.ode(rho_ode_func_td).set_integrator('zvode').set_initial_value(initial_vector, tlist[0]).set_f_params(H_func_and_args)
+
+    #
+    # start evolution
+    #
+    rho = Qobj(rho0)
+
+    t_idx = 0
+    for t in tlist:
+        if not r.successful():
+            break;
+
+        rho.data = r.y.reshape(rho0.shape)
+
+        # calculate all the expectation values, or output rho if no operators
+        if n_expt_op == 0:
+            result_list[t_idx] = Qobj(rho) # copy rho
+        else:
+            for m in range(0, n_expt_op):
+                result_list[m,t_idx] = expect(expt_op_list[m], rho)
+
+        r.integrate(r.t + dt)
+        t_idx += 1
+          
+    return result_list
+
+
+#
+# evaluate drho(t)/dt according to the master eqaution
+#
+def rho_ode_func_td(t, rho, H_func_and_args):
+
+    H_func = H_func_and_args[0]
+    H_args = H_func_and_args[1]
+    L0     = H_func_and_args[2]
+
+    H = H_func(t, H_args)
+
+    L = L0 - 1j*(spre(H) - spost(H))
+
+    return L.data * rho
 
 
 
