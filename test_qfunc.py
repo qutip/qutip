c9f8c69d169681faccc02d7c8d4df8ab6c732db0
Paul Nation <nonhermitian@gmail.com>
2011-05-10 03:56:01 +0000

    cleanup

--- test_qfunc.py
+++ test_qfunc.py
@@ -2,7 +2,6 @@ from qutip import *
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from pylab import *
-
 N = 20;
 g = 2;
 psi = basis(N,10) + basis(N,5)
@@ -10,8 +9,8 @@ xvec = arange(-5.,5.,.1)
 yvec = xvec
 X,Y = meshgrid(xvec, yvec)
 Q = qfunc(psi,xvec,xvec,g);
-fig1 = plt.figure()
+fig1 = figure()
 ax = Axes3D(fig1)
 ax.plot_surface(X, Y, Q, rstride=2, cstride=2, cmap=cm.jet, alpha=0.7)
-plt.show()
+show()
 
