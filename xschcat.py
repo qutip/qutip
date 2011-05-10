76798a5856d1ed3e0ff4ce5e4ae62a5481ed66f0
Paul Nation <nonhermitian@gmail.com>
2011-05-10 01:28:51 +0000

    cleaned up to put on WIKI

--- xschcat.py
+++ xschcat.py
@@ -6,9 +6,9 @@
 from qutip import *
 from pylab import *
 
-N = 20;
+N = 40;
 #amplitudes of coherent states
-alpha1=-2-1j
+alpha1=-1-1j
 alpha2=1+1j
 #define ladder oeprators
 a = destroy(N);
@@ -19,7 +19,7 @@ D2=(alpha2*dag(a)-conj(alpha2)*a).expm()
 psi = sqrt(2)**-1*(D1+D2)*basis(N,0); # Apply to vacuum state
 g=2.
 #calculate Wigner function
-xvec = arange(-40.,40.)*5./40
+xvec = arange(-100.,100.)*5./100
 yvec=xvec
 W=wigner(psi,xvec,yvec)
 pcolor(xvec,yvec,real(W))
@@ -27,8 +27,22 @@ xlim([-5,5])
 ylim([-5,5])
 title('Wigner function of Schrodinger cat')
 colorbar()
-show()
 
+
+
+#X,Y = meshgrid(xvec, xvec)
+#fig1 = plt.figure()
+#ax = Axes3D(fig1)
+#ax.plot_surface(X, Y, W, rstride=3, cstride=3, cmap=cm.jet, alpha=0.9)
+#ax.contour(X, Y, W, 15,zdir='x', offset=-6)
+#ax.contour(X, Y, W, 15,zdir='y', offset=6)
+#ax.contour(X, Y, W, 15,zdir='z', offset=-0.3)
+#ax.set_xlim3d(-5,5)
+#ax.set_xlim3d(-5,5)
+#ax.set_zlim3d(-0.2,0.2)
+#title('Wigner Function of Cat-State');
+
+show()
 #calculate Q function
 Q=qfunc(psi,xvec,yvec)
 pcolor(xvec,yvec,real(Q))
