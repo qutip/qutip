0ed6bcc3829da4263e70f2eeda6ab26df28bd7f8
Paul Nation <nonhermitian@gmail.com>
2011-05-17 13:54:03 +0000

    changed way sphere is produced to make data "inside" sphere.  reduced number of plotting points to speed up rendering. added more options.

--- qutip/Bloch.py
+++ qutip/Bloch.py
@@ -16,6 +16,7 @@
 # Copyright (C) 2011, Paul D. Nation & Robert J. Johansson
 #
 ###########################################################################
+import os
 from scipy import *
 from matplotlib import pyplot as plt
 from pylab import *
@@ -28,15 +29,20 @@ mpl.rcParams['text.usetex'] = True
 class Bloch():
     def __init__(self):
         #sphere options
-        self.sphere_color='#FFDDDD';self.sphere_alpha=0.3
+        self.sphere_color='#FFDDDD'
+        self.sphere_alpha=0.2
         #frame options
-        self.frame_color='gray';self.frame_alpha=0.2
+        self.frame_color='gray'
+        self.frame_alpha=0.2
         #font options
-        self.font_color='black';self.font_size=18
+        self.font_color='black'
+        self.font_size=18
         #vector options
-        self.vector_color=['b']
+        self.vector_color=['b','g','r','y']
         #point options
-        self.point_color=['r'];self.point_size=15
+        self.point_color=['b','g','r','y']
+        self.point_size=20
+        self.point_marker=['o','^','d','s']
         #data lists
         self.points=[]
         self.num_points=0
@@ -62,22 +68,47 @@ class Bloch():
         print str(len(self.vectors))+ ' vectors'
         print str(len(self.points))+ ' data points'
         return ''
-    def reset(self):
+    def clear(self):
         """Resets Bloch sphere"""
-        bloch_reset(self)
+        bloch_clear(self)
+    
     def add_points(self,points):
         points=array(points)
         self.points.append(points)
         self.num_points=len(self.points)
+        if self.num_points>len(self.point_color):
+            str1='num. of points > num. of point colors'
+        else:
+            str1=''
+        if self.num_points>len(self.point_marker):
+            str2='num. of points > num. of point markers'
+        else:
+            str2=''
+        if self.num_points>len(self.point_color) or self.num_points>len(self.point_marker):
+            print str1+'\n'+str2
+            
     def add_vectors(self,vectors): 
         self.vectors.append(vectors)
+        self.num_vectors=len(self.vectors)
+        if self.num_vectors>len(self.vector_color):
+            print 'num. of vectors > num. of vector colors'
+    
     def make_sphere(self):
-        self.sphere=0
-        #ion()
+        try:#close figure if self.show() has already been run
+            close(self.fig)
+        except:
+            pass
         #setup plot
         self.fig = figure()
         self.ax = Axes3D(self.fig)
         self.ax.grid(on=False)
+        self.plot_back()
+        self.plot_vectors()
+        self.plot_points()
+        self.plot_front()
+        self.plot_axes()
+    
+    def plot_back(self):    
         #----back half of sphere------------------
         u = linspace(0, pi, 25)
         v = linspace(0, pi, 25)
@@ -90,18 +121,8 @@ class Bloch():
         #equator
         self.ax.plot(1.0*cos(u),1.0*sin(u),zs=0, zdir='z',lw=1.0,color=self.frame_color)
         self.ax.plot(1.0*cos(u),1.0*sin(u),zs=0, zdir='x',lw=1.0,color=self.frame_color)
-        #-----------------------------------------
-        #data
-        self.plot_vectors()
-        self.plot_points()
-        #axes
-        span=linspace(-1.0,1.0,2)
-        self.ax.plot(span,0*span, zs=0, zdir='z', label='X',lw=1.0,color=self.frame_color)
-        self.ax.plot(0*span,span, zs=0, zdir='z', label='Y',lw=1.0,color=self.frame_color)
-        self.ax.plot(0*span,span, zs=0, zdir='y', label='Z',lw=1.0,color=self.frame_color)
-        self.ax.set_xlim3d(-1.2,1.2)
-        self.ax.set_ylim3d(-1.3,1.2)
-        self.ax.set_zlim3d(-1.2,1.2)
+    
+    def plot_front(self):    
         #front half of sphere-----------------------
         u = linspace(-pi, 0,25)
         v = linspace(0, pi, 25)
@@ -114,7 +135,8 @@ class Bloch():
         #equator
         self.ax.plot(1.0*cos(u),1.0*sin(u),zs=0, zdir='z',lw=1.0,color=self.frame_color)
         self.ax.plot(1.0*cos(u),1.0*sin(u),zs=0, zdir='x',lw=1.0,color=self.frame_color)
-        #------------------------------------------
+    
+    def plot_axes(self):
         #axes labels
         self.ax.text(0, -1.2, 0, r"$x$", color=self.font_color,fontsize=self.font_size)
         self.ax.text(1.1, 0, 0, r"$y$", color=self.font_color,fontsize=self.font_size)
@@ -126,57 +148,52 @@ class Bloch():
             a.set_visible(False)
         for a in self.ax.w_zaxis.get_ticklines()+self.ax.w_zaxis.get_ticklabels():
             a.set_visible(False)
-        self.sphere=self.ax
-        #ioff()
+        #axes
+        span=linspace(-1.0,1.0,2)
+        self.ax.plot(span,0*span, zs=0, zdir='z', label='X',lw=1.0,color=self.frame_color)
+        self.ax.plot(0*span,span, zs=0, zdir='z', label='Y',lw=1.0,color=self.frame_color)
+        self.ax.plot(0*span,span, zs=0, zdir='y', label='Z',lw=1.0,color=self.frame_color)
+        self.ax.set_xlim3d(-1.2,1.2)
+        self.ax.set_ylim3d(-1.3,1.2)
+        self.ax.set_zlim3d(-1.2,1.2)
+    
     def plot_vectors(self):
         if len(self.vectors)>0:
-            for k in range(len(self.vectors)):
+            for k in xrange(len(self.vectors)):
                 length=sqrt(self.vectors[k][0]**2+self.vectors[k][1]**2+self.vectors[k][2]**2)
-                self.ax.plot(self.vectors[0][0]*linspace(0,length,2),self.vectors[0][1]*linspace(0,length,2),self.vectors[0][2]*linspace(0,length,2),zs=0, zdir='z', label='Z',lw=3,color='b')
+                self.ax.plot(self.vectors[k][1]*linspace(0,length,2),-self.vectors[k][0]*linspace(0,length,2),self.vectors[k][2]*linspace(0,length,2),zs=0, zdir='z', label='Z',lw=3,color=self.vector_color[k])
+    
     def plot_points(self):
-        for k in range(self.num_points):
-            self.ax.scatter(real(self.points[k][1]),-real(self.points[k][0]),real(self.points[k][2]),s=15,alpha=1,edgecolor='none',zdir='z',color='r', marker='o')
+        for k in xrange(self.num_points):
+            self.ax.scatter(real(self.points[k][1]),-real(self.points[k][0]),real(self.points[k][2]),s=self.point_size,alpha=1,edgecolor='none',zdir='z',color=self.point_color[k], marker=self.point_marker[k])
+    
     def show(self):
         self.make_sphere()
         show()
+    
     def animate(self):
-        self.make_sphere()
+        bloch_animate(self)
         
 
-
-def bloch_reset(self):
-    #sphere options
-    self.sphere_color='#FFDDDD'
-    self.sphere_alpha=0.3
-    #frame options
-    self.frame_color='gray'
-    self.frame_alpha=0.2
-    #font options
-    self.font_color='black'
-    self.font_size=18
-    #vector options
-    self.vector_color=['b']
-    #point options
-    self.point_color=['r']
-    self.point_size=15
-    #data lists
+def bloch_clear(self):
     self.points=[]
     self.num_points=0
     self.vectors=[]
     self.num_vectors=0
-    self.sphere=0
 
-
-
+def bloch_animate(sphere):
+    num_plots=len(sphere.points[0][0])
+    print num_plots
+        
 
 if __name__=="__main__":
+    pass
     #vector,x and y axes are switched so the shading function works properly.
-    #ax.scatter([0.5], [-.5], [1-sqrt(2*.5**2)], s=15,alpha=1,edgecolor='none',zdir='z',color='r', marker='o')
-    #length=linspace(0,1,10)
-    #ax.plot(0.707*length,0.0*length,(0.707)*length, zs=0, zdir='z', label='Z',lw=3,color='b')
-    #plt.savefig('bloch.pdf',format='pdf')
     x=Bloch()
-    length=linspace(0,1,2)
-    vec=[0.707,0.0,0.707]
-    x.add_vectors(vec)
+    xvec=[1,0,0]
+    x.add_vectors(xvec)
+    yvec=[0,1,0]
+    x.add_vectors(yvec)
+    zvec=[0,0,1]
+    x.add_vectors(zvec)
     x.show()
