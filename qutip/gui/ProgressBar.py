797de981ebaf17e28f9dd0e52c84c8af1144ae95
Paul Nation <nonhermitian@gmail.com>
2011-05-25 07:16:34 +0000

    updated for PyQt

--- qutip/gui/ProgressBar.py
+++ qutip/gui/ProgressBar.py
@@ -1,80 +1,77 @@
-#This file is part of QuTIP.
-#
-#    QuTIP is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#    QuTIP is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with QuTIP.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Copyright (C) 2011, Paul D. Nation & Robert J. Johansson
-#
-###########################################################################
-import sys,time
+import sys,time,threading
+from multiprocessing import Pool
+
 try:
-    from PySide.QtCore import *
-    from PySide.QtGui import *
+    from PySide import QtCore
+    from PySide import QtGui
 except:
     try:
-        from PyQt4.QtCore import *
-        from PyQt4.QtGui import *
+        from PyQt4 import QtCore
+        from PyQt4 import QtGui
     except:
-        raise RuntimeError('PyQt4 or PySide GUI module is not installed.')
+        raise TypeError('no graphics installed')
+        
+if sys.platform.startswith("darwin"):#needed for PyQt on mac (because of pyobc) 
+    import Foundation
 
-
-class ProgressBar(QWidget):
-    def __init__(self, parent = None):
-        QWidget.__init__(self, parent)
-        self.thread = Worker()
-        self.percent = 0
-        self.pbar = QProgressBar(self)
+class ProgressBar(QtGui.QWidget):
+    def __init__(self,top,thread,mx,parent = None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setWindowFlags(QtCore.Qt.Window|QtCore.Qt.CustomizeWindowHint|QtCore.Qt.WindowTitleHint|QtCore.Qt.WindowMinimizeButtonHint)
+        self.top=top
+        self.max=mx
+        self.time=time.time()
+        self.num = 0
+        self.pbar = QtGui.QProgressBar(self)
         self.pbar.setStyleSheet("QProgressBar {width: 25px;border: 3px solid black; border-radius: 5px; background: white;text-align: center;padding: 0px;}" 
                                +"QProgressBar::chunk:horizontal {background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #6699FF, stop: 0.5 #66AAFF, stop: 0.6 #66CCFF, stop:1 #FFFFFF);}")
         self.pbar.setGeometry(25, 40, 300,40)
-        self.label = QLabel(self)
+        self.label = QtGui.QLabel(self)
         self.label.setStyleSheet("QLabel {font-size: 12px;}")
-        self.label.setText("Trajectories completed:                                       a")
+        self.label.setText("Trajectories completed:                                       ")
         self.label.move(25, 20)
         self.setWindowTitle('Monte-Carlo Trajectories')
         self.setGeometry(300, 300, 350, 120)
         #self.setWindowOpacity(0.9) #make transparent
-        screen = QDesktopWidget().screenGeometry()
+        screen = QtGui.QDesktopWidget().screenGeometry()
         size =  self.geometry()
         self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
-        self.setFixedSize(self.size());
-        self.connect(self.thread, SIGNAL("completed"), self.updateUi)  
-        self.connect(self.thread, SIGNAL("done"), self.end)
-        self.thread.start()
-    def updateUi(self):
-        self.percent+=1
-        self.pbar.setValue(self.percent)
-        self.label.setText('Trajectories completed: '+str(self.percent)+ '/100')
-    def end(self):
-        time.sleep(0.5)
-        return self.close()
-            
-class Worker(QThread):
-    def __init__(self, parent = None):
-        QThread.__init__(self, parent)
-        self.exiting = False
+        self.setFixedSize(self.size());   
+        self.thread=thread
+        self.connect(self.thread,QtCore.SIGNAL("completed"), self.update)
+        self.connect(self.thread,QtCore.SIGNAL("done"), self.end)
+    def update(self,*args):
+        # Old style: emits the signal using the SIGNAL function.
+        self.num+=1
+        self.pbar.setValue((100.0*self.num)/self.max)
+        self.label.setText("Trajectories completed: "+ str(self.num)+"/"+str(self.max))
     def run(self):
-       # Note: This is never called directly. It is called by Qt once the
-       # thread environment has been set up.
-       for n in range(1,101):
-           self.emit(SIGNAL("completed"))
-           time.sleep(.05)
-       return self.emit(SIGNAL("done"))
-       
-           
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    pbar = ProgressBar()
-    pbar.show()
-    pbar.raise_()
-    app.exec_()
+        self.thread.start()
+    def end(self):
+        return self.close()
+
+
+
+
+
+class Thread(QtCore.QThread):
+    def __init__(self,target=None,args=None,top=None,parent=None):
+        QtCore.QThread.__init__(self,parent)
+        self.target = target
+        self.args=args
+        self.top=top
+        self.exiting = True
+    def run(self):
+        if sys.platform.startswith("darwin"):#needed for PyQt on mac (because of pyobc) 
+            pool = Foundation.NSAutoreleasePool.alloc().init()
+        self.target(self.args,self)
+        return self.emit(QtCore.SIGNAL("done"))
+    def callback(self,args):
+        self.emit(QtCore.SIGNAL("completed"))
+        self.top.callback(args)
+
+
+
+
+
+ 
