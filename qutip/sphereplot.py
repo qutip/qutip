3828f23d364a23dd5b33b4c0e53afdfcb2e785a2
Paul Nation <nonhermitian@gmail.com>
2011-06-15 04:47:00 +0000

    added documentation

--- qutip/sphereplot.py
+++ qutip/sphereplot.py
@@ -18,6 +18,13 @@
 ###########################################################################
 from scipy import *
 def sphereplot(theta,phi,values):
+	"""
+	@brief Plots a matrix of values on a sphere
+	@param theta Angle with respect to z-axis
+	@param phi Angle in x-y plane 
+	@param values data set to be plotted
+	@return None Plots figure, but returns nothing
+	"""
 	from matplotlib import pyplot, mpl,cm
 	from pylab import plot,show,meshgrid,figure
 	from mpl_toolkits.mplot3d import Axes3D
@@ -27,12 +34,18 @@ def sphereplot(theta,phi,values):
 	zz=cos(thetam)
 	r=array(abs(values))
 	ph=angle(values)
+	#normalize color range based on phase angles in list ph
 	nrm=mpl.colors.Normalize(ph.min(),ph.max())
 	fig = figure()
 	ax =Axes3D(fig)
+	#plot with facecolors set to cm.jet colormap normalized to nrm
 	surf=ax.plot_surface(r*xx,r*yy,r*zz,rstride=1, cstride=1,facecolors=cm.jet(nrm(ph)),linewidth=0)
+	#create new axes on plot for colorbar and shrink it a bit.  pad shifts location of bar
+	#with repsect to the main plot
 	cax,kw=mpl.colorbar.make_axes(ax,shrink=.66,pad=.02)
+	#create new colorbar in axes cax with cm jet and normalized to nrm like our facecolors
 	cb1=mpl.colorbar.ColorbarBase(cax,cmap=cm.jet,norm=nrm)
+	#add our colorbar label
 	cb1.set_label('Angle')
 	show()
 	return 
