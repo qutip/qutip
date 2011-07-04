e15db75cb1f25de3bb5e670b97f8cf3289b3a9d1
Paul Nation <nonhermitian@gmail.com>
2011-07-04 07:35:41 +0000

    fixed error where demos could only be launched once per terminal session

--- qutip/demos.py
+++ qutip/demos.py
@@ -38,6 +38,7 @@ def demos():
     else:
         opts=array([123456,11,12,13,14,15,21,22,23,24,25,31,32,33,34,35,41,42,43,44,45])
         lopts=arange(len(opts))
+    exconfig.option=0
     while exconfig.option<123456:
         exconfig.option=123456
         if os.environ['QUTIP_GRAPHICS']=='YES':
