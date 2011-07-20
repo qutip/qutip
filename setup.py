eee0a6b1b3fa6c2691a69db78033a809ef1a7deb
Paul Nation <nonhermitian@gmail.com>
2011-07-20 14:24:31 +0000

    updated in preparation for release

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ Operating System :: Unix
 Operating System :: MacOS
 """
 
-MAJOR               = 0
-MINOR               = 1
+MAJOR               = 1
+MINOR               = 0
 MICRO               = 0
 ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
