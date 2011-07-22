58828452aca0b0ebcaadd543da8beee995c0814b
Paul Nation <nonhermitian@gmail.com>
2011-07-22 13:45:56 +0000

    updated about box to show GUI library versions as well

--- qutip/gui/AboutBox.py
+++ qutip/gui/AboutBox.py
@@ -49,24 +49,51 @@ class AboutBox(QtGui.QWidget):
         tlabel.setText("QuTiP: The Quantum Toolbox in Python")
         tlabel.move(30, 170)
         #
+        try:
+            import PySide
+            pyside_ver=PySide.__version__
+        except:
+            pyside_ver='None'
+        try:
+            import PyQt4
+            pyqt4_ver=PyQt4.__version__
+        except:
+            pyqt4_ver='None'
+        if sys.platform=='darwin':
+            try:
+                import Foundation
+                pyobjc='Yes'
+            except:
+                pyobjc='No'
         label = QtGui.QLabel(self)
         label.setStyleSheet("QLabel {font-weight: bold;font-size: 12px;}")
-        label.setText("QuTiP Version:          "+Qversion+"\n"
+        if sys.platform!='darwin':
+            label.setText("QuTiP Version:          "+Qversion+"\n"
                             +"NumPy Version:         "+str(numpy.__version__)+"\n"
                             +"SciPy Version:            "+str(scipy.__version__)+"\n"
-                            +"MatPlotLib Version:   "+str(matplotlib.__version__))
+                            +"MatPlotLib Version:   "+str(matplotlib.__version__)+"\n\n"
+                            +"PySide Version:         "+str(pyside_ver)+"\n"
+                            +"PyQt4 Version:          "+str(pyqt4_ver))
+        else:
+            label.setText("QuTiP Version:          "+Qversion+"\n"
+                            +"NumPy Version:         "+str(numpy.__version__)+"\n"
+                            +"SciPy Version:            "+str(scipy.__version__)+"\n"
+                            +"MatPlotLib Version:   "+str(matplotlib.__version__)+"\n\n"
+                            +"PySide Version:         "+str(pyside_ver)+"\n"
+                            +"PyQt4 Version:          "+str(pyqt4_ver)+"\n"
+                            +"PyObjc Installed:       "+str(pyobjc))
         label.move(115, 210)
         #
         alabel = QtGui.QLabel(self)
         alabel.setStyleSheet("QLabel {font-weight: bold;font-size: 12px;}")
         alabel.setText("Copyright (c) 2011, Paul D. Nation & Robert J. Johansson")
-        alabel.move(30, 300)
+        alabel.move(30, 350)
         #
         clabel = QtGui.QLabel(self)
         clabel.setStyleSheet("QLabel {font-weight: bold;font-size: 12px;}")
         clabel.setText("QuTiP is released under the GPL3.\n"
                         +"See the enclosed COPYING.txt\nfile for more information.")
-        clabel.move(30, 330)
+        clabel.move(30, 380)
         #BUTTONS-----------------
         quit = QtGui.QPushButton('Close', self)
         quit.setGeometry(340, 400, 80, 40)
