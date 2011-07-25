07214b93170ecd33f65d8216ea03dd5ba8f36d88
Paul Nation <nonhermitian@gmail.com>
2011-07-25 15:20:19 +0000

    cleaned up output print command

--- qutip/Odeoptions.py
+++ qutip/Odeoptions.py
@@ -20,16 +20,16 @@ class Odeoptions():
         ##Maximum order used by integrator (<=12 for 'adams', <=5 for 'bdf')
         self.order=12
     def __str__(self):
-        print "OPTIONS FOR ODE SOLVERS:"
-        print "------------------------------------"
-        print 'atol =',self.atol,': Absolute tolerance (default = 1e-12)\n'
-        print 'rtol =',self.rtol,': Relative tolerance (default = 1e-8)\n'
-        print 'method =',self.method,': Integration method (default="adams", use "bdf" for stiff problems)\n'
-        print 'nsteps =',self.nsteps,': Max. number of internal steps allowed per call (default=1000)\n'
-        print 'first_step =',self.first_step,': Size of initial step (default=0, determined by solver)\n'
-        print 'min_step =',self.min_step,': Minimal step size (default=0, determined by solver)\n'
-        print 'max_step =',self.max_step,': Max step size (default=0, determined by solver)\n'
-        print 'order =',self.order,': Maximum order used by integrator (<=12 for "adams", <=5 for "bdf")\n'
+        print "Odeoptions properties:"
+        print "----------------------"
+        print 'atol:       ',self.atol
+        print 'rtol:       ',self.rtol
+        print 'method:     ',self.method
+        print 'order:      ',self.order
+        print 'nsteps:     ',self.nsteps
+        print 'first_step: ',self.first_step
+        print 'min_step:   ',self.min_step
+        print 'max_step:   ',self.max_step
         return ''
 
 
