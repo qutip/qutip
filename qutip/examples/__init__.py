debd557baf632ad6fdbb6fdab936d0deb1bff7a4
Paul Nation <nonhermitian@gmail.com>
2011-07-28 03:44:45 +0000

    added copyright to example scripts

--- qutip/examples/__init__.py
+++ qutip/examples/__init__.py
@@ -1,3 +1,21 @@
+#This file is part of QuTIP.
+#
+#    QuTIP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#    QuTIP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with QuTIP.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2011, Paul D. Nation & Robert J. Johansson
+#
+###########################################################################
 from blochdemo import blochdemo
 from cavityqubitmc import cavityqubitmc
 from cqsteady import cqsteady
@@ -16,4 +34,4 @@ from jc_lasing import jc_lasing
 from td_rabi import td_rabi
 from td_landauzener import td_landauzener
 from fidelity_example import fidelity_example
-from esdemo import esdemo
+#from esdemo import esdemo
