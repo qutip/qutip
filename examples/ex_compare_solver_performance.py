d9393e0af7a555ec7c820639fe63492081e4f9af
Robert Johansson <jrjohansson@gmail.com>
2011-07-28 06:23:31 +0000

    test both unitary and nonunitary solvers

--- examples/ex_compare_solver_performance.py
+++ examples/ex_compare_solver_performance.py
@@ -1,5 +1,5 @@
 #
-# Textbook example: Rabi oscillation in the dissipative Jaynes-Cummings model.
+# Benchmark for solvers.
 # 
 #
 from qutip import *
@@ -34,13 +34,17 @@ def system_integrate(Na, Nb, wa, wb, wab, ga, gb, solver):
         c_op_list.append(sqrt(rate) * b)
 
     if solver == "me":
-        expt_list = odesolve(H, psi0, tlist, c_op_list, [na, nb])
+        expt_list = odesolve(H, psi0 * psi.dag(), tlist, c_op_list, [na, nb])
     elif solver == "wf":
         expt_list = odesolve(H, psi0, tlist, [], [na, nb])
     elif solver == "es":
         expt_list = essolve(H, psi0, tlist, c_op_list, [na, nb])
-    elif solver == "mc":
-        expt_list = mcsolve(H, psi0, tlist, 1, [], [na, nb])
+    elif solver == "mc1":
+        expt_list = mcsolve(H, psi0, tlist, 1, c_op_list, [na, nb])
+    elif solver == "mc250":
+        expt_list = mcsolve(H, psi0, tlist, 250, c_op_list, [na, nb])
+    elif solver == "mc500":
+        expt_list = mcsolve(H, psi0, tlist, 500, c_op_list, [na, nb])
     else:
         raise ValueError("unknown solver")
         
@@ -52,16 +56,18 @@ def system_integrate(Na, Nb, wa, wb, wab, ga, gb, solver):
 wa  = 1.0 * 2 * pi   # frequency of system a
 wb  = 1.0 * 2 * pi   # frequency of system a
 wab = 0.1 * 2 * pi   # coupling frequency
-ga = 0.01            # dissipation rate of system a
-gb = 0.00            # dissipation rate of system b
+ga = 0.0             # dissipation rate of system a
+gb = 0.0             # dissipation rate of system b
 Na = 2               # number of states in system a
 Nb = 2               # number of states in system b
 
 tlist = linspace(0, 10, 200)
 
-#solvers = ("me", "wf", "es", "mc")
-solvers = ("me", "wf", "mc")
-Na_vec = arange(2, 12, 1)
+#
+# First test unitary solvers
+#
+solvers = ("me", "wf", "mc1", "es")
+Na_vec = arange(2, 35, 1)
 
 times = zeros((len(Na_vec), len(solvers)))
 
@@ -78,16 +84,57 @@ for n_run in range(n_runs):
             start_time = time.time()
             na, nb = system_integrate(Na, Nb, wa, wb, wab, ga, gb, solver)
             times[n_idx, s_idx] += time.time() - start_time
-    
-            #figure(1)
-            #plot(tlist, real(na), 'r')
-            #plot(tlist, real(nb), 'b')
-
             s_idx += 1
 
-        #xlabel('Time')
-        #ylabel('Expectation value')
-        #show()
+        n_idx += 1
+
+times = times / n_runs
+
+#
+# plot benchmark data
+#
+print "times =", times
+
+figure(1)
+s_idx = 0
+
+for solver in solvers:
+        
+    plot(Na_vec * Nb, times[:,s_idx])  
+    s_idx += 1
+
+xlabel('Numbers of quantum states')
+ylabel('Time to evolve system (seconds)')
+title('Comparison of solver performance for unitary evolution')
+legend(solvers)
+savefig("solver-performance-unitary.png")
+show()
+
+#
+# Second, test nonunitary solvers solvers
+#
+ga = 0.05            # dissipation rate of system a
+gb = 0.0             # dissipation rate of system b
+
+solvers = ("me", "mc250", "mc500", "es")
+Na_vec = arange(2, 35, 1)
+
+times = zeros((len(Na_vec), len(solvers)))
+
+n_runs = 1
+
+for n_run in range(n_runs):
+    print "run number %d" % n_run
+    n_idx = 0
+    for Na in Na_vec:   
+        print "using %d states" % (Na * Nb)
+        s_idx = 0
+        for solver in solvers:
+        
+            start_time = time.time()
+            na, nb = system_integrate(Na, Nb, wa, wb, wab, ga, gb, solver)
+            times[n_idx, s_idx] += time.time() - start_time
+            s_idx += 1
 
         n_idx += 1
 
@@ -108,7 +155,10 @@ for solver in solvers:
 
 xlabel('Numbers of quantum states')
 ylabel('Time to evolve system (seconds)')
+title('Comparison of solver performance for nonunitary evolution')
 legend(solvers)
+savefig("solver-performance-nonunitary.png")
+
 show()
 
 
