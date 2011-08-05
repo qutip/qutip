645894b6c3284b4b84b050bb5d47d0d533361571
Robert Johansson <jrjohansson@gmail.com>
2011-08-05 05:57:41 +0000

    ultra-strong coupling example now much faster when using eig to find the ground state (of course...)

--- examples/ex_ultrastrong_coupling.py
+++ examples/ex_ultrastrong_coupling.py
@@ -7,36 +7,15 @@ from pylab import *
 import time
 from mpl_toolkits.mplot3d import Axes3D
 
-def jc_solve(N, wc, wa, glist, kappa, gamma, psi0, use_rwa):
+def compute(N, wc, wa, glist, use_rwa):
 
-    # Hamiltonian
-    idc = qeye(N)
-    ida = qeye(2)
-
-    a  = tensor(destroy(N), ida)
-    sm = tensor(idc, destroy(2))
+    # Pre-compute operators for the hamiltonian
+    a  = tensor(destroy(N), qeye(2))
+    sm = tensor(qeye(N), destroy(2))
     nc = a.dag() * a
     na = sm.dag() * sm
         
-    # collapse operators
-    c_op_list = []
-
-    n_th_a = 0.0 # zero temperature
-
-    rate = kappa * (1 + n_th_a)
-    if rate > 0.0:
-        c_op_list.append(sqrt(rate) * a)
-
-    rate = kappa * n_th_a
-    if rate > 0.0:
-        c_op_list.append(sqrt(rate) * a.dag())
-
-    rate = gamma
-    if rate > 0.0:
-        c_op_list.append(sqrt(rate) * sm)
-
     idx = 0
-
     na_expt = zeros(shape(glist))
     nc_expt = zeros(shape(glist))
     for g in glist:
@@ -47,38 +26,29 @@ def jc_solve(N, wc, wa, glist, kappa, gamma, psi0, use_rwa):
         else:
             H = wc * nc + wa * na + g * (a.dag() + a) * (sm + sm.dag())
 
-        # find the steady state of the composite system
-        rhoss = steadystate(H, c_op_list)
-
-        na_expt[idx] = expect(na, rhoss)
-        nc_expt[idx] = expect(nc, rhoss)
+        # find the groundstate of the composite system
+        ekets, evals = H.eigenstates()
+        psi_gnd = ekets[0]
+        na_expt[idx] = expect(na, psi_gnd)
+        nc_expt[idx] = expect(nc, psi_gnd)
 
         idx += 1
 
-    return nc_expt, na_expt, rhoss
+    return nc_expt, na_expt, ket2dm(psi_gnd)
     
 #
 # set up the calculation
 #
 wc = 1.0 * 2 * pi   # cavity frequency
 wa = 1.0 * 2 * pi   # atom frequency
+N = 20              # number of cavity fock states
+use_rwa = False     # Set to True to see that non-RWA is necessary in this regime
 
-kappa = 0.25        # cavity dissipation rate
-gamma = 0.25        # atom dissipation rate
-
-N = 10              # number of cavity fock states
-
-use_rwa = False
-
-# intial state
-psi0 = tensor(basis(N,0),    basis(2,0))    # start with an excited atom 
-
-glist = linspace(0,1.5,50) * 2 * pi # coupling strength
+glist = linspace(0, 2.5, 50) * 2 * pi # coupling strength vector
 
 start_time = time.time()
-nc, na, rhoss_final = jc_solve(N, wc, wa, glist, kappa, gamma, psi0, use_rwa)
+nc, na, rhoss_final = compute(N, wc, wa, glist, use_rwa)
 print 'time elapsed = ' +str(time.time() - start_time) 
-
 
 #
 # plot the cavity and atom occupation numbers as a function of 
@@ -97,15 +67,15 @@ savefig("ultra-strong-coupling-occupation-numbers.png")
 #
 fig = plt.figure(2, figsize=(9, 6))
 rho_cavity = ptrace(rhoss_final, 0)
-xvec = linspace(-5.,5.,100)
+xvec = linspace(-7.5,7.5,100)
 X,Y = meshgrid(xvec, xvec)
 W = wigner(rho_cavity, xvec, xvec)
 ax = Axes3D(fig, azim=-107, elev=49)
 surf=ax.plot_surface(X, Y, W, rstride=1, cstride=1, cmap=cm.jet, alpha=1.0, linewidth=0.05, vmax=0.25, vmin=-0.25)
-ax.set_xlim3d(-5, 5)
-ax.set_ylim3d(-5, 5)
-#ax.set_zlim3d(-0.25, 0.25)
+ax.set_xlim3d(-7.5, 7.5)
+ax.set_ylim3d(-7.5, 7.5)
 fig.colorbar(surf, shrink=0.65, aspect=20)
+title("Wigner function for the cavity groundstate\n(ultra strong coupling to a qubit)")
 savefig("ultra-strong-coupling-wigner.png")
 show()
 
