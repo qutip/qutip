e3d1ab5575d1d2770c5b640a92f426814b597d1c
Paul Nation <nonhermitian@gmail.com>
2011-10-07 02:39:30 +0000

    minor spelling errors

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """QuTiP: The Quantum Toolbox in Python
 
-QuTiP is open-source software for simulating the dynamcis of 
+QuTiP is open-source software for simulating the dynamics of 
 open quantum systems.  The QuTiP library depends on the 
 excellent Numpy and Scipy numerical packages. In addition, 
 graphical output is provided by Matplotlib.  QuTiP aims
 to provide user-friendly and efficient numerical simulations
-of a wide vairety of Hamiltonian's, including those with 
-arbitrary time-dependent, commonly found in a wide range of 
-physics applications. QuTiP is freely avaliable for use and/or 
+of a wide variety of Hamiltonian's, including those with 
+arbitrary time-dependence, commonly found in a wide range of 
+physics applications. QuTiP is freely available for use and/or 
 modification on all major platforms. Being free of any licensing 
 fees, QuTiP is ideal for exploring quantum mechanics and 
 dynamics in the classroom.
