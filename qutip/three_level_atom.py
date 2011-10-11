e5749d1f764520d9c39a17af53f7f67fd2c2625c
Paul Nation <nonhermitian@gmail.com>
2011-10-11 03:42:52 +0000

    changed three level atom code to qutrit_ops /qutrit_basis as the name was a little long.  Also made output to array.  Added contributer statement to file.  Should maybe consider moving to operators.py file at some point.

--- qutip/three_level_atom.py
+++ qutip/three_level_atom.py
@@ -18,7 +18,7 @@
 ###########################################################################
 '''
 This module provides functions that are useful for simulating the
-three level atom with QuTiP.  A three level atom has three states,
+three level atom with QuTiP.  A three level atom (qutrit) has three states,
 which are linked by dipole transitions so that 1 <-> 2 <-> 3.
 Depending on there relative energies they are in the ladder, lambda or
 vee configuration. The structure of the relevant operators is the same
@@ -40,22 +40,35 @@ Ladder:          Lambda:                 Vee:
 The naming of qutip operators follows the convention in the book "The
 Theory of Coherent Atomic excitation" by B. W. Shore.
 
+Contributed by Markus Biden, Oct. 07, 2011
+
 '''
 
-from . import *
+from basis import *
+from scipy import array
 
-def three_level_basis():
-    ''' Return basis states for three level atom
+def qutrit_basis():
+    ''' 
+    @brief Return the basis states for a three level system (qutrit)
+    
+    @params None
+    
+    @returns *array* of qutrit basis vectors
     '''
-    return basis(3,0), basis(3,1), basis(3,2)
-
-def three_level_operators():
-    ''' Return operators for a three level system
+    return array([basis(3,0), basis(3,1), basis(3,2)])
+def qutrit_ops():
+    ''' 
+    @brief Return the operators for a three level system (qutrit)
+    
+    @params None
+    
+    @returns *array* of qutrit operators
     '''
-    one, two, three = three_level_basis()
+    one, two, three = qutrit_basis()
     sig11 = one * one.dag()
     sig22 = two * two.dag()
     sig33 = three * three.dag()
     sig12 = one * two.dag()
     sig32 = three * two.dag()
-    return sig11, sig22, sig33, sig12, sig32
+    return array([sig11, sig22, sig33, sig12, sig32])
+
