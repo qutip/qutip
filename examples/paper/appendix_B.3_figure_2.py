8b8d2272a7f0abf00502bf5a4e0dc5f40dc2ebe9
Robert Johansson <jrjohansson@gmail.com>
2011-10-11 05:35:20 +0000

    unified figure 2 code with how it is presented in the appendix in the paper

--- examples/paper/appendix_B.3_figure_2.py
+++ examples/paper/appendix_B.3_figure_2.py
@@ -27,6 +27,41 @@ the cavity mode at the largest coupling strength g=2.5, which is well
 approximated by Eq.4 from the paper.
 
 """
+
+from qutip import *
+## set up the calculation ## 
+wc = 1.0 * 2 * pi # cavity frequency
+wa = 1.0 * 2 * pi # atom frequency
+N = 20            # number of cavity states
+g = linspace(0, 2.5, 50)*2*pi # coupling strength vector
+## create operators ## 
+a  = tensor(destroy(N), qeye(2))
+sm = tensor(qeye(N), destroy(2))
+nc = a.dag() * a
+na = sm.dag() * sm
+## initialize output arrays ##
+na_expt = zeros(len(g))
+nc_expt = zeros(len(g))
+## run calculation ## 
+for k in range(len(g)):
+    ## recalculate the hamiltonian for each value of g ## 
+    H = wc*nc+wa*na+g[k]*(a.dag()+a)*(sm+sm.dag())
+    ## find the groundstate ## 
+    ekets, evals = H.eigenstates()
+    psi_gnd = ekets[0]
+    ## expectation values ## 
+    na_expt[k] = expect(na, psi_gnd) # qubit occupation
+    nc_expt[k] = expect(nc, psi_gnd) # cavity occupation 
+## Calculate Wigner function for coupling g=2.5 ## 
+rho_cavity = ptrace(psi_gnd,0) # trace out qubit
+xvec = linspace(-7.5,7.5,200)
+## Wigner function ## 
+W = wigner(rho_cavity, xvec, xvec)
+
+
+# ------------------------------------------------------------------------------
+# Plot the results (omitted from the code listing in the appendix in the paper)
+#
 from matplotlib import rcParams
 rcParams['font.family'] = 'serif'
 rcParams['font.serif'] = 'Times New Roman'
@@ -37,52 +72,14 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot, mpl,cm
 from matplotlib.ticker import MaxNLocator
 
-
-
-def compute(N, wc, wa, glist, use_rwa):
-    # Pre-compute operators for the hamiltonian
-    a  = tensor(destroy(N), qeye(2))
-    sm = tensor(qeye(N), destroy(2))
-    nc = a.dag() * a
-    na = sm.dag() * sm    
-    idx = 0
-    na_expt = zeros(shape(glist))
-    nc_expt = zeros(shape(glist))
-    for g in glist:
-        # recalculate the hamiltonian for each value of g
-        if use_rwa: 
-            H = wc * nc + wa * na + g * (a.dag() * sm + a * sm.dag())
-        else:
-            H = wc * nc + wa * na + g * (a.dag() + a) * (sm + sm.dag())
-        # find the groundstate of the composite system
-        ekets, evals = H.eigenstates()
-        psi_gnd = ekets[0]
-        na_expt[idx] = expect(na, psi_gnd)
-        nc_expt[idx] = expect(nc, psi_gnd)
-        idx += 1
-
-    return nc_expt, na_expt, ket2dm(psi_gnd)
-    
-#
-# set up the calculation
-#
-wc = 1.0 * 2 * pi   # cavity frequency
-wa = 1.0 * 2 * pi   # atom frequency
-N = 20              # number of cavity fock states
-use_rwa = False     # Set to True to see that non-RWA is necessary in this regime
-
-glist = linspace(0, 2.5, 50) * 2 * pi # coupling strength vector
-
-nc, na, rhoss_final = compute(N, wc, wa, glist, use_rwa)
-
 #
 # plot the cavity and atom occupation numbers as a function of 
 #
-fig=figure()
+fig=figure(1)
 ax = fig.add_subplot(111)
 ax2=ax.twinx()
-ax2.plot(glist/(2*pi), nc,lw=2)
-ax2.plot(glist/(2*pi), na,'r--',lw=2)
+ax2.plot(g/(2*pi), na_expt, lw=2)
+ax2.plot(g/(2*pi), nc_expt, 'r--', lw=2)
 ax.set_xlabel(r'Coupling strength $g/\omega_{0}$',fontsize=18)
 ax2.set_ylabel(r'Occupation number',fontsize=18)
 for a in ax.yaxis.get_ticklines()+ax.yaxis.get_ticklabels():
@@ -91,16 +88,12 @@ for tick in ax.xaxis.get_major_ticks():
     tick.label1.set_fontsize(16)
 for tick in ax2.yaxis.get_ticklabels():
     tick.set_fontsize(16)
-show()
-close(fig)
+
 #
 # calculate wigner function of cavity mode at final coupling strength g=2.5.
 #
-rho_cavity = ptrace(rhoss_final, 0)
-xvec = linspace(-7.5,7.5,150)
-X,Y = meshgrid(xvec, xvec)
-W = wigner(rho_cavity, xvec, xvec)
 fig = plt.figure(figsize=(7,5))
+X,Y = meshgrid(xvec, xvec)
 
 #
 # plot the cavity wigner function.
