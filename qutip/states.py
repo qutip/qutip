19bdea0174304ae8d6bbc45e21e912b0a895c6b1
Robert Johansson <jrjohansson@gmail.com>
2011-10-27 05:54:52 +0000

    removed duplicate implementation of the basis function. moved basis_qutrit qutip/states.py where other state functions are located. removed unused file qutip/basis.py

--- qutip/states.py
+++ qutip/states.py
@@ -22,43 +22,56 @@ from operators import destroy
 
 def basis(N,*args):
     """
-    @brief Generate the vector representation of a number state.
+    Generate the vector representation of a number state.
 	
-    a subtle incompability with the quantum optics toolbox: here
+    a subtle incompability with the quantum optics toolbox: In QuTiP:
+ 
         basis(N, 0) = ground state
+
     but in QO toolbox:
+
         basis(N, 1) = ground state
 	
-    @param N the number of states
-    @param args integer corresponding to desired number state
+    N the number of states
+    args integer corresponding to desired number state
     
-    @returns quantum object representing the requested number state |args>
+    Returns quantum object representing the requested number state |args>
     """
-    if (not isinstance(N,int)) or N<0:#check if N is int and N>0
+    if (not isinstance(N,int)) or N<0:
         raise ValueError("N must be integer N>=0")
     if not any(args):#if no args then assume vacuum state 
         args=0
     if not isinstance(args,int):#if input arg!=0
-        if not isinstance(args[0],int):#check if args is not int
+        if not isinstance(args[0],int):
             raise ValueError("need integer for basis vector index")
         args=args[0]
     if args<0 or args>(N-1): #check if args is within bounds
         raise ValueError("basis vector index need to be in 0=<indx<=N-1")
     bas=zeros([N,1]) #column vector of zeros
     bas[args]=1 # 1 located at position args
-    return Qobj(bas) #return Qobj
+    return Qobj(bas)
+
+
+def qutrit_basis():
+    """
+    Return the basis states for a three level system (qutrit)
+    
+    Parameters None
+    
+    Returns *array* of qutrit basis vectors
+    """
+    return array([basis(3,0), basis(3,1), basis(3,2)])
 
 
 def coherent(N,alpha):
     """
-    @brief Generates a coherent state with eigenvalue alpha in a 
-        N-dimensional Hilbert space via displacement 
-        operator on vacuum state
+    Generates a coherent state with eigenvalue alpha in a 
+    N-dimensional Hilbert space via displacement operator on vacuum state
     
-    @param N number of levels in truncated Hilbert space
-    @param alpha eigenvalue for coherent state
+    N number of levels in truncated Hilbert space
+    alpha eigenvalue for coherent state
     
-    @returns Qobj quantum object for coherent state
+    Returns Qobj quantum object for coherent state
     """
     x=basis(N,0)
     a=destroy(N)
@@ -67,13 +80,13 @@ def coherent(N,alpha):
 
 def coherent_dm(N, alpha):
     """
-    @brief Generate the density matrix representation of a coherent 
-        state via outer product
+    Generate the density matrix representation of a coherent 
+    state via outer product
     
-    @param N number of levels in truncated Hilbert space
-    @param alpha eigenvalue for coherent state
+    N number of levels in truncated Hilbert space
+    alpha eigenvalue for coherent state
     
-    @returns Qobj density matrix representation of coherent state
+    Returns Qobj density matrix representation of coherent state
     """
     psi = coherent(N,alpha)
     return psi * psi.dag()
@@ -81,8 +94,9 @@ def coherent_dm(N, alpha):
 def coherent_fast(N,alpha):
     """
     Generate a coherent state	
-    @param N the number of states
-    @param alpha the coherent state amplitude (complex)
+
+    N the number of states
+    alpha the coherent state amplitude (complex)
     """
     data = zeros([N,1],dtype=complex)
     n = arange(N)
@@ -92,20 +106,21 @@ def coherent_fast(N,alpha):
 def coherent_dm_fast(N,alpha):
     """
     Generate a coherent state	
-    @param N the number of states
-    @param alpha the coherent state amplitude (complex)
+
+    N the number of states
+    alpha the coherent state amplitude (complex)
     """
     psi = coherent_fast(N, alpha)
     return psi * psi.dag()
 
 def fock_dm(N, *args):
     """
-    @brief Generate the density matrix representation of a Fock state via outer product.
+    Generate the density matrix representation of a Fock state via outer product.
     
-    @param N number of levels in truncated Hilbert space
-    @param m int corresponding to desired number state, defaults to 0 if omitted
+    N number of levels in truncated Hilbert space
+    m int corresponding to desired number state, defaults to 0 if omitted
     
-    @returns Qobj density matrix representation of Fock state
+    Returns Qobj density matrix representation of Fock state
     """
     if not args:
         psi=basis(N)
@@ -115,13 +130,13 @@ def fock_dm(N, *args):
 
 def fock(N, *args):
     """
-    @brief Generates the vector representation of a bosonic Fock (number) state. 
-        Same as `basis' function.
+    Generates the vector representation of a bosonic Fock (number) state. 
+    Same as `basis' function.
     
-    @param N the number of states in the Hilbert space
-    @param m int corresponding to desired number state, defaults to 0 if omitted
+    N the number of states in the Hilbert space
+    m int corresponding to desired number state, defaults to 0 if omitted
     
-    @returns Qobj quantum object representing the requested number state |args>
+    Returns Qobj quantum object representing the requested number state |args>
     """
     if not args:
         return basis(N)
@@ -132,8 +147,8 @@ def thermal_dm(N, n):
     """
     Generates the density matrix for a thermal state of n particles
 
-    @param N: the number of states
-    @param n: expectational value for number of particles in thermal state
+    N: the number of states
+    n: expectational value for number of particles in thermal state
     """
 
     i=arange(N)  
@@ -143,12 +158,12 @@ def thermal_dm(N, n):
 
 def ket2dm(Q):
     """
-    @brief Takes input ket or bra vector and returns density matrix 
-        formed by outer product.
+    Takes input ket or bra vector and returns density matrix 
+    formed by outer product.
     
-    @param Q Ket or bra vector
+    Q Ket or bra vector
     
-    @returns Qobj Density matrix formed by outer product
+    Returns Qobj Density matrix formed by outer product
     """
     if Q.type=='ket':
         out=Q*Q.dag()
