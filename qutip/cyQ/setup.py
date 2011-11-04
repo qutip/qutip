a08c89a9e69ec8bac34caaee0df2295fe33cf6d6
Paul Nation <nonhermitian@gmail.com>
2011-11-04 01:22:02 +0000

    removed openmp flags as they were not used, and are not supported by all compilers

--- qutip/cyQ/setup.py
+++ qutip/cyQ/setup.py
@@ -6,6 +6,6 @@ import numpy as np
 setup(
     cmdclass = {'build_ext': build_ext},
     include_dirs = [np.get_include()],
-    ext_modules = [Extension("ode_rhs", ["ode_rhs.pyx"],extra_compile_args=['-ffast-math','-fopenmp'],extra_link_args=['-fopenmp']),
-                   Extension("cy_mc_funcs", ["cy_mc_funcs.pyx"],extra_compile_args=['-ffast-math','-fopenmp'],extra_link_args=['-fopenmp'])]
+    ext_modules = [Extension("ode_rhs", ["ode_rhs.pyx"],extra_compile_args=['-ffast-math'],extra_link_args=[]),
+                   Extension("cy_mc_funcs", ["cy_mc_funcs.pyx"],extra_compile_args=['-ffast-math'],extra_link_args=[])]
 )
