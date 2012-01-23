a7086631b55153271f4fb73532ec961f365bcaef
Paul Nation <nonhermitian@gmail.com>
2012-01-23 14:19:09 +0000

    added new figure for title page of latex output and favicon for webpage

--- doc/conf.py
+++ doc/conf.py
@@ -111,12 +111,12 @@ html_theme = "agogo"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = 'logo.jpg'
+#html_logo = 'wide_logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -190,7 +190,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'qutip_logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
