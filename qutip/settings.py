fa4a2f6de769149b1ef9073ddb6cc27c5461b64f
Paul Nation <nonhermitian@gmail.com>
2012-03-07 02:04:21 +0000

    module for easy setting of global qutip variables (do not need environ flags)

---
+++ qutip/settings.py
@@ -0,0 +1,33 @@
+#This file is part of QuTIP.
+#
+#    QuTIP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#    QuTIP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with QuTIP.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2011-2012, Paul D. Nation & Robert J. Johansson
+#
+###########################################################################
+"""
+This module contains settings for the QuTiP GUI,multiprocessing,
+and tidyup functionality.
+"""
+
+#QuTiP Graphics (set at qutip import)
+qutip_graphics=None
+#QuTiP GUI selection (set at qutip import)
+qutip_gui=None
+#use auto tidyup
+auto_tidyup=True
+#the Franco flag
+franco=False
+#number of cpus (set at qutip import)
+num_cpus=1
