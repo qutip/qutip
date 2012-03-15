5ba34cbff94eb6da7f70a73cf539e072ae73f576
Paul Nation <nonhermitian@gmail.com>
2012-03-15 04:50:19 +0000

    updated sparse eigenvalue/vector solver.  Currently uses sparse solver for dims>=10 (will be changed later to N>1000 or so)

--- qutip/sparse.py
+++ qutip/sparse.py
@@ -21,8 +21,9 @@
 This module contains a collection of sparse routines to get around 
 having to use dense matrices.
 """
+
 import scipy.sparse as sp
-from scipy import floor,mod,union1d,real,array
+from scipy import ceil,floor,mod,union1d,real,array,sort,flipud,arange
 import scipy.linalg as la
 
 def sp_L2_norm(op):
@@ -49,45 +50,149 @@ def sp_one_norm(op):
     return max(array([sum(abs((op.data[:,k]).data)) for k in xrange(op.shape[1])]))
 
 
-def sp_eigs(op,vecs=True):
+def sp_eigs(op,vecs=True,sparse=None,sort='low',eigvals=0,tol=0,maxiter=10000):
     """
-    Returns Eigenvalues and Eigenvectors for Qobj.  Uses sparse eigen-solver.
+    Returns Eigenvalues and Eigenvectors for Qobj.  Uses sparse eigen-solver if dims >=10 and sparse!=False.
     
     Args:
     
         op (Qobj): Input Qobj
         
-        vecs (bool): Return eigenvectors
+        vecs (bool): Return eigenvectors?
+        
+        sparse (bool): Use sparse solver?. default = None (automatic)
+        
+        sort: (str): Return lowest or highest eigenvals/vecs
+        
+        eigvals (int): Number of eigenvals/vecs to return.  Default = 0 (return all)
+        
+        tol (float): Tolerance for sparse eigensolver.  Default = 0 (Machine precision)
+        
+        maxiter (int): Max. number of iterations used by sparse sigensolver.
     
     Returns:
     
-        Eigenvalues and (by default) Eigenvectors
+        Array of eigenvalues and (by default) array of corresponding Eigenvectors.
     """
     if op.type=='ket' or op.type=='bra':
         raise TypeError("Can only diagonalize operators and superoperators")
     N=op.shape[0]
-    if N>=10:
-        D=int(floor(N/2))
-        if mod(N,2):D+=1 #if odd dimensions
-        if vecs:
-            big_vals,big_vecs=sp.linalg.eigs(op.data,k=N-D,which='LM')
-            big_vecs=sp.csr_matrix(big_vecs,dtype=complex)
-            small_vals,small_vecs=sp.linalg.eigs(op.data,k=N-(N-D),which='SM')
-            small_vecs=sp.csr_matrix(small_vecs,dtype=complex)
-            evecs=sp.hstack([big_vecs,small_vecs],format='csr') #combine eigenvector sets
+    if eigvals==N:eigvals=0
+    if eigvals>N:raise ValueError("Number of requested eigen vals/vecs must be <= N.")
+    if eigvals>0 and not op.isherm:sparse=True #only saprse routine can get slected eigs for nonherm matricies
+    #set sparse=True for dims >= 10 if not already set
+    if N>=10 and sparse==None:
+        sparse=True
+    if N<10 and sparse==None:
+        sparse==False
+    #set number of large and small eigenvals/vecs
+    if eigvals==0:#user wants all eigs (default)
+        D=int(ceil(N/2.0))
+        num_large=N-D
+        if not mod(N,2):M=D
+        else:M=D-1
+        num_small=N-M
+    else:#if user wants only a few eigen vals/vecs
+        if sort=='low':
+            num_small=eigvals
+            num_large=0
+        elif sort=='high':
+            num_large=eigvals
+            num_small=0
         else:
-            big_vals=sp.linalg.eigs(op.data,k=N-D,which='LM',return_eigenvectors=False)
-            small_vals=sp.linalg.eigs(op.data,k=N-(N-D),which='SM',return_eigenvectors=False)
-        evals=union1d(big_vals,small_vals)#combine eigenvalue sets
-    else:#for dims <10 use faster dense routine
+            raise ValueError("Invalid option for 'sort'.")
+    
+    #Sparse routine
+    if sparse:        
         if vecs:
-            evals,evecs=la.eig(op.full())
+            #big values
+            if num_large>0:
+                if op.isherm:
+                    big_vals,big_vecs=sp.linalg.eigsh(op.data,k=num_large,which='LR',tol=tol,maxiter=maxiter)
+                else:
+                    big_vals,big_vecs=sp.linalg.eigs(op.data,k=num_large,which='LR',tol=tol,maxiter=maxiter)
+                big_vecs=sp.csr_matrix(big_vecs,dtype=complex)
+            #small values
+            if num_small>0:
+                if op.isherm:
+                    small_vals,small_vecs=sp.linalg.eigsh(op.data,k=num_small,which='SR',tol=tol,maxiter=maxiter)
+                else:
+                    small_vals,small_vecs=sp.linalg.eigs(op.data,k=num_small,which='SR',tol=tol,maxiter=maxiter)
+                small_vecs=sp.csr_matrix(small_vecs,dtype=complex)
+            if num_large!=0 and num_small!=0:
+                evecs=sp.hstack([small_vecs,big_vecs],format='csr') #combine eigenvector sets
+            elif num_large!=0 and num_small==0:
+                evecs=big_vecs
+            elif num_large==0 and num_small!=0:
+                evecs=small_vecs
+        else:
+            if op.isherm:
+                if num_large>0:
+                    big_vals=sp.linalg.eigsh(op.data,k=num_large,which='LR',return_eigenvectors=False,tol=tol,maxiter=maxiter)
+                if num_small>0:
+                    small_vals=sp.linalg.eigsh(op.data,k=num_small,which='SR',return_eigenvectors=False,tol=tol,maxiter=maxiter)
+            else:
+                if num_large>0:
+                    big_vals=sp.linalg.eigs(op.data,k=num_large,which='LR',return_eigenvectors=False,tol=tol,maxiter=maxiter)
+                if num_small>0:
+                    small_vals=sp.linalg.eigs(op.data,k=num_small,which='SR',return_eigenvectors=False,tol=tol,maxiter=maxiter)
+        if num_large!=0 and num_small!=0:
+            evals=union1d(small_vals,big_vals)
+        elif num_large!=0 and num_small==0:
+            evals=large_vals
+        elif num_large==0 and num_small!=0:
+            evals=small_vals
+        if op.isherm:evals=real(evals)
+        perm=arange(len(evals))#return values aleady sorted
+    
+    #Dense routine for dims <10 use faster dense routine (or use if user set sparse==False)
+    else:
+        if vecs:
+            if op.isherm:
+                if eigvals==0:
+                    evals,evecs=la.eigh(op.full())
+                else:
+                    if num_small>0:
+                        evals,evecs=la.eigh(op.full(),eigvals=[0,num_small-1])
+                    if num_large>0:
+                        evals,evecs=la.eigh(op.full(),eigvals=[N-num_large,N-1])
+            else:
+                evals,evecs=la.eig(op.full())
             evecs=sp.csr_matrix(evecs,dtype=complex)
         else:
-            evals=la.eigvals(op.full())
-    if op.isherm:#hermitian eigvals are always real
-        evals=real(evals)
+            if op.isherm:
+                if eigvals==0:
+                    evals=la.eigvalsh(op.full())
+                else:
+                    if num_small>0:
+                        evals=la.eigvalsh(op.full(),eigvals=[0,num_small-1])
+                    if num_large>0:
+                        evals=la.eigvalsh(op.full(),eigvals=[N-num_large,N-1])
+            else:
+                evals=la.eigvals(op.full())
+        #sort return values
+        _zipped = zip(evals, range(len(evals)))
+        _zipped.sort()
+        evals,perm = zip(*_zipped)
+        if op.isherm:evals=real(evals)
+        perm=array(perm)
+    
+    #return eigenvectors
     if vecs:
+        evecs=array([evecs[:,k] for k in perm])
+    if sort=='high':#flip arrays to largest values first
+        evecs=flipud(evecs)
+        evals=flipud(evals)
+    if vecs:    
         return evals,evecs
     else:
         return evals
+
+
+
+
+
+
+
+
+
