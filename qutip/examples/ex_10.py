d85b354a9fd872990934fa249d9e868aedb3771e
Paul Nation <nonhermitian@gmail.com>
2012-03-15 06:37:17 +0000

    example 10

---
+++ qutip/examples/ex_10.py
@@ -0,0 +1,42 @@
+#
+#Calculates the Schrodinger cat state for
+#a superposition of two coherent states.
+#
+from ..operators import *
+from ..states import *
+from ..wigner import *
+from pylab import * #loads matplotlib
+
+def run():
+    #Number of basis states
+    N = 20 
+
+    #amplitude of coherent states
+    alpha=2.0+2j
+
+    #define ladder oeprators
+    a = destroy(N)
+
+    #define displacement oeprators
+    D1=displace(N,alpha)
+    D2=displace(N,-alpha)
+
+    #create superposition of coherent states
+    psi=(D1+D2)*basis(N,0)
+
+    #calculate Wigner function
+    xvec = linspace(-5,5,200)
+    yvec=xvec
+    W=wigner(psi,xvec,yvec)
+
+    #plot Wigner function as filled contour
+    plt=contourf(xvec,yvec,W,100)
+    xlim([-5,5])
+    ylim([-5,5])
+    title('Wigner function of Schrodinger cat')
+
+    #add a colorbar for pseudoprobability
+    cbar=colorbar(plt)#create colorbar
+    cbar.ax.set_ylabel('Pseudoprobability')
+    #show plot
+    show()
