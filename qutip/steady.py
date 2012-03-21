fdb7df666135c68f60db7d1ea588e9813f43d231
Paul Nation <nonhermitian@gmail.com>
2012-03-21 03:42:05 +0000

    fixed bug in steady where output diagonals were twice as big

--- qutip/steady.py
+++ qutip/steady.py
@@ -86,7 +86,7 @@ def steady(L,maxiter=100,tol=1e-6,method='solve'):
 		rhoss.shape=[prod(rhoss.dims[0]),1]
 	n=prod(rhoss.shape)
 	L1=L.data+eps*sp_inf_norm(L)*sp.eye(n,n,format='csr')
-	v=2.0*(random.random((n,1))+1.0j*random.random((n,1)))-(1.0+1.0j)
+	v=randn(n,1)
 	it=0
 	while (la.norm(L.data*v,inf)>tol) and (it<maxiter):
 		if method=='bicg':
@@ -103,11 +103,11 @@ def steady(L,maxiter=100,tol=1e-6,method='solve'):
 		trow=trow.reshape((1,n)).tocsr()
 		data=v/sum(trow.dot(v))
 	else:
-		data=data/la.norm(data)
+		data=data/la.norm(v)
 	data=reshape(data,(rhoss.shape[0],rhoss.shape[1])).T
 	data=sp.csr_matrix(data)
 	data=sp.triu(data,format='csr')#take only upper triangle
-	rhoss.data=data+data.conj().T #output should be hermitian, but not guarenteed using iterative meth
+	rhoss.data=0.5*sp.eye(rhoss.shape[0],rhoss.shape[1],format='csr')*(data+data.conj().T) #output should be hermitian, but not guarenteed using iterative meth
 	if qset.auto_tidyup:
 	    return Qobj(rhoss).tidyup()
 	else:
