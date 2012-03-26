90f1ef3561209efbb544657f3d7c961682bb8a8e
Paul Nation <nonhermitian@gmail.com>
2012-03-26 04:49:36 +0000

    use mathjax instead of mathmpl

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ needs_sphinx = '1.1'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['matplotlib.sphinxext.mathmpl',
+extensions = ['sphinx.ext.mathjax',
               'matplotlib.sphinxext.only_directives',
               'matplotlib.sphinxext.plot_directive',
               'sphinx.ext.autodoc',
