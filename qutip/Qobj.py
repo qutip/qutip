b222c8f55ea921568b2ed75577e422a10173b27e
Robert Johansson <jrjohansson@gmail.com>
2012-03-27 02:12:02 +0000

    need to import matrix symbol in Qobj

--- qutip/Qobj.py
+++ qutip/Qobj.py
@@ -22,7 +22,7 @@ it's methods.
 """
 
 import types
-from scipy import array,ndarray,zeros,column_stack,dot,frexp,randn,inf,finfo,real,imag
+from scipy import array,ndarray,zeros,column_stack,dot,frexp,randn,inf,finfo,real,imag,matrix
 import scipy.sparse as sp
 import scipy.linalg as la
 import qutip.settings as qset
