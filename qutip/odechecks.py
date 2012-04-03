5ee9eca50b9cb43a006103ccfec36bf979d48d13
Paul Nation <nonhermitian@gmail.com>
2012-04-03 11:38:06 +0000

    updated odechecks for mcsolver

--- qutip/odechecks.py
+++ qutip/odechecks.py
@@ -21,47 +21,50 @@ from types import FunctionType
 from Qobj import *
 
 
-def _ode_checks(H,c_ops):
+def _ode_checks(H,c_ops,solver='me'):
     # do some basic sanity checks on the format of H and c_ops: this should
     # be moved to a function that can be reused.
-    n_const = 0
-    n_func  = 0
-    n_str   = 0
+    h_const = 0
+    h_func  = 0
+    h_str   = 0
     # check H for incorrect format
     if isinstance(H, Qobj):    
-        n_const += 1
+        h_const += 1
     elif isinstance(H, FunctionType):
         pass #n_func += 1
     elif isinstance(H, list):
         for h in H:
             if isinstance(h, Qobj):
-                n_const += 1
+                h_const += 1
             elif isinstance(h, list):
                 if len(h) != 2 or not isinstance(h[0], Qobj):
                     raise TypeError("Incorrect hamiltonian specification")
                 else:
                     if isinstance(h[1], FunctionType):
-                        n_func += 1
+                        h_func += 1
                     elif isinstance(h[1], str):
-                        n_str += 1
+                        h_str += 1
                     else:
                         raise TypeError("Incorrect hamiltonian specification")
     else:
         raise TypeError("Incorrect hamiltonian specification")
     
     # the the whole thing again for c_ops 
+    c_const = 0
+    c_func  = 0
+    c_str   = 0
     if isinstance(c_ops, list):
         for c in c_ops:
             if isinstance(c, Qobj):
-                n_const += 1
+                c_const += 1
             elif isinstance(c, list):
                 if len(c) != 2 or not isinstance(c[0], Qobj):
                     raise TypeError("Incorrect collapse operator specification")
                 else:
                     if isinstance(c[1], FunctionType):
-                        n_func += 1
+                        c_func += 1
                     elif isinstance(c[1], str):
-                        n_str += 1
+                        c_str += 1
                     else:
                         raise TypeError("Incorrect collapse operator specification")
     else:
@@ -71,7 +74,10 @@ def _ode_checks(H,c_ops):
     # if n_str == 0 and n_func == 0:
     #     # no time-dependence at all
     #
-    if n_str > 0 and n_func > 0:
+    if (h_str > 0 and h_func > 0) or (c_str > 0 and c_func > 0):
         raise TypeError("Cannot mix string and function type time-dependence formats")       
     
-    return n_const,n_func,n_str
+    if solver=='me':
+        return [h_const+c_const,h_func+c_func,h_str+c_str]
+    elif solver=='mc':
+        return [h_const,h_func,h_str],[c_const,c_func,c_str]
