036d31afebaf43dd17fd7009cefa8a01dcb7ebf5
Robert Johansson <jrjohansson@gmail.com>
2012-04-24 06:25:25 +0000

    example of generating a bloch sphere animation using matplotlib

---
+++ examples/ex_bloch_sphere_animation.py
@@ -0,0 +1,65 @@
+#
+# Animation with qutip and matplotlib: decaying qubit visualized in a Bloch sphere
+#
+from qutip import *
+
+def qubit_integrate(w, theta, gamma1, gamma2, psi0, tlist):
+    # operators and the hamiltonian
+    sx = sigmax(); sy = sigmay(); sz = sigmaz(); sm = sigmam()
+    H = w * (cos(theta) * sz + sin(theta) * sx)
+    # collapse operators
+    c_op_list = []
+    n_th = 0.5 # temperature
+    rate = gamma1 * (n_th + 1)
+    if rate > 0.0: c_op_list.append(sqrt(rate) * sm)
+    rate = gamma1 * n_th
+    if rate > 0.0: c_op_list.append(sqrt(rate) * sm.dag())
+    rate = gamma2
+    if rate > 0.0: c_op_list.append(sqrt(rate) * sz)
+
+
+    # evolve and calculate expectation values
+    output = mesolve(H, psi0, tlist, c_op_list, [sx, sy, sz])  
+    return output.expect[0], output.expect[1], output.expect[2]
+    
+## calculate the dynamics
+w     = 1.0 * 2 * pi   # qubit angular frequency
+theta = 0.2 * pi       # qubit angle from sigma_z axis (toward sigma_x axis)
+gamma1 = 0.5      # qubit relaxation rate
+gamma2 = 0.2      # qubit dephasing rate
+# initial state
+a = 1.0
+psi0 = (a* basis(2,0) + (1-a)*basis(2,1))/(sqrt(a**2 + (1-a)**2))
+tlist = linspace(0,10,500)
+sx, sy, sz = qubit_integrate(w, theta, gamma1, gamma2, psi0, tlist)
+
+## animate the dynamics on the Bloch sphere
+
+from pylab import *
+
+import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = figure()
+ax = Axes3D(fig,azim=-40,elev=30)
+
+sphere=Bloch(axes=ax)
+
+def animate(i):
+    sphere.clear()
+    sphere.add_points([sx[i],sy[i],sz[i]])
+    sphere.plot_points()
+    return ax,
+
+def init():
+    sphere.vector_color = ['r']
+    sphere.add_vectors([sin(theta),0,cos(theta)])
+    sphere.make_sphere()
+    return ax,
+
+ani = animation.FuncAnimation(fig, animate, np.arange(len(sx)), init_func=init, blit=True, repeat=False)
+
+ani.save('bloch_sphere.mp4', fps=20, clear_temp=True)
+
+
+
