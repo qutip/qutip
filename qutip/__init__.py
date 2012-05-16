b289361246c7706f45552aa60d721946334b5be6
Paul Nation <nonhermitian@gmail.com>
2012-05-16 13:11:51 +0000

    added check for qutip._version.release==False so that we dont get message about running in install directory for testing

--- qutip/__init__.py
+++ qutip/__init__.py
@@ -19,6 +19,7 @@
 
 import os,sys,platform,multiprocessing
 import qutip.settings
+import qutip._version
 
 top_path=os.path.dirname(os.path.dirname(__file__))
 try:
@@ -26,7 +27,7 @@ try:
 except:
     pass
 else:
-    if 'QuTiP' in setup_file.readlines()[1][3:]:
+    if ('QuTiP' in setup_file.readlines()[1][3:]) and qutip._version.release==True:
         print("You are in the installation directory. Change directories before running QuTiP.")
     setup_file.close()
 #automatically set number of threads used by MKL
