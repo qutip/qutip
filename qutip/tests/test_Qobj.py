de7092d77bf4ee2df03bd60589324544e16a875f
Paul Nation <nonhermitian@gmail.com>
2012-08-13 11:22:42 +0000

    added qobj tests

---
+++ qutip/tests/test_Qobj.py
@@ -0,0 +1,338 @@
+#This file is part of QuTIP.
+#
+#    QuTIP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#    QuTIP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with QuTIP.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2011, Paul D. Nation & Robert J. Johansson
+#
+###########################################################################
+
+
+
+from scipy import *
+from qutip import *
+import scipy.sparse as sp
+from numpy.testing import assert_equal
+
+
+#-------- test_ the Qobj properties ------------#
+def test_QobjData():
+    "Qobj data"
+    N=10
+    data1=random.random((N,N))+1j*random.random((N,N))-(0.5+0.5j)
+    q1=Qobj(data1)
+    #check if data is a csr_matrix if originally array
+    assert_equal(sp.isspmatrix_csr(q1.data),True)
+    #check if dense ouput is equal to original data
+    assert_equal(all(q1.data.todense()-matrix(data1)),0)
+    
+    data2=random.random((N,N))+1j*random.random((N,N))-(0.5+0.5j)
+    data2=sp.csr_matrix(data2)
+    q2=Qobj(data2)
+    #check if data is a csr_matrix if originally csr_matrix
+    assert_equal(sp.isspmatrix_csr(q2.data),True)
+    
+    data3=1
+    q3=Qobj(data3)
+    #check if data is a csr_matrix if originally int
+    assert_equal(sp.isspmatrix_csr(q3.data),True)
+    
+    data4=random.random((N,N))+1j*random.random((N,N))-(0.5+0.5j)
+    data4=matrix(data4)
+    q4=Qobj(data4)
+    #check if data is a csr_matrix if originally csr_matrix
+    assert_equal(sp.isspmatrix_csr(q4.data),True)
+    assert_equal(all(q4.data.todense()-matrix(data4)),0)
+
+
+def test_QobjType():
+    "Qobj type"
+    N=int(ceil(10.0*random.random()))+5
+    
+    ket_data=random.random((N,1))
+    ket_qobj=Qobj(ket_data)
+    assert_equal(ket_qobj.type,'ket')
+    
+    bra_data=random.random((1,N))
+    bra_qobj=Qobj(bra_data)
+    assert_equal(bra_qobj.type,'bra')
+    
+    oper_data=random.random((N,N))
+    oper_qobj=Qobj(oper_data)
+    assert_equal(oper_qobj.type,'oper')
+    
+    N=9
+    super_data=random.random((N,N))
+    super_qobj=Qobj(super_data,dims=[[[3]],[[3]]])
+    assert_equal(super_qobj.type,'super')
+    
+def test_QobjHerm():
+    "Qobj Hermicity" 
+    N=10
+    data=random.random((N,N))+1j*random.random((N,N))-(0.5+0.5j)
+    q1=Qobj(data)
+    assert_equal(q1.isherm,False)
+    
+    data=data+data.conj().T
+    q2=Qobj(data)
+    assert_equal(q2.isherm,True)
+
+def test_QobjDimsShape():
+    "Qobj shape" 
+    N=10
+    data=random.random((N,N))+1j*random.random((N,N))-(0.5+0.5j)
+    
+    q1=Qobj(data)
+    assert_equal(q1.dims,[[10],[10]])
+    assert_equal(q1.shape,[10,10])
+    
+    data=random.random((N,1))+1j*random.random((N,1))-(0.5+0.5j)
+    
+    q1=Qobj(data)
+    assert_equal(q1.dims,[[10],[1]])
+    assert_equal(q1.shape,[10,1])
+    
+    N=4
+    
+    data=random.random((N,N))+1j*random.random((N,N))-(0.5+0.5j)
+    
+    q1=Qobj(data,dims=[[2,2],[2,2]])
+    assert_equal(q1.dims,[[2,2],[2,2]])
+    assert_equal(q1.shape,[4,4])
+
+#-------- test_ the Qobj math ------------#
+def test_QobjAddition():
+    "Qobj addition"
+    data1 = array([[1,2], [3,4]])
+    data2 = array([[5,6], [7,8]])       
+
+    data3 = data1 + data2
+
+    q1 = Qobj(data1)
+    q2 = Qobj(data2)
+    q3 = Qobj(data3)
+
+    q4 = q1 + q2
+    
+    assert_equal(q4.type,ischeck(q4))
+    assert_equal(q4.isherm,isherm(q4))
+    
+    # check elementwise addition/subtraction
+    assert_equal(q3, q4)
+
+    # check that addition is commutative
+    assert_equal(q1+q2, q2+q1)
+    
+    data=random.random((5,5))
+    q=Qobj(data)
+    
+    x1=q+5
+    x2=5+q
+    
+    data=data+5
+    assert_equal(all(x1.data.todense()-(matrix(data))),0)
+    assert_equal(all(x2.data.todense()-(matrix(data))),0)
+    
+    data=random.random((5,5))
+    q=Qobj(data)
+    x3=q+data
+    x4=data+q
+    
+    data=2.0*data
+    assert_equal(all(x3.data.todense()-(matrix(data))),0)
+    assert_equal(all(x4.data.todense()-(matrix(data))),0)
+    
+
+def test_QobjSubtraction():
+    "Qobj subtraction"
+    data1=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    q1=Qobj(data1)
+    
+    data2=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    q2=Qobj(data2)
+    
+    q3=q1-q2
+    data3=data1-data2
+    
+    
+    
+    assert_equal(all(q3.data.todense()-matrix(data3)),0)
+    
+    q4=q2-q1
+    data4=data2-data1
+    
+    assert_equal(all(q4.data.todense()-matrix(data4)),0)
+    
+def test_QobjMultiplication():
+    "Qobj multiplication"
+    data1 = array([[1,2], [3,4]])
+    data2 = array([[5,6], [7,8]])       
+
+    data3 = dot(data1, data2)
+
+    q1 = Qobj(data1)
+    q2 = Qobj(data2)
+    q3 = Qobj(data3)
+
+    q4 = q1 * q2
+
+    assert_equal(q3, q4)
+    
+
+def test_QobjDivision():
+    "Qobj division"
+    data=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    q=Qobj(data)
+    randN=10*random.random()
+    q=q/randN
+    assert_equal(all(q.data.todense()-matrix(data)/randN),0)
+
+
+def test_QobjPower():
+    "Qobj power"
+    data=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    q=Qobj(data)
+    
+    q2=q**2
+    assert_equal(all(q2.data.todense()-matrix(data)**2),0)
+    
+    q3=q**3
+    assert_equal(all(q3.data.todense()-matrix(data)**3),0)
+
+
+
+def test_QobjNeg():
+    "Qobj negation"
+    data=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    q=Qobj(data)
+    x=-q
+    assert_equal(all(x.data.todense()+matrix(data)),0)
+
+
+def test_QobjEquals():
+    "Qobj equals"
+    data=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    q1=Qobj(data)
+    q2=Qobj(data)
+    assert_equal(q1,q2)
+    
+    q1=Qobj(data)
+    q2=Qobj(-data)
+    assert_equal(q1!=q2,True)
+
+def test_QobjGetItem():
+    "Qobj getitem"
+    data=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    q=Qobj(data)
+    assert_equal(q[0,0],data[0,0])
+    assert_equal(q[-1,2],data[-1,2])
+
+def test_CheckMulType():
+    "Qobj multiplication type"
+    psi=basis(5)
+    dm=psi*psi.dag()
+    assert_equal(dm.type,'oper')
+    assert_equal(dm.isherm,True)
+    
+    nrm=psi.dag()*psi
+    assert_equal(dm.type,'oper')
+    assert_equal(dm.isherm,True)
+    
+    H1=rand_herm(3)
+    H2=rand_herm(3)
+    out=H1*H2
+    assert_equal(out.type,'oper')
+    assert_equal(out.isherm,isherm(out))
+    
+    U=rand_unitary(5)
+    out=U.dag()*U
+    assert_equal(out.type,'oper')
+    assert_equal(out.isherm,True)
+    
+    N=num(5)
+    
+    out=N*N
+    assert_equal(out.type,'oper')
+    assert_equal(out.isherm,True)
+    
+    
+#-------- test_ the Qobj methods ------------#
+
+def test_QobjConjugate():
+    "Qobj conjugate"
+    data=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    A=Qobj(data)
+    B=A.conj()
+    assert_equal(all(B.data.todense()-matrix(data.conj())),0)
+
+def test_QobjDagger():
+    "Qobj adjoint (dagger)"
+    data=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    A=Qobj(data)
+    B=A.dag()
+    assert_equal(all(B.data.todense()-matrix(data.conj().T)),0)
+
+def test_QobjDiagonals():
+    "Qobj diagonals"
+    data=random.random((5,5))+1j*random.random((5,5))-(0.5+0.5j)
+    A=Qobj(data)
+    b=A.diag()
+    assert_equal(all(b-diag(data)),0)
+
+def test_QobjEigenEnergies():
+    "Qobj eigenenergies"
+    data=eye(5)
+    A=Qobj(data)
+    b=A.eigenenergies()
+    assert_equal(all(b-ones(5)),0)
+    
+    data=diag(arange(10))
+    A=Qobj(data)
+    b=A.eigenenergies()
+    assert_equal(all(b-arange(10)),0)
+    
+    
+    data=diag(arange(10))
+    A=5*Qobj(data)
+    b=A.eigenenergies()
+    assert_equal(all(b-5*arange(10)),0)
+
+
+def test_QobjEigenStates():
+    "Qobj eigenstates"
+    data=eye(5)
+    A=Qobj(data)
+    b,c=A.eigenstates()
+    assert_equal(all(b-ones(5)),0)
+    
+    kets=array([basis(5,k) for k in range(5)])
+    
+    for k in range(5):
+        assert_equal(c[k],kets[k])
+    
+
+def test_QobjExpm():
+    "Qobj expm"
+    data=random.random((15,15))+1j*random.random((15,15))-(0.5+0.5j)
+    A=Qobj(data)
+    B=A.expm()
+    assert_equal(all(B.data.todense()-matrix(expm(data,13)))<1e-15,True)
+
+def test_QobjFull():
+    "Qobj full"
+    data=random.random((15,15))+1j*random.random((15,15))-(0.5+0.5j)
+    A=Qobj(data)
+    b=A.full()
+    assert_equal(all(b-data),0)
+    
+
--- qutip/tests/test_operators.py
+++ qutip/tests/test_operators.py
@@ -23,8 +23,8 @@ from numpy import allclose
 from numpy.testing import assert_equal
 
 
-def test_Jmat_12():
-
+def test_jmat_12():
+    "Spin 1/2 operators"
     spinhalf = jmat(1/2.)
     
     paulix=array([[ 0.0+0.j,  0.5+0.j],[ 0.5+0.j,  0.0+0.j]])
@@ -39,8 +39,8 @@ def test_Jmat_12():
     assert_equal(allclose(jmat(1/2.,'+').full(),sigmap),True)
     assert_equal(allclose(jmat(1/2.,'-').full(),sigmam),True)
     
-def test_Jamt_32():
-    
+def test_jmat_32():
+    "Spin 3/2 operators"
     spin32=jmat(3/2.)
     
     paulix32=array([[ 0.0000000+0.j,  0.8660254+0.j,  0.0000000+0.j,  0.0000000+0.j],
@@ -62,16 +62,18 @@ def test_Jamt_32():
     assert_equal(allclose(spin32[1].full(),pauliy32),True)
     assert_equal(allclose(spin32[2].full(),pauliz32),True)
 
-def test_Jmat_42():
+def test_jmat_42():
+    "Spin 2 operators"
     spin42 = jmat(4/2.,'+')
     assert_equal(spin42.dims==[[5],[5]],True)
 
-def test_Jamt_52():
-    
+def test_jmat_52():
+    "Spin 5/2 operators"
     spin52 = jmat(5/2.,'+')
     assert_equal(spin52.shape==[6,6],True)
     
-def test_Destroy():
+def test_destroy():
+    "Destruction operator"
     b4=basis(5,4)
     d5=destroy(5)
     test1=d5*b4
@@ -83,7 +85,8 @@ def test_Destroy():
     
     assert_equal(allclose(matrix3,d3.full()),True)
 
-def test_Create():
+def test_create():
+    "Creation operator"
     b3=basis(5,3)
     c5=create(5)
     test1=c5*b3
@@ -96,15 +99,18 @@ def test_Create():
     assert_equal(allclose(matrix3,c3.full()),True)
     
     
-def test_Qeye():
+def test_qeye():
+    "Identity operator"
     eye3=qeye(5)
     assert_equal(allclose(eye3.full(),eye(5,dtype=complex)),True)
     
-def test_Num():
+def test_num():
+    "Number operator"
     n5=num(5)
     assert_equal(allclose(n5.full(),diag([0+0j,1+0j,2+0j,3+0j,4+0j])),True)
 
-def test_Squeez():
+def test_squeez():
+    "Squeezing operator"
     sq=squeez(4,0.1+0.1j)
     sqmatrix=array([[ 0.99500417+0.j        ,  0.00000000+0.j        ,
              0.07059289-0.07059289j,  0.00000000+0.j        ],
@@ -117,7 +123,8 @@ def test_Squeez():
              
     assert_equal(allclose(sq.full(),sqmatrix),True)
     
-def test_Displace():
+def test_displace():
+    "Displacement operator"
     dp=displace(4,0.25)
     dpmatrix=array([[ 0.96923323+0.j, -0.24230859+0.j,  0.04282883+0.j, -0.00626025+0.j],
            [ 0.24230859+0.j,  0.90866411+0.j, -0.33183303+0.j,  0.07418172+0.j],
