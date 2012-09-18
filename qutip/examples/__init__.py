7e7c13430eb25196670fcb6aa7464e09f268d77f
Robert Johansson <jrjohansson@gmail.com>
2012-09-18 08:12:48 +0000

    code cleanup: shorten long lines

--- qutip/examples/__init__.py
+++ qutip/examples/__init__.py
@@ -1,9 +1,9 @@
-#This file is part of QuTIP.
+# This file is part of QuTIP.
 #
 #    QuTIP is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+#    (at your option) any later version.
 #
 #    QuTIP is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,11 +13,24 @@
 #    You should have received a copy of the GNU General Public License
 #    along with QuTIP.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2011, Paul D. Nation & Robert J. Johansson
+# Copyright (C) 2011-2012, Paul D. Nation & Robert J. Johansson
 #
-###########################################################################
-import qutip.examples.ex_10,qutip.examples.ex_11,qutip.examples.ex_12,qutip.examples.ex_13,qutip.examples.ex_14,qutip.examples.ex_15,qutip.examples.ex_16,qutip.examples.ex_17,qutip.examples.ex_18,qutip.examples.ex_19
-import qutip.examples.ex_20,qutip.examples.ex_21,qutip.examples.ex_22,qutip.examples.ex_23,qutip.examples.ex_24,qutip.examples.ex_25,qutip.examples.ex_26,qutip.examples.ex_27
-import qutip.examples.ex_30,qutip.examples.ex_31,qutip.examples.ex_32,qutip.examples.ex_33,qutip.examples.ex_34,qutip.examples.ex_35
-import qutip.examples.ex_40,qutip.examples.ex_41,qutip.examples.ex_42,qutip.examples.ex_43,qutip.examples.ex_44,qutip.examples.ex_45
-import qutip.examples.ex_50,qutip.examples.ex_51,qutip.examples.ex_52,qutip.examples.ex_53
+################################################################################
+
+import qutip.examples.ex_10, qutip.examples.ex_11, qutip.examples.ex_12, \
+       qutip.examples.ex_13, qutip.examples.ex_14, qutip.examples.ex_15, \
+       qutip.examples.ex_16, qutip.examples.ex_17, qutip.examples.ex_18, \
+       qutip.examples.ex_19
+
+import qutip.examples.ex_20, qutip.examples.ex_21, qutip.examples.ex_22, \
+       qutip.examples.ex_23, qutip.examples.ex_24, qutip.examples.ex_25, \
+       qutip.examples.ex_26, qutip.examples.ex_27
+
+import qutip.examples.ex_30, qutip.examples.ex_31, qutip.examples.ex_32, \
+       qutip.examples.ex_33, qutip.examples.ex_34, qutip.examples.ex_35 
+
+import qutip.examples.ex_40, qutip.examples.ex_41, qutip.examples.ex_42, \
+       qutip.examples.ex_43, qutip.examples.ex_44, qutip.examples.ex_45
+
+import qutip.examples.ex_50, qutip.examples.ex_51, qutip.examples.ex_52, \
+        qutip.examples.ex_53
