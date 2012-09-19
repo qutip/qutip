45c62a915afee441cac3fc0e98776eb45db1f1d6
Paul Nation <nonhermitian@gmail.com>
2012-09-19 04:53:44 +0000

    modified LZS example so that it does not try to compile the RHS function when qutip is loaded.

--- qutip/examples/ex_52.py
+++ qutip/examples/ex_52.py
@@ -2,52 +2,53 @@
 # Landau-Zener-Stuckelberg interferometry: steady state of repeated Landau-Zener
 # like avoided-level crossing, as a function of driving amplitude and bias.
 #
+# Note: In order to get this example to work properly in the demos window,
+# we have had to pass many more variables to parfor than is typically necessary.
+
 from qutip import *
 from pylab import *
-
-# set up the parameters and start calculation
-delta    = 0.1  * 2 * pi  # qubit sigma_x coefficient
-w        = 2.0  * 2 * pi  # driving frequency
-T        = 2 * pi / w     # driving period 
-gamma1   = 0.00001        # relaxation rate
-gamma2   = 0.005          # dephasing  rate
-eps_list = linspace(-10.0, 10.0, 101) * 2 * pi
-A_list   = linspace(0.0, 20.0, 101) * 2 * pi
-
-# pre-calculate the necessary operators
-sx = sigmax(); sz = sigmaz(); sm = destroy(2); sn = num(2)
-# collapse operators
-c_op_list = [sqrt(gamma1) * sm, sqrt(gamma2) * sz]  # relaxation and dephasing
-
-# setup time-dependent Hamiltonian (list-string format)
-H0 = -delta/2.0 * sx
-H1 = [sz,'-eps/2.0+A/2.0*sin(w * t)']
-H_td = [H0,H1]
-Hargs = {'w': w,'eps':eps_list[0],'A':A_list[0]}
-
-# ODE settings (for reusing list-str format Hamiltonian)
-opts = Odeoptions(rhs_reuse = True)
-
-# pre-generate RHS so we can use parfor
-rhs_generate(H_td,c_op_list,Hargs,name='lz_func')
 
 # a task function for the for-loop parallelization: 
 # the m-index is parallelized in loop over the elements of p_mat[m,n]
 def task(args):
-    m, eps = args
+    m, H_td, c_op_list, sn, A_list, T, w, eps = args
     p_mat_m = zeros(len(A_list))
     for n, A in enumerate(A_list):
         # change args sent to solver, w is really a constant though.
         Hargs = {'w': w, 'eps': eps,'A': A}
-        U = propagator(H_td, T, c_op_list, Hargs, opts)
+        # settings (for reusing list-str format Hamiltonian)
+        U = propagator(H_td, T, c_op_list, Hargs, Odeoptions(rhs_reuse = True))
         rho_ss = propagator_steadystate(U)
         p_mat_m[n] = expect(sn, rho_ss)
     return [m, p_mat_m]
 
 def run():
+    
+    # set up the parameters and start calculation
+    delta    = 0.1  * 2 * pi  # qubit sigma_x coefficient
+    w        = 2.0  * 2 * pi  # driving frequency
+    T        = 2 * pi / w     # driving period 
+    gamma1   = 0.00001        # relaxation rate
+    gamma2   = 0.005          # dephasing  rate
+    eps_list = linspace(-10.0, 10.0, 101) * 2 * pi
+    A_list   = linspace(0.0, 20.0, 101) * 2 * pi
 
+    # pre-calculate the necessary operators
+    sx = sigmax(); sz = sigmaz(); sm = destroy(2); sn = num(2)
+    # collapse operators
+    c_op_list = [sqrt(gamma1) * sm, sqrt(gamma2) * sz]#relaxation and dephasing
+
+    # setup time-dependent Hamiltonian (list-string format)
+    H0 = -delta/2.0 * sx
+    H1 = [sz,'-eps/2.0+A/2.0*sin(w * t)']
+    H_td = [H0,H1]
+    Hargs = {'w': w,'eps':eps_list[0],'A':A_list[0]}
+
+    # pre-generate RHS so we can use parfor
+    rhs_generate(H_td,c_op_list,Hargs,name='lz_func')
     # start a parallel for loop over bias point values (eps_list)
-    p_mat_list = parfor(task, enumerate(eps_list))
+    parfor_args=[[k,H_td,c_op_list,sn,A_list,T,w,eps_list[k]] for k in range(len(eps_list))]
+    p_mat_list = parfor(task, parfor_args)
 
     # assemble a matrix p_mat from list of (index,array) tuples returned by parfor
     p_mat = zeros((len(eps_list),len(A_list)))
