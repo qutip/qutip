6d09d30419d411970b3ca50fdf4c845b2938f4e0
Robert Johansson <jrjohansson@gmail.com>
2012-10-10 17:05:21 +0900

    fixed bug in returing states from bloch redfield solver

--- qutip/bloch_redfield.py
+++ qutip/bloch_redfield.py
@@ -88,7 +88,12 @@ def brmesolve(H, psi0, tlist, c_ops, e_ops=[], spectra_cb=[], args={}, options=O
     output = Odedata()
     output.times = tlist
 
-    output.expect = bloch_redfield_solve(R, ekets, psi0, tlist, e_ops, options)
+    result = bloch_redfield_solve(R, ekets, psi0, tlist, e_ops, options)
+
+    if len(e_ops):
+        output.expect = results
+    else:
+        output.states = results
 
     return output
 
