fixed some broken units tests (rand form scipy was not imported)

qutip/tests/test_basis_transformation.py
@@ -20,10 +20,11 @@
 import sys
 from qutip import *
 from numpy.testing import assert_equal
+import scipy
 
 def test_Transformation1():
     "Transform 2-level to eigenbasis and back"
-    H1 = rand() * sigmax() + rand() * sigmay() + rand() * sigmaz()
+    H1 = scipy.rand() * sigmax() + scipy.rand() * sigmay() + scipy.rand() * sigmaz()
     evals, ekets = H1.eigenstates()
     Heb = H1.transform(ekets)       # eigenbasis (should be diagonal)
     H2 = Heb.transform(ekets, True) # back to original basis
@@ -33,7 +34,7 @@ def test_Transformation1():
 def test_Transformation2():
     "Transform 10-level real-values to eigenbasis and back"
     N = 10
-    H1 = Qobj((0.5-rand(N,N)))
+    H1 = Qobj((0.5-scipy.rand(N,N)))
     H1 = H1 + H1.dag()
     evals, ekets = H1.eigenstates()
     Heb = H1.transform(ekets)       # eigenbasis (should be diagonal)
@@ -44,7 +45,7 @@ def test_Transformation2():
 def test_Transformation3():
     "Transform 10-level to eigenbasis and back"
     N = 10
-    H1 = Qobj((0.5-rand(N,N)) + 1j*(0.5-rand(N,N)))
+    H1 = Qobj((0.5-scipy.rand(N,N)) + 1j*(0.5-scipy.rand(N,N)))
     H1 = H1 + H1.dag()
     evals, ekets = H1.eigenstates()
     Heb = H1.transform(ekets)       # eigenbasis (should be diagonal)
@@ -55,7 +56,7 @@ def test_Transformation3():
 def test_Transformation4():
     "Transform 10-level imag to eigenbasis and back"
     N = 10
-    H1 = Qobj(1j*(0.5-rand(N,N)))
+    H1 = Qobj(1j*(0.5-scipy.rand(N,N)))
     H1 = H1 + H1.dag()
     evals, ekets = H1.eigenstates()
     Heb = H1.transform(ekets)       # eigenbasis (should be diagonal)

qutip/tests/test_eigenstates.py
@@ -20,14 +20,14 @@
 import sys
 from qutip import *
 from numpy.testing import assert_equal
-
+import scipy
 
 def test_diagHamiltonian1():
     """
     Diagonalization of random two-level system
     """
 
-    H = rand() * sigmax() + rand() * sigmay() + rand() * sigmaz()
+    H = scipy.rand() * sigmax() + scipy.rand() * sigmay() + scipy.rand() * sigmaz()
 
     evals, ekets = H.eigenstates()
 
@@ -40,8 +40,8 @@ def test_diagHamiltonian2():
     Diagonalization of composite systems
     """
 
-    H1 = rand() * sigmax() + rand() * sigmay() + rand() * sigmaz()
-    H2 = rand() * sigmax() + rand() * sigmay() + rand() * sigmaz()
+    H1 = scipy.rand() * sigmax() + scipy.rand() * sigmay() + scipy.rand() * sigmaz()
+    H2 = scipy.rand() * sigmax() + scipy.rand() * sigmay() + scipy.rand() * sigmaz()
 
     H = tensor(H1, H2)
 
@@ -56,7 +56,7 @@ def test_diagHamiltonian2():
 
     a1 = tensor(destroy(N1), qeye(N2))
     a2 = tensor(qeye(N1), destroy(N2))
-    H = rand() * a1.dag() * a1 + rand() * a2.dag() * a2 + rand() * (a1 + a1.dag()) * (a2 + a2.dag())
+    H = scipy.rand() * a1.dag() * a1 + scipy.rand() * a2.dag() * a2 + scipy.rand() * (a1 + a1.dag()) * (a2 + a2.dag())
     evals, ekets = H.eigenstates()
     
     for n in range(len(evals)):

qutip/tests/test_fileio.py
@@ -19,6 +19,7 @@
 
 import os
 from numpy.testing import assert_
+import scipy
 
 from qutip import *
 
@@ -32,7 +33,7 @@ class TestFileIO:
         
         # create some random data
         N = 10
-        data = (1-2*rand(N,N))
+        data = (1-2*scipy.rand(N,N))
 
         file_data_store("test.dat", data)
         data2 = file_data_read("test.dat")
@@ -45,7 +46,7 @@ class TestFileIO:
 
         # create some random data
         N = 10
-        data = (1-2*rand(N,N))
+        data = (1-2*scipy.rand(N,N))
 
         file_data_store("test.dat", data, "real", "decimal")
         data2 = file_data_read("test.dat", ",")
@@ -58,7 +59,7 @@ class TestFileIO:
 
         # create some random data
         N = 10
-        data = (1-2*rand(N,N))
+        data = (1-2*scipy.rand(N,N))
 
         file_data_store("test.dat", data, "real", "exp")
         data2 = file_data_read("test.dat", ",")
@@ -71,7 +72,7 @@ class TestFileIO:
 
         # create some random data
         N = 10
-        data = (1-2*rand(N,N)) + 1j*(1-2*rand(N,N))
+        data = (1-2*scipy.rand(N,N)) + 1j*(1-2*scipy.rand(N,N))
 
         file_data_store("test.dat", data)
         data2 = file_data_read("test.dat")
@@ -84,7 +85,7 @@ class TestFileIO:
 
         # create some random data
         N = 10
-        data = (1-2*rand(N,N)) + 1j*(1-2*rand(N,N))
+        data = (1-2*scipy.rand(N,N)) + 1j*(1-2*scipy.rand(N,N))
 
         file_data_store("test.dat", data, "complex", "decimal")
         data2 = file_data_read("test.dat", ",")
@@ -97,7 +98,7 @@ class TestFileIO:
 
         # create some random data
         N = 10
-        data = (1-2*rand(N,N)) + 1j*(1-2*rand(N,N))
+        data = (1-2*scipy.rand(N,N)) + 1j*(1-2*scipy.rand(N,N))
 
         file_data_store("test.dat", data, "complex", "exp")
         data2 = file_data_read("test.dat", ",")
@@ -110,7 +111,7 @@ class TestFileIO:
 
         # create some random data
         N = 10
-        data = (1-2*rand(N,N)) + 1j*(1-2*rand(N,N))
+        data = (1-2*scipy.rand(N,N)) + 1j*(1-2*scipy.rand(N,N))
 
         # comma separated values
         file_data_store("test.dat", data, "complex", "exp", ",")