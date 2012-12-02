da0d9a878b5c3470a1fed95efe30b37f0da693e8
Markus Baden <markus.baden@gmail.com>
2012-12-02 16:37:11 +0800

    PEP8: Fixed most PEP8 issues in entropy.py

--- qutip/entropy.py
+++ qutip/entropy.py
@@ -25,107 +25,109 @@ from qutip.tensor import tensor
 from qutip.operators import sigmay
 from qutip.sparse import sp_eigs
 
-def entropy_vn(rho,base=e,sparse=False):
+
+def entropy_vn(rho, base=e, sparse=False):
     """
     Von-Neumann entropy of density matrix
-    
+
     Parameters
     ----------
     rho : qobj
         Density matrix.
-    base : {e,2} 
+    base : {e,2}
         Base of logarithm.
     sparse : {False,True}
         Use sparse eigensolver.
-    
+
     Returns
-    ------- 
+    -------
     entropy : float
         Von-Neumann entropy of `rho`.
-    
+
     Examples
     --------
     >>> rho=0.5*fock_dm(2,0)+0.5*fock_dm(2,1)
     >>> entropy_vn(rho,2)
     1.0
-    
+
     """
-    if rho.type=='ket' or rho.type=='bra':
-        rho=ket2dm(rho)
-    vals=sp_eigs(rho,vecs=False,sparse=sparse)
-    nzvals=vals[vals!=0]
-    if base==2:
-        logvals=log2(nzvals)
-    elif base==e:
-        logvals=log(nzvals)
+    if rho.type == 'ket' or rho.type == 'bra':
+        rho = ket2dm(rho)
+    vals = sp_eigs(rho, vecs=False, sparse=sparse)
+    nzvals = vals[vals != 0]
+    if base == 2:
+        logvals = log2(nzvals)
+    elif base == e:
+        logvals = log(nzvals)
     else:
         raise ValueError("Base must be 2 or e.")
-    return float(real(-sum(nzvals*logvals)))
+    return float(real(-sum(nzvals * logvals)))
 
 
 def entropy_linear(rho):
     """
     Linear entropy of a density matrix.
-    
+
     Parameters
     ----------
     rho : qobj
         sensity matrix or ket/bra vector.
-    
+
     Returns
     -------
     entropy : float
         Linear entropy of rho.
-    
+
     Examples
-    -------- 
+    --------
     >>> rho=0.5*fock_dm(2,0)+0.5*fock_dm(2,1)
     >>> entropy_linear(rho)
     0.5
-    
-    """
-    if rho.type=='ket' or rho.type=='bra':
-        rho=ket2dm(rho)
-    return float(real(1.0-(rho**2).tr()))
 
+    """
+    if rho.type == 'ket' or rho.type == 'bra':
+        rho = ket2dm(rho)
+    return float(real(1.0 - (rho**2).tr()))
 
 
 def concurrence(rho):
     """
-    Calculate the concurrence entanglement measure for 
+    Calculate the concurrence entanglement measure for
     a two-qubit state.
-    
+
     Parameters
     ----------
-    rho : qobj 
+    rho : qobj
         Density matrix for two-qubits.
-    
+
     Returns
     -------
     concur : float
         Concurrence
-    
+
     """
-    if rho.dims!=[[2, 2], [2, 2]]:
+    if rho.dims != [[2, 2], [2, 2]]:
         raise Exception("Density matrix must be tensor product of two qubits.")
-    
+
     sysy = tensor(sigmay(), sigmay())
 
     rho_tilde = (rho * sysy) * (rho.conj() * sysy)
 
     evals = rho_tilde.eigenenergies()
 
-    evals = abs(sort(real(evals))) # abs to avoid problems with sqrt for very small negative numbers
+    # abs to avoid problems with sqrt for very small negative numbers
+    evals = abs(sort(real(evals)))
 
     lsum = sqrt(evals[3]) - sqrt(evals[2]) - sqrt(evals[1]) - sqrt(evals[0])
 
     return max(0, lsum)
 
 
-def entropy_mutual(rho,selA,selB,base=e,sparse=False):
+def entropy_mutual(rho, selA, selB, base=e, sparse=False):
     """
-    Calculates the mutual information S(A:B) between selection components of a system density matrix.
-    
+    Calculates the mutual information S(A:B) between selection
+    components of a system density matrix.
+
     Parameters
     ----------
     rho : qobj
@@ -134,99 +136,103 @@ def entropy_mutual(rho,selA,selB,base=e,sparse=False):
         `int` or `list` of first selected density matrix components.
     selB : int/list
         `int` or `list` of second selected density matrix components.
-    base : {e,2} 
+    base : {e,2}
         Base of logarithm.
     sparse : {False,True}
         Use sparse eigensolver.
-    
+
     Returns
     -------
-    ent_mut : float 
+    ent_mut : float
        Mutual information between selected components.
-    
+
     """
-    if isinstance(selA,int):
-        selA=[selA]
-    if isinstance(selB,int):
-        selB=[selB]
-    if rho.type!='oper':
+    if isinstance(selA, int):
+        selA = [selA]
+    if isinstance(selB, int):
+        selB = [selB]
+    if rho.type != 'oper':
         raise TypeError("Input must be a density matrix.")
-    if (len(selA)+len(selB))!=len(rho.dims[0]):
-        raise TypeError("Number of selected components must match total number.")
-    
-    rhoA=ptrace(rho,selA)
-    rhoB=ptrace(rho,selB)
-    out=entropy_vn(rhoA,base,sparse=sparse)+entropy_vn(rhoB,base,sparse=sparse)-entropy_vn(rho,base,sparse=sparse)
+    if (len(selA) + len(selB)) != len(rho.dims[0]):
+        raise TypeError("Number of selected components must match " +
+                        "total number.")
+
+    rhoA = ptrace(rho, selA)
+    rhoB = ptrace(rho, selB)
+    out = (entropy_vn(rhoA, base, sparse=sparse) +
+           entropy_vn(rhoB, base, sparse=sparse) -
+           entropy_vn(rho, base, sparse=sparse))
     return out
 
 
-def _entropy_relative(rho,sigma,base=e,sparse=False):
+def _entropy_relative(rho, sigma, base=e, sparse=False):
     """
     ****NEEDS TO BE WORKED ON**** (after 2.0 release)
-    
-    Calculates the relative entropy S(rho||sigma) between two density matricies.
-    
+
+    Calculates the relative entropy S(rho||sigma) between two density
+    matricies.
+
     Parameters
     ----------
-    rho : qobj 
+    rho : qobj
         First density matrix.
-    sigma : qobj 
+    sigma : qobj
         Second density matrix.
-    base : {e,2} 
+    base : {e,2}
         Base of logarithm.
-    
+
     Returns
     -------
     rel_ent : float
         Value of relative entropy.
-    
-    """ 
-    if rho.type!='oper' or sigma.type!='oper':
+
+    """
+    if rho.type != 'oper' or sigma.type != 'oper':
         raise TypeError("Inputs must be density matricies.")
     #sigma terms
-    svals=sp_eigs(sigma,vecs=False,sparse=sparse)
-    snzvals=svals[svals!=0]
-    if base==2:
-        slogvals=log2(snzvals)
-    elif base==e:
-        slogvals=log(snzvals)
+    svals = sp_eigs(sigma,vecs=False, sparse=sparse)
+    snzvals = svals[svals != 0]
+    if base == 2:
+        slogvals = log2(snzvals)
+    elif base == e:
+        slogvals = log(snzvals)
     else:
         raise ValueError("Base must be 2 or e.")
     #rho terms
-    rvals=sp_eigs(rho,vecs=False,sparse=sparse)
-    rnzvals=rvals[rvals!=0]
+    rvals = sp_eigs(rho, vecs=False, sparse=sparse)
+    rnzvals = rvals[rvals != 0]
     #calculate tr(rho*log sigma)
-    rel_trace=float(real(sum(rnzvals*slogvals)))
-    return -entropy_vn(rho,base,sparse)-rel_trace
+    rel_trace = float(real(sum(rnzvals * slogvals)))
+    return -entropy_vn(rho, base, sparse) - rel_trace
 
 
-def entropy_conditional(rho,selB,base=e,sparse=False):
+def entropy_conditional(rho, selB, base=e, sparse=False):
     """
-    Calculates the conditional entropy :math:`S(A|B)=S(A,B)-S(B)` 
+    Calculates the conditional entropy :math:`S(A|B)=S(A,B)-S(B)`
     of a slected density matrix component.
 
     Parameters
     ----------
-    rho : qobj 
+    rho : qobj
         Density matrix of composite object
-    selB : int/list 
+    selB : int/list
         Selected components for density matrix B
-    base : {e,2} 
+    base : {e,2}
         Base of logarithm.
     sparse : {False,True}
         Use sparse eigensolver.
-    
+
     Returns
     -------
     ent_cond : float
         Value of conditional entropy
-    
-    """
-    if rho.type!='oper':
-        raise TypeError("Input must be density matrix.")
-    if isinstance(selB,int):
-        selB=[selB]
-    B=ptrace(rho,selB)
-    out=entropy_vn(rho,base,sparse=sparse)-entropy_vn(B,base,sparse=sparse)
-    return out
 
+    """
+    if rho.type != 'oper':
+        raise TypeError("Input must be density matrix.")
+    if isinstance(selB, int):
+        selB = [selB]
+    B = ptrace(rho, selB)
+    out = (entropy_vn(rho, base, sparse=sparse) -
+           entropy_vn(B, base, sparse=sparse))
+    return out
