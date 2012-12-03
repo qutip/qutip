d7125ab8438498e882f6c7fde71df8188c0b7392
Robert Johansson <jrjohansson@gmail.com>
2012-12-03 17:37:40 +0100

    made code PEP8 compliant

--- qutip/propagator.py
+++ qutip/propagator.py
@@ -30,6 +30,7 @@ from qutip.states import basis
 from qutip.states import projection
 from qutip.odeoptions import Odeoptions
 
+
 def propagator(H, t, c_op_list, H_args=None, opt=None):
     """
     Calculate the propagator U(t) for the density matrix or wave function such
@@ -37,33 +38,33 @@ def propagator(H, t, c_op_list, H_args=None, opt=None):
     :math:`\\rho_{\mathrm vec}(t) = U(t) \\rho_{\mathrm vec}(0)`
     where :math:`\\rho_{\mathrm vec}` is the vector representation of the
     density matrix.
-    
+
     Parameters
     ----------
     H : qobj or list
-        Hamiltonian as a Qobj instance of a nested list of Qobjs and 
+        Hamiltonian as a Qobj instance of a nested list of Qobjs and
         coefficients in the list-string or list-function format for
-        time-dependent Hamiltonians (see description in :func:`qutip.mesolve`).  
-    t : float or array-like 
-        Time or list of times for which to evaluate the propagator.   
-    c_op_list : list 
+        time-dependent Hamiltonians (see description in :func:`qutip.mesolve`).
+    t : float or array-like
+        Time or list of times for which to evaluate the propagator.
+    c_op_list : list
         List of qobj collapse operators.
-    H_args : list/array/dictionary 
+    H_args : list/array/dictionary
         Parameters to callback functions for time-dependent Hamiltonians.
-    
+
     Returns
     -------
-     a : qobj 
+     a : qobj
         Instance representing the propagator :math:`U(t)`.
-    
+
     """
 
-    if opt == None:
+    if opt is None:
         opt = Odeoptions()
         opt.rhs_reuse = True
 
-    tlist = [0, t] if isinstance(t,(int,float,np.int64,np.float64)) else t
-    
+    tlist = [0, t] if isinstance(t, (int, float, np.int64, np.float64)) else t
+
     if len(c_op_list) == 0:
         # calculate propagator for the wave function
 
@@ -73,7 +74,7 @@ def propagator(H, t, c_op_list, H_args=None, opt=None):
             dims = H0.dims
         elif isinstance(H, list):
             H0 = H[0][0] if isinstance(H[0], list) else H[0]
-            N = H0.shape[0] 
+            N = H0.shape[0]
             dims = H0.dims
         else:
             N = H.shape[0]
@@ -85,7 +86,7 @@ def propagator(H, t, c_op_list, H_args=None, opt=None):
             psi0 = basis(N, n)
             output = mesolve(H, psi0, tlist, [], [], H_args, opt)
             for k, t in enumerate(tlist):
-                u[:,n,k] = output.states[k].full().T
+                u[:, n, k] = output.states[k].full().T
 
         # todo: evolving a batch of wave functions:
         #psi_0_list = [basis(N, n) for n in range(N)]
@@ -94,70 +95,70 @@ def propagator(H, t, c_op_list, H_args=None, opt=None):
         #    u[:,n] = psi_t_list[n][1].full().T
 
     else:
-        # calculate the propagator for the vector representation of the 
+        # calculate the propagator for the vector representation of the
         # density matrix (a superoperator propagator)
 
         if isinstance(H, types.FunctionType):
             H0 = H(0.0, H_args)
-            N = H0.shape[0]            
+            N = H0.shape[0]
             dims = [H0.dims, H0.dims]
         elif isinstance(H, list):
             H0 = H[0][0] if isinstance(H[0], list) else H[0]
-            N = H0.shape[0]            
+            N = H0.shape[0]
             dims = [H0.dims, H0.dims]
         else:
             N = H.shape[0]
             dims = [H.dims, H.dims]
 
-        u = np.zeros([N*N, N*N, len(tlist)], dtype=complex)
-        
-        for n in range(0, N*N):
-            psi0  = basis(N*N, n)
-            rho0  = Qobj(vec2mat(psi0.full()))
+        u = np.zeros([N * N, N * N, len(tlist)], dtype=complex)
+
+        for n in range(0, N * N):
+            psi0 = basis(N * N, n)
+            rho0 = Qobj(vec2mat(psi0.full()))
             output = mesolve(H, rho0, tlist, c_op_list, [], H_args, opt)
             for k, t in enumerate(tlist):
-                u[:,n,k] = mat2vec(output.states[k].full()).T
+                u[:, n, k] = mat2vec(output.states[k].full()).T
 
     if len(tlist) == 2:
-        return Qobj(u[:,:,1], dims=dims)
+        return Qobj(u[:, :, 1], dims=dims)
     else:
-        return [Qobj(u[:,:,k], dims=dims) for k in range(len(tlist))]
+        return [Qobj(u[:, :, k], dims=dims) for k in range(len(tlist))]
 
 
-def _get_min_and_index(lst): 
+def _get_min_and_index(lst):
     """
     Private function for obtaining min and max indicies.
     """
-    minval,minidx = lst[0],0 
-    for i,v in enumerate(lst[1:]): 
-        if v < minval: 
-            minval,minidx = v,i+1 
-    return minval,minidx 
+    minval, minidx = lst[0], 0
+    for i, v in enumerate(lst[1:]):
+        if v < minval:
+            minval, minidx = v, i + 1
+    return minval, minidx
 
 
 def propagator_steadystate(U):
     """Find the steady state for successive applications of the propagator
     :math:`U`.
-    
+
     Parameters
     ----------
-    U : qobj 
+    U : qobj
         Operator representing the propagator.
-    
+
     Returns
-    ------- 
+    -------
     a : qobj
         Instance representing the steady-state density matrix.
-    
+
     """
 
-    evals,evecs = la.eig(U.full())
+    evals, evecs = la.eig(U.full())
 
-    ev_min, ev_idx = _get_min_and_index(abs(evals-1.0))
+    ev_min, ev_idx = _get_min_and_index(abs(evals - 1.0))
 
     N = int(np.sqrt(len(evals)))
     evecs = evecs.T
     rho = Qobj(vec2mat(evecs[ev_idx]))
-    rho = rho * (1.0/rho.tr())
-    rho = 0.5*(rho+rho.dag()) #make sure rho is herm
+    rho = rho * (1.0 / rho.tr())
+    rho = 0.5 * (rho + rho.dag())  # make sure rho is herm
     return rho
