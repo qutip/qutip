034e0b10791fe56c5a232febc96ec09e514f2306
Robert Johansson <jrjohansson@gmail.com>
2013-01-09 13:57:28 +0900

    pep8 fixes

--- qutip/metrics.py
+++ qutip/metrics.py
@@ -1,9 +1,9 @@
-#This file is part of QuTIP.
+# This file is part of QuTIP.
 #
 #    QuTIP is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+#    (at your option) any later version.
 #
 #    QuTIP is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,7 +18,7 @@
 ###########################################################################
 
 """
-A Module containing a collection of metrics 
+A Module containing a collection of metrics
 (distance measures) between density matricies.
 """
 
@@ -28,71 +28,71 @@ import numpy as np
 from qutip.sparse import sp_eigs
 
 
-def fidelity(A,B):
+def fidelity(A, B):
     """
     Calculates the fidelity (pseudo-metric) between two density matricies.
     See: Nielsen & Chuang, "Quantum Computation and Quantum Information"
-    
+
     Parameters
     ----------
     A : qobj
         Density matrix
-    B : qobj 
+    B : qobj
         Density matrix with same dimensions as A.
-    
+
     Returns
     -------
     fid : float
         Fidelity pseudo-metric between A and B.
-    
+
     Examples
     --------
     >>> x=fock_dm(5,3)
     >>> y=coherent_dm(5,1)
     >>> fidelity(x,y)
     0.24104350624628332
-        
+
     """
-    if A.dims!=B.dims:
+    if A.dims != B.dims:
         raise TypeError('Density matricies do not have same dimensions.')
     else:
-        A=A.sqrtm()
-        return float(np.real((A*(B*A)).sqrtm().tr()))
+        A = A.sqrtm()
+        return float(np.real((A * (B * A)).sqrtm().tr()))
 
 
-def tracedist(A,B,sparse=False,tol=0):
+def tracedist(A, B, sparse=False, tol=0):
     """
     Calculates the trace distance between two density matricies.
     See: Nielsen & Chuang, "Quantum Computation and Quantum Information"
-    
+
     Parameters
     ----------
-    A : qobj 
+    A : qobj
         Density matrix.
     B : qobj:
         Density matrix with same dimensions as A.
-    tol : float 
+    tol : float
         Tolerance used by sparse eigensolver. (0=Machine precision)
     sparse : {False, True}
         Use sparse eigensolver.
-    
+
     Returns
     -------
     tracedist : float
         Trace distance between A and B.
-    
+
     Examples
-    --------    
+    --------
     >>> x=fock_dm(5,3)
     >>> y=coherent_dm(5,1)
     >>> tracedist(x,y)
     0.9705143161472971
-    
+
     """
-    if A.dims!=B.dims:
+    if A.dims != B.dims:
         raise TypeError('Density matricies do not have same dimensions.')
     else:
-        diff=A-B
-        diff=diff.dag()*diff
-        vals=sp_eigs(diff,vecs=False,sparse=sparse,tol=tol)
-        return float(np.real(0.5*np.sum(np.sqrt(np.abs(vals)))))
+        diff = A - B
+        diff = diff.dag() * diff
+        vals = sp_eigs(diff, vecs=False, sparse=sparse, tol=tol)
+        return float(np.real(0.5 * np.sum(np.sqrt(np.abs(vals)))))
