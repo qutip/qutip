f813e47d8d6391600276d3e6bf8f6e9e58ef78f8
Robert Johansson <jrjohansson@gmail.com>
2013-01-09 14:16:22 +0900

    pep8 fixes

--- qutip/floquet.py
+++ qutip/floquet.py
@@ -78,11 +78,11 @@ def floquet_modes(H, T, args=None, sort=False):
     # the quasi energy is in the interval [-pi/T, pi/T] where T is the
     # period of the driving.  eargs += (eargs <= -2*pi) * (2*pi) +
     # (eargs > 0) * (-2*pi)
-    eargs  += (eargs <= -pi) * (2 * pi) + (eargs > pi) * (-2 * pi)
+    eargs += (eargs <= -pi) * (2 * pi) + (eargs > pi) * (-2 * pi)
     e_quasi = -eargs / T
 
     # sort by the quasi energy
-    if sort == True:
+    if sort:
         order = np.argsort(-e_quasi)
     else:
         order = list(range(len(evals)))
@@ -695,7 +695,6 @@ def floquet_markov_mesolve(R, ekets, rho0, tlist, e_ops, options=None):
     else:
         raise TypeError("Expectation parameter must be a list or a function")
 
-
     #
     # transform the initial density matrix and the e_ops opterators to the
     # eigenbasis: from computational basis to the floquet basis
@@ -829,7 +828,7 @@ def fmmesolve(H, rho0, tlist, c_ops, e_ops=[], spectra_cb=[], T=None,
                                           H, T, args)
 
     # get w_th from args if it exists
-    if args.has_key('w_th'):
+    if 'w_th' in args:
         w_th = args['w_th']
     else:
         w_th = 0
