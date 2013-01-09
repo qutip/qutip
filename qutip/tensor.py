9ca374ff4aa31ad75831b9a9c6d0663897554e0f
Robert Johansson <jrjohansson@gmail.com>
2013-01-09 11:14:34 +0900

    cleanup and pep8ified

--- qutip/tensor.py
+++ qutip/tensor.py
@@ -1,9 +1,9 @@
-#This file is part of QuTIP.
+# This file is part of QuTIP.
 #
 #    QuTIP is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+#    (at your option) any later version.
 #
 #    QuTIP is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -26,24 +26,25 @@ import scipy.sparse as sp
 from qutip.qobj import Qobj
 import qutip.settings
 
+
 def tensor(*args):
-    """Calculates the tensor product of input operators. 
-    
+    """Calculates the tensor product of input operators.
+
     Parameters
     ----------
-    args : array_like 
+    args : array_like
         ``list`` or ``array`` of quantum objects for tensor product.
-        
+
     Returns
     --------
     obj : qobj
         A composite quantum object.
-    
+
     Examples
-    --------    
+    --------
     >>> tensor([sigmax(), sigmax()])
     Quantum object: dims = [[2, 2], [2, 2]], shape = [4, 4], type = oper, isHerm = True
-    Qobj data = 
+    Qobj data =
     [[ 0.+0.j  0.+0.j  0.+0.j  1.+0.j]
      [ 0.+0.j  0.+0.j  1.+0.j  0.+0.j]
      [ 0.+0.j  1.+0.j  0.+0.j  0.+0.j]
@@ -52,51 +53,55 @@ def tensor(*args):
     """
     if not args:
         raise TypeError("Requires at least one input argument")
-    num_args=len(args)
-    step=0
+    num_args = len(args)
+    step = 0
     for n in range(num_args):
-        if isinstance(args[n],Qobj):
-            qos=args[n]
-            if step==0:
-                dat=qos.data
-                dim=qos.dims
-                shp=qos.shape
-                step=1
+        if isinstance(args[n], Qobj):
+            qos = args[n]
+            if step == 0:
+                dat = qos.data
+                dim = qos.dims
+                shp = qos.shape
+                step = 1
             else:
-                dat=sp.kron(dat,qos.data, format='csr') #sparse Kronecker product
-                dim=[dim[0]+qos.dims[0],dim[1]+qos.dims[1]] #append dimensions of Qobjs
-                shp=[dat.shape[0],dat.shape[1]] #new shape of matrix
-                
-        elif isinstance(args[n],(list,ndarray)):#checks if input is list/array of Qobjs
-            qos=args[n]
-            items=len(qos) #number of inputs
-            if not all([isinstance(k,Qobj) for k in qos]): #raise error if one of the inputs is not a quantum object
+                dat = sp.kron(dat, qos.data, format='csr')
+                dim = [dim[0] + qos.dims[0],
+                       dim[1] + qos.dims[1]]  # append dimensions of Qobjs
+                shp = [dat.shape[0], dat.shape[1]]  # new shape of matrix
+
+        elif isinstance(args[n], (list, ndarray)):
+            qos = args[n]
+            items = len(qos)  # number of inputs
+            if not all([isinstance(k, Qobj) for k in qos]):
+                # raise error if one of the inputs is not a quantum object
                 raise TypeError("One of inputs is not a quantum object")
-            if items==1:# if only one Qobj, do nothing
-                if step==0: 
-                    dat=qos[0].data
-                    dim=qos[0].dims
-                    shp=qos[0].shape
-                    step=1
+            if items == 1:  # if only one Qobj, do nothing
+                if step == 0:
+                    dat = qos[0].data
+                    dim = qos[0].dims
+                    shp = qos[0].shape
+                    step = 1
                 else:
-                    dat=sp.kron(dat,qos[0].data, format='csr') #sparse Kronecker product
-                    dim=[dim[0]+qos[0].dims[0],dim[1]+qos[0].dims[1]] #append dimensions of Qobjs
-                    shp=[dat.shape[0],dat.shape[1]] #new shape of matrix
-            elif items!=1:
-                if step==0:
-                    dat=qos[0].data
-                    dim=qos[0].dims
-                    shp=qos[0].shape
-                    step=1
-                for k in range(items-1): #cycle over all items
-                    dat=sp.kron(dat,qos[k+1].data, format='csr') #sparse Kronecker product
-                    dim=[dim[0]+qos[k+1].dims[0],dim[1]+qos[k+1].dims[1]] #append dimensions of Qobjs
-                    shp=[dat.shape[0],dat.shape[1]] #new shape of matrix
-    out=Qobj()
-    out.data=dat
-    out.dims=dim
-    out.shape=shp
+                    dat = sp.kron(dat, qos[0].data, format='csr')
+                    dim = [dim[0] + qos[0].dims[0],
+                           dim[1] + qos[0].dims[1]]  # append dimensions of qos
+                    shp = [dat.shape[0], dat.shape[1]]  # new shape of matrix
+            elif items != 1:
+                if step == 0:
+                    dat = qos[0].data
+                    dim = qos[0].dims
+                    shp = qos[0].shape
+                    step = 1
+                for k in range(items - 1):  # cycle over all items
+                    dat = sp.kron(dat, qos[k + 1].data, format='csr')
+                    dim = [dim[0] + qos[k + 1].dims[0],
+                           dim[1] + qos[k + 1].dims[1]]
+                    shp = [dat.shape[0], dat.shape[1]]  # new shape of matrix
+    out = Qobj()
+    out.data = dat
+    out.dims = dim
+    out.shape = shp
     if qutip.settings.auto_tidyup:
-        return Qobj(out).tidyup() #returns tidy Qobj
+        return Qobj(out).tidyup()  # returns tidy Qobj
     else:
         return Qobj(out)
