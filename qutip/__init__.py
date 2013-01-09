b8cb5661de0f4fc7c4c7e4ebdfb854e5c74118b4
Robert Johansson <jrjohansson@gmail.com>
2013-01-09 14:07:30 +0900

    pep8 changes

--- qutip/__init__.py
+++ qutip/__init__.py
@@ -16,58 +16,67 @@
 # Copyright (C) 2011-2013, Paul D. Nation & Robert J. Johansson
 #
 ###########################################################################
-import os,sys,platform,multiprocessing
+import os
+import sys
+import platform
+import multiprocessing
 import qutip.settings
 import qutip._version
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Check for minimum requirements of dependencies, give the user a warning
 # if the requirements aren't fulfilled
 #
+
+
 def version2int(version_string):
-    return sum([int(d)*(100**(3-n)) for n,d in enumerate(version_string.split('.')[:3])])
-    
+    return sum([int(d) * (100 ** (3 - n))
+                for n, d in enumerate(version_string.split('.')[:3])])
+
 numpy_requirement = "1.6.0"
 try:
-    import numpy 
+    import numpy
     if version2int(numpy.__version__) < version2int(numpy_requirement):
-        print("QuTiP warning: old version of numpy detected (%s), requiring %s." %
-              (numpy.__version__, numpy_requirement))
+        print("QuTiP warning: old version of numpy detected " +
+              ("(%s), requiring %s." %
+               (numpy.__version__, numpy_requirement)))
 except:
     print("QuTiP warning: numpy not found.")
 
 scipy_requirement = "0.9.0"
 try:
-    import scipy 
+    import scipy
     if version2int(scipy.__version__) < version2int(scipy_requirement):
-        print("QuTiP warning: old version of scipy detected (%s), requiring %s." %
-              (scipy.__version__, scipy_requirement))
+        print("QuTiP warning: old version of scipy detected " +
+              ("(%s), requiring %s." %
+               (scipy.__version__, scipy_requirement)))
 except:
     print("QuTiP warning: scipy not found.")
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # check to see if running from install directory for released versions.
 #
-top_path=os.path.dirname(os.path.dirname(__file__))
+top_path = os.path.dirname(os.path.dirname(__file__))
 try:
-    setup_file=open(top_path+'/setup.py', 'r')
+    setup_file = open(top_path + '/setup.py', 'r')
 except:
     pass
 else:
-    if ('QuTiP' in setup_file.readlines()[1][3:]) and qutip._version.release==True:
-        print("You are in the installation directory. Change directories before running QuTiP.")
+    if ('QuTiP' in setup_file.readlines()[1][3:]) and qutip._version.release:
+        print("You are in the installation directory. " +
+              "Change directories before running QuTiP.")
     setup_file.close()
 
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # default configuration settings
 #
-qutip.settings.num_cpus=multiprocessing.cpu_count()
-qutip.settings.qutip_graphics="YES"
-qutip.settings.qutip_gui="NONE"
+qutip.settings.num_cpus = multiprocessing.cpu_count()
+qutip.settings.qutip_graphics = "YES"
+qutip.settings.qutip_gui = "NONE"
 
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Load user configuration if present: override defaults.
 #
 try:
@@ -76,22 +85,25 @@ try:
     with open(qutip_rc_file) as f:
         for line in f.readlines():
             if line[0] != "#":
-                var, val = line.strip().split("=")  
+                var, val = line.strip().split("=")
 
                 if var == "qutip_graphics":
-                    qutip.settings.qutip_graphics = "NO" if val == "NO" else "YES"
+                    qutip.settings.qutip_graphics = "NO" \
+                        if val == "NO" else "YES"
 
                 elif var == "qutip_gui":
                     qutip.settings.qutip_gui = val
 
                 elif var == "auto_tidyup":
-                    qutip.settings.auto_tidyup = True if val == "True" else False
+                    qutip.settings.auto_tidyup = True \
+                        if val == "True" else False
 
                 elif var == "auto_tidyup_atol":
                     qutip.settings.auto_tidyup_atol = float(val)
 
                 elif var == "auto_herm":
-                    qutip.settings.auto_herm = True if val == "True" else False
+                    qutip.settings.auto_herm = True \
+                        if val == "True" else False
 
                 elif var == "num_cpus":
                     qutip.settings.num_cpus = int(val)
@@ -100,8 +112,8 @@ except Exception as e:
     pass
 
 
-#-------------------------------------------------------------------------------
-# Load configuration from environment variables: override defaults and 
+#------------------------------------------------------------------------------
+# Load configuration from environment variables: override defaults and
 # configuration file.
 #
 
@@ -115,19 +127,19 @@ if not ('QUTIP_GUI' in os.environ):
 else:
     qutip.settings.qutip_gui = os.environ['QUTIP_GUI']
 
-#check if being run remotely
+# check if being run remotely
 if sys.platform != 'darwin' and not ('DISPLAY' in os.environ):
-    #no graphics if DISPLAY isn't set
-    os.environ['QUTIP_GRAPHICS']="NO"
-    qutip.settings.qutip_graphics="NO"
-    os.environ['QUTIP_GUI']="NONE"
-    qutip.settings.qutip_gui="NONE"
+    # no graphics if DISPLAY isn't set
+    os.environ['QUTIP_GRAPHICS'] = "NO"
+    qutip.settings.qutip_graphics = "NO"
+    os.environ['QUTIP_GUI'] = "NONE"
+    qutip.settings.qutip_gui = "NONE"
 
-#automatically set number of threads used by MKL
-os.environ['MKL_NUM_THREADS']=str(multiprocessing.cpu_count())
-os.environ['NUM_THREADS']=str(multiprocessing.cpu_count())
+# automatically set number of threads used by MKL
+os.environ['MKL_NUM_THREADS'] = str(multiprocessing.cpu_count())
+os.environ['NUM_THREADS'] = str(multiprocessing.cpu_count())
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Check that import modules are compatible with requested configuration
 #
 
@@ -135,46 +147,46 @@ os.environ['NUM_THREADS']=str(multiprocessing.cpu_count())
 try:
     import matplotlib
 except:
-    os.environ['QUTIP_GRAPHICS']="NO"
-    qutip.settings.qutip_graphics='NO'
+    os.environ['QUTIP_GRAPHICS'] = "NO"
+    qutip.settings.qutip_graphics = 'NO'
 
 # if being run locally, check for installed gui modules
-if qutip.settings.qutip_graphics=='YES':
+if qutip.settings.qutip_graphics == 'YES':
 
     if qutip.settings.qutip_gui == "NONE":
         # no preference, try PYSIDE first, the PYQT4
         try:
             import PySide
-            os.environ['QUTIP_GUI']="PYSIDE"
-            qutip.settings.qutip_gui="PYSIDE"
+            os.environ['QUTIP_GUI'] = "PYSIDE"
+            qutip.settings.qutip_gui = "PYSIDE"
         except:
             try:
                 import PyQt4
-                os.environ['QUTIP_GUI']="PYQT4"
-                qutip.settings.qutip_gui="PYQT4"
+                os.environ['QUTIP_GUI'] = "PYQT4"
+                qutip.settings.qutip_gui = "PYQT4"
             except:
-                qutip.settings.qutip_gui="NONE"
+                qutip.settings.qutip_gui = "NONE"
 
     elif qutip.settings.qutip_gui == "PYSIDE":
         # PYSIDE was requested
         try:
             import PySide
-            os.environ['QUTIP_GUI']="PYSIDE"
-            qutip.settings.qutip_gui="PYSIDE"
+            os.environ['QUTIP_GUI'] = "PYSIDE"
+            qutip.settings.qutip_gui = "PYSIDE"
         except:
-            qutip.settings.qutip_gui="NONE"
+            qutip.settings.qutip_gui = "NONE"
 
     elif qutip.settings.qutip_gui == "PYQT4":
         # PYQT4 was requested
         try:
             import PyQt4
-            os.environ['QUTIP_GUI']="PYQT4"
-            qutip.settings.qutip_gui="PYQT4"
+            os.environ['QUTIP_GUI'] = "PYQT4"
+            qutip.settings.qutip_gui = "PYQT4"
         except:
-            qutip.settings.qutip_gui="NONE"
+            qutip.settings.qutip_gui = "NONE"
 
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Load modules
 #
 
@@ -191,16 +203,18 @@ from qutip.sparse import sp_eigs
 import qutip.settings
 
 # graphics
-if qutip.settings.qutip_graphics=='YES':
+if qutip.settings.qutip_graphics == 'YES':
     from qutip.bloch import Bloch
     from qutip.graph import hinton, energy_level_diagram
     from qutip.sphereplot import *
     from qutip.orbital import *
     from qutip.tomography import *
-    #load mayavi dependent functions if available
-    try: import mayavi
-    except: pass
-    else: 
+    # load mayavi dependent functions if available
+    try:
+        import mayavi
+    except:
+        pass
+    else:
         from qutip.bloch3d import Bloch3d
 
 # library functions
@@ -208,16 +222,17 @@ from qutip.wigner import *
 from qutip.rand import *
 from qutip.simdiag import *
 from qutip.clebsch import clebsch
-from qutip.entropy import entropy_vn,entropy_linear,entropy_mutual,concurrence,entropy_conditional
+from qutip.entropy import (entropy_vn, entropy_linear, entropy_mutual,
+                           concurrence, entropy_conditional)
 from qutip.gates import *
-from qutip.metrics import fidelity,tracedist
+from qutip.metrics import fidelity, tracedist
 from qutip.partial_transpose import partial_transpose
 
 # evolution
 import qutip.odeconfig
 from qutip.odeoptions import Odeoptions
 from qutip.odedata import Odedata
-from qutip.rhs_generate import rhs_generate,rhs_clear
+from qutip.rhs_generate import rhs_generate, rhs_clear
 from qutip.mesolve import mesolve, odesolve
 from qutip.mcsolve import mcsolve
 from qutip.essolve import *
@@ -231,5 +246,5 @@ from qutip.bloch_redfield import *
 # utilities
 from qutip.fileio import *
 from qutip.demos import demos
-#import qutip.examples
+# import qutip.examples
 from qutip.about import *
