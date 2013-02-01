fe49af3d37fe37ff3f8b9b586a5d20d0805d458a
Robert Johansson <jrjohansson@gmail.com>
2013-02-01 16:36:56 +0900

    more pep8 fixes for examples

--- qutip/examples/ex_22.py
+++ qutip/examples/ex_22.py
@@ -4,34 +4,37 @@
 from qutip import *
 from pylab import *
 
+
 def run():
 
     # Configure parameters
-    N = 12          # number of cavity fock states
-    wc = 2*pi*1.0   # cavity frequency
-    wa = 2*pi*1.0   # atom frequency
-    g  = 2*pi*0.1   # coupling strength
-    kappa = 0.05    # cavity dissipation rate
-    gamma = 0.0     # atom dissipation rate
-    pump  = 0.4     # atom pump rate
+    N = 12              # number of cavity fock states
+    wc = 2 * pi * 1.0   # cavity frequency
+    wa = 2 * pi * 1.0   # atom frequency
+    g = 2 * pi * 0.1    # coupling strength
+    kappa = 0.05        # cavity dissipation rate
+    gamma = 0.0         # atom dissipation rate
+    pump = 0.4          # atom pump rate
     use_rwa = True
-    
+
     # start without any excitations
-    psi0  = tensor(basis(N,0), basis(2,0))
+    psi0 = tensor(basis(N, 0), basis(2, 0))
 
     # Hamiltonian
-    a  = tensor(destroy(N), qeye(2))
+    a = tensor(destroy(N), qeye(2))
     sm = tensor(qeye(N), destroy(2))
-    
-    if use_rwa: # use the rotating wave approxiation
-        H = wc * a.dag() * a + wa * sm.dag() * sm + g * (a.dag() * sm + a * sm.dag()) 
+
+    if use_rwa:  # use the rotating wave approxiation
+        H = wc * a.dag() * a + wa * sm.dag() * sm + \
+            g * (a.dag() * sm + a * sm.dag())
     else:
-        H = wc * a.dag() * a + wa * sm.dag() * sm + g * (a.dag() + a) * (sm + sm.dag())
-         
+        H = wc * a.dag() * a + wa * sm.dag() * sm + \
+            g * (a.dag() + a) * (sm + sm.dag())
+
     # collapse operators
     c_op_list = []
 
-    n_th_a = 0.0 # zero temperature
+    n_th_a = 0.0  # zero temperature
     rate = kappa * (1 + n_th_a)
     if rate > 0.0:
         c_op_list.append(sqrt(rate) * a)
@@ -50,25 +53,25 @@ def run():
 
     # evolve the system
     tlist = linspace(0, 200, 500)
-    output = mesolve(H, psi0, tlist, c_op_list, [])  
+    output = mesolve(H, psi0, tlist, c_op_list, [])
 
     # calculate expectation values
-    nc = expect(a.dag()  *  a, output.states) 
+    nc = expect(a.dag() * a, output.states)
     na = expect(sm.dag() * sm, output.states)
 
     #
     # plot the time-evolution of the cavity and atom occupation
     #
     figure(1)
-    plot(tlist, real(nc), 'r-',   tlist, real(na), 'b-',lw=2)
-    xlabel('Time');
-    ylabel('Occupation probability');
+    plot(tlist, real(nc), 'r-', tlist, real(na), 'b-', lw=2)
+    xlabel('Time')
+    ylabel('Occupation probability')
     legend(("Cavity occupation", "Atom occupation"))
 
     #
     # plot the final photon distribution in the cavity
     #
-    rho_final  = output.states[-1]
+    rho_final = output.states[-1]
     rho_cavity = ptrace(rho_final, 0)
 
     figure(2)
@@ -82,12 +85,11 @@ def run():
     #
     xvec = linspace(-5, 5, 100)
     W = wigner(rho_cavity, xvec, xvec)
-    X,Y = meshgrid(xvec, xvec)
+    X, Y = meshgrid(xvec, xvec)
     figure(3)
     contourf(X, Y, W, 100)
     colorbar()
     show()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     run()
-
--- qutip/examples/ex_24.py
+++ qutip/examples/ex_24.py
@@ -4,14 +4,15 @@
 from qutip import *
 from pylab import *
 
+
 def integrate(N, h, Jx, Jy, Jz, psi0, tlist, gamma, solver):
 
     #
     # Hamiltonian
     #
-    # H = - 0.5 sum_n^N h_n sigma_z(n)  
-    #     - 0.5 sum_n^(N-1) [ Jx_n sigma_x(n) sigma_x(n+1) + 
-    #                         Jy_n sigma_y(n) sigma_y(n+1) + 
+    # H = - 0.5 sum_n^N h_n sigma_z(n)
+    #     - 0.5 sum_n^(N-1) [ Jx_n sigma_x(n) sigma_x(n+1) +
+    #                         Jy_n sigma_y(n) sigma_y(n+1) +
     #                         Jz_n sigma_z(n) sigma_z(n+1)]
     #
     si = qeye(2)
@@ -32,19 +33,19 @@ def integrate(N, h, Jx, Jy, Jz, psi0, tlist, gamma, solver):
         sy_list.append(tensor(op_list))
         op_list[n] = sz
         sz_list.append(tensor(op_list))
-        
+
     # construct the hamiltonian
-    H = 0    
-    
+    H = 0
+
     # energy splitting terms
     for n in range(N):
         H += - 0.5 * h[n] * sz_list[n]
-        
+
     # interaction terms
-    for n in range(N-1):
-        H += - 0.5 * Jx[n] * sx_list[n] * sx_list[n+1]
-        H += - 0.5 * Jy[n] * sy_list[n] * sy_list[n+1]
-        H += - 0.5 * Jz[n] * sz_list[n] * sz_list[n+1] 
+    for n in range(N - 1):
+        H += - 0.5 * Jx[n] * sx_list[n] * sx_list[n + 1]
+        H += - 0.5 * Jy[n] * sy_list[n] * sy_list[n + 1]
+        H += - 0.5 * Jz[n] * sz_list[n] * sz_list[n + 1]
 
     # collapse operators
     c_op_list = []
@@ -53,7 +54,7 @@ def integrate(N, h, Jx, Jy, Jz, psi0, tlist, gamma, solver):
     for n in range(N):
         if gamma[n] > 0.0:
             c_op_list.append(sqrt(gamma[n]) * sz_list[n])
-            
+
     # evolve and calculate expectation values
     if solver == "me":
         output = mesolve(H, psi0, tlist, c_op_list, sz_list)
@@ -62,30 +63,27 @@ def integrate(N, h, Jx, Jy, Jz, psi0, tlist, gamma, solver):
 
     return output.expect
 
+
 def run():
-   
-    #
-    # set up the calculation
-    #
-    
+
     solver = "me"   # select solver "me" or "mc"
     N = 4           # number of spins
-    
+
     # array of spin energy splittings and coupling strengths. here we use
     # uniform parameters, but in general we don't have too
-    h  = 1.0 * 2 * pi * ones(N) 
+    h = 1.0 * 2 * pi * ones(N)
     Jz = 0.1 * 2 * pi * ones(N)
     Jx = 0.1 * 2 * pi * ones(N)
     Jy = 0.1 * 2 * pi * ones(N)
-    
+
     # dephasing rate
     gamma = 0.01 * ones(N)
-    
+
     # intial state, first spin in state |1>, the rest in state |0>
     psi_list = []
-    psi_list.append(basis(2,1))
-    for n in range(N-1):
-        psi_list.append(basis(2,0))
+    psi_list.append(basis(2, 1))
+    for n in range(N - 1):
+        psi_list.append(basis(2, 0))
     psi0 = tensor(psi_list)
 
     tlist = linspace(0, 50, 300)
@@ -94,14 +92,14 @@ def run():
 
     rc('font', family='Bitstream Vera Sans')
     for n in range(N):
-        plot(tlist, real(sz_expt[n]), label=r'$\langle\sigma_z($'+str(n)+r'$)\rangle$',lw=2)
-    xlabel(r'Time [ns]',fontsize=14)
-    ylabel(r'$\langle\sigma_{z}\rangle$',fontsize=14)
+        plot(tlist, real(sz_expt[n]), label=r'$\langle\sigma_z($' +
+             str(n) + r'$)\rangle$', lw=2)
+    xlabel(r'Time [ns]', fontsize=14)
+    ylabel(r'$\langle\sigma_{z}\rangle$', fontsize=14)
     title(r'Dynamics of a Heisenberg spin chain')
-    legend(loc = "lower right")
+    legend(loc="lower right")
 
     show()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     run()
-
