1c500fb183b2c6ba03c647fc64f3c9fb2db9a975
Robert Johansson <jrjohansson@gmail.com>
2013-02-02 12:27:56 +0900

    pep8 and code cleanup

--- qutip/examples/ex_42.py
+++ qutip/examples/ex_42.py
@@ -4,6 +4,7 @@
 from qutip import *
 from pylab import *
 
+
 def run():
 
     def hamiltonian_t(t, args):
@@ -12,28 +13,28 @@ def run():
         H1 = args[1]
         return H0 + t * H1
 
-    # 
+    #
     # set up the parameters
     #
-    delta = 0.5 * 2 * pi   # qubit sigma_x coefficient
-    eps0  = 0.0 * 2 * pi   # qubit sigma_z coefficient
-    A     = 2.0 * 2 * pi   # sweep rate
-    gamma1 = 0.0           # relaxation rate
-    n_th = 0.0             # average number of thermal photons
-    psi0 = basis(2,0)      # initial state
+    delta = 0.5 * 2 * pi  # qubit sigma_x coefficient
+    eps0 = 0.0 * 2 * pi   # qubit sigma_z coefficient
+    A = 2.0 * 2 * pi      # sweep rate
+    gamma1 = 0.0          # relaxation rate
+    n_th = 0.0            # average number of thermal photons
+    psi0 = basis(2, 0)    # initial state
 
-    # 
+    #
     # Hamiltonian
     #
     sx = sigmax()
     sz = sigmaz()
     sm = destroy(2)
 
-    H0 = - delta/2.0 * sx - eps0/2.0 * sz
-    H1 = - A/2.0 * sz   
+    H0 = - delta / 2.0 * sx - eps0 / 2.0 * sz
+    H1 = - A / 2.0 * sz
     args = (H0, H1)
 
-    # 
+    #
     # collapse operators, only active if gamma1 > 0
     #
     c_ops = []
@@ -44,25 +45,25 @@ def run():
 
     rate = gamma1 * n_th
     if rate > 0.0:
-        c_ops.append(sqrt(rate) * sm.dag()) # excitation
+        c_ops.append(sqrt(rate) * sm.dag())  # excitation
 
-    # 
+    #
     # evolve and calculate expectation values
     #
     tlist = linspace(-10.0, 10.0, 1500)
-    output = mesolve(hamiltonian_t, psi0, tlist, c_ops, [sm.dag() * sm], args)  
- 
-    # 
+    output = mesolve(hamiltonian_t, psi0, tlist, c_ops, [sm.dag() * sm], args)
+
+    #
     # Plot the results
-    #    
-    plot(tlist, real(output.expect[0]), 'b', tlist, real(1-output.expect[0]), 'r')
-    plot(tlist, 1 - exp( - pi * delta **2 / (2 * A)) * ones(shape(tlist)), 'k')
+    #
+    plot(tlist, real(output.expect[0]), 'b',
+         tlist, real(1 - output.expect[0]), 'r')
+    plot(tlist, 1 - exp(- pi * delta ** 2 / (2 * A)) * ones(shape(tlist)), 'k')
     xlabel('Time')
     ylabel('Occupation probability')
     title('Excitation probabilty the two-level system')
     legend(("Excited state", "Ground state", "Landau-Zener formula"), loc=0)
     show()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     run()
-
