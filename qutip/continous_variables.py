fixed factor 2 bug in covariance_matrix

qutip/continous_variables.py
@@ -70,7 +70,7 @@ def covariance_matrix(basis, rho):
 
     .. math::
 
-        V_{mn} = \\langle a_m a_n + a_n a_m \\rangle - \\langle a_m \\rangle \\langle a_n\\rangle
+        V_{mn} = \\langle a_m a_n + a_n a_m \\rangle - 2\\langle a_m \\rangle \\langle a_n\\rangle
 
     Parameters
     ----------
@@ -89,7 +89,7 @@ def covariance_matrix(basis, rho):
 
     """
     return np.array([[expect(op1 * op2 + op2 * op1, rho) -
-                      expect(op1, rho) * expect(op2, rho)
+                      2 * expect(op1, rho) * expect(op2, rho)
                       for op1 in basis] for op2 in basis])
 
 
@@ -165,7 +165,7 @@ def wigner_covariance_matrix(a1=None, a2=None, R=None, rho=None):
     Calculate the wigner covariance matrix 
     :math:`V_{ij} = \\frac{1}{2}(R_{ij} + R_{ji})`, given
     the quadrature correlation matrix 
-    :math:`R_{ij} = \\langle R_{i} R_{j}\\rangle`, where 
+    :math:`R_{ij} = \\langle R_{i} R_{j}\\rangle - \\langle R_{i}\\rangle \\langle R_{j}\\rangle`, where 
     :math:`R = (q_1, p_1, q_2, p_2)^T` is the vector with quadrature operators
     for the two modes.
 