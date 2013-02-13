64f8ed85a6463f298e479d5f548038eee8a33b80
Paul Nation <nonhermitian@gmail.com>
2013-02-13 21:28:22 +0900

    UPDATED: Added new benchmarks

--- benchmark/matlab_benchmarks.m
+++ benchmark/matlab_benchmarks.m
@@ -1,9 +1,10 @@
 function []=matlab_benchmarks()
 %Create array to hold test results
-test_results=zeros(1,2);
+test_results=zeros(1,4);
 
 
-%perform basic operator algebra to construct JC-model
+%test #1
+%perform basic operator algebra to construct JC-Hamiltonian
 Nc=20;
 wc = 1.0 * 2 * pi;   
 wa = 1.0 * 2 * pi;
@@ -17,6 +18,7 @@ H=wc * a' * a + wa * sm' * sm + g * (a' + a) * (sm + sm');
 time=toc;
 test_results(1,1)=time;
 
+%test #2
 %matrix exponentiation to construct squeezed state and coherent state
 N=25;
 alpha=2+2i;
@@ -30,4 +32,61 @@ coh_state=D_oper*grnd;
 sqz_state=S_oper*grnd;
 time=toc;
 test_results(1,2)=time;
+
+%test #3
+%cavity+qubit steady state
+N=5;kappa = 2; gamma = 0.2; g = 1;
+wc = 0; w0 = 0; wl = 0; E = 0.5;
+tic;
+ida = identity(N); idatom = identity(2); 
+a  = tensor(destroy(N),idatom);
+sm = tensor(ida,sigmam);
+H = (w0-wl)*sm'*sm + (wc-wl)*a'*a + i*g*(a'*sm - sm'*a) + E*(a'+a);
+C1    = sqrt(2*kappa)*a;% Collapse operators
+C2    = sqrt(gamma)*sm;
+C1dC1 = C1'*C1;
+C2dC2 = C2'*C2;
+LH = -i * (spre(H) - spost(H)); % Calculate the Liouvillian
+L1 = spre(C1)*spost(C1')-0.5*spre(C1dC1)-0.5*spost(C1dC1);
+L2 = spre(C2)*spost(C2')-0.5*spre(C2dC2)-0.5*spost(C2dC2);
+L  = LH+L1+L2;
+rhoss = steady(L);% Find steady state
+time=toc;
+test_results(1,3)=time;
+
+%test #4
+%cavity+qubit master equation
+kappa = 2; gamma = 0.2; g = 1;
+wc = 0; w0 = 0; wl = 0; E = 0.5;
+N = 10;
+tlist = linspace(0,10,200);
+tic;
+ida = identity(N); idatom = identity(2); 
+a  = tensor(destroy(N),idatom);
+sm = tensor(ida,sigmam);
+H = (w0-wl)*sm'*sm + (wc-wl)*a'*a + i*g*(a'*sm - sm'*a) + E*(a'+a);
+C1  = sqrt(2*kappa)*a;
+C2  = sqrt(gamma)*sm;
+C1dC1 = C1'*C1;
+C2dC2 = C2'*C2;
+LH = -i * (spre(H) - spost(H)); 
+L1 = spre(C1)*spost(C1')-0.5*spre(C1dC1)-0.5*spost(C1dC1);
+L2 = spre(C2)*spost(C2')-0.5*spre(C2dC2)-0.5*spost(C2dC2);
+L = LH+L1+L2;
+psi0 = tensor(basis(N,1),basis(2,2));
+rho0 = psi0 * psi0';
+ode2file('file1.dat',L,rho0,tlist);
+odesolve('file1.dat','file2.dat');
+fid = fopen('file2.dat','rb');
+rho = qoread(fid,dims(rho0),size(tlist));
+time=toc;
+test_results(1,4)=time;
+
+
+%test #5
+%cavity+qubit monte carlo
+
+
+
+
 xlswrite('matlab_benchmarks',test_results);
--- benchmark/qutip_benchmarks.py
+++ benchmark/qutip_benchmarks.py
@@ -17,12 +17,14 @@ matlab_times = genfromtxt('matlab_benchmarks.csv', delimiter=',')
 #setup list for python times
 python_times=[]
 test_names=[]
+
 #---------------------
 #Run Python Benchmarks
 #---------------------
-num_tests=2
+num_tests=4
 
 #Construct Jaynes-Cumming Hamiltonian with Nc=20, Na=2.
+test_names+=['Build JC Hamiltonian']
 wc = 1.0 * 2 * pi  
 wa = 1.0 * 2 * pi
 g  = 0.05 * 2 * pi
@@ -33,9 +35,9 @@ sm=tensor(qeye(Nc),sigmam())
 H=wc*a.dag()*a+wa*sm.dag()*sm+g*(a.dag()+a)*(sm.dag()+sm)
 toc=time()
 python_times+=[toc-tic]
-test_names+=['JC-model']
 
 #Construct Jaynes-Cumming Hamiltonian with Nc=20, Na=2.
+test_names+=['Operator expm']
 N=25
 alpha=2+2j
 sp=1.25j
@@ -44,11 +46,52 @@ coherent(N,alpha)
 squeez(N,sp)
 toc=time()
 python_times+=[toc-tic]
-test_names+=['Operator expm']
 
-barh(arange(num_tests),matlab_times/array(python_times),align='center',height=0.5)
+#cavity+qubit steady state
+test_names+=['cavity+qubit steady state']
+kappa=2;gamma=0.2;g=1;wc=0
+w0=0;N=5;E=0.5;wl=0
+tic=time()
+ida=qeye(N)
+idatom=qeye(2)
+a=tensor(destroy(N),idatom)
+sm=tensor(ida,sigmam())
+H=(w0-wl)*sm.dag()*sm+(wc-wl)*a.dag()*a+1j*g*(a.dag()*sm-sm.dag()*a)+E*(a.dag()+a)
+C1=sqrt(2*kappa)*a
+C2=sqrt(gamma)*sm
+C1dC1=C1.dag() * C1
+C2dC2=C2.dag() * C2
+L = liouvillian(H, [C1, C2])
+rhoss=steady(L)
+toc=time()
+python_times+=[toc-tic]
+
+#cavity+qubit master equation
+test_names+=['cavity+qubit master equation']
+kappa = 2; gamma = 0.2; g = 1;
+wc = 0; w0 = 0; wl = 0; E = 0.5;
+N = 10;
+tlist = linspace(0,10,200);
+tic=time()
+ida    = qeye(N)
+idatom = qeye(2)
+a  = tensor(destroy(N),idatom)
+sm = tensor(ida,sigmam())
+H = (w0-wl)*sm.dag()*sm + (wc-wl)*a.dag()*a + 1j*g*(a.dag()*sm - sm.dag()*a) + E*(a.dag()+a)
+C1=sqrt(2*kappa)*a
+C2=sqrt(gamma)*sm
+C1dC1=C1.dag()*C1
+C2dC2=C2.dag()*C2
+psi0 = tensor(basis(N,0),basis(2,1))
+rho0 = psi0.dag() * psi0
+mesolve(H, psi0, tlist, [C1, C2], [C1dC1, C2dC2, a])
+toc=time()
+python_times+=[toc-tic]
+
+#Generate figure
+barh(0.5*arange(num_tests),matlab_times/array(python_times),align='center',height=0.5,color='#C098EA')
 for ii in range(num_tests):
-    text(0.5,ii,test_names[ii],color='w',fontsize=16,verticalalignment='center')
+    text(0.5,0.5*ii,test_names[ii],color='k',fontsize=16,verticalalignment='center')
 frame = gca()
 for y in frame.axes.get_yticklabels():
     y.set_fontsize(0.0)
