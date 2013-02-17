8a350a7691124fd0f2c52f211329547137054c56
Paul Nation <nonhermitian@gmail.com>
2013-02-17 17:27:05 +0900

    FIXED: import errors

--- qutip/fortran/__init__.py
+++ qutip/fortran/__init__.py
@@ -5,7 +5,4 @@
 
 from mcsolve_f90 import mcsolve_f90
 #from mcsolve_f90_par import mcsolve_f90_serial, mcsolve_f90_par
-from tester import alldemos, rundemo
-from testing import test
-import compare
-import examples
+
