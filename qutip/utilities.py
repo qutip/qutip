c67410c6b6d13a2867c2afbdcfeccde2b2c2f18a
Robert Johansson <jrjohansson@gmail.com>
2013-02-18 16:54:02 +0900

    fixed missing import

--- qutip/utilities.py
+++ qutip/utilities.py
@@ -22,6 +22,9 @@ This module contains utility functions that are commonly needed in other
 qutip modules.
 """
 
+import numpy
+
+
 def n_thermal(w, w_th):
     """
     Return the number of photons in thermal equilibrium for an harmonic 
@@ -31,4 +34,4 @@ def n_thermal(w, w_th):
     if (w_th > 0) and exp(w / w_th) != 1.0:
         return 1.0 / (exp(w / w_th) - 1.0)
     else:
-        return 0.0 * np.ones(np.shape(w))
+        return 0.0 * numpy.ones(np.shape(w))
