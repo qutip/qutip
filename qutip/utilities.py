be9f9e4a3daf3e89d81e5dcdf75d358165fc371b
Paul Nation <nonhermitian@gmail.com>
2013-02-21 11:16:00 +0900

    ADDED: linspace function with added elements
    Creates a linspace sequences with additional elements inserted.  Good
    if you want to make sure some specific elements are in your set.

--- qutip/utilities.py
+++ qutip/utilities.py
@@ -22,7 +22,7 @@ This module contains utility functions that are commonly needed in other
 qutip modules.
 """
 
-import numpy
+import numpy as np
 
 
 def n_thermal(w, w_th):
@@ -52,11 +52,44 @@ def n_thermal(w, w_th):
 
     """
 
-    if type(w) is numpy.ndarray:
-        return 1.0 / (numpy.exp(w / w_th) - 1.0)
+    if type(w) is np.ndarray:
+        return 1.0 / (np.exp(w / w_th) - 1.0)
 
     else:
-        if (w_th > 0) and numpy.exp(w / w_th) != 1.0:
-            return 1.0 / (numpy.exp(w / w_th) - 1.0)
+        if (w_th > 0) and np.exp(w / w_th) != 1.0:
+            return 1.0 / (np.exp(w / w_th) - 1.0)
         else:
             return 0.0
+
+def linspace_with(start,stop,num=50,elems=[]):
+    """
+    Return an array of numbers sampled over specified interval
+    with additional elements added.
+    
+    Returns `num` spaced array with elements from `elems` inserted
+    if not already included in set.
+    
+    Returned sample array is not evenly spaced if addtional elements
+    are added.
+    
+    Parameters
+    ----------
+    start : int
+        The starting value of the sequence.
+    stop : int
+        The stoping values of the sequence.
+    num : int, optional
+        Number of samples to generate.
+    elems : list/ndarray, optional
+        Requested elements to include in array
+    
+    Returns
+    -------
+    samples : ndadrray
+        Original equally spaced sample array with additional
+        elements added.
+    """
+    elems=np.array(elems)
+    lspace=np.linspace(start,stop,num)
+    return np.union1d(lspace,elems)
+    
