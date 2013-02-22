df1a6c49c981498941df98aa2de218d16355a767
Paul Nation <nonhermitian@gmail.com>
2013-02-22 13:49:09 +0900

    FIXED: hardware_info now works for non throttled systems

--- qutip/hardware_info.py
+++ qutip/hardware_info.py
@@ -27,6 +27,8 @@ def _mac_hardware_info():
 	results.update({'cpus': int(info['physicalcpu'])})
 	results.update({'cpu_freq': int(info['cpufrequency'])/(1000.**3)})
 	results.update({'memsize':int(info['memsize'])/(1024**2)})
+	#add OS information
+	results.update({'os':'Mac OSX'})
 	return results
 
 
@@ -40,19 +42,26 @@ def _linux_hardware_info():
 	cores_per_socket=int(cpu_info['Core(s) per socket'])
 	results.update({'cpus':sockets*cores_per_socket})
 	#get cpu frequency directly (bypasses freq scaling)
-	cpu_freq=os.popen('cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq').readlines()[0]
-	cpu_freq=float(cpu_freq.strip('\n'))
-	results.update({'cpu_freq':cpu_freq/(1000.**2)})
+	try:
+		cpu_freq=os.popen('cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq').readlines()[0]
+	except:
+		cpu_freq=float(cpu_info['CPU MHz'])/1000.
+		results.update({'cpu_freq':cpu_freq/(1000.**2)})
+	else:		
+		cpu_freq=float(cpu_freq.strip('\n'))
+		results.update({'cpu_freq':cpu_freq/(1000.**2)})
 	#get total amount of memory
 	mem_info = dict()
 	for l in [l.split(':') for l in os.popen('cat /proc/meminfo').readlines()]:
 		mem_info[l[0]] = l[1].strip('.\n ').strip('kB')
 	results.update({'memsize':int(mem_info['MemTotal'])/1024})
+	#add OS information
+	results.update({'os':'Linux'})
 	return results
 
 
 def _win_hardware_info():
-    return {}
+    return {'os': 'Windows'}
 
 def hardware_info():
     """
