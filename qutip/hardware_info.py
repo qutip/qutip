c2c07ab974b8bd71578229ede19820fa00b109d9
Robert Johansson <jrjohansson@gmail.com>
2013-02-25 11:40:55 +0900

    pep8ified code, simplified code: use open(..).readlines instead of pipe open

--- qutip/hardware_info.py
+++ qutip/hardware_info.py
@@ -19,77 +19,80 @@
 import os
 import sys
 
+
 def _mac_hardware_info():
-    info=dict()
+    info = dict()
     results = dict()
     for l in [l.split(':') for l in os.popen('sysctl hw').readlines()[1:20]]:
-        info[l[0].strip(' "').replace(' ', '_').lower().strip('hw.')] = l[1].strip('.\n ')
+        info[l[0].strip(' "').replace(' ', '_').lower().strip('hw.')] = \
+            l[1].strip('.\n ')
     results.update({'cpus': int(info['physicalcpu'])})
-    results.update({'cpu_freq': int(info['cpufrequency'])/(1000.**3)})
-    results.update({'memsize':int(info['memsize'])/(1024**2)})
-    #add OS information
-    results.update({'os':'Mac OSX'})
+    results.update({'cpu_freq': int(info['cpufrequency']) / (1000. ** 3)})
+    results.update({'memsize': int(info['memsize']) / (1024 ** 2)})
+    # add OS information
+    results.update({'os': 'Mac OSX'})
     return results
 
 
 def _linux_hardware_info():
-	results={}
-	#get cpu number
-	cpu_info = dict()
-	for l in [l.split(':') for l in os.popen('lscpu').readlines()]:
-		cpu_info[l[0]] = l[1].strip('.\n ').strip('kB')
-	sockets=int(cpu_info['Socket(s)'])
-	cores_per_socket=int(cpu_info['Core(s) per socket'])
-	results.update({'cpus':sockets*cores_per_socket})
-	#get cpu frequency directly (bypasses freq scaling)
-	try:
-		open('/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq')
-	except:
-		cpu_freq=float(cpu_info['CPU MHz'])/1000.
-		results.update({'cpu_freq':cpu_freq})
-	else:		
-		cpu_freq=os.popen('cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq').readlines()[0]
-		cpu_freq=float(cpu_freq.strip('\n'))
-		results.update({'cpu_freq':cpu_freq/(1000.**2)})
-	#get total amount of memory
-	mem_info = dict()
-	for l in [l.split(':') for l in os.popen('cat /proc/meminfo').readlines()]:
-		mem_info[l[0]] = l[1].strip('.\n ').strip('kB')
-	results.update({'memsize':int(mem_info['MemTotal'])/1024})
-	#add OS information
-	results.update({'os':'Linux'})
-	return results
+    results = {}
+    # get cpu number
+    cpu_info = dict()
+    for l in [l.split(':') for l in os.popen('lscpu').readlines()]:
+        cpu_info[l[0]] = l[1].strip('.\n ').strip('kB')
+    sockets = int(cpu_info['Socket(s)'])
+    cores_per_socket = int(cpu_info['Core(s) per socket'])
+    results.update({'cpus': sockets * cores_per_socket})
+    # get cpu frequency directly (bypasses freq scaling)
+    try:
+        file = "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
+        cpu_freq = open(file).readlines()[0]
+        cpu_freq = float(cpu_freq.strip('\n'))
+        results.update({'cpu_freq': cpu_freq / (1000. ** 2)})
+    except:
+        cpu_freq = float(cpu_info['CPU MHz']) / 1000.
+        results.update({'cpu_freq': cpu_freq})
+
+    # get total amount of memory
+    mem_info = dict()
+    for l in [l.split(':') for l in open("/proc/meminfo").readlines()]:
+        mem_info[l[0]] = l[1].strip('.\n ').strip('kB')
+    results.update({'memsize': int(mem_info['MemTotal']) / 1024})
+    # add OS information
+    results.update({'os': 'Linux'})
+    return results
 
 
 def _win_hardware_info():
     return {'os': 'Windows'}
 
+
 def hardware_info():
     """
     Returns basic hardware information about the computer.
-    
-    Gives actual number of CPU's in the machine, even
-    when hyperthreading is turned on.
-    
+
+    Gives actual number of CPU's in the machine, even when hyperthreading is
+    turned on.
+
     Returns
     -------
     info : dict
         Dictionary containing cpu and memory informaton.
-    
+
     """
     try:
         if sys.platform == 'darwin':
-            out=_mac_hardware_info()
+            out = _mac_hardware_info()
         elif sys.platform == 'win32':
-            out=_win_hardware_info()
-        elif sys.platform in ['linux','linux2']:
-            out=_linux_hardware_info()
+            out = _win_hardware_info()
+        elif sys.platform in ['linux', 'linux2']:
+            out = _linux_hardware_info()
         else:
-            out={}
+            out = {}
     except:
         return {}
     else:
         return out
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print(hardware_info())
