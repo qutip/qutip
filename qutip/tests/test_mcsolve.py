daafafe82168e26bd4a3f6048393daffcaab53cd
Robert Johansson <jrjohansson@gmail.com>
2013-02-25 13:16:53 +0900

    pep8ified mcsolve tests

--- qutip/tests/test_mcsolve.py
+++ qutip/tests/test_mcsolve.py
@@ -96,8 +96,9 @@ def test_MCNoCollStrExpt():
     kappa = 0.2  # coupling to oscillator
     c_op_list = []
     tlist = linspace(0, 10, 100)
-    mcdata = mcsolve(H, psi0, tlist, c_op_list, [a.dag(
-    ) * a], args={'c': 0.0}, options=Odeoptions(gui=False))
+    mcdata = mcsolve(H, psi0, tlist, c_op_list,
+                     [a.dag() * a], args={'c': 0.0},
+                     options=Odeoptions(gui=False))
     expt = mcdata.expect[0]
     actual_answer = 9.0 * ones(len(tlist))
     diff = mean(abs(actual_answer - expt) / actual_answer)
@@ -290,64 +291,69 @@ def test_TDStr():
 def test_mc_dtypes1():
     "Monte-carlo: check for correct dtypes (mc_avg=True)"
     # set system parameters
-    kappa=2.0 #mirror coupling
-    gamma=0.2 #spontaneous emission rate
-    g=1 #atom/cavity coupling strength
-    wc=0 #cavity frequency
-    w0=0 #atom frequency
-    wl=0 #driving frequency
-    E=0.5 #driving amplitude
-    N=5 #number of cavity energy levels (0->3 Fock states)
-    tlist=linspace(0,10,5) #times for expectation values
+    kappa = 2.0  # mirror coupling
+    gamma = 0.2  # spontaneous emission rate
+    g = 1  # atom/cavity coupling strength
+    wc = 0  # cavity frequency
+    w0 = 0  # atom frequency
+    wl = 0  # driving frequency
+    E = 0.5  # driving amplitude
+    N = 5  # number of cavity energy levels (0->3 Fock states)
+    tlist = linspace(0, 10, 5)  # times for expectation values
     # construct Hamiltonian
-    ida=qeye(N)
-    idatom=qeye(2)
-    a=tensor(destroy(N),idatom)
-    sm=tensor(ida,sigmam())
-    H=(w0-wl)*sm.dag()*sm+(wc-wl)*a.dag()*a+1j*g*(a.dag()*sm-sm.dag()*a)+E*(a.dag()+a)
-    #collapse operators
-    C1=sqrt(2*kappa)*a
-    C2=sqrt(gamma)*sm
-    C1dC1=C1.dag()*C1
-    C2dC2=C2.dag()*C2
-    #intial state
-    psi0=tensor(basis(N,0),basis(2,1))
-    opts=Odeoptions(gui=False,mc_avg=True)
-    data=mcsolve(H,psi0,tlist,[C1,C2],[C1dC1,C2dC2,a],ntraj=5,options=opts)
-    assert_equal(isinstance(data.expect[0][0],float), True)
-    assert_equal(isinstance(data.expect[1][0],float), True)
-    assert_equal(isinstance(data.expect[2][0],complex), True)
+    ida = qeye(N)
+    idatom = qeye(2)
+    a = tensor(destroy(N), idatom)
+    sm = tensor(ida, sigmam())
+    H = (w0 - wl) * sm.dag() * sm + (wc - wl) * a.dag() * a + \
+        1j * g * (a.dag() * sm - sm.dag() * a) + E * (a.dag() + a)
+    # collapse operators
+    C1 = sqrt(2 * kappa) * a
+    C2 = sqrt(gamma) * sm
+    C1dC1 = C1.dag() * C1
+    C2dC2 = C2.dag() * C2
+    # intial state
+    psi0 = tensor(basis(N, 0), basis(2, 1))
+    opts = Odeoptions(gui=False, mc_avg=True)
+    data = mcsolve(
+        H, psi0, tlist, [C1, C2], [C1dC1, C2dC2, a], ntraj=5, options=opts)
+    assert_equal(isinstance(data.expect[0][0], float), True)
+    assert_equal(isinstance(data.expect[1][0], float), True)
+    assert_equal(isinstance(data.expect[2][0], complex), True)
+
 
 def test_mc_dtypes2():
     "Monte-carlo: check for correct dtypes (mc_avg=False)"
     # set system parameters
-    kappa=2.0 #mirror coupling
-    gamma=0.2 #spontaneous emission rate
-    g=1 #atom/cavity coupling strength
-    wc=0 #cavity frequency
-    w0=0 #atom frequency
-    wl=0 #driving frequency
-    E=0.5 #driving amplitude
-    N=5 #number of cavity energy levels (0->3 Fock states)
-    tlist=linspace(0,10,5) #times for expectation values
+    kappa = 2.0  # mirror coupling
+    gamma = 0.2  # spontaneous emission rate
+    g = 1  # atom/cavity coupling strength
+    wc = 0  # cavity frequency
+    w0 = 0  # atom frequency
+    wl = 0  # driving frequency
+    E = 0.5  # driving amplitude
+    N = 5  # number of cavity energy levels (0->3 Fock states)
+    tlist = linspace(0, 10, 5)  # times for expectation values
     # construct Hamiltonian
-    ida=qeye(N)
-    idatom=qeye(2)
-    a=tensor(destroy(N),idatom)
-    sm=tensor(ida,sigmam())
-    H=(w0-wl)*sm.dag()*sm+(wc-wl)*a.dag()*a+1j*g*(a.dag()*sm-sm.dag()*a)+E*(a.dag()+a)
-    #collapse operators
-    C1=sqrt(2*kappa)*a
-    C2=sqrt(gamma)*sm
-    C1dC1=C1.dag()*C1
-    C2dC2=C2.dag()*C2
-    #intial state
-    psi0=tensor(basis(N,0),basis(2,1))
-    opts=Odeoptions(gui=False,mc_avg=False)
-    data=mcsolve(H,psi0,tlist,[C1,C2],[C1dC1,C2dC2,a],ntraj=5,options=opts)
-    assert_equal(isinstance(data.expect[0][0][0],float), True)
-    assert_equal(isinstance(data.expect[0][1][0],float), True)
-    assert_equal(isinstance(data.expect[0][2][0],complex), True)
+    ida = qeye(N)
+    idatom = qeye(2)
+    a = tensor(destroy(N), idatom)
+    sm = tensor(ida, sigmam())
+    H = (w0 - wl) * sm.dag() * sm + (wc - wl) * a.dag() * a + \
+        1j * g * (a.dag() * sm - sm.dag() * a) + E * (a.dag() + a)
+    # collapse operators
+    C1 = sqrt(2 * kappa) * a
+    C2 = sqrt(gamma) * sm
+    C1dC1 = C1.dag() * C1
+    C2dC2 = C2.dag() * C2
+    # intial state
+    psi0 = tensor(basis(N, 0), basis(2, 1))
+    opts = Odeoptions(gui=False, mc_avg=False)
+    data = mcsolve(
+        H, psi0, tlist, [C1, C2], [C1dC1, C2dC2, a], ntraj=5, options=opts)
+    assert_equal(isinstance(data.expect[0][0][0], float), True)
+    assert_equal(isinstance(data.expect[0][1][0], float), True)
+    assert_equal(isinstance(data.expect[0][2][0], complex), True)
 
 if __name__ == "__main__":
     run_module_suite()
--- qutip/tests/test_mcsolve_f90.py
+++ qutip/tests/test_mcsolve_f90.py
@@ -48,6 +48,7 @@ def const_H1_coeff(t, args):
 # average error for failure
 mc_error = 5e-2  # 5% for ntraj=500
 
+
 @unittest.skipIf(fortran_found == 0, 'fortran files not found')
 def test_MCNoCollExpt():
     "mcsolve_f90: Constant H with no collapse ops (expect)"
@@ -60,11 +61,12 @@ def test_MCNoCollExpt():
     c_op_list = []
     tlist = linspace(0, 10, 100)
     mcdata = mcsolve_f90(H, psi0, tlist, c_op_list,
-                     [a.dag() * a], options=Odeoptions(gui=False))
+                         [a.dag() * a], options=Odeoptions(gui=False))
     expt = mcdata.expect[0]
     actual_answer = 9.0 * ones(len(tlist))
     diff = mean(abs(actual_answer - expt) / actual_answer)
     assert_equal(diff < error, True)
+
 
 @unittest.skipIf(fortran_found == 0, 'fortran files not found')
 def test_MCNoCollStates():
@@ -97,7 +99,7 @@ def test_MCSimpleConst():
     c_op_list = [sqrt(kappa) * a]
     tlist = linspace(0, 10, 100)
     mcdata = mcsolve_f90(H, psi0, tlist, c_op_list,
-                     [a.dag() * a], options=Odeoptions(gui=False))
+                         [a.dag() * a], options=Odeoptions(gui=False))
     expt = mcdata.expect[0]
     actual_answer = 9.0 * exp(-kappa * tlist)
     avg_diff = mean(abs(actual_answer - expt) / actual_answer)
@@ -115,7 +117,7 @@ def test_MCSimpleSingleCollapse():
     c_op_list = [sqrt(kappa) * a]
     tlist = linspace(0, 10, 100)
     mcdata = mcsolve_f90(H, psi0, tlist, c_op_list,
-                     [a.dag() * a], options=Odeoptions(gui=False))
+                         [a.dag() * a], options=Odeoptions(gui=False))
     expt = mcdata.expect[0]
     actual_answer = 9.0 * exp(-kappa * tlist)
     avg_diff = mean(abs(actual_answer - expt) / actual_answer)
@@ -132,8 +134,8 @@ def test_MCSimpleSingleExpect():
     kappa = 0.2  # coupling to oscillator
     c_op_list = [sqrt(kappa) * a]
     tlist = linspace(0, 10, 100)
-    mcdata = mcsolve_f90(
-        H, psi0, tlist, c_op_list, [a.dag() * a], options=Odeoptions(gui=False))
+    mcdata = mcsolve_f90(H, psi0, tlist, c_op_list,
+                         [a.dag() * a], options=Odeoptions(gui=False))
     expt = mcdata.expect[0]
     actual_answer = 9.0 * exp(-kappa * tlist)
     avg_diff = mean(abs(actual_answer - expt) / actual_answer)
@@ -144,67 +146,70 @@ def test_MCSimpleSingleExpect():
 def test_mcf90_dtypes1():
     "mcsolve_f90: check for correct dtypes (mc_avg=True)"
     # set system parameters
-    kappa=2.0 #mirror coupling
-    gamma=0.2 #spontaneous emission rate
-    g=1 #atom/cavity coupling strength
-    wc=0 #cavity frequency
-    w0=0 #atom frequency
-    wl=0 #driving frequency
-    E=0.5 #driving amplitude
-    N=5 #number of cavity energy levels (0->3 Fock states)
-    tlist=linspace(0,10,5) #times for expectation values
+    kappa = 2.0  # mirror coupling
+    gamma = 0.2  # spontaneous emission rate
+    g = 1  # atom/cavity coupling strength
+    wc = 0  # cavity frequency
+    w0 = 0  # atom frequency
+    wl = 0  # driving frequency
+    E = 0.5  # driving amplitude
+    N = 5  # number of cavity energy levels (0->3 Fock states)
+    tlist = linspace(0, 10, 5)  # times for expectation values
     # construct Hamiltonian
-    ida=qeye(N)
-    idatom=qeye(2)
-    a=tensor(destroy(N),idatom)
-    sm=tensor(ida,sigmam())
-    H=(w0-wl)*sm.dag()*sm+(wc-wl)*a.dag()*a+1j*g*(a.dag()*sm-sm.dag()*a)+E*(a.dag()+a)
-    #collapse operators
-    C1=sqrt(2*kappa)*a
-    C2=sqrt(gamma)*sm
-    C1dC1=C1.dag()*C1
-    C2dC2=C2.dag()*C2
-    #intial state
-    psi0=tensor(basis(N,0),basis(2,1))
-    opts=Odeoptions(gui=False,mc_avg=True)
-    data=mcsolve_f90(H,psi0,tlist,[C1,C2],[C1dC1,C2dC2,a],ntraj=5,options=opts)
-    assert_equal(isinstance(data.expect[0][0],float), True)
-    assert_equal(isinstance(data.expect[1][0],float), True)
-    assert_equal(isinstance(data.expect[2][0],complex), True)
+    ida = qeye(N)
+    idatom = qeye(2)
+    a = tensor(destroy(N), idatom)
+    sm = tensor(ida, sigmam())
+    H = (w0 - wl) * sm.dag() * sm + (wc - wl) * a.dag() * a + \
+        1j * g * (a.dag() * sm - sm.dag() * a) + E * (a.dag() + a)
+    # collapse operators
+    C1 = sqrt(2 * kappa) * a
+    C2 = sqrt(gamma) * sm
+    C1dC1 = C1.dag() * C1
+    C2dC2 = C2.dag() * C2
+    # intial state
+    psi0 = tensor(basis(N, 0), basis(2, 1))
+    opts = Odeoptions(gui=False, mc_avg=True)
+    data = mcsolve_f90(
+        H, psi0, tlist, [C1, C2], [C1dC1, C2dC2, a], ntraj=5, options=opts)
+    assert_equal(isinstance(data.expect[0][0], float), True)
+    assert_equal(isinstance(data.expect[1][0], float), True)
+    assert_equal(isinstance(data.expect[2][0], complex), True)
 
 
 @unittest.skipIf(fortran_found == 0, 'fortran files not found')
 def test_mcf90_dtypes2():
     "mcsolve_f90: check for correct dtypes (mc_avg=False)"
     # set system parameters
-    kappa=2.0 #mirror coupling
-    gamma=0.2 #spontaneous emission rate
-    g=1 #atom/cavity coupling strength
-    wc=0 #cavity frequency
-    w0=0 #atom frequency
-    wl=0 #driving frequency
-    E=0.5 #driving amplitude
-    N=5 #number of cavity energy levels (0->3 Fock states)
-    tlist=linspace(0,10,5) #times for expectation values
+    kappa = 2.0  # mirror coupling
+    gamma = 0.2  # spontaneous emission rate
+    g = 1  # atom/cavity coupling strength
+    wc = 0  # cavity frequency
+    w0 = 0  # atom frequency
+    wl = 0  # driving frequency
+    E = 0.5  # driving amplitude
+    N = 5  # number of cavity energy levels (0->3 Fock states)
+    tlist = linspace(0, 10, 5)  # times for expectation values
     # construct Hamiltonian
-    ida=qeye(N)
-    idatom=qeye(2)
-    a=tensor(destroy(N),idatom)
-    sm=tensor(ida,sigmam())
-    H=(w0-wl)*sm.dag()*sm+(wc-wl)*a.dag()*a+1j*g*(a.dag()*sm-sm.dag()*a)+E*(a.dag()+a)
-    #collapse operators
-    C1=sqrt(2*kappa)*a
-    C2=sqrt(gamma)*sm
-    C1dC1=C1.dag()*C1
-    C2dC2=C2.dag()*C2
-    #intial state
-    psi0=tensor(basis(N,0),basis(2,1))
-    opts=Odeoptions(gui=False,mc_avg=False)
-    data=mcsolve_f90(H,psi0,tlist,[C1,C2],[C1dC1,C2dC2,a],ntraj=5,options=opts)
-    assert_equal(isinstance(data.expect[0][0][0],float), True)
-    assert_equal(isinstance(data.expect[0][1][0],float), True)
-    assert_equal(isinstance(data.expect[0][2][0],complex), True)
-
+    ida = qeye(N)
+    idatom = qeye(2)
+    a = tensor(destroy(N), idatom)
+    sm = tensor(ida, sigmam())
+    H = (w0 - wl) * sm.dag() * sm + (wc - wl) * a.dag() * a + \
+        1j * g * (a.dag() * sm - sm.dag() * a) + E * (a.dag() + a)
+    # collapse operators
+    C1 = sqrt(2 * kappa) * a
+    C2 = sqrt(gamma) * sm
+    C1dC1 = C1.dag() * C1
+    C2dC2 = C2.dag() * C2
+    # intial state
+    psi0 = tensor(basis(N, 0), basis(2, 1))
+    opts = Odeoptions(gui=False, mc_avg=False)
+    data = mcsolve_f90(
+        H, psi0, tlist, [C1, C2], [C1dC1, C2dC2, a], ntraj=5, options=opts)
+    assert_equal(isinstance(data.expect[0][0][0], float), True)
+    assert_equal(isinstance(data.expect[0][1][0], float), True)
+    assert_equal(isinstance(data.expect[0][2][0], complex), True)
 
 
 if __name__ == "__main__":
