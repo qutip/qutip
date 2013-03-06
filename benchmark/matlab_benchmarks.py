6b9ac84c79cd88c2e9b1efaa0ad5851e913a166f
Robert Johansson <jrjohansson@gmail.com>
2013-03-06 17:39:04 +0900

    added qutip header to benchmark files

--- benchmark/matlab_benchmarks.py
+++ benchmark/matlab_benchmarks.py
@@ -1,3 +1,22 @@
+# This file is part of QuTiP.
+#
+#    QuTiP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    QuTiP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with QuTiP.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2011-2013, Paul D. Nation & Robert J. Johansson
+#
+###########################################################################
+
 import sys
 import csv
 import subprocess as sproc
