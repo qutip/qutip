bf4f60e30eb6acc0ef721d0dfc7b43bdc7ce4c78
Robert Johansson <jrjohansson@gmail.com>
2013-03-14 15:05:44 +0900

    generalize Distribution so that it can used for different number of dimensions

--- qutip/distributions.py
+++ qutip/distributions.py
@@ -46,6 +46,17 @@ class Distribution:
     def visualize(self, fig=None, ax=None, figsize=(8, 6),
                   colorbar=True, cmap=None):
 
+        n = len(self.xvecs)
+        if n == 2:
+            self.visualize_2d(fig=fig, ax=ax, figsize=figsize,
+                              colorbar=colorbar, cmap=cmap)
+        else:
+            raise NotImplementedError("Distribution visualization in " + 
+                                      "%d dimensions is not implemented." % n)
+
+    def visualize_2d(self, fig=None, ax=None, figsize=(8, 6),
+                     colorbar=True, cmap=None):
+
 
         if not fig and not ax:
             fig, ax = plt.subplots(1, 1, figsize=figsize)
@@ -55,12 +66,12 @@ class Distribution:
 
         lim = abs(self.data).max()
 
-        cf = ax.contourf(self.xvec, self.xvec, self.data, 100,
+        cf = ax.contourf(self.xvecs[0], self.xvecs[1], self.data, 100,
                          norm=mpl.colors.Normalize(-lim, lim),
                          cmap=cmap)
 
-        ax.set_xlabel(self.xlabel, fontsize=12)
-        ax.set_ylabel(self.ylabel, fontsize=12)
+        ax.set_xlabel(self.xlabels[0], fontsize=12)
+        ax.set_ylabel(self.xlabels[1], fontsize=12)
 
         if colorbar:
             cb = fig.colorbar(cf, ax=ax)
@@ -72,36 +83,34 @@ class WignerDistribution(Distribution):
 
     def __init__(self, rho=None, extent=[[-5, 5], [-5, 5]], steps=250):
 
-        self.xvec = np.linspace(extent[0][0], extent[0][1], steps)
-        self.yvec = np.linspace(extent[1][0], extent[1][1], steps)
+        self.xvecs = [np.linspace(extent[0][0], extent[0][1], steps),
+                      np.linspace(extent[1][0], extent[1][1], steps)]
                 
-        self.xlabel = r'$\rm{Re}(\alpha)$'
-        self.ylabel = r'$\rm{Im}(\alpha)$'
+        self.xlabels = [r'$\rm{Re}(\alpha)$', r'$\rm{Im}(\alpha)$']
     
         if rho:
             self.update(rho)
 
     def update(self, rho):
 
-        self.data = wigner(rho, self.xvec, self.yvec)
+        self.data = wigner(rho, self.xvecs[0], self.xvecs[1])
 
 
 class QDistribution(Distribution):
 
     def __init__(self, rho=None, extent=[[-5, 5], [-5, 5]], steps=250):
 
-        self.xvec = np.linspace(extent[0][0], extent[0][1], steps)
-        self.yvec = np.linspace(extent[1][0], extent[1][1], steps)
+        self.xvecs = [np.linspace(extent[0][0], extent[0][1], steps),
+                      np.linspace(extent[1][0], extent[1][1], steps)]
                 
-        self.xlabel = r'$\rm{Re}(\alpha)$'
-        self.ylabel = r'$\rm{Im}(\alpha)$'
+        self.xlabels = [r'$\rm{Re}(\alpha)$', r'$\rm{Im}(\alpha)$']
     
         if rho:
             self.update(rho)
 
     def update(self, rho):
 
-        self.data = qfunc(rho, self.xvec, self.yvec)
+        self.data = qfunc(rho, self.xvecs[0], self.xvecs[1])
 
 
 class TwoModeQuadratureCorrelation(Distribution):
@@ -109,11 +118,10 @@ class TwoModeQuadratureCorrelation(Distribution):
     def __init__(self, rho=None, theta1=0.0, theta2=0.0,
                  extent=[[-5, 5], [-5, 5]], steps=250):
 
-        self.xvec = np.linspace(extent[0][0], extent[0][1], steps)
-        self.yvec = np.linspace(extent[1][0], extent[1][1], steps)
+        self.xvecs = [np.linspace(extent[0][0], extent[0][1], steps),
+                      np.linspace(extent[1][0], extent[1][1], steps)]
                 
-        self.xlabel = r'$X_1(\theta_1)$'
-        self.ylabel = r'$X_2(\theta_2)$'
+        self.xlabels = [r'$X_1(\theta_1)$', r'$X_2(\theta_2)$']
     
         self.theta1 = theta1
         self.theta2 = theta2
@@ -127,9 +135,9 @@ class TwoModeQuadratureCorrelation(Distribution):
         outcomes given a two-mode wavefunction/density matrix
         """
         
-        X1, X2 = np.meshgrid(self.xvec, self.yvec)
+        X1, X2 = np.meshgrid(self.xvecs[0], self.xvecs[1])
         
-        p = np.zeros((len(self.xvec), len(self.yvec)), dtype=complex)
+        p = np.zeros((len(self.xvecs[0]), len(self.xvecs[1])), dtype=complex)
         N = rho.dims[0][0]
         
         for n1 in range(N):
@@ -141,9 +149,8 @@ class TwoModeQuadratureCorrelation(Distribution):
                 kn2 = exp(-1j * self.theta2 * n2) / \
                     sqrt(sqrt(pi) * 2**n2 * factorial(n2)) * \
                     exp(-X2**2/2.0) * np.polyval(hermite(n2), X2)
-
                 i = state_number_index([N, N], [n1, n2])
-                p += kn1 * kn2 * rho.data[i,0]
+                p += kn1 * kn2 * rho.data[i, 0]
 
         self.data = abs(p)**2
 
