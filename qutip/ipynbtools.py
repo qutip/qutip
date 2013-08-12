eeb686d92ba4fa08a5f7f22e8c0e1e0e8fb3a59c
Robert Johansson <jrjohansson@gmail.com>
2013-08-12 16:01:10 +0900

    added convenience function that use matplotlib's animation to generate an animated visualization of a Odedata instance, for use in the ipython notebook

--- qutip/ipynbtools.py
+++ qutip/ipynbtools.py
@@ -25,6 +25,7 @@ from IPython.parallel import Client
 from IPython.display import HTML, Javascript, display
 
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 import datetime
 import uuid
@@ -248,3 +249,32 @@ def parfor(task, task_vec, args=None, client=None, view=None,
         _visualize_parfor_data(metadata)
 
     return [ar.get() for ar in ar_list]
+
+
+def plot_animation(plot_setup_func, plot_func, result, name="movie",
+                   verbose=False):
+    """
+    Create an animated plot of a Odedata object, as returned by one of
+    the qutip evolution solvers.
+
+    .. note :: experimental
+    """
+
+    fig, axes = plot_setup_func(result)
+
+    def update(n): 
+        plot_func(result, n, fig=fig, axes=axes)
+
+    anim = animation.FuncAnimation(fig, update, frames=len(result.times), blit=True)
+
+    anim.save(name + '.mp4', fps=10, writer='ffmpeg', codec='libx264')
+
+    plt.close(fig)
+    
+    if verbose:
+        print("Created %s.m4v" % name)
+    
+    video = open(name + '.mp4', "rb").read()
+    video_encoded = video.encode("base64")
+    video_tag = '<video controls src="data:video/x-m4v;base64,{0}">'.format(video_encoded)
+    return HTML(video_tag)
