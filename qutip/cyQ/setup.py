f023a7153129e3218dd9d58e01b7c187f42e5653
Paul Nation <paul@Pauls-Air.local>
2014-01-09 15:53:31 +0900

    ADDED: Additional compiler flags
    Hope to squeeze out a little more performance for free

--- qutip/cyQ/setup.py
+++ qutip/cyQ/setup.py
@@ -18,7 +18,7 @@ def configuration(parent_package='', top_path=None):
             config.add_extension(ext,
                                  sources=[ext + ".c"],
                                  include_dirs=[np.get_include()],
-                                 extra_compile_args=['-w -ffast-math -O3'],
+                                 extra_compile_args=['-w -ffast-math -O3 -march=native -mfpmath=sse'],
                                  extra_link_args=[])
 
     else:
@@ -26,7 +26,7 @@ def configuration(parent_package='', top_path=None):
             config.add_extension(ext,
                                  sources=[ext + ".pyx"],
                                  include_dirs=[np.get_include()],
-                                 extra_compile_args=['-w -ffast-math -O3'],
+                                 extra_compile_args=['-w -ffast-math -O3 -march=native -mfpmath=sse'],
                                  extra_link_args=[])
         config.ext_modules = cythonize(config.ext_modules)
    
@@ -39,15 +39,15 @@ if __name__ == '__main__':
         cmdclass={'build_ext': build_ext},
         include_dirs=[np.get_include()],
         ext_modules=[Extension("spmatfuncs", ["spmatfuncs.pyx"],
-                               extra_compile_args=['-w -ffast-math -O3'],
+                               extra_compile_args=['-w -ffast-math -O3 -march=native -mfpmath=sse'],
                                extra_link_args=[]),
                      Extension("stochastic", ["stochastic.pyx"],
-                               extra_compile_args=['-w -ffast-math -O3'],
+                               extra_compile_args=['-w -ffast-math -O3 -march=native -mfpmath=sse'],
                                extra_link_args=[]),
                      Extension("sparse_utils", ["sparse_utils.pyx"],
-                               extra_compile_args=['-w -ffast-math -O3'],
+                               extra_compile_args=['-w -ffast-math -O3 -march=native -mfpmath=sse'],
                                extra_link_args=[]),
                      Extension("graph_utils", ["graph_utils.pyx"],
-                               extra_compile_args=['-w -ffast-math -O3'],
+                               extra_compile_args=['-w -ffast-math -O3 -march=native -mfpmath=sse'],
                                extra_link_args=[])]
     )
