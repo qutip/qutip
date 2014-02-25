2de87dd68fb0f4cb2502d749185cba4dd49cc020
Robert Johansson <jrjohansson@gmail.com>
2014-02-25 12:59:28 +0900

    avoid nosetests error when importing qutip.fortran if fortran solver is not included

--- qutip/fortran/__init__.py
+++ qutip/fortran/__init__.py
@@ -3,6 +3,10 @@
 # what to import when using import *
 #all = ['']
 
-from qutip.fortran.mcsolve_f90 import mcsolve_f90
+try:
+    from qutip.fortran.mcsolve_f90 import mcsolve_f90
+except:
+    pass
+
 #from mcsolve_f90_par import mcsolve_f90_serial, mcsolve_f90_par
 
