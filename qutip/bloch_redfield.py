93096c0f96bdddc4d506030e98f2b47e98683d5c
Robert Johansson <jrjohansson@gmail.com>
2014-03-24 10:41:17 +0900

    pep8 fixes

--- qutip/bloch_redfield.py
+++ qutip/bloch_redfield.py
@@ -3,11 +3,11 @@
 #    Copyright (c) 2011 and later, Paul D. Nation and Robert J. Johansson.
 #    All rights reserved.
 #
-#    Redistribution and use in source and binary forms, with or without 
-#    modification, are permitted provided that the following conditions are 
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are
 #    met:
 #
-#    1. Redistributions of source code must retain the above copyright notice, 
+#    1. Redistributions of source code must retain the above copyright notice,
 #       this list of conditions and the following disclaimer.
 #
 #    2. Redistributions in binary form must reproduce the above copyright
@@ -18,16 +18,16 @@
 #       of its contributors may be used to endorse or promote products derived
 #       from this software without specific prior written permission.
 #
-#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-#    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-#    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-#    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-#    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-#    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-#    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
-#    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+#    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+#    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 
@@ -100,7 +100,7 @@ def brmesolve(H, psi0, tlist, a_ops, e_ops=[], spectra_cb=[],
 
     output = Odedata()
     output.times = tlist
-    
+
     results = bloch_redfield_solve(R, ekets, psi0, tlist, e_ops, options)
 
     if e_ops:
@@ -193,9 +193,9 @@ def bloch_redfield_solve(R, ekets, rho0, tlist, e_ops=[], options=None):
     r = scipy.integrate.ode(cy_ode_rhs)
     r.set_f_params(R.data.data, R.data.indices, R.data.indptr)
     r.set_integrator('zvode', method=options.method, order=options.order,
-                     atol=options.atol, rtol=options.rtol, nsteps=options.nsteps,
-                     first_step=options.first_step, min_step=options.min_step,
-                     max_step=options.max_step)
+                     atol=options.atol, rtol=options.rtol,
+                     nsteps=options.nsteps, first_step=options.first_step,
+                     min_step=options.min_step, max_step=options.max_step)
     r.set_initial_value(initial_vector, tlist[0])
 
     #
@@ -285,7 +285,7 @@ def bloch_redfield_tensor(H, a_ops, spectra_cb, use_secular=True):
             W[m, n] = np.real(evals[m] - evals[n])
 
     for k in range(K):
-        #A[k,n,m] = a_ops[k].matrix_element(ekets[n], ekets[m])
+        # A[k,n,m] = a_ops[k].matrix_element(ekets[n], ekets[m])
         A[k, :, :] = a_ops[k].transform(ekets).full()
 
     dw_min = abs(W[W.nonzero()]).min()
@@ -300,7 +300,7 @@ def bloch_redfield_tensor(H, a_ops, spectra_cb, use_secular=True):
             c, d = vec2mat_index(N, J)
 
             # unitary part: use spre and spost above, same as this:
-            #R.data[I,J] = -1j * W[a,b] * (a == c) * (b == d)
+            # R.data[I,J] = -1j * W[a,b] * (a == c) * (b == d)
 
             if use_secular is False or abs(W[a, b] - W[c, d]) < dw_min / 10.0:
 
