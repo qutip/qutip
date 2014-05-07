4b51250648ea88f97adbbe7f03352d06ec1f0b38
Paul Nation <nonhermitian@gmail.com>
2014-05-07 15:59:20 +0900

    UPDATED: mcf90 inks to accelerate and openblas
    Fortran mcsolve now links to accelerate on OSX and OPENBLAS, if
    available, on other platforms.

--- qutip/fortran/setup.py
+++ qutip/fortran/setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from os.path import join
-
+import sys
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.system_info import get_info, NotFoundError
-
+    
     config = Configuration('fortran', parent_package, top_path)
 
     sources = [
@@ -22,45 +22,54 @@ def configuration(parent_package='', top_path=None):
 
     config.add_library('zvode', sources=[join('zvode', '*.f')])
 
-    #
-    # LAPACK?
-    #
-    lapack_opt = get_info('lapack', notfound_action=1)
-
-    if not lapack_opt:
-        # raise NotFoundError,'no lapack resources found'
-        print("Warning: No lapack resource found. Linear algebra routines"
-              + " like 'eigenvalues' and 'entropy' will not be available.")
-        sources.append('qutraj_nolinalg.f90')
-    else:
+   
+    if sys.platform=='darwin':
+        blas_opt=get_info('blas_opt')
+        extra_compile_args=blas_opt['extra_compile_args']
+        extra_link_args=blas_opt['extra_link_args']
+        newblas = {}
         sources.append('qutraj_linalg.f90')
-        libs.extend(lapack_opt['libraries'])
-
-    #
-    # BLAS
-    #
-    if not lapack_opt:
-        blas_opt = get_info('blas', notfound_action=2)
     else:
-        blas_opt = lapack_opt
+        extra_compile_args=[]
+        extra_link_args=[]
+        #
+        # LAPACK?
+        #
+        lapack_opt = get_info('lapack_opt', notfound_action=1)
 
-    # Remove libraries key from blas_opt
-    if 'libraries' in blas_opt:  # key doesn't exist on OS X ...
-        libs.extend(blas_opt['libraries'])
-    newblas = {}
-    for key in blas_opt.keys():
-        if key == 'libraries':
-            continue
-        newblas[key] = blas_opt[key]
+        if not lapack_opt:
+            # raise NotFoundError,'no lapack resources found'
+            print("Warning: No lapack resource found. Linear algebra routines"
+                  + " like 'eigenvalues' and 'entropy' will not be available.")
+            sources.append('qutraj_nolinalg.f90')
+        else:
+            sources.append('qutraj_linalg.f90')
+            libs.extend(lapack_opt['libraries'])
+
+        #
+        # BLAS
+        #
+        if not lapack_opt:
+            blas_opt = get_info('blas_opt', notfound_action=2)
+        else:
+            blas_opt = lapack_opt
+
+        # Remove libraries key from blas_opt
+        if 'libraries' in blas_opt:  # key doesn't exist on OS X ...
+            libs.extend(blas_opt['libraries'])
+        newblas = {}
+        for key in blas_opt.keys():
+            if key == 'libraries':
+                continue
+            newblas[key] = blas_opt[key]
 
     # Add this last
     sources.append('qutraj_run.f90')
 
     config.add_extension('qutraj_run',
                          sources=sources,
-                         extra_compile_args=[
-                             #'-DF2PY_REPORT_ON_ARRAY_COPY=1',
-                         ],
+                         extra_compile_args=extra_compile_args,
+                         extra_link_args=extra_link_args,
                          libraries=libs,
                          **newblas
                          )
