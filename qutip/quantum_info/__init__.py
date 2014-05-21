27bc055c6b9424029c9fa6296227bce4dfabe8ca
Anubhav Vardhan <anubhavvardhan@gmail.com>
2014-05-21 11:33:30 +0900

    Added QubitCircuit and Gate

--- qutip/quantum_info/__init__.py
+++ qutip/quantum_info/__init__.py
@@ -1,4 +1,4 @@
 from qutip.quantum_info.register import Register
-from qutip.quantum_info.circuit import Circuit
+from qutip.quantum_info.circuit import Circuit, QubitCircuit, Gate
 from qutip.quantum_info.states import *
 from qutip.quantum_info.gates import *
