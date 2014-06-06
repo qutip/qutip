daad9aa36230fb37041d76ba09713a610cf4e42a
Anubhav Vardhan <anubhavvardhan@gmail.com>
2014-06-06 13:34:29 +0900

    Fixed gate resolution in CNOT to ISWAP

--- qutip/qip/circuit.py
+++ qutip/qip/circuit.py
@@ -292,7 +292,7 @@ class QubitCircuit(object):
                     self.gates_resolved.append(Gate("RY", gate.targets, None,
                                                     arg_value=-np.pi/2, arg_label=r"-\pi/2"))
                     self.gates_resolved.append(Gate("RZ", gate.targets, None,
-                                                    arg_value=np.pi, arg_label=r"\pi"))
+                                                    arg_value=np.pi/2, arg_label=r"\pi/2"))
                 else:
                     self.gates_resolved.append(gate)
         elif basis == "SQRTISWAP":
