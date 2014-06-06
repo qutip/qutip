bf1abf5a84c4f67ee960eda34f769339a62288f7
Anubhav Vardhan <anubhavvardhan@gmail.com>
2014-06-06 13:52:35 +0900

    Fixed gate resolution in CNOT to SQRTISWAP

--- qutip/qip/circuit.py
+++ qutip/qip/circuit.py
@@ -315,7 +315,7 @@ class QubitCircuit(object):
                     self.gates_resolved.append(Gate("RZ", gate.controls, None,
                                                     arg_value=np.pi, arg_label=r"\pi"))
                     self.gates_resolved.append(Gate("GLOBALPHASE", None, None,
-                                                    arg_value=3*np.pi/2, arg_label=r"\3pi/"))
+                                                    arg_value=3*np.pi/2, arg_label=r"3\pi/2"))
                 else:
                     self.gates_resolved.append(gate)
         elif basis == "SQRTSWAP":
