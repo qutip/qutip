3f8e109c5c6a1855eb3b1ce1f1ef1b311bf08029
Piotr Migdal <pmigdal@gmail.com>
2014-06-23 17:59:28 +0200

    now qubism font size scales well also for subplots

--- qutip/visualization.py
+++ qutip/visualization.py
@@ -1377,7 +1377,8 @@ def plot_qubism(ket, theme='light', how='pairs',
         scale_y = float(size_y) / np.prod(dim_list_y[:label_n])
         shift_y = 0.5 * scale_y
 
-        fontsize = 30 * figsize[0] / np.prod(dim_list_x[:label_n]) / label_n
+        bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        fontsize = 35 * bbox.width / np.prod(dim_list_x[:label_n]) / label_n
         opts = {'fontsize': fontsize,
                 'color': theme2color_of_lines[theme],
                 'horizontalalignment': 'center',
