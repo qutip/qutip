6a42ddfc5caa9b79eb4a65af590fb94d0cfcb8af
Anubhav Vardhan <anubhavvardhan@gmail.com>
2014-06-23 13:53:04 +0900

    Removed non-general classes from default import

--- qutip/qip/models/__init__.py
+++ qutip/qip/models/__init__.py
@@ -31,7 +31,3 @@
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 from qutip.qip.models.circuitprocessor import CircuitProcessor
-from qutip.qip.models.spinchain import SpinChain
-from qutip.qip.models.spinchain import LinearSpinChain, CircularSpinChain
-from qutip.qip.models.dispersivecQED import DispersivecQED
-
