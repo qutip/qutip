ac6c6fc526782b8375cb7deb256060b912d20a72
Robert Johansson <jrjohansson@gmail.com>
2014-06-25 14:03:47 +0900

    moved and renamed Odeoptions and Odedata to qutip.solver

--- qutip/solver.py
+++ qutip/solver.py
@@ -36,17 +36,17 @@ import os
 import warnings
 
 
-class Odeoptions():
+class Options():
     """
     Class of options for evolution solvers such as :func:`qutip.mesolve` and
     :func:`qutip.mcsolve`. Options can be specified either as arguments to the
     constructor::
 
-        opts = Odeoptions(order=10, ...)
+        opts = Options(order=10, ...)
 
     or by changing the class attributes after creation::
 
-        opts = Odeoptions()
+        opts = Options()
         opts.order = 10
 
     Returns options class to be used as options in evolution solvers.
@@ -162,7 +162,7 @@ class Odeoptions():
 
     def __str__(self):
         s = ""
-        s += "Odeoptions:\n"
+        s += "Options:\n"
         s += "-----------\n"
         s += "atol:              " + str(self.atol) + "\n"
         s += "rtol:              " + str(self.rtol) + "\n"
@@ -186,3 +186,82 @@ class Odeoptions():
         s += "store_final_state: " + str(self.store_final_state) + "\n"
 
         return s
+
+
+class SolverResult():
+    """Class for storing simulation results from any of the dynamics solvers.
+
+    Attributes
+    ----------
+
+    solver : str
+        Which solver was used ['mesolve', 'mcsolve', 'brmesolve', 'floquet']
+    times : list/array
+        Times at which simulation data was collected.
+    expect : list/array
+        Expectation values (if requested) for simulation.
+    states : array
+        State of the simulation (density matrix or ket) evaluated at ``times``.
+    num_expect : int
+        Number of expectation value operators in simulation.
+    num_collapse : int
+        Number of collapse operators in simualation.
+    ntraj : int/list
+        Number of monte-carlo trajectories (if using mcsolve).  List indicates
+        that averaging of expectation values was done over a subset of total
+        number of trajectories.
+    col_times : list
+        Times at which state collpase occurred. Only for Monte Carlo solver.
+    col_which : list
+        Which collapse operator was responsible for each collapse in
+        ``col_times``. Only for Monte Carlo solver.
+
+    """
+    def __init__(self):
+        self.solver = None
+        self.times = None
+        self.states = []
+        self.expect = []
+        self.num_expect = 0
+        self.num_collapse = 0
+        self.ntraj = None
+        self.col_times = None
+        self.col_which = None
+
+    def __str__(self):
+        s = "SolverResult object "
+        if self.solver:
+            s += "with " + self.solver + " data.\n"
+        else:
+            s += "missing solver information.\n"
+        s += "-" * (len(s) - 1) + "\n"
+        if self.states is not None and len(self.states) > 0:
+            s += "states = True\n"
+        elif self.expect is not None and len(self.expect) > 0:
+            s += "expect = True\nnum_expect = " + str(self.num_expect) + ", "
+        else:
+            s += "states = True, expect = True\n" + \
+                "num_expect = " + str(self.num_expect) + ", "
+        s += "num_collapse = " + str(self.num_collapse)
+        if self.solver == 'mcsolve':
+            s += ", ntraj = " + str(self.ntraj)
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __getstate__(self):
+        # defines what happens when Qobj object gets pickled
+        self.__dict__.update({'qutip_version': __version__[:5]})
+        return self.__dict__
+
+    def __setstate__(self, state):
+        # defines what happens when loading a pickled Qobj
+        if 'qutip_version' in state.keys():
+            del state['qutip_version']
+        (self.__dict__).update(state)
+
+
+# for backwards compatibility
+Odeoptions = Options
+Odedata = SolverResult
