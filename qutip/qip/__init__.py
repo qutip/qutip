bd5141491c5f75f0342f4f6dc070c37aefbed6d0
Anubhav <anubhavvardhan@gmail.com>
2014-07-01 10:15:24 +0900

    Updated __init__.py

--- qutip/qip/__init__.py
+++ qutip/qip/__init__.py
@@ -30,7 +30,6 @@
 #    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
-from qutip.qip.register import Register
 from qutip.qip.circuit import Circuit, QubitCircuit, Gate
 from qutip.qip.gates import *
 from qutip.qip.qft import qft, qft_steps 
