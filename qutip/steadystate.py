c2e36aebc7bbd5bb283987942801eaf4a125f5a8
Paul Nation <paul@Pauls-MacBook-Pro.local>
2014-08-30 21:11:39 +0900

    Added docstrings to build_preconditioner

--- qutip/steadystate.py
+++ qutip/steadystate.py
@@ -692,6 +692,68 @@ def build_preconditioner(A, c_op_list=[], **kwargs):
     """Constructs a iLU preconditioner necessary for solving for
     the steady state density matrix using the iterative linear solvers
     in the 'steadystate' function.
+    
+    Parameters
+    ----------
+    A : qobj
+        A Hamiltonian or Liouvillian operator.
+
+    c_op_list : list
+        A list of collapse operators.
+
+    return_info : bool, optional, default = False
+        Return a dictionary of solver-specific infomation about the 
+        solution and how it was obtained.
+
+    use_rcm : bool, optional, default = False
+        Use reverse Cuthill-Mckee reordering to minimize fill-in in the
+        LU factorization of the Liouvillian.
+
+    use_wbm : bool, optional, default = False
+        Use Weighted Bipartite Matching reordering to make the Liouvillian
+        diagonally dominant.  This is useful for iterative preconditioners
+        only, and is set to ``True`` by default when finding a preconditioner.
+
+    weight : float, optional
+        Sets the size of the elements used for adding the unity trace condition
+        to the linear solvers.  This is set to the average abs value of the
+        Liouvillian elements if not specified by the user.
+
+    permc_spec : str, optional, default='COLAMD'
+        Column ordering used internally by superLU for the 
+        'direct' LU decomposition method. Options include 'COLAMD' and 
+        'NATURAL'. If using RCM then this is set to 'NATURAL' automatically 
+        unless explicitly specified.
+
+    fill_factor : float, optional, default = 100
+        Specifies the fill ratio upper bound (>=1) of the iLU
+        preconditioner.  Lower values save memory at the cost of longer
+        execution times and a possible singular factorization.
+
+    drop_tol : float, optional, default = 1e-4
+        Sets the threshold for the magnitude of preconditioner
+        elements that should be dropped.  Can be reduced for a courser
+        factorization at the cost of an increased number of iterations, and a
+        possible singular factorization.
+
+    diag_pivot_thresh : float, optional, default = None
+        Sets the threshold between [0,1] for which diagonal
+        elements are considered acceptable pivot points when using a
+        preconditioner.  A value of zero forces the pivot to be the diagonal
+        element.
+
+    ILU_MILU : str, optional, default = 'smilu_2'
+        Selects the incomplete LU decomposition method algoithm 
+        used in creating the preconditoner. Should only be used by advanced users.
+    
+    Returns
+    -------
+    lu : object
+        Returns a SuperLU object representing iLU preconditioner.
+    
+    info : dict, optional
+        Dictionary containing solver-specific information.
+    
     """
     ss_args = _default_steadystate_args()
     for key in kwargs.keys():
