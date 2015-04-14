25af1f4a08ca77df855c828debc4e50ed338289c
Chris Granade <cgranade@cgranade.com>
2015-04-14 16:57:40 +1000

    New tests of dnorm() for qubit channels.

--- qutip/tests/test_metrics.py
+++ qutip/tests/test_metrics.py
@@ -37,15 +37,18 @@ the qutip.metrics module.
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 import numpy as np
-from numpy import abs, sqrt
-from numpy.testing import assert_, run_module_suite
+from numpy import abs, sqrt, ones, diag
+from numpy.testing import assert_, run_module_suite, assert_approx_equal
 import scipy
 
 from qutip.operators import create, destroy, jmat, identity, qdiags
 from qutip.states import fock_dm
 from qutip.propagator import propagator
-from qutip.random_objects import rand_herm, rand_dm, rand_unitary, rand_ket
+from qutip.random_objects import rand_herm, rand_dm, rand_unitary, rand_ket, rand_super_bcsz
 from qutip.metrics import *
+from qutip.superop_reps import to_choi
+from qutip.operators import qeye, sigmax
+from qutip.qobj import Qobj
 
 """
 A test class for the metrics and pseudo-metrics included with QuTiP.
@@ -153,6 +156,34 @@ def test_hilbert_dist():
     r1 = qdiags(diag1, 0)
     r2 = qdiags(diag2, 0)
     assert_(abs(hilbert_dist(r1, r2)-1) <= 1e-6)
+
+def test_dnorm_qubit_bounds():
+    """
+    dnorm: bounded [0, 2] for random qubit channels.
+    """
+    def case(chan1, chan2):
+        assert_(0 <= (chan1 - chan2).dnorm() <= 2)
+    for idx in xrange(4):
+        yield case, rand_super_bcsz(2), rand_super_bcsz(2)
+
+def test_dnorm_qubit_known_cases():
+    """
+    dnorm: agree with known cases for special qubit channels?
+    """
+    def case(chan1, chan2, expected, picos_args=None):
+        assert_approx_equal((chan1 - chan2).dnorm(picos_args), expected)
+
+    id_chan = to_choi(qeye(2))
+    X_chan = to_choi(sigmax())
+    depol = to_choi(Qobj(
+        diag(ones((4,))),
+        dims=[[[2], [2]], [[2], [2]]], superrep='chi'
+    ))
+    
+    # We need to restrict the number of iterations for things on the boundary,
+    # such as perfectly distinguishable channels.
+    yield case, id_chan, X_chan, 2, dict(maxit=10)
+    yield case, id_chan, depol, 1.5
 
 if __name__ == "__main__":
     run_module_suite()
