df6178c5b13fd0cf00e75665188b55a84e66d1d0
vardhan <anubhavvardhan@gmail.com>
2015-06-18 10:26:25 +0900

    pep8 fixes in hsolve.py

--- qutip/hsolve.py
+++ qutip/hsolve.py
@@ -35,8 +35,8 @@ This module provides exact solvers for a system-bath setup using the
 hierachical method.
 """
 
-#Author: Neill Lambert, Anubhav Vardhan
-#Contact: nwlambert@gmail.com
+# Author: Neill Lambert, Anubhav Vardhan
+# Contact: nwlambert@gmail.com
 
 __all__ = ['hsolve']
 
@@ -50,7 +50,8 @@ from scipy.misc import factorial
 from qutip.cy.spmatfuncs import cy_ode_rhs
 from qutip import spre, spost, sprepost, Options, dims, qeye
 from qutip import liouvillian, mat2vec, state_number_enumerate
-from qutip import enr_state_dictionaries 
+from qutip import enr_state_dictionaries
+
 
 def _heom_state_dictionaries(dims, excitations):
     """
@@ -121,7 +122,7 @@ def _heom_number_enumerate(dims, excitations=None, state=None, idx=0):
             yield np.array(state)
         else:
             yield tuple(state)
-            
+
     else:
         for n in range(dims[idx]):
             state[idx] = n
@@ -143,7 +144,7 @@ def cot(x):
 def hsolve(H, psi0, tlist, Q, gam, lam0, Nc, N, w_th, options=None):
     """
     Function to solve for an open quantum system using the
-    hierarchy model. 
+    hierarchy model.
 
     Parameters
     ----------
@@ -164,10 +165,10 @@ def hsolve(H, psi0, tlist, Q, gam, lam0, Nc, N, w_th, options=None):
     N: Integer
         Number of matsubara terms.
     w_th: Float
-        Temperature. 
+        Temperature.
     options : :class:`qutip.Options`
         With options for the solver.
-     
+
     Returns
     -------
     output: Result
@@ -175,29 +176,29 @@ def hsolve(H, psi0, tlist, Q, gam, lam0, Nc, N, w_th, options=None):
     """
     if options is None:
         options = Options()
-    
+
     # Set up terms of the matsubara and tanimura boundaries
 
     # Parameters and hamiltonian
-    hbar=1.
-    kb=1.
+    hbar = 1.
+    kb = 1.
 
-    # Set by system 
+    # Set by system
     dimensions = dims(H)
-    Nsup = dimensions[0][0] * dimensions[0][0] 
+    Nsup = dimensions[0][0] * dimensions[0][0]
     unit = qeye(dimensions[0])
 
     # Ntot is the total number of ancillary elements in the hierarchy
     Ntot = int(round(factorial(Nc+N) / (factorial(Nc) * factorial(N))))
-    c0 =(lam0 * gam * (cot(gam * hbar / (2. * kb * w_th)) - (1j))) / hbar
-    LD1 = -2.* spre(Q) * spost(Q.dag()) + spre(Q.dag() * Q) + spost(Q.dag() * Q)
-    pref = ((2. * lam0 * kb * w_th / (gam * hbar)) -1j * lam0) / hbar
+    c0 = (lam0 * gam * (cot(gam * hbar / (2. * kb * w_th)) - (1j))) / hbar
+    LD1 = (-2. * spre(Q) * spost(Q.dag()) + spre(Q.dag()*Q) + spost(Q.dag()*Q))
+    pref = ((2. * lam0 * kb * w_th / (gam * hbar)) - 1j * lam0) / hbar
     gj = 2 * np.pi * kb * w_th / hbar
-    L12 = -pref * LD1 + (c0 / gam) * LD1 
+    L12 = -pref * LD1 + (c0 / gam) * LD1
 
-    for i1 in range(1,N):
-        ci = (4 * lam0 * gam * kb * w_th * i1
-        	  * gj/((i1 * gj)**2 - gam**2)) / (hbar**2)
+    for i1 in range(1, N):
+        num = (4 * lam0 * gam * kb * w_th * i1 * gj/((i1 * gj)**2 - gam**2))
+        ci = num / (hbar**2)
         L12 = L12 + (ci / gj) * LD1
 
     # Setup liouvillian
@@ -205,62 +206,61 @@ def hsolve(H, psi0, tlist, Q, gam, lam0, Nc, N, w_th, options=None):
     L = liouvillian(H, [L12])
     Ltot = L.data
     unit = sp.csr_matrix(np.identity(Ntot))
-    Lbig = sp.kron(unit,Ltot.tocsr())
+    Lbig = sp.kron(unit, Ltot.tocsr())
     rho0big1 = np.zeros((Nsup * Ntot), dtype=complex)
-
 
     # Prepare initial state:
 
-    rhotemp =  mat2vec(np.array(psi0.full(), dtype=complex))
+    rhotemp = mat2vec(np.array(psi0.full(), dtype=complex))
 
-    for idx,element in enumerate(rhotemp):
+    for idx, element in enumerate(rhotemp):
         rho0big1[idx] = element[0]
     rho0big = sp.csr_matrix(rho0big1)
-    
-    nstates, state2idx, idx2state = enr_state_dictionaries([Nc+1]*(N),Nc)
-    for nlabelt in state_number_enumerate([Nc+1]*(N),Nc):
-        nlabel = list(nlabelt)                    
+
+    nstates, state2idx, idx2state = enr_state_dictionaries([Nc+1]*(N), Nc)
+    for nlabelt in state_number_enumerate([Nc+1]*(N), Nc):
+        nlabel = list(nlabelt)
         ntotalcheck = 0
         for ncheck in range(N):
-            ntotalcheck = ntotalcheck + nlabel[ncheck]                            
+            ntotalcheck = ntotalcheck + nlabel[ncheck]
         current_pos = int(round(state2idx[tuple(nlabel)]))
         Ltemp = sp.lil_matrix((Ntot, Ntot))
-        Ltemp[current_pos,current_pos] = 1
+        Ltemp[current_pos, current_pos] = 1
         Ltemp.tocsr()
-        Lbig = Lbig + sp.kron(Ltemp,(-nlabel[0] * gam * spre(unit).data))
-        
-        for kcount in range(1,N):
-            Lbig = Lbig + sp.kron(Ltemp,(-nlabel[kcount] * (kcount * gj)
-            	   			 * spre(unit).data))
-        
+        Lbig = Lbig + sp.kron(Ltemp, (-nlabel[0] * gam * spre(unit).data))
+
+        for kcount in range(1, N):
+            counts = -nlabel[kcount] * kcount * gj * spre(unit).data
+            Lbig = Lbig + sp.kron(Ltemp, counts)
+
         for kcount in range(N):
             if nlabel[kcount] >= 1:
-            # find the position of the neighbour
+                # find the position of the neighbour
                 nlabeltemp = copy(nlabel)
-                nlabel[kcount] = nlabel[kcount] -1
+                nlabel[kcount] = nlabel[kcount] - 1
                 current_pos2 = int(round(state2idx[tuple(nlabel)]))
-                Ltemp = sp.lil_matrix(np.zeros((Ntot,Ntot)))
+                Ltemp = sp.lil_matrix(np.zeros((Ntot, Ntot)))
                 Ltemp[current_pos, current_pos2] = 1
                 Ltemp.tocsr()
-            # renormalized version:    
-                ci =  (4 * lam0 * gam * kb * w_th * kcount
-                	   * gj/((kcount * gj)**2 - gam**2)) / (hbar**2)
+                # renormalized version:
+                ci = (4 * lam0 * gam * kb * w_th * kcount
+                      * gj/((kcount * gj)**2 - gam**2)) / (hbar**2)
                 if kcount == 0:
-                    Lbig = Lbig + sp.kron(Ltemp,(-1j
-                    				 * (np.sqrt(nlabeltemp[kcount]
-                    				    / abs(c0)))
-                    				 * ((c0) * spre(Q).data
-                    		  	 	 - (np.conj(c0))
-                    				 * spost(Q).data)))
-                if kcount > 0:                                                
-                    ci =  (4 * lam0 * gam * kb * w_th * kcount
-                    	   * gj/((kcount * gj)**2 - gam**2)) / (hbar**2)
-                    Lbig = Lbig + sp.kron(Ltemp,(-1j
-                    				 * (np.sqrt(nlabeltemp[kcount]
-                    				    / abs(ci)))
-                    				 * ((ci) * spre(Q).data
-                    				 - (np.conj(ci))
-                    				 * spost(Q).data)))
+                    Lbig = Lbig + sp.kron(Ltemp, (-1j
+                                          * (np.sqrt(nlabeltemp[kcount]
+                                             / abs(c0)))
+                                          * ((c0) * spre(Q).data
+                                             - (np.conj(c0))
+                                             * spost(Q).data)))
+                if kcount > 0:
+                    ci = (4 * lam0 * gam * kb * w_th * kcount
+                          * gj/((kcount * gj)**2 - gam**2)) / (hbar**2)
+                    Lbig = Lbig + sp.kron(Ltemp, (-1j
+                                          * (np.sqrt(nlabeltemp[kcount]
+                                             / abs(ci)))
+                                          * ((ci) * spre(Q).data
+                                             - (np.conj(ci))
+                                             * spost(Q).data)))
                 nlabel = copy(nlabeltemp)
 
         for kcount in range(N):
@@ -269,22 +269,22 @@ def hsolve(H, psi0, tlist, Q, gam, lam0, Nc, N, w_th, options=None):
                 nlabel[kcount] = nlabel[kcount] + 1
                 current_pos3 = int(round(state2idx[tuple(nlabel)]))
             if current_pos3 <= (Ntot):
-                Ltemp = sp.lil_matrix(np.zeros((Ntot,Ntot)))
+                Ltemp = sp.lil_matrix(np.zeros((Ntot, Ntot)))
                 Ltemp[current_pos, current_pos3] = 1
                 Ltemp.tocsr()
-            # renormalized   
+            # renormalized
                 if kcount == 0:
-                    Lbig = Lbig + sp.kron(Ltemp,-1j
-                    			  * (np.sqrt((nlabeltemp[kcount]+1)
-                    			     * abs(c0)))
-                    			  * (spre(Q)- spost(Q)).data)
+                    Lbig = Lbig + sp.kron(Ltemp, -1j
+                                          * (np.sqrt((nlabeltemp[kcount]+1)
+                                             * abs(c0)))
+                                          * (spre(Q) - spost(Q)).data)
                 if kcount > 0:
                     ci = (4 * lam0 * gam * kb * w_th * kcount
-                    	  * gj/((kcount * gj)**2 - gam**2)) / (hbar**2)
-                    Lbig = Lbig + sp.kron(Ltemp,-1j
-                    			  * (np.sqrt((nlabeltemp[kcount]+1) 
-                    			     * abs(ci)))
-                    			  * (spre(Q)- spost(Q)).data)
+                          * gj/((kcount * gj)**2 - gam**2)) / (hbar**2)
+                    Lbig = Lbig + sp.kron(Ltemp, -1j
+                                          * (np.sqrt((nlabeltemp[kcount]+1)
+                                             * abs(ci)))
+                                          * (spre(Q) - spost(Q)).data)
             nlabel = copy(nlabeltemp)
 
     output = []
@@ -294,17 +294,16 @@ def hsolve(H, psi0, tlist, Q, gam, lam0, Nc, N, w_th, options=None):
     Lbig2 = Lbig.tocsr()
     r.set_f_params(Lbig2.data, Lbig2.indices, Lbig2.indptr)
     r.set_integrator('zvode', method=options.method, order=options.order,
-    		     atol=options.atol, rtol=options.rtol,
-    		     nsteps=options.nsteps, first_step=options.first_step,
-    		     min_step=options.min_step,max_step=options.max_step)
-    
+                     atol=options.atol, rtol=options.rtol,
+                     nsteps=options.nsteps, first_step=options.first_step,
+                     min_step=options.min_step, max_step=options.max_step)
+
     r.set_initial_value(rho0big1, tlist[0])
-    dt = tlist[1] - tlist[0]    
+    dt = tlist[1] - tlist[0]
 
     for t_idx, t in enumerate(tlist):
         r.integrate(r.t + dt)
-        for idx,element in enumerate(rhotemp):        
+        for idx, element in enumerate(rhotemp):
             output[idx].append(r.y[idx])
 
     return output
-    
