8fe72e3b1ab5816243cdb461b8a1489a4e3e435e
Chris Granade <cgranade@cgranade.com>
2015-07-28 16:49:55 +1000

    Revert "Started work revamping and expanding superop reps."
    This reverts commit 2c8f1d071546e563b5c2cc92d895697a5b430117.

--- doc/_themes/bootstrap/static/js/mathconf.js
+++
@@ -1,9 +0,0 @@
-/* See http://stackoverflow.com/a/19268549. */
-MathJax.Hub.Config({
-    TeX: {
-        Macros: {
-            llangle: '\\langle\\!\\langle',
-            rrangle: '\\rangle\\!\\rangle'
-        }
-    }
-})
