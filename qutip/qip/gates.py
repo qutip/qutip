0d2abd6db3a4a5c03456ed4fe1a883a6ae773cfd
Christopher Granade <cgranade@cgranade.com>
2015-08-07 10:39:12 +1000

    Fixes for Py3, comments explaining partial(reduce, mul).

--- qutip/qip/gates.py
+++ qutip/qip/gates.py
@@ -47,7 +47,7 @@ from qutip.tensor import tensor
 from qutip.states import fock_dm
 
 from itertools import product
-from functools import partial
+from functools import partial, reduce
 from operator import mul
 
 
@@ -718,10 +718,14 @@ def gate_sequence_product(U_list, left_to_right=True):
     return U_overall
 
 def _powers(op, N):
+    """
+    Generator that yields powers of an operator `op`,
+    through to `N`.
+    """
     acc = qeye(op.dims[0])
     yield acc
 
-    for _ in xrange(N - 1):
+    for _ in range(N - 1):
         acc *= op
         yield acc
 
@@ -748,14 +752,32 @@ def qubit_clifford_group(N=None, target=0):
         Clifford operators, represented as Qobj instances.
     """
 
+    # The Ross-Selinger presentation of the single-qubit Clifford
+    # group expresses each element in the form C_{ijk} = E^i X^j S^k
+    # for gates E, X and S, and for i in range(3), j in range(2) and
+    # k in range(4).
+    #
+    # We start by defining these gates. E is defined in terms of H,
+    # \omega and S, so we define \omega and H first.
     w = np.exp(1j * 2 * np.pi / 8)
     H = snot()
 
+    X = sigmax()
     S = phasegate(np.pi / 2)
     E = H * (S ** 3) * w ** 3
-    X = sigmax()
 
-    for op in map(partial(reduce, mul), product(_powers(E, 3), _powers(X, 2), _powers(S, 4))):
+
+    for op in map(
+          # partial(reduce, mul) returns a function that takes products of its argument,
+          # by analogy to sum. Note that by analogy, sum can be written partial(reduce, add).
+          partial(reduce, mul),
+          # product(...) yields the Cartesian product of its arguments. Here, each element is
+          # a tuple (E**i, X**j, S**k) such that partial(reduce, mul) acting on the tuple
+          # yields E**i * X**j * S**k.
+          product(_powers(E, 3), _powers(X, 2), _powers(S, 4))
+        ):
+
+        # Finally, we optionally expand the gate.
         if N is not None:
             yield gate_expand_1toN(op, N, target)
         else:
