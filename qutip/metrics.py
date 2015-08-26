New more numerically stable fidelity (fixes #361)
This new function doesn't calculate the explicit sqrtm() unless
absolutely necessary to improve numerical stability. Also includes new
tests to check for regressions of #361.

qutip/metrics.py
@@ -71,15 +71,31 @@ def fidelity(A, B):
 
     """
     if A.isket or A.isbra:
-        A = ket2dm(A)
-    if B.isket or B.isbra:
-        B = ket2dm(B)
+        # Take advantage of the fact that the density operator for A
+        # is a projector to avoid a sqrtm call.
+        sqrtmA = ket2dm(A)
+        # Check whether we have to turn B into a density operator, too.
+        if B.isket or B.isbra:
+            B = ket2dm(B)
+    else:
+        if B.isket or B.isbra:
+            # Swap the order so that we can take a more numerically
+            # stable square root of B.
+            return fidelity(B, A)
+        # If we made it here, both A and B are operators, so
+        # we have to take the sqrtm of one of them.
+        sqrtmA = A.sqrtm()
 
-    if A.dims != B.dims:
+    if sqrtmA.dims != B.dims:
         raise TypeError('Density matrices do not have same dimensions.')
 
-    A = A.sqrtm()
-    return float(np.real((A * (B * A)).sqrtm().tr()))
+    # We don't actually need the whole matrix here, just the trace
+    # of its square root, so let's just get its eigenenergies instead.
+    # We also truncate negative eigenvalues to avoid nan propagation;
+    # even for positive semidefinite matrices, small negative eigenvalues
+    # can be reported.
+    eig_vals = (sqrtmA * B * sqrtmA).eigenenergies()
+    return float(np.real(np.sqrt(eig_vals[eig_vals > 0]).sum()))
 
 
 def process_fidelity(U1, U2, normalize=True):