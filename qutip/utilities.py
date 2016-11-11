2b3fe2f55487a57a2657940d80f77c2233fed21f
Paul Nation <nonhermitian@gmail.com>
2016-11-10 22:00:25 -0700

    BUG: Check if libraries in BLAS keys (#558)

--- qutip/utilities.py
+++ qutip/utilities.py
@@ -401,10 +401,13 @@ def _version2int(version_string):
 def _blas_info():
     config = np.__config__
     blas_info = config.blas_opt_info
+    _has_lib_key = 'libraries' in blas_info.keys()
     blas = None
-    if hasattr(config,'mkl_info') or any('mkl' in lib for lib in blas_info['libraries']):
+    if hasattr(config,'mkl_info') or \
+            (_has_lib_key and any('mkl' in lib for lib in blas_info['libraries'])):
         blas = 'INTEL MKL'
-    elif hasattr(config,'openblas_info') or any('openblas' in lib for lib in blas_info['libraries']):
+    elif hasattr(config,'openblas_info') or \
+            (_has_lib_key and any('openblas' in lib for lib in blas_info['libraries'])):
         blas = 'OPENBLAS'
     elif 'extra_link_args' in blas_info.keys() and ('-Wl,Accelerate' in blas_info['extra_link_args']):
         blas = 'Accelerate'
