30ad8666977b7de36a8f4a122ed179f254d613c1
Paul Nation <nonhermitian@gmail.com>
2016-12-05 22:00:57 -0700

    remove pyx files from control package data

--- setup.py
+++ setup.py
@@ -92,8 +92,7 @@ PACKAGES = ['qutip', 'qutip/ui', 'qutip/cy', 'qutip/qip', 'qutip/qip/models',
 PACKAGE_DATA = {
     'qutip': ['configspec.ini'],
     'qutip/tests': ['bucky.npy', 'bucky_perm.npy'],
-    'qutip/cy': ['*.pxi', '*.pxd'],
-    'qutip/control': ['*.pyx']
+    'qutip/cy': ['*.pxi', '*.pxd']
 }
 # If we're missing numpy, exclude import directories until we can
 # figure them out properly.
