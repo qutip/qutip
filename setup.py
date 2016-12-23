557d97bc158759d0fb60ac35ba2ae1683d26cfb8
Paul Nation <nonhermitian@gmail.com>
2016-12-23 16:14:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ except ImportError:
 MAJOR = 4
 MINOR = 0
 MICRO = 0
-ISRELEASED = False
+ISRELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 REQUIRES = ['numpy (>=1.8)', 'scipy (>=0.15)', 'cython (>=0.21)']
 INSTALL_REQUIRES = ['numpy>=1.8', 'scipy>=0.15', 'cython>=0.21']
