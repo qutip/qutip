95a56b4586c9313807a3dedee131c8d730f49c0b
Paul Nation <nonhermitian@gmail.com>
2016-12-30 20:52:36 -0700

    Remove unneeded import in fastsparse (#596)

--- qutip/fastsparse.py
+++ qutip/fastsparse.py
@@ -35,8 +35,7 @@ import operator
 from scipy.sparse import (_sparsetools, isspmatrix, isspmatrix_csr,
                           csr_matrix, coo_matrix, csc_matrix, dia_matrix)
 from scipy.sparse.sputils import (upcast, upcast_char, to_native, isdense, isshape,
-                      getdtype, isscalarlike, IndexMixin, get_index_dtype,
-                      downcast_intp_index, get_sum_dtype)
+                      getdtype, isscalarlike, IndexMixin, get_index_dtype)
 from scipy.sparse.base import spmatrix, isspmatrix, SparseEfficiencyWarning
 from warnings import warn
 
