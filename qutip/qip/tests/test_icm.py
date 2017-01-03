dcea5ef1881a5533276f5381a66af274619ff5fc
Shahnawaz Ahmed <shahnawaz.ahmed95@gmail.com>
2017-01-04 05:24:31 +0900

    Tests for ancilla cost calculation

--- qutip/qip/tests/test_icm.py
+++ qutip/qip/tests/test_icm.py
@@ -45,9 +45,29 @@ def test_decomposition():
     assert_(decomposed.gates[3].name == "RX")
     assert_(decomposed.gates[3].arg_label == r"\pi/2")
     assert_(decomposed.gates[3].arg_value == np.pi / 2)
-    
+
     for gate in decomposed_gates:
         assert_(gate in icm_gate_set)
+
+
+def test_ancilla_cost():
+    """
+    Test if ancilla cost calculation is correct.
+    correct.
+    """
+    qcirc = QubitCircuit(5, reverse_states=False)
+    qcirc.add_gate("TOFFOLI", controls=[0, 1], targets=[2])
+    qcirc.add_gate("SNOT", targets=[3])
+    qcirc.add_gate("RX", targets=[5], arg_value=np.pi, arg_label=r'\pi')
+    qcirc.add_gate("TOFFOLI", controls=[1, 2], targets=[0])
+    qcirc.add_gate("SNOT", targets=[2])
+
+    icm_model = Icm(qcirc)
+    ancilla = icm_model.ancilla_cost()
+
+    assert_(ancilla["TOFFOLI"] == 84)
+    assert_(ancilla["SNOT"] == 6)
+    assert_(ancilla["V"] == 2)
 
 if __name__ == "__main__":
     run_module_suite()
