404656bd4e46a110280fc72f6cf10faa20af18d2
Shahnawaz Ahmed <shahnawaz.ahmed95@gmail.com>
2017-01-03 20:20:37 +0900

    Fixed pep8 issues, ignore line break [W503]

--- qutip/qip/circuit.py
+++ qutip/qip/circuit.py
@@ -31,13 +31,14 @@
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 
-__all__ = ['Gate', 'QubitCircuit']
-
 import numpy as np
 import warnings
 
 from qutip.qip.circuit_latex import _latex_compile
 from qutip.qip.gates import *
+
+
+__all__ = ['Gate', 'QubitCircuit']
 
 
 class Gate(object):
@@ -253,7 +254,6 @@ class QubitCircuit(object):
             The circuit block to be added to the main circuit.
         start : Integer
             The qubit on which the first gate is applied.
-        
         """
 
         if self.N - start < len(qc.gates):
@@ -284,7 +284,7 @@ class QubitCircuit(object):
 
     def remove_gate(self, index=None, end=None, name=None, remove="first"):
         """
-        Removes a gate from a specific index or between two indexes or the 
+        Removes a gate from a specific index or between two indexes or the
         first, last or all instances of a particular gate.
 
         Parameters
@@ -295,7 +295,6 @@ class QubitCircuit(object):
             Gate name to be removed.
         remove : String
             If first or all gate are to be removed.
-        
         """
         if index is not None and index <= self.N:
             if end is not None and end <= self.N:
@@ -303,7 +302,7 @@ class QubitCircuit(object):
                     self.gates.pop(index + i)
             elif end is not None and end > self.N:
                 raise ValueError("End target exceeds number of gates.")
-            else:            
+            else:
                 self.gates.pop(index)
 
         elif name is not None and remove == "first":
@@ -335,7 +334,7 @@ class QubitCircuit(object):
         qc : QubitCircuit
             Returns QubitCircuit of resolved gates for the qubit circuit in the
             reverse order.
-        
+
         """
         temp = QubitCircuit(self.N, self.reverse_states)
 
@@ -360,7 +359,6 @@ class QubitCircuit(object):
         qc : QubitCircuit
             Returns QubitCircuit of resolved gates for the qubit circuit in the
             desired basis.
-        
         """
         qc_temp = QubitCircuit(self.N, self.reverse_states)
         temp_resolved = []
