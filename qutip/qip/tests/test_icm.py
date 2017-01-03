6802e86cd51d097113c8b05171bd7fa7a901534a
Shahnawaz Ahmed <shahnawaz.ahmed95@gmail.com>
2017-01-04 04:58:58 +0900

    Some tests for gate decomposition

--- qutip/qip/tests/test_icm.py
+++ qutip/qip/tests/test_icm.py
@@ -9,20 +9,45 @@ from qutip.qip.circuit import Gate
 from qutip.qip.icm import Icm
 from qutip import QubitCircuit
 
-def test_decomposition():
-	"""
-	Test if initial decomposition in terms of T, V, P and CNOT is
-	correct.
-	"""
-	qcirc = QubitCircuit(4, reverse_states=False)
-	qcirc.add_gate("TOFFOLI", controls=[1, 2], targets=[0])
-	model = Icm(qcirc)
-	decomposed = model.decompose_gates()
-	decomposed_gates = set([gate.name for gate in decomposed.gates])
 
-	icm_gate_set = ["CNOT", "T", "V", "P", "T_dagger", "V_dagger", "P_dagger"]
-	for gate in decomposed_gates:
-		assert_(gate in icm_gate_set)
+def test_decomposition():
+    """
+    Test if initial decomposition in terms of T, V, P and CNOT is
+    correct.
+    """
+    qcirc = QubitCircuit(5, reverse_states=False)
+    qcirc.add_gate("TOFFOLI", controls=[0, 1], targets=[2])
+    qcirc.add_gate("SNOT", targets=[3])
+    qcirc.add_gate("RX", targets=[5], arg_value=np.pi, arg_label=r'\pi')
+
+    icm_model = Icm(qcirc)
+    decomposed = icm_model.decompose_gates()
+
+    decomposed_gates = set([(gate.name, gate.arg_label)
+                            for gate in decomposed.gates])
+
+    icm_gate_set = [("CNOT", None),
+                    ("SNOT", None),
+                    ("TOFFOLI", None),
+                    ("RZ", r"\pi/2"),
+                    ("RZ", r"\pi/4"),
+                    ("RX", r"\pi/2"),
+                    ("RZ", r"-\pi/2"),
+                    ("RZ", r"-\pi/4"),
+                    ("RX", r"-\pi/2")]
+
+    assert_(decomposed.gates[0].name == "TOFFOLI")
+    assert_(decomposed.gates[1].name == "SNOT")
+    assert_(decomposed.gates[2].name == "RX")
+    assert_(decomposed.gates[2].arg_label == r"\pi/2")
+    assert_(decomposed.gates[2].arg_value == np.pi / 2)
+
+    assert_(decomposed.gates[3].name == "RX")
+    assert_(decomposed.gates[3].arg_label == r"\pi/2")
+    assert_(decomposed.gates[3].arg_value == np.pi / 2)
+    
+    for gate in decomposed_gates:
+        assert_(gate in icm_gate_set)
 
 if __name__ == "__main__":
     run_module_suite()
