93f1d2f04a92906b0bd4e14ac18be38b0d7a4cf2
Paul Nation <nonhermitian@gmail.com>
2017-03-02 21:53:04 -0700

    remove pesky pyx data

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ PACKAGE_DATA = {
     '.': ['README.md', 'LICENSE.txt'],
     'qutip': ['configspec.ini'],
     'qutip/tests': ['bucky.npy', 'bucky_perm.npy', '*.ini'],
-    'qutip/cy': ['*.pxi', '*.pxd', '*.pyx']
+    'qutip/cy': ['*.pxi', '*.pxd']
 }
 # If we're missing numpy, exclude import directories until we can
 # figure them out properly.
