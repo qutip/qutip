9bc626b1fcd24e1a9cde9b94ce87fb4d733cada3
Paul Nation <nonhermitian@gmail.com>
2017-03-09 23:19:36 -0700

    remove usual *.pyx

--- qutip/cy/utilities.py
+++ qutip/cy/utilities.py
@@ -36,6 +36,14 @@ def _cython_build_cleanup(tdname, build_dir=None):
     if build_dir is None:
         build_dir = os.path.join(os.path.expanduser('~'), '.pyxbld')
     
+    # Remove tdname.pyx
+    pyx_file = tdname + ".pyx"
+    try:
+        os.remove(pyx_file)
+    except:
+        pass
+    
+    # Remove temp build files
     for dirpath, subdirs, files in os.walk(build_dir):
         for f in files:
             if f.startswith(tdname):
