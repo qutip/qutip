ce077205e2e42af570c7544fe8a0a1bbf9bdc5b4
Sebastian Kraemer <basti.kr@gmail.com>
2017-04-08 00:37:11 +0200

    Improve analytic calculation of coherent states.
    Instead of calculating the alpha**n/sqrt(n!) for every entry, calculate
    it recursively by x_{n+1} = x_{n}*alpha/sqrt(n), thereby avoiding the
    explicit calculation of the factorial. This way it works also for
    dimensions>400 where it would fail before.

--- qutip/states.py
+++ qutip/states.py
@@ -123,11 +123,6 @@ def qutrit_basis():
     return np.array([basis(3, 0), basis(3, 1), basis(3, 2)], dtype=object)
 
 
-def _sqrt_factorial(n_vec):
-    # take the square root before multiplying
-    return np.array([np.prod(np.sqrt(np.arange(1, n + 1))) for n in n_vec])
-
-
 def coherent(N, alpha, offset=0, method='operator'):
     """Generates a coherent state with eigenvalue alpha.
 
@@ -188,9 +183,14 @@ def coherent(N, alpha, offset=0, method='operator'):
     elif method == "analytic" or offset > 0:
 
         data = np.zeros([N, 1], dtype=complex)
-        n = arange(N) + offset
-        data[:, 0] = np.exp(-(abs(alpha) ** 2) / 2.0) * (alpha ** (n)) / \
-            _sqrt_factorial(n)
+        if offset == 0:
+            data[0] = np.exp(-abs(alpha)**2 / 2.0)
+        else:
+            s = np.prod(np.sqrt(np.arange(1, offset + 1))) # sqrt factorial
+            data[0] = np.exp(-abs(alpha)**2 / 2.0) * alpha**(offset) / s
+        for n in range(N-1):
+            data[n+1] = data[n]*alpha/np.sqrt(offset + n + 1)
+
         return Qobj(data)
 
     else:
@@ -478,7 +478,7 @@ def projection(N, n, m, offset=0):
     -------
     oper : qobj
          Requested projection operator.
-    
+
     """
     ket1 = basis(N, n, offset=offset)
     ket2 = basis(N, m, offset=offset)
