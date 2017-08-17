95d61df05f014ac5ccd18873e4e501454f14c78c
Shahnawaz Ahmed <shahnawaz.ahmed95@gmail.com>
2017-08-17 19:55:41 +0900

    bump to 4.2.1

--- setup.py
+++ setup.py
@@ -57,8 +57,8 @@ from Cython.Distutils import build_ext
 # all information about QuTiP goes here
 MAJOR = 4
 MINOR = 2
-MICRO = 0
-ISRELEASED = True
+MICRO = 1
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 REQUIRES = ['numpy (>=1.8)', 'scipy (>=0.15)', 'cython (>=0.21)']
 INSTALL_REQUIRES = ['numpy>=1.8', 'scipy>=0.15', 'cython>=0.21']
