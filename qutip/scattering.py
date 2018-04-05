Reverted normalization fixes; changed way that probs is counted in scattering_probability()

qutip/scattering.py
@@ -49,8 +49,7 @@ __all__ = ['temporal_basis_vector',
            'scattering_probability']
 
 import numpy as np
-from math import factorial
-from itertools import product, combinations_with_replacement
+from itertools import product, combinations
 from qutip import propagator, Options, basis, tensor, zero_ket, Qobj
 
 
@@ -202,7 +201,7 @@ def photon_scattering_operator(evolver, c_ops, taus_list):
 
     # Add the <0|Uff(TP, tm)|0> term
     tmax = evolver.times[-1]
-    taumax = taus[-1][0]
+    taumax, _ = taus[-1]
     # if taus[-1] < tmax:
     omega = evolver.prop(tmax, taumax) * omega
 
@@ -306,8 +305,7 @@ def temporal_scattered_state(H, psi0, n_emissions, c_ops, tlist,
 
     evolver = Evolver(Heff, tlist)
 
-    indicesList = product(range(T), repeat = n_emissions)
-    # indicesList = combinations_with_replacement(range(T), n_emissions)
+    indicesList = combinations(range(T), n_emissions)
 
     if system_zero_state is None:
         system_zero_state = psi0
@@ -320,8 +318,7 @@ def temporal_scattered_state(H, psi0, n_emissions, c_ops, tlist,
         for partitioned_taus, partitioned_indices in \
                 zip(set_partition(taus, W), set_partition(indices, W)):
             omega = photon_scattering_operator(evolver, c_ops, partitioned_taus)
-            phi_n_amp = system_zero_state.dag() * omega * psi0 / \
-                        np.sqrt(factorial(n_emissions))
+            phi_n_amp = system_zero_state.dag() * omega * psi0
             # Add scatter amplitude times temporal basis to overall state
             phi_n += phi_n_amp * temporal_basis_vector(partitioned_indices, T)
 
@@ -375,23 +372,18 @@ def scattering_probability(H, psi0, n_emissions, c_ops, tlist,
                                      construct_effective_hamiltonian)
     T = len(tlist)
     W = len(c_ops)
-    amplitudes = np.zeros([T] * n_emissions, dtype = np.complex64)
 
     # Compute <omega_tau> for all combinations of tau
-    # indicesList = combinations_with_replacement(range(T), n_emissions)
-    indicesList = product(range(T), repeat = n_emissions)
+    indicesList = combinations(range(T), n_emissions)
+    probs = np.zeros([T] * n_emissions)
 
     # Project scattered state onto temporal basis
     for indices in indicesList:
         wg_indices_list = list(set_partition(indices, W))
-        sum_wg_projectors = sum([temporal_basis_vector(wg_indices, T)
-                                 for wg_indices in wg_indices_list])
-        amplitudes[indices] = (sum_wg_projectors.dag() * phi_n).full().item() / \
-            np.sqrt(len(wg_indices_list))
-
-    # Conjugate amplitudes and normalize for double-counting to get probability
-    probs = amplitudes.conj() * amplitudes# / \
-            # (factorial(n_emissions) * (W ** n_emissions))
+        for wg_indices in wg_indices_list:
+            projector = temporal_basis_vector(wg_indices, T)
+            amplitude = (projector.dag() * phi_n).full().item()
+            probs[indices] += np.real(amplitude.conjugate() * amplitude)
 
     # Iteratively integrate to obtain single value
     while probs.shape != ():