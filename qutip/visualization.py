ea9fd805b67a9214622e8245794ffcc01344dab0
Nithin Ramu <ramu.nithin@gmail.com>
2019-05-01 16:57:30 +0200

    added 'plot_wigner_sphere' to __all__.

--- qutip/visualization.py
+++ qutip/visualization.py
@@ -42,7 +42,7 @@ __all__ = ['hinton', 'sphereplot', 'energy_level_diagram',
            'plot_expectation_values', 'plot_spin_distribution_2d',
            'plot_spin_distribution_3d', 'plot_qubism', 'plot_schmidt',
            'complex_array_to_rgb', 'matrix_histogram',
-           'matrix_histogram_complex', 'sphereplot']
+           'matrix_histogram_complex', 'sphereplot', 'plot_wigner_sphere']
 
 import warnings
 import itertools as it
