443c5d769ee11c12701f43331d87747ad6062a87
Boxi Li <etamin1201@gmail.com>
2019-06-03 21:54:08 +0200

    fix deprecated Iterable import

--- qutip/control/optimizer.py
+++ qutip/control/optimizer.py
@@ -1067,7 +1067,7 @@ class OptimizerCrab(Optimizer):
         if self.pulse_generator is None:
             pulse_gen_valid = False
             err_msg = "pulse_generator attribute is None"
-        elif not isinstance(self.pulse_generator, collections.Iterable):
+        elif not isinstance(self.pulse_generator, collections.abc.Iterable):
             pulse_gen_valid = False
             err_msg = "pulse_generator is not iterable"
 
