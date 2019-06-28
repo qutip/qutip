90b95d40ddd2d5c16a66503f4332e8aefeeed75c
Ericgig <eric.giguere@calculquebec.ca>
2019-06-28 15:29:40 -0400

    version number reset

--- setup.py
+++ setup.py
@@ -57,9 +57,9 @@ from Cython.Distutils import build_ext
 # all information about QuTiP goes here
 MAJOR = 4
 MINOR = 4
-MICRO = 3
+MICRO = 0
 ISRELEASED = True
-VERSION = '%d.%d.post%d' % (MAJOR, MINOR, MICRO)
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 REQUIRES = ['numpy (>=1.12)', 'scipy (>=1.0)', 'cython (>=0.21)']
 EXTRAS_REQUIRE = {'graphics':['matplotlib(>=1.2.1)']}
 INSTALL_REQUIRES = ['numpy>=1.12', 'scipy>=1.0', 'cython>=0.21']
