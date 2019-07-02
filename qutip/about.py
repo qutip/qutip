e40115a06bce87e0cf9d687894a49ff576a767c7
Eric Giguère <eric.giguere@calculquebec.ca>
2019-07-02 09:41:35 -0400

    Typo in Eric

--- qutip/about.py
+++ qutip/about.py
@@ -58,7 +58,7 @@ def about():
     print("Original developers: R. J. Johansson & P. D. Nation.")
     print("Current admin team: Alexander Pitchford, Paul D. Nation, "
             "Nathan Shammah, Shahnawaz Ahmed, "
-            "Neill Lambert, and Éric Giguère.")
+            "Neill Lambert, and Eric Giguère.")
     print("Project Manager: Franco Nori.")
     print("Currently developed through wide collaboration. "
           "See https://github.com/qutip for details.")
