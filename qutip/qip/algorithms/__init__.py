ccd5bc781ab3fd131e779809358a0cf8ef7e6268
Boxi Li <etamin1201@gmail.com>
2019-08-13 13:08:10 +0200

    PR does not show the most recent update?

--- qutip/qip/algorithms/__init__.py
+++ qutip/qip/algorithms/__init__.py
@@ -30,3 +30,4 @@
 #    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
+from qutip.qip.algorithms.qft import qft, qft_steps, qft_gate_sequence
