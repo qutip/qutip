fdedbf38327056b6b7c415f7f30356e7d9393a86
Boxi Li <etamin1201@gmail.com>
2019-08-14 23:58:31 +0200

    wrapper for the qip deprecation warning

--- qutip/__init__.py
+++ qutip/__init__.py
@@ -220,8 +220,67 @@ from qutip.nonmarkov import *
 from qutip.interpolate import *
 from qutip.scattering import *
 
+########################################################################
 # quantum information
-from qutip.qip import *
+# from qutip.qip import *
+
+# This section exists only for the deprecation of
+# importing members of the qip submodule directly from the qutip namespace.
+
+import inspect
+from functools import wraps as _func_wrap
+import qutip.qip as _temp_qip
+# Deprecation warning is by default ignored if not in the main workspace,
+# this forces it to show up. 
+warnings.simplefilter('once', DeprecationWarning)
+
+def _qip_importation_warning():
+    warnings.warn(
+        "Importing functions/classes of the qip submodule directly from "
+        "the namespace qutip is deprecated. "
+        "Please import them from the submodule instead, e.g. "
+        "qutip.qip import `func_name`.", DeprecationWarning, stacklevel=3)
+    # stacklevel makes the warning refer to the original caller
+    # instead of the wrapper.
+
+def _qip_func_wrapper(func):
+    """Function wrapper for adding a deprecation warning."""
+    @_func_wrap(func)
+    def deprecated_func(*args, **kwargs):
+        _qip_importation_warning()
+        return func(*args, **kwargs)
+    return deprecated_func
+
+#  Wrap all qip functions with a deprecation warning
+_func_pairs = inspect.getmembers(_temp_qip, inspect.isfunction)
+for _name, _func in _func_pairs:
+    locals()[_name] = _qip_func_wrapper(_func)
+del _name, _func, _func_pairs, _qip_func_wrapper
+
+def _qip_class_wrapper(original_cls):
+    """Class wrapper for adding a deprecation warning."""
+    class Deprecated_cls(original_cls):
+        def __init__(self, *args, **kwargs):
+            _qip_importation_warning()
+            super(Deprecated_cls, self).__init__(*args, **kwargs)
+    # copy information form the original class, similar to functools.wraps
+    for attr in ('__module__', '__name__', '__qualname__', '__doc__'):
+        try:
+            value = getattr(original_cls, attr)
+        except AttributeError:
+            pass
+        else:
+            setattr(Deprecated_cls, attr, value)
+    return Deprecated_cls
+
+#  Wrap all qip classes with a deprecation warning
+_cls_pairs = inspect.getmembers(_temp_qip, inspect.isclass)
+for _name, _cls in _cls_pairs:
+    locals()[_name] = _qip_class_wrapper(_cls)
+del _name, _cls, _cls_pairs, _qip_class_wrapper
+
+del _temp_qip
+########################################################################
 
 # utilities
 from qutip.parallel import *
