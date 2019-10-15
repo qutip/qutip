22e99bf0962068df798b5436150a1d5aee6d4156
tehruhn <trolldemort9@gmail.com>
2019-10-15 11:31:13 +0900

    changed docstring to be in same order as args

--- qutip/bloch.py
+++ qutip/bloch.py
@@ -661,12 +661,12 @@ class Bloch():
             Name of saved image. Must include path and format as well.
             i.e. '/Users/Paul/Desktop/bloch.png'
             This overrides the 'format' and 'dirc' arguments.
-        dpin : int
-            Resolution in dots per inch.
         format : str
             Format of output image.
         dirc : str
             Directory for output images. Defaults to current working directory.
+        dpin : int
+            Resolution in dots per inch.
 
         Returns
         -------
