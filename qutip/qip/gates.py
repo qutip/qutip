01fa4a4c5492b0cbd72d2ca880b3531b864f61a1
Boxi Li <etamin1201@gmail.com>
2020-01-23 21:07:07 +0100

    fix a typo

--- qutip/qip/gates.py
+++ qutip/qip/gates.py
@@ -35,5 +35,5 @@ import warnings
 from qutip.qip.operations.gates import *
 warnings.warn(
     "Importation from qutip.qip.gates is deprecated."
-    "Please use e.g.\n from qutip.qip.operations import cnot",
-    DeprecationWarning, stacklevel=1)
+    "Please use e.g.\n from qutip.qip.operations import cnot\n",
+    DeprecationWarning, stacklevel=2)
