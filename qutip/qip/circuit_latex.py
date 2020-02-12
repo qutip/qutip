75119a86e1a18a08e6feab4d839ae32c5fbad903
Jihwan Kim <49479931+JiihwanKim@users.noreply.github.com>
2020-02-12 23:17:35 +0900

    fix for codeclimate
    fix for codeclimate

--- qutip/qip/circuit_latex.py
+++ qutip/qip/circuit_latex.py
@@ -58,8 +58,8 @@ def _latex_compile(code, filename="qcirc", format="png"):
     os.system("mv %s-tmp.pdf %s.pdf" % (filename, filename))
 
     if format == 'png':
-        os.system("magick convert -density %s %s.pdf %s.png"
-                      % (100, filename, filename))
+        os.system("magick convert -density %s %s.pdf %s.png" % (100,
+                                               filename, filename))
         with open("%s.png" % filename, "rb") as f:
             result = f.read()
     else:
