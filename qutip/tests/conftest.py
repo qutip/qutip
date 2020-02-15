9bf552a4a86d13907f7740a2739bd675a87e3878
Jake Lishman <jakelishman@gmail.com>
2020-02-15 16:55:51 +0000

    Add repeat marker
    This allows the explicit marking of a test with a number of repeats.
    This is typically only useful for tests which randomise their testing
    input.
    
    The repetition is achieved by 'paramtrisation' of a hidden parameter.

--- qutip/tests/conftest.py
+++ qutip/tests/conftest.py
@@ -31,67 +31,19 @@
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 
-import pytest
-import numpy as np
-import scipy.sparse
-import qutip
-from qutip.fastsparse import fast_csr_matrix
-from qutip.cy.checks import (_test_sorting, _test_coo2csr_inplace_struct,
-                             _test_csr2coo_struct, _test_coo2csr_struct)
-from qutip.random_objects import rand_jacobi_rotation
+def _add_repeats_if_marked(metafunc):
+    """
+    If the metafunc is marked with the 'repeat' mark, then add the requisite
+    number of repeats via parametrisation.
+    """
+    marker = metafunc.definition.get_closest_marker('repeat')
+    if marker:
+        count = marker.args[0]
+        metafunc.fixturenames.append('_repeat_count')
+        metafunc.parametrize('_repeat_count',
+                             range(count),
+                             ids=["rep({})".format(x+1) for x in range(count)])
 
 
-def _unsorted_csr(N, density=0.5):
-    M = scipy.sparse.diags(np.arange(N), 0, dtype=complex, format='csr')
-    nvals = N**2 * density
-    while M.nnz < 0.95*nvals:
-        M = rand_jacobi_rotation(M)
-    M = M.tocsr()
-    return fast_csr_matrix((M.data, M.indices, M.indptr), shape=M.shape)
-
-
-def sparse_arrays_equal(a, b):
-    return not (a != b).data.any()
-
-
-@pytest.mark.repeat(20)
-def test_coo2csr_struct():
-    "Cython structs : COO to CSR"
-    A = qutip.rand_dm(5, 0.5).data
-    assert sparse_arrays_equal(A, _test_coo2csr_struct(A.tocoo()))
-
-
-@pytest.mark.repeat(20)
-def test_indices_sort():
-    "Cython structs : sort CSR indices inplace"
-    A = _unsorted_csr(10, 0.25)
-    B = A.copy()
-    B.sort_indices()
-    _test_sorting(A)
-    assert np.all(A.data == B.data)
-    assert np.all(A.indices == B.indices)
-
-
-@pytest.mark.repeat(20)
-def test_coo2csr_inplace_nosort():
-    "Cython structs : COO to CSR inplace (no sort)"
-    A = qutip.rand_dm(5, 0.5).data
-    B = _test_coo2csr_inplace_struct(A.tocoo(), sorted=0)
-    assert sparse_arrays_equal(A, B)
-
-
-@pytest.mark.repeat(20)
-def test_coo2csr_inplace_sort():
-    "Cython structs : COO to CSR inplace (sorted)"
-    A = qutip.rand_dm(5,0.5).data
-    B = _test_coo2csr_inplace_struct(A.tocoo(), sorted=1)
-    assert sparse_arrays_equal(A, B)
-
-
-@pytest.mark.repeat(20)
-def test_csr2coo():
-    "Cython structs : CSR to COO"
-    A = qutip.rand_dm(5, 0.5).data
-    B = A.tocoo()
-    C = _test_csr2coo_struct(A)
-    assert sparse_arrays_equal(B, C)
+def pytest_generate_tests(metafunc):
+    _add_repeats_if_marked(metafunc)
