d0f06c576b479bd5aac56b79b649da05f20c8d7f
Boxi Li <etamin1201@gmail.com>
2020-06-13 17:07:14 +0200

    use deepcopy in the test

--- qutip/tests/test_scheduler.py
+++ qutip/tests/test_scheduler.py
@@ -39,65 +39,183 @@ from qutip.qip.operations.gates import gate_sequence_product
 from qutip import process_fidelity, qeye, tracedist
 
 
-circuit1 = QubitCircuit(7)
-circuit1.add_gate("SNOT", 3)
-circuit1.add_gate("CZ", 5, 3)
-circuit1.add_gate("CZ", 4, 3)
-circuit1.add_gate("CZ", 2, 3)
-circuit1.add_gate("CZ", 6, 5)
-circuit1.add_gate("CZ", 2, 6)
-circuit1.add_gate("SWAP", [0, 2])
+def _circuit1():
+    circuit1 = QubitCircuit(6)
+    circuit1.add_gate("SNOT", 2)
+    circuit1.add_gate("CNOT", 4, 2)
+    circuit1.add_gate("CNOT", 3, 2)
+    circuit1.add_gate("CNOT", 1, 2)
+    circuit1.add_gate("CNOT", 5, 4)
+    circuit1.add_gate("CNOT", 1, 5)
+    circuit1.add_gate("SWAP", [0, 1])
+    return circuit1
 
-circuit2 = QubitCircuit(8)
-circuit2.add_gate("SNOT", 1)
-circuit2.add_gate("SNOT", 2)
-circuit2.add_gate("SNOT", 3)
-circuit2.add_gate("CNOT", 4, 5)
-circuit2.add_gate("CNOT", 4, 6)
-circuit2.add_gate("CNOT", 3, 4)
-circuit2.add_gate("CNOT", 3, 5)
-circuit2.add_gate("CNOT", 3, 7)
-circuit2.add_gate("CNOT", 2, 4)
-circuit2.add_gate("CNOT", 2, 6)
-circuit2.add_gate("CNOT", 2, 7)
-circuit2.add_gate("CNOT", 1, 5)
-circuit2.add_gate("CNOT", 1, 6)
-circuit2.add_gate("CNOT", 1, 7)
+def _circuit2():
+    circuit2 = QubitCircuit(8)
+    circuit2.add_gate("SNOT", 1)
+    circuit2.add_gate("SNOT", 2)
+    circuit2.add_gate("SNOT", 3)
+    circuit2.add_gate("CNOT", 4, 5)
+    circuit2.add_gate("CNOT", 4, 6)
+    circuit2.add_gate("CNOT", 3, 4)
+    circuit2.add_gate("CNOT", 3, 5)
+    circuit2.add_gate("CNOT", 3, 7)
+    circuit2.add_gate("CNOT", 2, 4)
+    circuit2.add_gate("CNOT", 2, 6)
+    circuit2.add_gate("CNOT", 2, 7)
+    circuit2.add_gate("CNOT", 1, 5)
+    circuit2.add_gate("CNOT", 1, 6)
+    circuit2.add_gate("CNOT", 1, 7)
+    return circuit2
 
-circuit3 = QubitCircuit(4)
-instruction_list1 = [
-    Instruction("X", [0], duration=1),
-    Instruction("X", [1], duration=1),
-    Instruction("CNOT", [2], [3], duration=2),
-    Instruction("CNOT", [1], [2], duration=2),
-    Instruction("CNOT", [1], [0], duration=2),
-    Instruction("X", [3], duration=1),
-    Instruction("CNOT", [1], [3], duration=2),
-    Instruction("CNOT", [1], [3], duration=2)
-]
-circuit3.gates = instruction_list1
+def _circuit3():
+    circuit3 = QubitCircuit(4)
+    instruction_list1 = [
+        Instruction("SNOT", [0], duration=1),
+        Instruction("SNOT", [1], duration=1),
+        Instruction("CNOT", [2], [3], duration=2),
+        Instruction("CNOT", [1], [2], duration=2),
+        Instruction("CNOT", [1], [0], duration=2),
+        Instruction("SNOT", [3], duration=1),
+        Instruction("CNOT", [1], [3], duration=2),
+        Instruction("CNOT", [1], [3], duration=2)
+    ]
+    circuit3.gates = instruction_list1
+    return circuit3
 
 
 @pytest.mark.parametrize(
     "circuit, method, expected_length, random_shuffle, gates_schedule",
     [
-        (circuit1, "ASAP", 4, False, False),
-        (circuit1, "ALAP", 4, False, False),
-        (circuit2, "ASAP", 4, False, False),
-        (circuit2, "ALAP", 5, False, False),
-        # (circuit2, "ALAP", 4, True,  False),  # with random shuffling
-        (circuit3, "ASAP", 7, False, False),
-        (circuit3, "ALAP", 7, False, False),
-        (circuit3, "ASAP", 4, False, True),  # treat instructions as gates
-        (circuit3, "ALAP", 4, False, True),  # treat instructions as gates
+        (deepcopy(_circuit1()), "ASAP", 4, False, False),
+        (deepcopy(_circuit1()), "ALAP", 4, False, False),
     ])
-def test_scheduling_gates(
+def test_scheduling_gates1(
         circuit, method, expected_length, random_shuffle, gates_schedule):
     if random_shuffle:
         repeat_num = 5
     else:
         repeat_num = 0
-    circuit = deepcopy(circuit)
+    result0 = gate_sequence_product(circuit.propagators())
+
+    # run the scheduler
+    scheduler = Scheduler(method)
+    gate_cycle_indices = scheduler.schedule(
+        circuit, gates_schedule=gates_schedule, repeat_num=repeat_num)
+
+    # check if the scheduled length is expected
+    assert(max(gate_cycle_indices) == expected_length)
+    scheduled_gate = [[] for i in range(max(gate_cycle_indices)+1)]
+
+    # check if the scheduled circuit is correct
+    for i, cycles in enumerate(gate_cycle_indices):
+        scheduled_gate[cycles].append(circuit.gates[i])
+    circuit.gates = sum(scheduled_gate, [])
+    result1 = gate_sequence_product(circuit.propagators())
+    assert(tracedist(result0*result1.dag(), qeye(result0.dims[0])) < 1.0e-7)
+
+
+@pytest.mark.parametrize(
+    "circuit, method, expected_length, random_shuffle, gates_schedule",
+    [
+        (deepcopy(_circuit2()), "ASAP", 4, False, False),
+    ])
+def test_scheduling_gates2(
+        circuit, method, expected_length, random_shuffle, gates_schedule):
+    if random_shuffle:
+        repeat_num = 5
+    else:
+        repeat_num = 0
+    result0 = gate_sequence_product(circuit.propagators())
+
+    # run the scheduler
+    scheduler = Scheduler(method)
+    gate_cycle_indices = scheduler.schedule(
+        circuit, gates_schedule=gates_schedule, repeat_num=repeat_num)
+
+    # check if the scheduled length is expected
+    assert(max(gate_cycle_indices) == expected_length)
+    scheduled_gate = [[] for i in range(max(gate_cycle_indices)+1)]
+
+    # check if the scheduled circuit is correct
+    for i, cycles in enumerate(gate_cycle_indices):
+        scheduled_gate[cycles].append(circuit.gates[i])
+    circuit.gates = sum(scheduled_gate, [])
+    result1 = gate_sequence_product(circuit.propagators())
+    assert(tracedist(result0*result1.dag(), qeye(result0.dims[0])) < 1.0e-7)
+
+@pytest.mark.parametrize(
+    "circuit, method, expected_length, random_shuffle, gates_schedule",
+    [
+        (deepcopy(_circuit2()), "ALAP", 5, False, False),
+    ])
+def test_scheduling_gates2(
+        circuit, method, expected_length, random_shuffle, gates_schedule):
+    if random_shuffle:
+        repeat_num = 5
+    else:
+        repeat_num = 0
+    result0 = gate_sequence_product(circuit.propagators())
+
+    # run the scheduler
+    scheduler = Scheduler(method)
+    gate_cycle_indices = scheduler.schedule(
+        circuit, gates_schedule=gates_schedule, repeat_num=repeat_num)
+
+    # check if the scheduled length is expected
+    assert(max(gate_cycle_indices) == expected_length)
+    scheduled_gate = [[] for i in range(max(gate_cycle_indices)+1)]
+
+    # check if the scheduled circuit is correct
+    for i, cycles in enumerate(gate_cycle_indices):
+        scheduled_gate[cycles].append(circuit.gates[i])
+    circuit.gates = sum(scheduled_gate, [])
+    result1 = gate_sequence_product(circuit.propagators())
+    assert(tracedist(result0*result1.dag(), qeye(result0.dims[0])) < 1.0e-7)
+
+@pytest.mark.parametrize(
+    "circuit, method, expected_length, random_shuffle, gates_schedule",
+    [
+        (deepcopy(_circuit2()), "ALAP", 4, True,  False),  # with random shuffling
+    ])
+def test_scheduling_gates2(
+        circuit, method, expected_length, random_shuffle, gates_schedule):
+    if random_shuffle:
+        repeat_num = 5
+    else:
+        repeat_num = 0
+    result0 = gate_sequence_product(circuit.propagators())
+
+    # run the scheduler
+    scheduler = Scheduler(method)
+    gate_cycle_indices = scheduler.schedule(
+        circuit, gates_schedule=gates_schedule, repeat_num=repeat_num)
+
+    # check if the scheduled length is expected
+    assert(max(gate_cycle_indices) == expected_length)
+    scheduled_gate = [[] for i in range(max(gate_cycle_indices)+1)]
+
+    # check if the scheduled circuit is correct
+    for i, cycles in enumerate(gate_cycle_indices):
+        scheduled_gate[cycles].append(circuit.gates[i])
+    circuit.gates = sum(scheduled_gate, [])
+    result1 = gate_sequence_product(circuit.propagators())
+    assert(tracedist(result0*result1.dag(), qeye(result0.dims[0])) < 1.0e-7)
+
+@pytest.mark.parametrize(
+    "circuit, method, expected_length, random_shuffle, gates_schedule",
+    [
+        (deepcopy(_circuit3()), "ASAP", 7, False, False),
+        (deepcopy(_circuit3()), "ALAP", 7, False, False),
+        (deepcopy(_circuit3()), "ASAP", 4, False, True),  # treat instructions as gates
+        (deepcopy(_circuit3()), "ALAP", 4, False, True),  # treat instructions as gates
+    ])
+def test_scheduling_gates3(
+        circuit, method, expected_length, random_shuffle, gates_schedule):
+    if random_shuffle:
+        repeat_num = 5
+    else:
+        repeat_num = 0
     result0 = gate_sequence_product(circuit.propagators())
 
     # run the scheduler
