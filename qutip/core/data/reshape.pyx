cc9f33c768f0e4c2d4eca958631b43e600963171
Jake Lishman <jake@binhbar.com>
2020-07-02 17:31:24 +0100

    Speed up reshape_csr
    Remove the branching within the for-loop, since branch prediction would
    not be especially effective on it.  It's faster just to do the quotient
    and remainder, and then cumsum the row indices at the end, so there's no
    branching at any point once the calculation starts properly.
    
    This really makes things about ~2x faster for very sparse matrices being
    reshaped to have many columns.
    
    It's slightly faster to use libc.stdlib.div for very dense matrices
    (~10% speedup for a full 100x100 matrix) as opposed to the standard //
    and %.

--- qutip/core/data/reshape.pyx
+++ qutip/core/data/reshape.pyx
@@ -1,7 +1,8 @@
 #cython: language_level=3
 #cython: boundscheck=False, wraparound=False, initializedcheck=False
 
-from libc.string cimport memcpy
+from libc.stdlib cimport div, div_t
+from libc.string cimport memcpy, memset
 
 cimport cython
 
@@ -14,6 +15,7 @@ cpdef CSR reshape_csr(CSR matrix, idxint n_rows_out, idxint n_cols_out):
     cdef size_t ptr, row_in, row_out=0, loc, cur=0
     cdef size_t n_rows_in=matrix.shape[0], n_cols_in=matrix.shape[1]
     cdef idxint nnz = csr.nnz(matrix)
+    cdef div_t res
     cdef CSR out
     if n_rows_out * n_cols_out != n_rows_in * n_cols_in:
         message = "".join([
@@ -28,16 +30,17 @@ cpdef CSR reshape_csr(CSR matrix, idxint n_rows_out, idxint n_cols_out):
         csr.sort_indices(matrix)
         # Since the indices are now sorted, the data arrays will be identical.
         memcpy(&out.data[0], &matrix.data[0], nnz*sizeof(double complex))
-        out.row_index[0] = out.row_index[1] = 0
+        memset(&out.row_index[0], 0, (n_rows_out + 1) * sizeof(idxint))
         for row_in in range(n_rows_in):
             for ptr in range(matrix.row_index[row_in], matrix.row_index[row_in+1]):
                 loc = cur + matrix.col_index[ptr]
-                while loc >= (row_out + 1)*n_cols_out:
-                    row_out += 1
-                    out.row_index[row_out + 1] = out.row_index[row_out]
-                out.col_index[ptr] = loc % n_cols_out
-                out.row_index[row_out + 1] += 1
+                # This stdlib.div method is a little bit faster when working
+                # with very dense large matrices, and doesn't make a difference
+                # for smaller ones.
+                res = div(loc, n_cols_out)
+                out.row_index[res.quot + 1] += 1
+                out.col_index[ptr] = res.rem
             cur += n_cols_in
-        for row_out in range(row_out, n_rows_out + 1):
-            out.row_index[row_out + 1] = nnz
+        for row_out in range(n_rows_out + 1):
+            out.row_index[row_out + 1] += out.row_index[row_out]
     return out
