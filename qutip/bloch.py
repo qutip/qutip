dab617c5534fde88ba4a922a1a1aa2855cdaae59
rajathshetty20 <rajathshetty20@gmail.com>
2020-07-09 13:36:19 +0530

    added colors option to Bloch.add_vectors

--- qutip/bloch.py
+++ qutip/bloch.py
@@ -173,6 +173,8 @@ class Bloch():
         # ---vector options---
         # List of colors for Bloch vectors, default = ['b','g','r','y']
         self.vector_color = ['g', '#CC6600', 'b', 'r']
+        # Dictionary of vectors whose color is specified, where the vector-index and color are key-value pair 
+        self.vector_color_specified = {}
         #: Width of Bloch vectors, default = 5
         self.vector_width = 3
         #: Style of Bloch vectors, default = '-|>' (or 'simple')
@@ -375,19 +377,27 @@ class Bloch():
             elif kind == 'point':
                 self.add_points(vec)
 
-    def add_vectors(self, vectors):
+    def add_vectors(self, vectors, colors=None):
         """Add a list of vectors to Bloch sphere.
 
         Parameters
         ----------
         vectors : array_like
             Array with vectors of unit length or smaller.
+        colors : array_like
+            Optional array with colors for the vectors.
 
         """
         if isinstance(vectors[0], (list, ndarray)):
-            for vec in vectors:
-                self.vectors.append(vec)
+            for k in range(len(vectors)):
+                if colors is not None:
+                    vector_index = len(self.vectors)
+                    self.vector_color_specified[vector_index] = colors[k] 
+                self.vectors.append(vectors[k])
         else:
+            if colors is not None:
+                vector_index = len(self.vectors)
+                self.vector_color_specified[vector_index] = colors
             self.vectors.append(vectors)
 
     def add_annotation(self, state_or_vector, text, **kwargs):
@@ -560,7 +570,7 @@ class Bloch():
             ys3d = -self.vectors[k][0] * array([0, 1])
             zs3d = self.vectors[k][2] * array([0, 1])
 
-            color = self.vector_color[mod(k, len(self.vector_color))]
+            color = self.vector_color_specified.get(k, self.vector_color[mod(k, len(self.vector_color))])
 
             if self.vector_style == '':
                 # simple line style
