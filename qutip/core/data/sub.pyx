820543869f0a2ead28dd18473f1eba11eafa3aac
Jake Lishman <jake@binhbar.com>
2020-07-19 21:19:17 +0100

    Use add's scale argument to improve sub

--- qutip/core/data/sub.pyx
+++ qutip/core/data/sub.pyx
@@ -1,7 +1,7 @@
 #cython: language_level=3
 #cython: boundscheck=False, wrapround=False, initializedcheck=False
 
-from qutip.core.data cimport csr, CSR, neg_csr, add_csr
+from qutip.core.data cimport csr, CSR, add_csr
 
 cpdef CSR sub_csr(CSR left, CSR right):
-    return add_csr(left, neg_csr(right))
+    return add_csr(left, right, -1)
