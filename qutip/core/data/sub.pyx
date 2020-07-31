ee545bdf402a31dcc1dfb9a243e2aa1067c2f4cb
Jake Lishman <jake@binhbar.com>
2020-07-31 22:36:25 +0100

    Add scalar mul methods for Dense

--- qutip/core/data/sub.pyx
+++ qutip/core/data/sub.pyx
@@ -1,7 +1,10 @@
 #cython: language_level=3
 #cython: boundscheck=False, wrapround=False, initializedcheck=False
 
-from qutip.core.data cimport csr, CSR, add_csr
+from qutip.core.data cimport CSR, add_csr, Dense, add_dense
 
 cpdef CSR sub_csr(CSR left, CSR right):
     return add_csr(left, right, -1)
+
+cpdef Dense sub_dense(Dense left, Dense right):
+    return add_dense(left, right, -1)
