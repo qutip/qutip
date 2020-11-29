36cec630c6a880344492f10b1e16d57db88635d4
jakobjakobson13 <43045863+jakobjakobson13@users.noreply.github.com>
2020-11-29 01:00:33 +0100

    remove obsolete imports

--- qutip/qip/noise.py
+++ qutip/qip/noise.py
@@ -2,13 +2,10 @@ import numbers
 from collections.abc import Iterable
 from copy import deepcopy
 import numpy as np
-from numpy.random import normal
 
-from qutip.qobjevo import QobjEvo, EvoElement
-from qutip.qip.operations import expand_operator
+from qutip.qobjevo import QobjEvo
 from qutip.qobj import Qobj
-from qutip.operators import sigmaz, destroy, identity
-from qutip.tensor import tensor
+from qutip.operators import sigmaz, destroy
 from qutip.qip.pulse import Pulse
 
 
