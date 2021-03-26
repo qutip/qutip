5aa5ede202d4ef764413effb1247029051d7975e
Jake Lishman <jake@binhbar.com>
2021-03-26 16:36:08 +0000

    Make qutip.__init__ less forgiving of exceptions
    There were many cases in qutip.__init__ that ignored any exception
    thrown, and a couple of places where `try` statements covered large
    tracts of code that shouldn't have been allowed to fail.
    
    For example, the only part of the Cython import process that is
    acceptable to fail is the
        import Cython
    statement (if the user doesn't have Cython, which is an optional
    dependency).  If they _do_ have it, then any subsequent failure in
    setting up the pyximport mechanism should be treated as an unhandled
    exception.  From the user's side, they have fulfilled the requirement to
    use Cython, so we shouldn't mask failures setting up the Cython
    environment.  That makes it harder to debug issues for both us and the
    users.
    
    It is possible that this change will cause errors for previously working
    code.  However, any issues turned up are more likely to be caused by
    other factors, just we've been completely ignoring them up til now.

--- qutip/__init__.py
+++ qutip/__init__.py
@@ -35,57 +35,60 @@ import os
 import sys
 import warnings
 
-from .settings import settings
-from . import version
-from .version import version as __version__
-from .utilities import _version2int
+import qutip.settings
+import qutip.version
+from qutip.version import version as __version__
+
+# -----------------------------------------------------------------------------
+# Check if we're in IPython.
+try:
+    __IPYTHON__
+    qutip.settings.ipython = True
+except NameError:
+    qutip.settings.ipython = False
 
 
+# -----------------------------------------------------------------------------
+# Look to see if we are running with OPENMP
+#
+# Set environ variable to determin if running in parallel mode
+# (i.e. in parfor or parallel_map)
+os.environ['QUTIP_IN_PARALLEL'] = 'FALSE'
+
+try:
+    from qutip.cy.openmp.parfuncs import spmv_csr_openmp
+except ImportError:
+    qutip.settings.has_openmp = False
+else:
+    qutip.settings.has_openmp = True
+    # See Pull #652 for why this is here.
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+import platform
+from .utilities import _blas_info
+qutip.settings.eigh_unsafe = (_blas_info() == "OPENBLAS" and
+                              platform.system() == 'Darwin')
+del platform
 # -----------------------------------------------------------------------------
 # setup the cython environment
 #
-_cython_requirement = "0.21.0"
 try:
-    import Cython
-    import numpy
-    if _version2int(Cython.__version__) < _version2int(_cython_requirement):
-        print("QuTiP warning: old version of cython detected " +
-              ("(%s), requiring %s." %
-               (Cython.__version__, _cython_requirement)))
-    # Setup pyximport
-    from . import _pyxbuilder as pbldr
-    pbldr.install(setup_args={'include_dirs': [numpy.get_include()]})
-    del pbldr
-except Exception:
+    import Cython as _Cython
+except ImportError:
     pass
 else:
-    del Cython, numpy
+    from qutip.utilities import _version2int
+    _cy_require = "0.29.20"
+    if _version2int(_Cython.__version__) < _version2int(_cy_require):
+        warnings.warn(
+            "Old version of Cython detected: needed {}, got {}."
+            .format(_cy_require, _Cython.__version__)
+        )
+    # Setup pyximport
+    from qutip import _pyxbuilder
+    _pyxbuilder.install()
+    del _pyxbuilder, _Cython, _version2int
 
-
-# -----------------------------------------------------------------------------
-# cpu/process configuration
-#
-import multiprocessing
-
-"""
-# Check if environ flag for qutip processes is set
-if 'QUTIP_NUM_PROCESSES' in os.environ:
-    settings.num_cpus = int(os.environ['QUTIP_NUM_PROCESSES'])
-else:
-    os.environ['QUTIP_NUM_PROCESSES'] = str(settings.num_cpus)
-
-if settings.num_cpus == 0:
-    # if num_cpu is 0 set it to the available number of cores
-    from . import hardware_info
-    info = hardware_info.hardware_info()
-    if 'cpus' in info:
-        settings.num_cpus = info['cpus']
-    else:
-        try:
-            settings.num_cpus = multiprocessing.cpu_count()
-        except:
-            settings.num_cpus = 1
-"""
 
 
 # Find MKL library if it exists
@@ -100,7 +103,7 @@ from . import _mkl
 # Check for Matplotlib
 try:
     import matplotlib
-except:
+except ImportError:
     warnings.warn("matplotlib not found: Graphics will not work.")
 else:
     del matplotlib
@@ -164,4 +167,4 @@ if configrc.has_qutip_rc():
 # -----------------------------------------------------------------------------
 # Clean name space
 #
-del os, sys, multiprocessing, distutils, configrc, warnings
+del os, sys, multiprocessing, distutils, warnings
