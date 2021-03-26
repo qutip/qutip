a63ee0ae5381489c36b58309d1d2d38d8dba481a
Jake Lishman <jake@binhbar.com>
2021-03-26 17:10:29 +0000

    Remove unneeded imports
    We don't support Python 2, so the __future__ imports aren't necessary
    any more.  sys was imported by not used.

--- qutip/__init__.py
+++ qutip/__init__.py
@@ -30,9 +30,7 @@
 #    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
-from __future__ import division, print_function, absolute_import
 import os
-import sys
 import warnings
 
 import qutip.settings
@@ -160,4 +158,4 @@ if configrc.has_qutip_rc():
 # -----------------------------------------------------------------------------
 # Clean name space
 #
-del os, sys, multiprocessing, warnings
+del os, warnings
