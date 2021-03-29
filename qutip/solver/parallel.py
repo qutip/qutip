c65ed8e95fcb26a7046375db7e63a1700713ac07
Eric Giguère <eric.giguere@calculquebec.ca>
2021-03-29 14:03:14 -0400

    Update qutip/solver/parallel.py
    Co-authored-by: Simon Cross <hodgestar+github@gmail.com>

--- qutip/solver/parallel.py
+++ qutip/solver/parallel.py
@@ -32,7 +32,7 @@
 ###############################################################################
 """
 This function provides functions for parallel execution of loops and function
-mappings, using the builtin Python module multiprocessing.
+mappings, using the builtin Python module multiprocessing or the loky parallel execution library. 
 """
 __all__ = ['parallel_map', 'serial_map', 'loky_pmap', 'get_map']
 
