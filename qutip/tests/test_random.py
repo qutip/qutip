7449fefccb433e21a1e168c425269aa0ec07abe8
Purva Thakre <purva.athakre@gmail.com>
2021-05-03 14:44:38 -0500

    add function for dims to tensor shape

--- qutip/tests/test_random.py
+++ qutip/tests/test_random.py
@@ -74,6 +74,9 @@ def check_func_dims(func, args, kwargs, dims):
     resdims = func(*args, **kwargs).dims
     assert_(resdims == dims, "Checking {}; expected dimensions of {}, got {}.".format(func.__name__, dims, resdims))
 
+def check_func_shape(func, args, kwargs, shape):
+    resdims = func(*args, **kwargs).dims
+    assert_(dims_to_tensor_shape(resdims)==shape)
 
 def test_rand_vector_dims():
     FUNCS = [rand_ket, rand_ket_haar]
@@ -82,10 +85,13 @@ def test_rand_vector_dims():
         assert_raises(ValueError,check_func_dims,func,(),{},[])
         # both N and dims (named argument) are specified
         check_func_dims( func, (6, ), {'dims': [[2,3], [1,1]]}, [[2,3], [1,1]])
+        check_func_shape( func, (6, ), {'dims': [[2,3], [1,1]]}, (2,3,1,1))
         # only N is specified and dims is defined via default
         check_func_dims( func, (7, ), {}, [[7], [1]])
+        check_func_shape( func, (7, ), {}, (7,1))
         # only dims is specified and N has to be determined
         check_func_dims( func, (), {'dims': [[2,3], [1,1]]},[[2,3], [1,1]])
+        check_func_shape( func, (), {'dims': [[2,3], [1,1]]},(2,3,1,1))
 
 def test_rand_oper_dims():
     FUNCS = [rand_unitary, rand_herm, rand_dm, rand_unitary_haar, rand_dm_ginibre, rand_dm_hs]
