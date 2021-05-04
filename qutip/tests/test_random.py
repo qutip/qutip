73a69cb8740205ce9a7252c2d13f0e7fb8ce61d8
Purva Thakre <purva.athakre@gmail.com>
2021-05-04 08:35:20 -0500

    change error message of check_dims

--- qutip/tests/test_random.py
+++ qutip/tests/test_random.py
@@ -70,7 +70,8 @@ def check_func_dims(func, args, kwargs, dims):
     # TODO: promote this out of test_random, as it's generically useful
     #       in writing tests.
     resdims = func(*args, **kwargs).dims
-    assert resdims == dims
+    assert resdims == dims, message = "Calculated value of dims (left) does \
+    not match with expected value (right)."
 
 def check_func_N(func, args, kwargs, N):
     new_state_shape=func(*args, **kwargs).shape
