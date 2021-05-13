93431cdb4c1587ce031cc2f5f7ec1eae20a81eba
Simon Cross <hodgestar@gmail.com>
2021-05-13 10:00:44 +0200

    Vectorize calculation of P_ij (thanks @jakelishman).

--- qutip/entropy.py
+++ qutip/entropy.py
@@ -35,7 +35,7 @@ __all__ = ['entropy_vn', 'entropy_linear', 'entropy_mutual', 'negativity',
            'concurrence', 'entropy_conditional', 'entangling_power',
            'entropy_relative']
 
-from numpy import e, inf, real, sort, sqrt, vdot
+from numpy import conj, e, inf, inner, real, sort, sqrt, vdot
 from numpy.lib.scimath import log, log2
 from qutip.qobj import ptrace
 from qutip.states import ket2dm
@@ -280,16 +280,15 @@ def entropy_relative(rho, sigma, base=e, sparse=False, tol=1e-12):
     nzrvals = rvals[abs(rvals) >= tol]
     # Calculate S
     S = sum(nzrvals * log_base(nzrvals))
+    P = abs(inner(rvecs, conj(svecs))) ** 2
     for i in range(len(rvals)):
         for j in range(len(svals)):
-            ij_dot = vdot(rvecs[i], svecs[j])
-            P_ij = ij_dot * ij_dot.conjugate()
             if abs(svals[j]) < tol and not (
-                    abs(rvals[i]) < tol or abs(P_ij) < tol):
+                    abs(rvals[i]) < tol or abs(P[i, j]) < tol):
                 # kernel of sigma intersects support of rho
                 return inf
             if svals[j] >= tol:
-                S -= rvals[i] * P_ij * log_base(svals[j])
+                S -= rvals[i] * P[i, j] * log_base(svals[j])
     # the relative entropy is guaranteed to be >= 0, so we clamp the
     # calculated value to 0 to avoid small violations of the lower bound.
     return max(0, real(S))
