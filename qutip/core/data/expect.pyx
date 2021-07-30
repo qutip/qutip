bc7b0058bc2ef67a65a96d5e3533c515cde50934
Asier Galicia <A.GaliciaMartinez@student.tudelft.nl>
2021-07-30 10:45:17 +0200

    Added expect tests for data layer.

--- qutip/core/data/expect.pyx
+++ qutip/core/data/expect.pyx
@@ -21,19 +21,27 @@ __all__ = [
 ]
 
 cdef void _check_shape_ket(Data op, Data state) nogil except *:
-    if op.shape[1] != state.shape[0] or state.shape[1] != 1:
+    if (op.shape[1] != state.shape[0]  # Matrix multiplication
+        or state.shape[1] != 1  # State is ket
+        or op.shape[0] != op.shape[1]  # op must be square matrix
+       ):
         raise ValueError("incorrect input shapes "
                          + str(op.shape) + " and " + str(state.shape))
 
 cdef void _check_shape_dm(Data op, Data state) nogil except *:
-    if op.shape[0] != state.shape[1] or op.shape[1] != state.shape[0]:
+    if (op.shape[0] != op.shape[1]  # Matrix multiplication
+        or state.shape[0] != state.shape[1]  # State is square 
+        or op.shape[1] != state.shape[0]  # Op is square
+       ):
         raise ValueError("incorrect input shapes "
                          + str(op.shape) + " and " + str(state.shape))
 
 cdef void _check_shape_super(Data op, Data state) nogil except *:
     if state.shape[1] != 1:
         raise ValueError("expected a column-stacked matrix")
-    if op.shape[1] != state.shape[0]:
+    if ( op.shape[1] != state.shape[0]  # Matrix multiplication
+       or op.shape[0] != op.shape[1]  # Square matrix
+       ):
         raise ValueError("incompatible shapes " + str(op.shape) + ", " + str(state.shape))
 
 
