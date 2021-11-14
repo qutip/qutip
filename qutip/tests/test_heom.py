0d3e94a4043347d671e7d898e382b11b1a5e1c23
Simon Cross <hodgestar@gmail.com>
2021-11-15 00:26:39 +0100

    Add tests for the heom module API.

---
+++ qutip/tests/test_heom.py
@@ -0,0 +1,36 @@
+"""
+Tests for qutip.nonmarkov.heom.
+"""
+
+from qutip.nonmarkov.heom import (
+    BathExponent,
+    Bath,
+    BosonicBath,
+    DrudeLorentzBath,
+    DrudeLorentzPadeBath,
+    FermionicBath,
+    HEOMSolver,
+    BosonicHEOMSolver,
+    FermionicHEOMSolver,
+    HSolverDL,
+)
+
+
+class TestBathAPI:
+    def test_api(self):
+        # just assert that the baths are importable
+        assert BathExponent
+        assert Bath
+        assert BosonicBath
+        assert DrudeLorentzBath
+        assert DrudeLorentzPadeBath
+        assert FermionicBath
+
+
+class TestSolverAPI:
+    def test_api(self):
+        # just assert that the baths are importable
+        assert HEOMSolver
+        assert BosonicHEOMSolver
+        assert FermionicHEOMSolver
+        assert HSolverDL
