80ae2b0729607d22bb5be5120229b343741ca952
Emi <76208994+emilianomfortes@users.noreply.github.com>
2022-02-16 19:30:48 -0300

    Update test_krylovsolve.py

--- qutip/tests/test_krylovsolve.py
+++ qutip/tests/test_krylovsolve.py
@@ -1,83 +1,29 @@
-# This file is part of QuTiP: Quantum Toolbox in Python.
-#
-#    Copyright (c) 2011 and later, Paul D. Nation and Robert J. Johansson.
-#    All rights reserved.
-#
-#    Redistribution and use in source and binary forms, with or without
-#    modification, are permitted provided that the following conditions are
-#    met:
-#
-#    1. Redistributions of source code must retain the above copyright notice,
-#       this list of conditions and the following disclaimer.
-#
-#    2. Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#
-#    3. Neither the name of the QuTiP: Quantum Toolbox in Python nor the names
-#       of its contributors may be used to endorse or promote products derived
-#       from this software without specific prior written permission.
-#
-#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-#    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-#    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-#    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-#    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-#    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-#    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-#    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-#    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-#    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-###############################################################################
-
 import numpy as np
-from numpy.testing import assert_, run_module_suite
 
 np.random.seed(0)
-
-# disable the progress bar
+import pytest
+import math
 import os
 
-from qutip import sigmax, sigmay, sigmaz, qeye
-from qutip import basis, expect
-from qutip import num, destroy, create
-from qutip.interpolate import Cubic_Spline
-from qutip import krylovsolve
+from qutip import rand_herm, rand_ket, rand_unitary_haar, num, destroy, create
+from qutip import sigmax, sigmay, sigmaz, qeye, jmat
+from qutip import tensor, Qobj, basis, expect
+from qutip import krylovsolve, sesolve
 from qutip.solver import Options
 from scipy.linalg import expm
 from qutip.sparse import eigh
-import pytest
-
-from qutip import qeye, sigmax, sigmay, sigmaz, tensor, Qobj, jmat
-from scipy.stats import ortho_group
-import numpy as np
 
 
 def h_sho(dim):
-    auts = np.linspace(0, dim, dim, dtype='complex')
-    H = np.diag(auts + 1 / 2)
-    O = ortho_group.rvs(dim)
-    A = np.matmul(O.conj().T, np.matmul(H, O))
-    A = A / np.linalg.norm(A, ord=2)
-    A = Qobj(A)
-    return A
+    U = rand_unitary_haar(dim)
+    H = U * (num(dim) + 0.5) * U.dag()
+    H = H / np.linalg.norm(H.full(), ord=2)
+    return H
 
 
-def h_random(dim, distribution="normal"):
-    if distribution == "uniform":
-        H = np.random.random([dim, dim]) + 1j * np.random.random([dim, dim])
-    if distribution == "normal":
-        H = np.random.normal(
-            size=[dim, dim]) + 1j * np.random.normal(size=[dim, dim])
-    H = (H.conj().T + H) / 2
-    H = H / np.linalg.norm(H, ord=2)
-    H = Qobj(H)
-    return (H)
-
-
-def h_ising_transverse(N: int, hx: float, hz: float, Jx: float, Jy: float,
-                       Jz: float):
+def h_ising_transverse(
+    N: int, hx: float, hz: float, Jx: float, Jy: float, Jz: float
+):
 
     hx = hx * np.ones(N)
     hz = hz * np.ones(N)
@@ -121,25 +67,16 @@ def h_ising_transverse(N: int, hx: float, hz: float, Jx: float, Jy: float,
         H += -Jx[n] * sx_list[n] * sx_list[n + 1]
         H += -Jy[n] * sy_list[n] * sy_list[n + 1]
         H += -Jz[n] * sz_list[n] * sz_list[n + 1]
+
+    _dims = H.dims
+    H = Qobj(H, dims=_dims)
     return H
 
 
-def exactsolver(H, psi0, tlist: list or np.ndarray):
-
-    _H = H.full()
-    _psi0 = psi0.full()
-    eigenvalues, eigenvectors = eigh(_H)
-
-    psi_base_diag = np.matmul(eigenvectors.conj().T, _psi0)
-    U = np.exp(np.outer(-1j * eigenvalues, tlist))
-    psi_list = np.matmul(eigenvectors,
-                         np.multiply(U, psi_base_diag.reshape(
-                             [-1, 1])))  # shape=(dim, len(tlist))
-    psi_list = [Qobj(psi) for psi in psi_list.T]  # thus we transpose
-    return psi_list
-
-
-os.environ['QUTIP_GRAPHICS'] = "NO"
+def magnitude(value):
+    if value == 0:
+        return 0
+    return int(math.floor(math.log10(abs(value))))
 
 
 class TestKrylovSolve:
@@ -147,128 +84,99 @@ class TestKrylovSolve:
     A test class for the QuTiP Krylov Approximation method Solver.
     """
 
-    def check_evolution_states(self,
-                               H,
-                               psi0,
-                               tlist,
-                               td_args={},
-                               tol=5e-3,
-                               krylov_dim=20):
+    def check_evolution_states(
+        self,
+        H,
+        psi0,
+        tlist,
+        td_args={},
+        tol=1e-5,
+        tol2=1e-4,
+        krylov_dim=25,
+        square_hamiltonian=True,
+    ):
         """
         Compare integrated evolution with analytical result
         If U0 is not None then operator evo is checked
         Otherwise state evo
         """
 
-        options = Options(atol=tol)
+        options = Options(store_states=True)
 
-        output = krylovsolve(H,
-                             psi0,
-                             tlist,
-                             krylov_dim,
-                             e_ops=None,
-                             options=options)
-        exact_output = exactsolver(H, psi0, tlist)
-
-        fidelity = [
-            1 - np.abs(np.vdot(psi_k.full(), psi_exact.full()))**2
-            for (psi_k, psi_exact) in zip(output.states, exact_output)
-        ]
-
-        for fid in fidelity:
-            assert fid < tol, "fidelity between exact solution and krylov \
-                               solution does not match"
-
-    def check_evolution_e_ops(self,
-                              H,
-                              psi0,
-                              tlist,
-                              td_args={},
-                              tol=5e-8,
-                              krylov_dim=20):
-        """
-        Compare integrated evolution with analytical result
-        If U0 is not None then operator evo is checked
-        Otherwise state evo
-        """
-
-        options = Options(atol=tol)
-
-        #dim_j = int(H.shape[0] / 2)
-        #print(dim_j)
         e_ops = [
-            jmat((H.shape[0] - 1) / 2., 'x'),
-            jmat((H.shape[0] - 1) / 2., 'y'),
-            jmat((H.shape[0] - 1) / 2., 'z')
+            jmat((H.shape[0] - 1) / 2.0, "x"),
+            jmat((H.shape[0] - 1) / 2.0, "y"),
+            jmat((H.shape[0] - 1) / 2.0, "z"),
         ]
-        output = krylovsolve(H,
-                             psi0,
-                             tlist,
-                             krylov_dim,
-                             e_ops=e_ops,
-                             options=options)
-        exact_output = exactsolver(H, psi0, tlist)
+        if not square_hamiltonian:
+            _e_ops = []
+            for op in e_ops:
+                op2 = op.copy()
+                op2.dims = H.dims
+                _e_ops.append(op2)
+            e_ops = _e_ops
 
-        jx, jy, jz = output.expect[0], output.expect[1], output.expect[2]
+        output = krylovsolve(
+            H, psi0, tlist, krylov_dim, e_ops=e_ops, options=options
+        )
+        output_ss = sesolve(
+            H, psi0, tlist, e_ops=e_ops, options=Options(atol=1e-8)
+        )
 
-        jx_analytic = np.array([expect(e_ops[0], psi) for psi in exact_output])
-        jy_analytic = np.array([expect(e_ops[1], psi) for psi in exact_output])
-        jz_analytic = np.array([expect(e_ops[2], psi) for psi in exact_output])
+        errors_states = [
+            1 - np.abs(psi_ss.overlap(psi_k)) ** 2
+            for (psi_k, psi_ss) in zip(output.states, output_ss.states)
+        ]
 
-        delta_jx = np.array([
-            abs(_jx - _jx_analytic)
-            for (_jx, _jx_analytic) in zip(jx, jx_analytic)
-        ])
-        delta_jy = np.array([
-            abs(_jy - _jy_analytic)
-            for (_jy, _jy_analytic) in zip(jy, jy_analytic)
-        ])
-        delta_jz = np.array([
-            abs(_jz - _jz_analytic)
-            for (_jz, _jz_analytic) in zip(jz, jz_analytic)
-        ])
+        for err in errors_states:
+            assert (
+                err <= tol
+            ), f"error between sesolve states and krylov states its {err}."
 
-        assert_(max(delta_jx) < tol, msg="expect X not matching analytic")
-        #assert_(max(abs(jy - jy_analytic)) < tol,
-        #        msg="expect Y not matching analytic")
-        #assert_(max(abs(jz - jz_analytic)) < tol,
-        #        msg="expect Z not matching analytic")
+        for i in range(len(e_ops)):
+
+            errors_operators = [
+                np.abs(op_k - op_ss)
+                for (op_k, op_ss) in zip(output.expect[i], output_ss.expect[i])
+            ]
+
+            # magnitud of the error is checked here, because the optimization 
+            # was done for the states not the operators.
+            for err in errors_operators:
+                mag_err = magnitude(err)
+                if mag_err == 0:
+                    pass
+                else:
+                    assert magnitude(err) <= magnitude(
+                        tol2
+                    ), f"error between sesolve and krylov operators is {err}."
 
     def test_01_states_with_constant_H_random(self):
         "krylovsolve: states with const H random"
         dim = 512
-        psi0 = np.random.random(dim) + 1j * np.random.random(dim)
-        psi0 = psi0 / np.linalg.norm(psi0)
-        psi0 = Qobj(psi0)
-        H = h_random(dim, distribution="normal")
-        tlist = np.linspace(0, 20, 200)
+        psi0 = rand_ket(dim)
+        H = rand_herm(dim)
+        tlist = np.linspace(0, 10, 200)
 
         self.check_evolution_states(H, psi0, tlist)
-        self.check_evolution_e_ops(H, psi0, tlist)
 
     def test_02_states_with_constant_H_ising_transverse(self):
         "krylovsolve: states with const H Ising Transverse Field"
         N = 8
         dim = 2**N
-        psi0 = np.random.random(dim) + 1j * np.random.random(dim)
-        psi0 = psi0 / np.linalg.norm(psi0)
-        psi0 = Qobj(psi0)
         H = h_ising_transverse(N, hx=0.1, hz=0.5, Jx=1, Jy=0, Jz=1)
+        _dims = H.dims
+        _dims2 = [d - 1 for d in _dims[0]]
+        psi0 = rand_ket(dim, dims=[_dims[0], _dims2])
         tlist = np.linspace(0, 20, 200)
 
-        self.check_evolution_states(H, psi0, tlist)
-        self.check_evolution_e_ops(H, psi0, tlist)
+        self.check_evolution_states(H, psi0, tlist, square_hamiltonian=False)
 
     def test_03_states_with_constant_H_sho(self):
         "krylovsolve: states with const H SHO"
-        dim = 30
-        psi0 = np.random.random(dim) + 1j * np.random.random(dim)
-        psi0 = psi0 / np.linalg.norm(psi0)
-        psi0 = Qobj(psi0)
+        dim = 100
+        psi0 = rand_ket(dim)
         H = h_sho(dim)
         tlist = np.linspace(0, 20, 200)
 
         self.check_evolution_states(H, psi0, tlist)
-        self.check_evolution_e_ops(H, psi0, tlist)
-
-
