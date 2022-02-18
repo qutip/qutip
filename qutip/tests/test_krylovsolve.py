e19f20c02d128fb02576754b2f7bc1edc63a0656
Emi <76208994+emilianomfortes@users.noreply.github.com>
2022-02-18 12:55:48 -0300

    Update qutip/tests/test_krylovsolve.py
    Co-authored-by: Eric Giguère <eric.giguere@calculquebec.ca>

--- qutip/tests/test_krylovsolve.py
+++ qutip/tests/test_krylovsolve.py
@@ -491,7 +491,7 @@ class TestKrylovSolve:
         dim = 2**N
         H = h_ising_transverse(N, hx=hx, hz=hz, Jx=Jx, Jy=0, Jz=Jz)
         _dims = H.dims
-        _dims2 = [d - 1 for d in _dims[0]]
+        _dims2 = [1] * N
 
         tlist = np.linspace(0, 20, 200)
         self.check_evolution_states(
