896770b1775f2c6243c8020371047cd4370eebe3
HGSilveri <henrique.silverio@tecnico.ulisboa.pt>
2022-06-08 11:54:49 +0200

    Changing url to `qutip-qip` docs

--- qutip/qip.py
+++ qutip/qip.py
@@ -9,5 +9,5 @@ except ImportError:
     raise ImportError(
         "Importing 'qutip.qip' requires the 'qutip_qip' package. Install it "
         "with `pip install qutip-qip` (for more details, go to "
-        "https://qutip-qip.rtfd.io/en/stable/)."
+        "https://qutip-qip.readthedocs.io/)."
     )
