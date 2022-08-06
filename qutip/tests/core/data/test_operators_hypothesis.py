57f0ee7e531e369e386e3a79622ca83ef9f4043a
Simon Cross <hodgestar@gmail.com>
2022-08-06 13:09:52 +0200

    Update tests to use new strategy helpers.

--- qutip/tests/core/data/test_operators_hypothesis.py
+++ qutip/tests/core/data/test_operators_hypothesis.py
@@ -8,22 +8,16 @@ from qutip.tests import strategies as qst
 
 @given(qst.qobj_np(), qst.qobj_dtypes())
 def test_data_create(np_array, dtype):
-    if len(np_array.shape) == 1:
-        np_array = numpy.atleast_2d(np_array).transpose()
-    else:
-        np_array = numpy.atleast_2d(np_array)
     data = _data.to(dtype, _data.create(np_array))
-    numpy.testing.assert_allclose(
-        data.to_array(), numpy.atleast_2d(np_array), rtol=1e-15
-    )
+    qst.note(data=data, np_array=np_array)
+    qst.assert_allclose(data.to_array(), np_array)
 
 
 @given(qst.qobj_datas())
 def test_data_neg_operator(data):
     neg = -data
-    numpy.testing.assert_allclose(
-        neg.to_array(), -data.to_array(), rtol=1e-15
-    )
+    qst.note(neg=neg, data=data)
+    qst.assert_allclose(neg.to_array(), -data.to_array())
 
 
 same_shape = st.shared(qst.qobj_shapes())
@@ -31,53 +25,49 @@ same_shape = st.shared(qst.qobj_shapes())
 @given(qst.qobj_datas(shape=same_shape), qst.qobj_datas(shape=same_shape))
 def test_data_add_operator(a, b):
     result = a + b
-    numpy.testing.assert_allclose(
-        result.to_array(), a.to_array() + b.to_array(), rtol=1e-15
-    )
+    qst.note(result=result, a=a, b=b)
+    qst.assert_allclose(result.to_array(), a.to_array() + b.to_array())
 
 
 @given(qst.qobj_datas(shape=same_shape), qst.qobj_datas(shape=same_shape))
 def test_data_minus_operator(a, b):
     result = a - b
-    numpy.testing.assert_allclose(
-        result.to_array(), a.to_array() - b.to_array(), rtol=1e-15
-    )
+    qst.note(result=result, a=a, b=b)
+    qst.assert_allclose(result.to_array(), a.to_array() - b.to_array())
 
 
 @given(qst.qobj_datas(shape=same_shape), qst.qobj_datas(shape=same_shape))
 def test_data_matmul_operator(a, b):
     result = a @ b
-    numpy.testing.assert_allclose(
-        result.to_array(), a.to_array() @ b.to_array(), rtol=1e-15
-    )
+    qst.note(result=result, a=a, b=b)
+    qst.assert_allclose(result.to_array(), a.to_array() @ b.to_array())
 
 
 @given(st.complex_numbers(), qst.qobj_datas(shape=same_shape))
 def test_data_scalar_multiplication_left_operator(x, a):
     result = x * a
-    numpy.testing.assert_allclose(
-        result.to_array(), x * a.to_array(), rtol=1e-15
-    )
+    qst.note(result=result, x=x, a=a)
+    qst.assert_allclose(result.to_array(), x * a.to_array())
 
 
 @given(qst.qobj_datas(shape=same_shape), st.complex_numbers())
 def test_data_scalar_multiplication_right_operator(a, x):
     result = a * x
-    numpy.testing.assert_allclose(
-        result.to_array(), a.to_array() * x, rtol=1e-15
-    )
+    qst.note(result=result, a=a, x=x)
+    qst.assert_allclose(result.to_array(), a.to_array() * x)
+
 
 @given(qst.qobj_datas(shape=same_shape), st.complex_numbers())
 def test_data_scalar_division_operator(a, x):
     result = a / x
-    numpy.testing.assert_allclose(
-        result.to_array(), a.to_array() / x, rtol=1e-15
-    )
+    qst.note(result=result, a=a, x=x)
+    qst.assert_allclose(result.to_array(), a.to_array() / x)
 
 
 @given(qst.qobj_datas(shape=same_shape), qst.qobj_datas(shape=same_shape))
 def test_data_equality_operator(a, b):
     result = (a == b)
+    qst.note(result=result, a=a, b=b)
     assert result == numpy.allclose(
         a.to_array(), b.to_array(), rtol=1e-15
     )
