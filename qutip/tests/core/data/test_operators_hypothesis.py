9a78395c4d3e346bd812add0b4a52f7169045f31
Simon Cross <hodgestar@gmail.com>
2022-08-06 18:31:47 +0200

    In addition test, ignore arithmetic warnings and treat inf as nans.

--- qutip/tests/core/data/test_operators_hypothesis.py
+++ qutip/tests/core/data/test_operators_hypothesis.py
@@ -26,7 +26,9 @@ same_shape = st.shared(qst.qobj_shapes())
 def test_data_add_operator(a, b):
     result = a + b
     qst.note(result=result, a=a, b=b)
-    qst.assert_allclose(result.to_array(), a.to_array() + b.to_array())
+    with qst.ignore_arithmetic_warnings():
+        expected = a.to_array() + b.to_array()
+    qst.assert_allclose(result.to_array(), expected, treat_inf_as_nan=True)
 
 
 @given(qst.qobj_datas(shape=same_shape), qst.qobj_datas(shape=same_shape))
