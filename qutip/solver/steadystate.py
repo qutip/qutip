0d1df042e83acbefc3962305d3b6b307bb77022c
Eric <eric.giguere@calculquebec.ca>
2022-10-05 13:12:45 -0400

    Fix style

--- qutip/solver/steadystate.py
+++ qutip/solver/steadystate.py
@@ -2,27 +2,28 @@ from qutip import liouvillian, lindblad_dissipator, Qobj
 import qutip.core.data as _data
 
 
-def permute_wbm(L, b):
+def _permute_wbm(L, b):
     perm = scipy.sparse.csgraph.maximum_bipartite_matching(L.as_scipy())
     L = _data.permute.indices(L, perm, None)
     b = _data.permute.indices(b, perm, None)
     return L, b
 
 
-def permute_rcm(L, b):
+def _permute_rcm(L, b):
     perm = scipy.sparse.csgraph.reverse_cuthill_mckee(L.as_scipy())
     L = _data.permute.indices(L, perm, perm)
     b = _data.permute.indices(b, perm, None)
     return L, b, perm
 
 
-def reverse_rcm(rho, perm):
+def _reverse_rcm(rho, perm):
     rev_perm = np.argsort(perm)
     rho = _data.permute.indices(rho, rev_perm, None)
     return rho
 
 
-def steadystate(A, c_ops=[], *, method='direct', solve_method=None, weight=0, **kwargs):
+def steadystate(A, c_ops=[], *, method='direct', solve_method=None, weight=0, 
+                **kwargs):
     """
     Calculates the steady state for quantum evolution subject to the supplied
     Hamiltonian or Liouvillian operator and (if given a Hamiltonian) a list of
@@ -48,13 +49,19 @@ def steadystate(A, c_ops=[], *, method='direct', solve_method=None, weight=0, **
 
     solver : str, default=None
         'direct' and 'power' methods only.
-        Solver to use when solving the ``L(rho) = 0`` equation. Default supported are:
-        - "solve", "lstsq": dense solver from numpy.linalg
-        - "spsolve", "gmres", "lgmres", "bicgstab": sparse solver from scipy.sparse.linalg
-        - "mkl_spsolve", sparse solver by mkl.
-        Extension to qutip, such as qutip-tensorflow, can use come with their own solver. When ``A`` and ``c_ops``
-        use these data backends, see the corresponding libraries ``linalg`` for available solver.
-        extra options for these solver can be passed in ``**kw``.
+        Solver to use when solving the ``L(rho) = 0`` equation.
+        Default supported solver are:
+        - "solve", "lstsq":
+          dense solver from numpy.linalg
+        - "spsolve", "gmres", "lgmres", "bicgstab":
+          sparse solver from scipy.sparse.linalg
+        - "mkl_spsolve",
+          sparse solver by mkl.
+        Extension to qutip, such as qutip-tensorflow, can use come with their
+        own solver. When ``A`` and ``c_ops`` use these data backends, see the
+        corresponding libraries ``linalg`` for available solver.
+
+        Extra options for these solver can be passed in ``**kw``.
 
     **kw :
         use_rcm
@@ -86,11 +93,13 @@ def steadystate(A, c_ops=[], *, method='direct', solve_method=None, weight=0, **
     if "-" in method:
         method, solver = method.split("-")
 
-    # We want the user to be able to use this without having to know what data type the liouvillian use.
-    # For extra data types (tensorflow) we can expect the users to know they are using them and choose an appropriate solver
+    # We want the user to be able to use this without having to know what data
+    # type the liouvillian use. For extra data types (tensorflow) we can expect
+    # the users to know they are using them and choose an appropriate solver
+    sparse_solvers = ["spsolve", "mkl_spsolve", "gmres", "lgmres", "bicgstab"]
     if isinstance(A.data, _data.csr) and solver in ["solve", "lstsq"]:
         A = A.to("dense")
-    elif isinstance(A.data, _data.Dense) and solver in ["spsolve", "mkl_spsolve", "gmres", "lgmres", "bicgstab"]:
+    elif isinstance(A.data, _data.Dense) and solver in sparse_solvers:
         A = A.to("csr")
 
     if method in ["direct", "iterative"]:
@@ -124,21 +133,24 @@ def _steadystate_direct(A, weight, **kw):
     L_row0 = _data.matmul(_data.one_element[dtype]((1, N), (0, 0), 1), A.data)
     weight_vec = _data.column_stack(_data.diag([weight] * n, 0, dtype=dtype))
     weight_vec = _data.add(weight_vec.transpose(), L_row0, -1)
-    weight_mat = _data.kron(weight_vec, _data.one_element[dtype]((N, 1), (0, 0), 1))
+    weight_mat = _data.kron(
+        weight_vec,
+        _data.one_element[dtype]((N, 1), (0, 0), 1)
+    )
     L = _data.add(weight_mat, A.data)
     b = _data.one_element[dtype]((N, 1), (0, 0), weight)
 
     # Permutation are part of scipy.sparse, thus only supported for CSR.
     if kw.pop("use_wbm", False) and isinstance(L, _data.CSR):
-        L, b = permute_wbm(L, b)
+        L, b = _permute_wbm(L, b)
     use_rcm = kw.pop("use_rcm", False) and isinstance(L, _data.CSR)
     if use_rcm:
-        L, b, perm = permute_rcm(L, b)
+        L, b, perm = _permute_rcm(L, b)
 
     steadystate = _data.solve(L, b, **kw)
 
     if use_rcm:
-        steadystate = reverse_rcm(steadystate, perm)
+        steadystate = _reverse_rcm(steadystate, perm)
 
     rho_ss = _data.column_unstack(steadystate, n)
     rho_ss = _data.add(rho_ss, rho_ss.adjoint()) * 0.5
@@ -148,7 +160,11 @@ def _steadystate_direct(A, weight, **kw):
 
 def _steadystate_eigen(L, **kw):
     # v4's implementation only uses sparse eigen solver
-    val, vec = (L.dag() @ L).eigenstates(eigvals=1, sort="low", sparse=kw.pop("sparse", True))
+    val, vec = (L.dag() @ L).eigenstates(
+        eigvals=1,
+        sort="low",
+        sparse=kw.pop("sparse", True)
+    )
     rho = qt.vector_to_operator(vec[0])
     return rho / rho.tr()
 
@@ -168,10 +184,10 @@ def _steadystate_power(A, **kw):
 
     # Permutation are part of scipy.sparse, thus only supported for CSR.
     if kw.pop("use_wbm", False) and isinstance(L, _data.CSR):
-        L, y = permute_wbm(L, y)
+        L, y = _permute_wbm(L, y)
     use_rcm = kw.pop("use_rcm", False) and isinstance(L, _data.CSR)
     if use_rcm:
-        L, y, perm = permute_rcm(L, y)
+        L, y, perm = _permute_rcm(L, y)
 
     it = 0
     maxiter = kw.pop("maxiter", 1000)
@@ -186,7 +202,7 @@ def _steadystate_power(A, **kw):
                         str(maxiter) + ' iterations')
 
     if use_rcm:
-        y = reverse_rcm(y, perm)
+        y = _reverse_rcm(y, perm)
 
     rho_ss = Qobj(_data.column_unstack(y, N**0.5), dims=A.dims[0], isherm=True)
     return rho_ss / rho_ss.tr()
