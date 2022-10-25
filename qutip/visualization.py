Fix hinton plot square area scaling
Fix #2010.

The previous scaling was only taking into account the diagonal elements,
leading to misleading representations for matrices with small diagonal
coefficients and high off-diagonal coefficients.

qutip/visualization.py
@@ -303,7 +303,7 @@ def hinton(rho, xlabels=None, ylabels=None, title=None, ax=None, cmap=None,
 
     height, width = W.shape
 
-    w_max = 1.25 * max(abs(np.diag(np.array(W))))
+    w_max = 1.25 * max(abs(np.array(W)).flatten())
     if w_max <= 0.0:
         w_max = 1.0
 