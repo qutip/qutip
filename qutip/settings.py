dfc0d6c90cf75eda3c5b1c9a0277a8234497ac4d
Simon Cross <hodgestar@gmail.com>
2023-02-07 14:34:28 +0100

    Remove unneeded singleton pattern from Settings class.

--- qutip/settings.py
+++ qutip/settings.py
@@ -122,12 +122,6 @@ class Settings:
     """
     Qutip's settings and options.
     """
-    def __new__(cls):
-        """Set Settings as a singleton."""
-        if not hasattr(cls, '_instance'):
-            cls._instance = super(Settings, cls).__new__(cls)
-        return cls._instance
-
     def __init__(self):
         self._mkl_lib = ""
         try:
