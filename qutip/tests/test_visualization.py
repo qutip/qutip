0959610bfe64c27ee931106dd38655e736e2a660
tamakoshi <tamakosiy@gmail.com>
2023-07-11 14:36:06 +0900

    change the way to parametrize

--- qutip/tests/test_visualization.py
+++ qutip/tests/test_visualization.py
@@ -3,47 +3,52 @@ import qutip
 import matplotlib as mpl
 
 
-@pytest.mark.parametrize('rho, x_basis, y_basis, color_style,\
-           label_top, cmap, colorbar, fig, ax', [
-    (qutip.rand_dm(5), None, None, "scaled", True, None, None, None, None),
-    (qutip.operator_to_vector(qutip.rand_dm(5)),
-     None, None, "scaled", True, None, None, None, None),
-    (qutip.operator_to_vector(qutip.rand_dm(5)).dag(),
-     None, None, "scaled", True, None, None, None, None),
-    (qutip.spre(qutip.rand_dm(4)),
-     None, None, "scaled", True, None, None, None, None),
-    (qutip.rand_dm(5),
-     [1, 2, 3, 4, 5], None, "scaled", True, None, None, None, None),
-    (qutip.rand_dm(5),
-     None, [1, 2, 3, 4, 5], "scaled", True, None, None, None, None),
-    (qutip.rand_dm(5), None, None, "scaled", True, None, None, None, None),
-    (qutip.rand_dm(5), None, None, "threshold", True, None, None, None, None),
-    (qutip.rand_dm(5), None, None, "phase", True, None, None, None, None),
+def to_oper_bra(oper):
+    return qutip.operator_to_vector(oper).dag()
+
+
+def to_oper(oper):
+    return oper
+
+
+@pytest.mark.parametrize('transform, args', [
+    (to_oper, {}),
+    (qutip.operator_to_vector, {}),
+    (to_oper_bra, {}),
+    (qutip.spre, {}),
+    (to_oper, {'x_basis': [0, 1, 2, 3]}),
+    (to_oper, {'y_basis': [0, 1, 2, 3]}),
+    (to_oper, {'color_style': 'threshold'}),
+    (to_oper, {'color_style': 'phase'}),
+    (to_oper, {'colorbar': False}),
 ])
-def test_hinton(rho, x_basis, y_basis, color_style,
-                label_top, cmap, colorbar, fig, ax):
-    fig, ax = qutip.hinton(rho, x_basis, y_basis, color_style,
-                           label_top, cmap=cmap, colorbar=colorbar,
-                           fig=fig, ax=ax)
+def test_hinton(transform, args):
+    rho = transform(qutip.rand_dm(4))
+    fig, ax = qutip.hinton(rho, **args)
 
     assert isinstance(fig, mpl.figure.Figure)
     assert isinstance(ax, mpl.axes.Axes)
 
 
-@pytest.mark.parametrize('rho, x_basis, y_basis, color_style,\
-           label_top, cmap, colorbar, fig, ax, expected', [
-    (qutip.basis(2, 0), None, None, "scaled", True, None, None,
-     None, None, "Input quantum object must be an operator or superoperator."),
-    (qutip.rand_dm(5), None, None, "color_style", True, None, None,
-     None, None, "Unknown color style color_style for Hinton diagrams."),
-    (qutip.spre(qutip.rand_dm(5)),
-     None, None, "scaled", True, None, None, None, None,
+@pytest.mark.parametrize('transform, args, error_message', [
+    (to_oper, {},
+     "Input quantum object must be an operator or superoperator.")
+])
+def test_hinton_ValueError0(transform, args, error_message):
+    rho = transform(qutip.basis(2, 0))
+    with pytest.raises(ValueError) as exc_info:
+        fig, ax = qutip.hinton(rho, **args)
+    assert str(exc_info.value) == error_message
+
+
+@pytest.mark.parametrize('transform, args, error_message', [
+    (to_oper, {'color_style': 'color_style'},
+     "Unknown color style color_style for Hinton diagrams."),
+    (qutip.spre, {},
      "Hinton plots of superoperators are currently only supported for qubits.")
 ])
-def test_hinton_ValueError(rho, x_basis, y_basis, color_style,
-                           label_top, cmap, colorbar, fig, ax, expected):
+def test_hinton_ValueError1(transform, args, error_message):
+    rho = transform(qutip.rand_dm(5))
     with pytest.raises(ValueError) as exc_info:
-        fig, ax = qutip.hinton(rho, x_basis, y_basis, color_style,
-                               label_top, cmap=cmap, colorbar=colorbar,
-                               fig=fig, ax=ax)
-    assert str(exc_info.value) == expected
+        fig, ax = qutip.hinton(rho, **args)
+    assert str(exc_info.value) == error_message
