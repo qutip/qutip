3b337ba7e03b16e97f0771a0691434385b21a1e2
Eric Giguere <eric.giguere@calculquebec.ca>
2024-02-15 14:50:13 -0500

    Protocul use in CoefficientLike definition

--- qutip/typing.py
+++ qutip/typing.py
@@ -2,7 +2,7 @@ from typing import Sequence, Union, Any, Callable, Protocol
 
 # from .core.cy.qobjEvo import QobjEvoLike, Element
 # from .core.coeffients import CoefficientLike
-from numbers import Number
+from numbers import Number, Real
 import numpy as np
 import scipy.interpolate
 
@@ -10,22 +10,27 @@ import scipy.interpolate
 __all__ = ["QobjEvoLike", "CoefficientLike", "LayerType"]
 
 
-class QEvoFunction(Protocol):
-    def __call__(self, t: Number, **kwargs) -> "Qobj":
+class QEvoProtocol(Protocol):
+    def __call__(self, t: Real, **kwargs) -> "Qobj":
+        ...
+
+
+class CoeffProtocol(Protocol):
+    def __call__(self, t: Real, **kwargs) -> Number:
         ...
 
 
 CoefficientLike = Union[
     "Coefficient",
     str,
-    Callable[[float, ...], complex],
+    CoeffProtocol,
     np.ndarray,
     scipy.interpolate.PPoly,
     scipy.interpolate.BSpline,
     Any,
 ]
 
-ElementType = Union[QEvoFunction, "Qobj", tuple["Qobj", CoefficientLike]]
+ElementType = Union[QEvoProtocol, "Qobj", tuple["Qobj", CoefficientLike]]
 
 QobjEvoLike = Union["Qobj", "QobjEvo", ElementType, Sequence[ElementType]]
 
