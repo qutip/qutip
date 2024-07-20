247117ed342256a191efe46c7b4d6450b9b8ea64
Rochisha Agarwal <rochisha.agarwal2302@gmail.com>
2024-07-20 14:26:28 +0530

    test numpy as it is used in metrics

--- qutip/tests/core/test_numpy_backend.py
+++ qutip/tests/core/test_numpy_backend.py
@@ -2,7 +2,7 @@ import pytest
 import numpy
 from unittest.mock import Mock
 
-from qutip.core.numpy_backend import np, NumpyBackend
+from qutip.core.numpy_backend import np
 from qutip import CoreOptions
 
 # Mocking JAX to demonstrate backend switching
@@ -11,22 +11,11 @@ mock_np = numpy
 
 
 class TestNumpyBackend:
-    def test_backend_property(self):
-        with CoreOptions(numpy_backend=mock_np):
-            np_backend = NumpyBackend()
-            assert np_backend.backend is mock_np
-
-        with CoreOptions(numpy_backend=mock_jax):
-            np_backend = NumpyBackend()
-            assert np_backend.backend is mock_jax
-
     def test_getattr_numpy(self):
         with CoreOptions(numpy_backend=mock_np):
-            np_backend = NumpyBackend()
-            assert np_backend.sum([1, 2, 3]) == numpy.sum([1, 2, 3])
+            assert np.sum([1, 2, 3]) == numpy.sum([1, 2, 3])
 
     def test_getattr_jax(self):
         with CoreOptions(numpy_backend=mock_jax):
-            np_backend = NumpyBackend()
             mock_jax.sum = Mock(return_value="jax_sum")
-            assert np_backend.sum([1, 2, 3]) == "jax_sum"
+            assert np.sum([1, 2, 3]) == "jax_sum"
