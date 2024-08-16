cc9808b71f314530b8b43bf3f9fcf59f942ef90c
Eric Giguere <eric.giguere@calculquebec.ca>
2024-08-16 11:17:58 -0400

    change attribute names

--- qutip/core/numpy_backend.py
+++ qutip/core/numpy_backend.py
@@ -1,11 +1,12 @@
 from ..settings import settings
 
+
 class NumpyBackend:
     def _qutip_setting_backend(self, np):
-        self.np = np
+        self._qt_np = np
 
     def __getattr__(self, name):
-        return getattr(self.np, name)
+        return getattr(self._qt_np, name)
 
 
 # Initialize the numpy backend
