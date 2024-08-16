60ab9ca8c34af7b1e72b75ee3be052f44db149b5
Eric Giguere <eric.giguere@calculquebec.ca>
2024-08-16 11:02:14 -0400

    numpy_backend as a property

--- qutip/core/numpy_backend.py
+++ qutip/core/numpy_backend.py
@@ -1,14 +1,11 @@
 from ..settings import settings
 
-
 class NumpyBackend:
-    @property
-    def backend(self):
-        return settings.core["numpy_backend"]
+    def _qutip_setting_backend(self, np):
+        self.np = np
 
     def __getattr__(self, name):
-        backend = object.__getattribute__(self, 'backend')
-        return getattr(backend, name)
+        return getattr(self.np, name)
 
 
 # Initialize the numpy backend
