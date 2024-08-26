4d4db0024d858d6909c7f1609d23fe0ecf6a9509
Paul Menczel <paul@menczel.net>
2024-08-26 15:08:54 +0900

    Fixed __init__.py files

--- qutip/__init__.py
+++ qutip/__init__.py
@@ -51,7 +51,6 @@ from .partial_transpose import *
 from .continuous_variables import *
 from .distributions import *
 from . import measurement
-from .core.environment import *
 
 # utilities
 from .utilities import *
