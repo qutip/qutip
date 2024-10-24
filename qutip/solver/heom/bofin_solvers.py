ac1fe1063a1b28c20103bacedd2e1ebb5e1f9c4c
mcditooss <gerardofingurbe@gmail.com>
2024-10-25 00:23:29 +0200

    multiple baths able to pass env and baths

--- qutip/solver/heom/bofin_solvers.py
+++ qutip/solver/heom/bofin_solvers.py
@@ -745,10 +745,9 @@ class HEOMSolver(Solver):
                 or isinstance(bath_spec[0],FermionicBath)
             )
         else:
-            if (isinstance(bath_spec,ExponentialBosonicEnvironment) or
-                isinstance(bath_spec,ExponentialFermionicEnvironment)):
+            if type(bath_spec) in [ExponentialBosonicEnvironment,ExponentialFermionicEnvironment]:
                 raise ValueError("Enviroments must be passed with their"
-                                 "corresponding coupling operator as a list"
+                                 " corresponding coupling operator as a list"
                                  " or tuple (env,Q)")
             return False
         if is_bath:
