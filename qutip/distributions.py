f994b70ff6b34e013ab4491ae1eacd8c8180eabf
Matheus Cordeiro <matheusgomescord@gmail.com>
2024-11-04 17:51:23 -0300

    fixing codeclimate issues part 1

--- qutip/distributions.py
+++ qutip/distributions.py
@@ -359,16 +359,17 @@ class TwoModeQuadratureCorrelation(Distribution):
 class HarmonicOscillatorWaveFunction(Distribution):
 
     """
-    This class uses a function from the wavefunction_cython module, which is a compiled version 
-    for the Numpy and Cython version of QuTip, from the wavefunction_cython module of the 
-    Fast_Wave <https://github.com/fobos123deimos/fast-wave> package.which is released under 
+    This class uses a function from the wavefunction_cython 
+    module, which is a compiled version for the Numpy and 
+    Cython version of QuTip, from the wavefunction_cython 
+    module of the Fast_Wave <https://github.com/fobos123deimos/fast-wave> 
+    package.which is released under 
     the BSD license, with the following copyright notice:
-    
-    
-    CORDEIRO, MATHEUS; BEZERRA, I. P. ; VASCONCELOS, H. H. M. . Efficient Computation of the Wave Function ψn(x) using Hermite 
-    Coefficient Matrix in Python. In: 7º Workshop Escola de Computação e Informação Quântica (7ª WECIQ), 2024, Rio de Janeiro. 
-    ANAIS DO 7º WORKSHOP-ESCOLA DE COMPUTAÇÃO E INFORMAÇÃO QUÂNTICA. Rio de Janeiro: CEFET/RJ, 2024. p. 56-60. 
-
+    CORDEIRO, MATHEUS; BEZERRA, I. P. ; VASCONCELOS, H. H. M. . Efficient 
+    Computation of the Wave Function ψn(x) using Hermite Coefficient Matrix in 
+    Python. In: 7º Workshop Escola de Computação e Informação Quântica (7ª WECIQ), 
+    2024, Rio de Janeiro.ANAIS DO 7º WORKSHOP-ESCOLA DE COMPUTAÇÃO E INFORMAÇÃO 
+    QUÂNTICA. Rio de Janeiro: CEFET/RJ, 2024. p. 56-60.
     """
 
     def __init__(self, psi=None, omega=1.0, extent=[-5, 5], steps=250):
@@ -390,7 +391,8 @@ class HarmonicOscillatorWaveFunction(Distribution):
         N = psi.shape[0]
 
         for n in range(N):
-            self.data += wc.psi_n_single_fock_multiple_position_complex(n,self.xvecs[0].astype(complex)) * psi[n, 0]
+            self.data += wc.psi_n_single_fock_multiple_position_complex(n, 
+                            self.xvecs[0].astype(complex)) * psi[n, 0]
 
 
 class HarmonicOscillatorProbabilityFunction(Distribution):
