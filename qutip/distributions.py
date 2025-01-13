5a6337797756c0d740cf907d45798b39886597bf
MathiB123 <114240186+MathiB123@users.noreply.github.com>
2025-01-13 09:08:35 -0500

    Removed WignerDistribution and QDistribution

--- qutip/distributions.py
+++ qutip/distributions.py
@@ -8,9 +8,7 @@ distributions, such as Wigner distributions, etc.
 
 """
 
-__all__ = ['Distribution', 'WignerDistribution', 'QDistribution',
-           'TwoModeQuadratureCorrelation',
-           'HarmonicOscillatorWaveFunction',
+__all__ = ['Distribution','HarmonicOscillatorWaveFunction',
            'HarmonicOscillatorProbabilityFunction']
 
 import numpy as np
@@ -244,88 +242,6 @@ class Distribution:
         return Distribution(data=self.data.max(axis=dim),
                             xvecs=[self.xvecs[dim]],
                             xlabels=[self.xlabels[dim]])
-
-
-class WignerDistribution(Distribution):
-    """A class for representing the Wigner distribution function.
-
-    Parameters
-    ----------
-    rho : Qobj, default : None
-        Density matrix for composite quantum systems. Data for the
-        distribution is generated from rho, but can be generated
-        later if not given as input.
-    extent : ArrayLike, default : [[-5, 5], [-5, 5]]
-        List of arrays with the bounds [a, b] for each coordinate.
-    steps : int, default : 250
-        The number of data points generated between the bounds for
-        each coordinate.
-
-    """
-
-    def __init__(self, rho: Qobj = None, extent: ArrayLike = [[-5, 5], [-5, 5]],
-                 steps: int = 250):
-
-        self.xvecs = [np.linspace(extent[0][0], extent[0][1], steps),
-                      np.linspace(extent[1][0], extent[1][1], steps)]
-
-        self.xlabels = [r'$\rm{Re}(\alpha)$', r'$\rm{Im}(\alpha)$']
-
-        if rho:
-            self.update(rho)
-
-    def update(self, rho: Qobj):
-        """Updates data for the Wigner distribution.
-
-        Parameters
-        ----------
-        rho : Qobj
-            Density matrix for composite quantum systems.
-
-        """
-
-        self.data = wigner(rho, self.xvecs[0], self.xvecs[1])
-
-
-class QDistribution(Distribution):
-    """A class for representing the Husimi-Q distribution function.
-
-    Parameters
-    ----------
-    rho : Qobj, default : None
-        Density matrix for composite quantum systems. Data for
-        the distribution is generated from rho, but can be
-        generated later if not given as input.
-    extent : ArrayLike, default : [[-5, 5], [-5, 5]]
-        List of arrays with the bounds [a, b] for each coordinate.
-    steps : int, default : 250
-        The number of data points generated between the bounds
-        for each coordinate.
-
-    """
-
-    def __init__(self, rho: Qobj = None, extent: ArrayLike = [[-5, 5], [-5, 5]],
-                 steps: int = 250):
-
-        self.xvecs = [np.linspace(extent[0][0], extent[0][1], steps),
-                      np.linspace(extent[1][0], extent[1][1], steps)]
-
-        self.xlabels = [r'$\rm{Re}(\alpha)$', r'$\rm{Im}(\alpha)$']
-
-        if rho:
-            self.update(rho)
-
-    def update(self, rho: Qobj):
-        """Updates data for the Husimi-Q distribution.
-
-        Parameters
-        ----------
-        rho : Qobj
-            Density matrix for composite quantum systems.
-
-        """
-
-        self.data = qfunc(rho, self.xvecs[0], self.xvecs[1])
 
 
 class TwoModeQuadratureCorrelation(Distribution):
