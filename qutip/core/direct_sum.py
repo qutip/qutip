e5e750f54d32cd0aa9292c38de9024d6ada7e2c6
Paul Menczel <paul@menczel.net>
2025-03-19 15:27:30 +0900

    Refactoring direct sum

--- qutip/core/direct_sum.py
+++ qutip/core/direct_sum.py
@@ -90,130 +90,66 @@ def direct_sum(qobjs: list[Qobj | float] | list[list[Qobj | float]]) -> Qobj:
         raise ValueError("Matrix of Qobjs in direct sum must be square.")
 
     if linear and all(_is_like_ket(qobj) for qobj in qobjs):
-        if not settings.core['auto_tidyup_dims']:
-            scalar_dim = _qobj_dims(qobjs[0]).from_
-            scalar_dims_match = all(
-                _qobj_dims(qobj).from_ == scalar_dim for qobj in qobjs
-            )
-            if not scalar_dims_match:
-                raise ValueError("Scalar dimensions do not match in"
-                                 " direct sum of vectors.")
-        else:
-            scalar_dim = Field()
-        vector_dim = SumSpace([_qobj_dims(qobj).to_ for qobj in qobjs])
-
-        out_data = _data.concat_data([[_qobj_data(qobj)] for qobj in qobjs],
-                                     _skip_checks=True)
-        return Qobj(out_data,
-                    dims=Dimensions(scalar_dim, vector_dim),
-                    copy=False)
-
-    if linear and all(_is_like_bra(qobj) for qobj in qobjs):
-        if not settings.core['auto_tidyup_dims']:
-            scalar_dim = _qobj_dims(qobjs[0]).to_
-            scalar_dims_match = all(
-                _qobj_dims(qobj).to_ == scalar_dim for qobj in qobjs
-            )
-            if not scalar_dims_match:
-                raise ValueError("Scalar dimensions do not match in"
-                                 " direct sum of covectors.")
-        else:
-            scalar_dim = Field()
-        vector_dim = SumSpace([_qobj_dims(qobj).from_ for qobj in qobjs])
-
-        out_data = _data.concat_data([[_qobj_data(qobj) for qobj in qobjs]],
-                                     _skip_checks=True)
-        return Qobj(out_data,
-                    dims=Dimensions(vector_dim, scalar_dim),
-                    copy=False)
-
-    if linear and all(_is_like_operator_ket(qobj) for qobj in qobjs):
-        oper_as_opket = [False] * len(qobjs)
-        for index, qobj in enumerate(qobjs):
+        qobjs = [[qobj] for qobj in qobjs]
+    elif linear and all(_is_like_operator_ket(qobj) for qobj in qobjs):
+        # for convenience, we call operator_to_vector on operators provided
+        # in matrix form
+        def _ensure_vector(qobj):
             if isinstance(qobj, Qobj) and qobj.type == 'oper':
-                oper_as_opket[index] = True
-                qobjs[index] = operator_to_vector(qobj)
-
-        if not settings.core['auto_tidyup_dims']:
-            scalar_dim = _qobj_dims(qobjs[0]).from_
-            scalar_dims_match = all(
-                _qobj_dims(qobj).from_ == scalar_dim for qobj in qobjs
-            )
-            if not scalar_dims_match:
-                raise ValueError("Scalar dimensions do not match in"
-                                 " direct sum of operator-kets.")
-        else:
-            scalar_dim = Field()
-        vector_dim = SumSpace([_qobj_dims(qobj).to_ for qobj in qobjs],
-                              oper_as_opket)
-
-        out_data = _data.concat_data([[_qobj_data(qobj)] for qobj in qobjs],
-                                     _skip_checks=True)
-        return Qobj(out_data,
-                    dims=Dimensions(scalar_dim, vector_dim),
-                    copy=False)
-
-    if linear and all(_is_like_operator_bra(qobj) for qobj in qobjs):
-        oper_as_opket = [False] * len(qobjs)
-        for index, qobj in enumerate(qobjs):
+                return operator_to_vector(qobj)
+            return qobj
+        qobjs = [[_ensure_vector(qobj)] for qobj in qobjs]
+    elif linear and all(_is_like_bra(qobj) for qobj in qobjs):
+        qobjs = [[qobj for qobj in qobjs]]
+    elif linear and all(_is_like_operator_bra(qobj) for qobj in qobjs):
+        # for convenience, we call operator_to_vector on operators provided
+        # in matrix form
+        def _ensure_vector(qobj):
             if isinstance(qobj, Qobj) and qobj.type == 'oper':
-                oper_as_opket[index] = True
-                qobjs[index] = operator_to_vector(qobj).dag()
+                return operator_to_vector(qobj).dag()
+            return qobj
+        qobjs = [[_ensure_vector(qobj) for qobj in qobjs]]
+    elif linear:
+        raise ValueError("Invalid combination of Qobj types"
+                         " for direct sum.")
+    else:
+        allsuper = all(_is_like_super(qobj) for row in qobjs for qobj in row)
+        alloper = all(_is_like_oper(qobj) for row in qobjs for qobj in row)
+        if not (allsuper or alloper):
+            raise ValueError("Invalid combination of Qobj types"
+                             " for direct sum.")
 
-        if not settings.core['auto_tidyup_dims']:
-            scalar_dim = _qobj_dims(qobjs[0]).to_
-            scalar_dims_match = all(
-                _qobj_dims(qobj).to_ == scalar_dim for qobj in qobjs
-            )
-            if not scalar_dims_match:
-                raise ValueError("Scalar dimensions do not match in"
-                                 " direct sum of covectors.")
-        else:
-            scalar_dim = Field()
-        vector_dim = SumSpace([_qobj_dims(qobj).from_ for qobj in qobjs],
-                              oper_as_opket)
+    from_dim = [_qobj_dims(qobj).from_ for qobj in qobjs[0]]
+    to_dim = [_qobj_dims(row[0]).to_ for row in qobjs]
+    dims_match = all(
+        _qobj_dims(qobj).from_ == from_dim[col_index]
+        and _qobj_dims(qobj).to_ == to_dim[row_index]
+        for row_index, row in enumerate(qobjs)
+        for col_index, qobj in enumerate(row)
+    )
+    if not dims_match:
+        raise ValueError("Mismatching dimensions in direct sum of operators.")
 
-        out_data = _data.concat_data([[_qobj_data(qobj) for qobj in qobjs]],
-                                     _skip_checks=True)
-        return Qobj(out_data,
-                    dims=Dimensions(vector_dim, scalar_dim),
-                    copy=False)
-
-    allsuper = all(_is_like_super(qobj) for row in qobjs for qobj in row)
-    alloper = all(_is_like_oper(qobj) for row in qobjs for qobj in row)
-    if allsuper and any(not _is_like_scalar(qobj) and qobj.superrep != 'super'
-                        for row in qobjs for qobj in row):
-        raise ValueError("Direct sums only accept superoperators"
-                         " in super representation.")
-
-    if not linear and (allsuper or alloper):
-        from_dim = [_qobj_dims(qobj).from_ for qobj in qobjs[0]]
-        to_dim = [_qobj_dims(row[0]).to_ for row in qobjs]
-        dims_match = all(
-            _qobj_dims(qobj).from_ == from_dim[col_index]
-            and _qobj_dims(qobj).to_ == to_dim[row_index]
-            for row_index, row in enumerate(qobjs)
-            for col_index, qobj in enumerate(row)
-        )
-        if not dims_match:
-            raise ValueError("Mismatching dimensions in"
-                             " direct sum of operators.")
-
-        out_data = _data.concat_data(
-            [[_qobj_data(qobj) for qobj in row] for row in qobjs],
-            _skip_checks=True
-        )
-        return Qobj(out_data,
-                    dims=Dimensions(SumSpace(from_dim), SumSpace(to_dim)),
-                    copy=False)
-
-    raise ValueError("Invalid combination of Qobj types for direct sum.")
+    out_data = _data.concat_data(
+        [[_qobj_data(qobj) for qobj in row] for row in qobjs],
+        _skip_checks=True
+    )
+    return Qobj(out_data,
+                dims=Dimensions(SumSpace(*from_dim), SumSpace(*to_dim)),
+                copy=False)
 
 
-def component(qobj: Qobj, *index: list[int]) -> Qobj:
+def _check_index(given, min, max):
+    if not (min <= given < max):
+        raise ValueError(f"Index ({given}) out of bounds ({min}, {max-1})"
+                          " for component of direct sum.")
+
+def component(qobj: Qobj, *index: int) -> Qobj:
     is_to_sum = isinstance(qobj._dims.to_, SumSpace)
     is_from_sum = isinstance(qobj._dims.from_, SumSpace)
     if not is_to_sum and not is_from_sum:
+        if index == [0] or index == [0, 0]:
+            return qobj
         raise ValueError("Qobj is not a direct sum.")
 
     if is_to_sum and is_from_sum:
@@ -221,6 +157,8 @@ def component(qobj: Qobj, *index: list[int]) -> Qobj:
             raise ValueError("Invalid number of indices provided for component"
                              " of direct sum (two indices required).")
         to_index, from_index = index
+        _check_index(to_index, 0, len(qobj._dims.to_.spaces))
+        _check_index(from_index, 0, len(qobj._dims.from_.spaces))
         to_data_start = qobj._dims.to_._space_cumdims[to_index]
         to_data_stop = qobj._dims.to_._space_cumdims[to_index + 1]
         from_data_start = qobj._dims.from_._space_cumdims[from_index]
@@ -234,12 +172,22 @@ def component(qobj: Qobj, *index: list[int]) -> Qobj:
                                     qobj._dims.to_.spaces[to_index]),
                     copy=False)
 
+    if len(index) == 2:
+        to_index, from_index = index
+        if not is_to_sum:
+            _check_index(to_index, 0, 1)
+            index = [from_index]
+        else:
+            _check_index(from_index, 0, 1)
+            index = [to_index]
+
     if len(index) != 1:
         raise ValueError("Invalid number of indices provided for component"
-                         " of direct sum (one index required).")
+                         " of direct sum (one or two indices required).")
 
     if is_to_sum:
         to_index, = index
+        _check_index(to_index, 0, len(qobj._dims.to_.spaces))
         to_data_start = qobj._dims.to_._space_cumdims[to_index]
         to_data_stop = qobj._dims.to_._space_cumdims[to_index + 1]
         from_data_start = 0
@@ -252,11 +200,10 @@ def component(qobj: Qobj, *index: list[int]) -> Qobj:
                       dims=Dimensions(qobj._dims.from_,
                                       qobj._dims.to_.spaces[to_index]),
                       copy=False)
-        if qobj._dims.to_._oper_as_opket[to_index]:
-            return vector_to_operator(result)
         return result
 
     from_index, = index
+    _check_index(from_index, 0, len(qobj._dims.from_.spaces))
     to_data_start = 0
     to_data_stop = qobj._dims.to_.size
     from_data_start = qobj._dims.from_._space_cumdims[from_index]
@@ -269,6 +216,4 @@ def component(qobj: Qobj, *index: list[int]) -> Qobj:
                   dims=Dimensions(qobj._dims.from_.spaces[from_index],
                                   qobj._dims.to_),
                   copy=False)
-    if qobj._dims.from_._oper_as_opket[from_index]:
-        return vector_to_operator(result.dag())
     return result
