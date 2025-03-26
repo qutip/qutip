bb9c64d0a33fed3f42fe81af9cf12a1c8dee6990
Paul Menczel <paul@menczel.net>
2025-03-27 00:19:38 +0900

    CSR block operations

--- qutip/core/data/block_operations.pyx
+++ qutip/core/data/block_operations.pyx
@@ -1,28 +1,27 @@
 #cython: language_level=3
 
-from .base cimport Data
-from .dense cimport Dense
-
-from .base import Data
-from .dense import Dense
+from .. import data as _data
+from . cimport base, CSR, Data, Dense
 
 import numpy as np
+cimport numpy as cnp
 
 is_numpy1 = np.lib.NumpyVersion(np.__version__) < '2.0.0b1'
 
 
 __all__ = [
     'concat_dense', 'slice_dense', 'insert_dense',
+    'concat_csr', 'slice_csr', 'insert_csr',
     'concat', 'slice', 'insert',
 ]
 
 
 cpdef Dense concat_dense(
     list[list[Data]] data_array,
-    list[object] block_widths = None,
-    list[object] block_heights = None
+    list[base.idxint] block_widths = None,
+    list[base.idxint] block_heights = None
 ):
-    cdef int columm, row
+    cdef base.idxint columm, row
 
     if len(data_array) == 0 or len(data_array[0]) == 0:
         raise ValueError("Cannot concatenate empty data array.")
@@ -43,9 +42,71 @@ cpdef Dense concat_dense(
                  copy=(False if is_numpy1 else None))
 
 
+cpdef CSR concat_csr(
+    list[list[Data]] data_array,
+    list[base.idxint] block_widths = None,
+    list[base.idxint] block_heights = None
+):
+    if len(data_array) == 0 or len(data_array[0]) == 0:
+        raise ValueError("Cannot concatenate empty data array.")
+
+    if block_widths is None or block_heights is None:
+        block_widths, block_heights = _check_widths_heights(data_array)
+
+    cdef base.idxint columm, row
+    cdef base.idxint num_rows = len(block_heights)
+    cdef base.idxint num_cols = len(block_widths)
+    cdef cnp.ndarray[base.idxint] row_pos = np.cumsum(
+        [0] + block_heights, dtype=_data.base.idxint_dtype)
+    cdef cnp.ndarray[base.idxint] col_pos = np.cumsum(
+        [0] + block_widths, dtype=_data.base.idxint_dtype)
+    cdef base.idxint shape1 = row_pos[num_rows]
+    cdef base.idxint shape2 = col_pos[num_cols]
+    cdef base.idxint nnz_ = 0
+    cdef Data op
+
+    for row in range(num_rows):
+        for column in range(num_cols):
+            op = data_array[row][column]
+            if op is None:
+                continue
+            if type(op) is not CSR:
+                op = _data.to(CSR, op)
+                data_array[row][column] = op
+            nnz_ += _data.csr.nnz(op)
+
+    if nnz_ == 0:
+        return _data.csr.zeros(shape1, shape2)
+
+    cdef CSR out = _data.csr.empty(shape1, shape2, nnz_)
+    cdef base.idxint op_row, op_row_start, op_row_end, op_row_len, i
+    cdef base.idxint end = 0
+    cdef CSR csr_op
+
+    out.row_index[0] = 0
+
+    for row in range(num_rows):
+        for op_row in range(block_heights[row]):
+            for column in range(num_cols):
+                csr_op = data_array[row][column]
+                if csr_op is None or _data.csr.nnz(csr_op) == 0:
+                    continue
+                op_row_start = csr_op.row_index[op_row]
+                op_row_end = csr_op.row_index[op_row + 1]
+                op_row_len = op_row_end - op_row_start
+                for i in range(op_row_len):
+                    out.col_index[end + i] =\
+                        csr_op.col_index[op_row_start + i] + col_pos[column]
+                    out.data[end + i] = csr_op.data[op_row_start + i]
+                end += op_row_len
+            out.row_index[row_pos[row] + op_row + 1] = end
+
+    return out
+
+
 cdef _check_widths_heights(list[list[Data]] data_array):
-    cdef int num_columns, num_rows
-    cdef int columm, row
+    cdef base.idxint num_columns, num_rows
+    cdef base.idxint columm, row
 
     num_columns = len(data_array[0])
     num_rows = len(data_array)
@@ -79,25 +140,52 @@ cdef _check_widths_heights(list[list[Data]] data_array):
     return block_widths, block_heights
 
 
-cpdef Dense slice_dense(Dense data, row_start, row_stop, col_start, col_stop):
-    array = data.as_ndarray()
+cpdef Dense slice_dense(Dense data,
+                        base.idxint row_start, base.idxint row_stop,
+                        base.idxint col_start, base.idxint col_stop):
+    cdef cnp.ndarray array = data.as_ndarray()
     return Dense(array[row_start:row_stop, col_start:col_stop], copy=True)
 
 
-cpdef Dense insert_dense(Data data, Dense block, above, before):
-    cdef int data_height, data_width, block_height, block_width
+cpdef CSR slice_csr(CSR data,
+                    base.idxint row_start, base.idxint row_stop,
+                    base.idxint col_start, base.idxint col_stop):
+    scipy = data.as_scipy()
+    return CSR(scipy[row_start:row_stop, col_start:col_stop], copy=True)
 
-    data_array = data.to_array()  # copies
-    block_array = block.as_ndarray()  # doesn't copy
+
+cpdef Dense insert_dense(Data data, Dense block,
+                         base.idxint above, base.idxint before):
+    cdef base.idxint data_height, data_width, block_height, block_width
 
     data_height, data_width = data.shape
     block_height, block_width = block.shape
     if above + block_height > data_height or before + block_width > data_width:
         raise IndexError("Cannot insert block into data: doesn't fit.")
 
+    cdef cnp.ndarray data_array = data.to_array()  # copies
+    cdef cnp.ndarray block_array = block.as_ndarray()  # doesn't copy
+
     data_array[above:(above+block_height),
                before:(before+block_width)] = block_array  # copies block
     return Dense(data_array, copy=False)
+
+
+cpdef CSR insert_csr(CSR data, CSR block,
+                     base.idxint above, base.idxint before):
+    cdef base.idxint data_height, data_width, block_height, block_width
+
+    data_height, data_width = data.shape
+    block_height, block_width = block.shape
+    if above + block_height > data_height or before + block_width > data_width:
+        raise IndexError("Cannot insert block into data: doesn't fit.")
+
+    data_scipy = data.as_scipy().copy()
+    block_scipy = block.as_scipy()
+
+    data_scipy[above:(above+block_height),
+               before:(before+block_width)] = block_scipy
+    return CSR(data_scipy, copy=False)
 
 
 from .dispatch import Dispatcher as _Dispatcher
@@ -123,7 +211,8 @@ concat.__doc__ =\
     Concatenates a 2D array of data blocks into a single large matrix.
     """
 concat.add_specialisations([
-    (Dense, concat_dense)
+    (Dense, concat_dense),
+    (CSR, concat_csr),
 ], _defer=True)
 
 
@@ -150,6 +239,7 @@ slice.__doc__ =\
     """
 slice.add_specialisations([
     (Dense, slice_dense),
+    (CSR, slice_csr),
 ], _defer=True)
 
 
@@ -171,6 +261,7 @@ insert.__doc__ =\
     """
 insert.add_specialisations([
     (Data, Dense, Dense, insert_dense),
+    (CSR, CSR, CSR, insert_csr),
 ], _defer=True)
 
 
