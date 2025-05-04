bbb1bccb49d78cfd79ae1e9d38b64284c7724404
Tanuj Rai <tanujrai898@gmail.com>
2025-05-04 15:21:36 +0530

    Fix qdiags Hermitian check for complex diagonals
    Corrected the isherm check in qdiags to use np.abs(np.imag(diagonals)) to properly
    identify Hermitian diagonal matrices. Fixes issue with phasegate for angles in (pi, 2pi).

--- qutip/core/operators.py
+++ qutip/core/operators.py
@@ -75,8 +75,8 @@ shape = [4, 4], type = oper, isherm = False
         isherm = False
         isunitary = False
     elif offsets == [0]:
-        isherm = np.all(np.imag(diagonals) <= settings.core["atol"])
-        isunitary = np.all(np.abs(diagonals) - 1 <= settings.core["atol"])
+        isherm = np.all(np.abs(np.imag(diagonals)) <= settings.core["atol"])
+        isunitary = np.all(np.abs(np.abs(diagonals) - 1) <= settings.core["atol"])
     else:
         isherm = None
         isunitary = None
