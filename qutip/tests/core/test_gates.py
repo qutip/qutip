01e0d31b08177046c3182da8b6d36c1fae191375
Tanuj Rai <tanujrai898@gmail.com>
2025-05-22 16:28:24 +0530

    Update test_gates.py

--- qutip/tests/core/test_gates.py
+++ qutip/tests/core/test_gates.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 import qutip
 from qutip.core import gates
+from qutip import Qobj
 
 
 def _infidelity(a, b):
@@ -198,4 +199,14 @@ def test_metadata(gate_func, args, alias):
     assert gate._isunitary == gate._calculate_isunitary()
     with qutip.CoreOptions(default_dtype=alias):
         gate = gate_func(*args)
-        assert isinstance(gate.data, dtype)
+        assert isinstance(gate.data, type)
+
+def test_phasegate_hermitian():
+    phi = 4 * np.pi / 3
+    pg = gates.phasegate(phi)
+    expected = Qobj([[1, 0], [0, np.exp(1j * phi)]])
+    assert not pg.isherm
+    assert np.allclose(pg.dag().full(), expected.dag().full())
+    diag_elements = np.diag(pg.full())
+    expected_diag = [1, np.exp(1j * phi)]
+    assert np.allclose(diag_elements, expected_diag)
