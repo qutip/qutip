922f7f38dabd8f05b507c9d687b0d32824b37a32
Eric Giguere <eric.giguere@calculquebec.ca>
2025-07-30 16:19:59 -0400

    Add register method

--- qutip/tests/core/data/test_dispatch.py
+++ qutip/tests/core/data/test_dispatch.py
@@ -97,6 +97,28 @@ def test_build_full(specialisation, output):
                         assert isinstance(out, out_dtype)
 
 
+def test_register():
+    def func_dense(left, right):
+        return "Dense"
+
+    dispatched = Dispatcher(func_dense, ("mat",), False)
+    dispatched.add_specialisations([
+        (_data.Dense, _data.Dense, func_dense),
+    ])
+
+    @dispatched.register(_data.CSR)
+    def func_csr(left, right):
+        return "CSR"
+
+    @dispatched.register(_data.Dia, _data.CSR)
+    def func_dia_csr(left, right):
+        return "Dia", "CSR"
+
+    assert dispatched[_data.Dense, _data.Dense] is func_dense
+    assert dispatched[_data.CSR, _data.CSR] is func_csr
+    assert dispatched[_data.Dia, _data.CSR] is func_dia_csr
+
+
 def test_Data_low_priority_one_dispatch():
     class func():
         __name__ = "dummy name"
