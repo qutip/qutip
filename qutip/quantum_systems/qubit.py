4e5e69892b34426d1b83d5e14d1ac08b55b4be42
Vanshaj0429 <vanshajbindal98@gmail.com>
2025-08-18 02:05:40 +0200

    fix minor pep8 formatting error

--- qutip/quantum_systems/qubit.py
+++ qutip/quantum_systems/qubit.py
@@ -3,13 +3,13 @@ import qutip as qt
 from .quantum_system import QuantumSystem  # Import the QuantumSystem class
 
 
-def qubit(omega: float = 1.0, decay_rate: float = 0.0, 
+def qubit(omega: float = 1.0, decay_rate: float = 0.0,
           dephasing_rate: float = 0.0) -> QuantumSystem:
     """
     Create two-level system (qubit)
 
     H = (omega_a/2) * sigma_z
-    
+
     Parameters:
     -----------
     omega : float
@@ -18,32 +18,32 @@ def qubit(omega: float = 1.0, decay_rate: float = 0.0,
         Relaxation rate (1/T1)
     dephasing_rate : float
         Dephasing rate (1/T2)
-        
+
     Returns:
     --------
     QuantumSystem instance configured as qubit
     """
-    # Build operators 
+    # Build operators
     operators = {}
     operators['sigma_minus'] = qt.destroy(2)
     operators['sigma_plus'] = qt.create(2)
     operators['sigma_z'] = qt.sigmaz()
     operators['sigma_x'] = qt.sigmax()
     operators['sigma_y'] = qt.sigmay()
-    
-    # Build Hamiltonian 
+
+    # Build Hamiltonian
     hamiltonian = 0.5 * omega * operators['sigma_z']
-    
-    # Build collapse operators 
+
+    # Build collapse operators
     c_ops = []
     if decay_rate > 0.0:
         c_ops.append(np.sqrt(decay_rate) * operators['sigma_minus'])
     if dephasing_rate > 0.0:
         c_ops.append(np.sqrt(dephasing_rate) * operators['sigma_z'])
-    
+
     # LaTeX representation
     latex = r"H = \frac{\omega}{2}\sigma_z"
-    
+
     # Create system with all components
     return QuantumSystem(
         name="Qubit",
