b02552eb04ac0f77e5f79fe617185f09f2f9e717
Eric Giguere <eric.giguere@calculquebec.ca>
2025-09-29 17:12:07 -0400

    Refracting

--- qutip/core/data/local_matmul_mix.pyx
+++ qutip/core/data/local_matmul_mix.pyx
@@ -5,141 +5,17 @@ from qutip.core.data.matmul cimport imatmul_data_dense, matmul_dense
 from qutip.core.data.matmul import matmul, matmul_dense_dia_dense
 from qutip.core.data.add cimport iadd_dense
 import numpy as np
-
-
-__all__ = ["one_mode_matmul_data_dense", "one_mode_matmul_dual_dense_data"]
-
+cimport cython
 
 #TODO:
 # - One head function
 # - Reusable interne functions + factory
 # - What about jax etc?
+# - merge left and right?
 
 
-cdef int _mul(list numbers) except -1:
-    cdef int t, out = 1, N = len(numbers)
-    for i in range(N):
-        out = out * numbers[i]
-    return out
-
-
-cdef Dense _null_dense(int rows, int cols, bint fortran=True):
-    cdef Dense out = Dense.__new__(Dense)
-    out.shape = (rows, cols)
-    out.data = <double complex *> 0
-    out._deallocate = False
-    out.fortran = fortran
-    return out
-
-
-cdef void _cut_dense(Dense base, Dense out, int loc):
-    out.data = &base.data[loc]
-
-
-cpdef Dense one_mode_matmul_data_dense(Data oper, Dense state, list hilbert, int mode):
-    cdef Dense out, out_loc, state_loc
-    assert oper.shape[1] == hilbert[mode]
-    cdef int step_state, step_out, i, N = _mul(hilbert)
-    assert state.shape[0] == N
-
-    cdef int before = _mul(hilbert[:mode])
-    cdef int after = _mul(hilbert[mode+1:])
-
-    if oper.shape[0] != oper.shape[1]:
-        N_out = N // oper.shape[1] * oper.shape[0]
-    else:
-        N_out = N
-
-    out = dense.zeros(N_out, state.shape[1], state.fortran)
-    if state.fortran:
-        # With fortran ordering
-        # Increments look like [[4, 2, 1], [32, 16, 8]]
-        # We want blocks with increment [[N], [1]]
-        # the column idx move farter, as with the before sizes
-        # So we need shape[1] * before loops of mode size * after block
-        step_state = oper.shape[1] * after
-        state_loc = _null_dense(oper.shape[1], after, False)
-        step_out = oper.shape[0] * after
-        out_loc = _null_dense(oper.shape[0], after, False)
-        for i in range(before * state.shape[1]):
-            _cut_dense(state, state_loc, i * step_state)
-            _cut_dense(out, out_loc, i * step_out)
-            imatmul_data_dense(oper, state_loc, 1., out_loc)
-    else:
-        # With C ordering, the rows idx move farter, as with the before sizes
-        # So we need before loops of shape[1] * after * mode size block
-        step_state = oper.shape[1] * after * state.shape[1]
-        state_loc = _null_dense(oper.shape[1], after * state.shape[1], False)
-        step_out = oper.shape[0] * after * state.shape[1]
-        out_loc = _null_dense(oper.shape[0], after * state.shape[1], False)
-        for i in range(before):
-            _cut_dense(state, state_loc, i * step_state)
-            _cut_dense(out, out_loc, i * step_out)
-            imatmul_data_dense(oper, state_loc, 1., out_loc)  # Need real inplace
-
-    return out
-
-
-cdef void imatmul_dense_data(Dense left, Data right, double complex scale, Dense out):
-    #if type(left) is CSR:
-    #    matmul_csr_dense_dense(left, right, scale, out)
-    if type(right) is Dia:
-        matmul_dense_dia_dense(left, right, scale, out)
-    elif type(right) is Dense:
-        matmul_dense(left, right, scale, out)
-    else:
-        iadd_dense(out, matmul(left, right, dtype=Dense), scale)
-
-
-cpdef Dense one_mode_matmul_dual_dense_data(Dense state, Data oper, list hilbert, int mode):
-    cdef Dense out, out_loc, state_loc
-    assert oper.shape[0] == hilbert[mode]
-    cdef int step_state, step_out, i, N = _mul(hilbert)
-    assert state.shape[1] == N
-
-    cdef int before = _mul(hilbert[:mode])
-    cdef int after = _mul(hilbert[mode+1:])
-
-    if oper.shape[0] != oper.shape[1]:
-        N_out = N // oper.shape[0] * oper.shape[1]
-    else:
-        N_out = N
-
-    out = dense.zeros(state.shape[0], N_out, state.fortran)
-    if state.fortran:
-        # With fortran ordering
-        # Increments look like [[4, 2, 1], [64, 32, 16]]
-        # We want blocks with increment [[1], [N]]
-        # the column idx move farter, as with the before sizes
-        # So we need before loops of mode size * after * shape[0] block
-        step_state = oper.shape[0] * after * state.shape[0]
-        state_loc = _null_dense(after * state.shape[0], oper.shape[0], True)
-        step_out = oper.shape[1] * after * state.shape[0]
-        out_loc = _null_dense(after * state.shape[0], oper.shape[1], True)
-        for i in range(before):
-            _cut_dense(state, state_loc, i * step_state)
-            _cut_dense(out, out_loc, i * step_out)
-            imatmul_dense_data(state_loc, oper, 1., out_loc)
-    else:
-        # With C ordering, the rows idx move farter, as with the before sizes
-        # Increments look like [[64, 32, 16], [4, 2, 1]]
-        # We want blocks with increment [[1], [N]]
-        # So we need before * shape[0] loops of  after * mode size block
-        step_state = oper.shape[0] * after
-        state_loc = _null_dense(after, oper.shape[1], True)
-        step_out = oper.shape[1] * after
-        out_loc = _null_dense(after, oper.shape[0], True)
-        for i in range(before * state.shape[0]):
-            _cut_dense(state, state_loc, i * step_state)
-            _cut_dense(out, out_loc, i * step_out)
-            imatmul_dense_data(state_loc, oper, 1., out_loc)  # Need real inplace
-
-    return out
-
-
-"""
 cdef class Hilbert_Iterator:
-    \"""
+    """
     Allow to iterate over the ravelsome mode of an hilbert space.
     if:
 
@@ -149,30 +25,42 @@ cdef class Hilbert_Iterator:
     then we want to iterate with:
 
         0, 1, 2, 3, 12, 13, 14, 16
-    \"""
+    """
+    # There are 2 methods implemented in parallel:
+    # - Create an array of all indexes at initialization
+    # - Compute the index as needed
     cdef:
-        object np_arr
+        readonly object np_arr, np_index
         int[::1] iterator, step, sizes
+        int[::1] indexes
         int N_mode, position
+        bint pre_computed
 
-    def __init__(self, hilbert, mode):
+    def __init__(self, hilbert, mode, pre_compute_size=1024):
         self.N_mode = len(mode)
         self.np_arr = np.zeros((3, self.N_mode), dtype=np.int32)
         self.position = 0
         self.iterator = self.np_arr[0, :]
         self.step = self.np_arr[1, :]
         self.sizes = self.np_arr[2, :]
+        self.pre_computed = False
 
         hilberts_steps = [1]
-        for i in range(len(hilbert)-1):
+        for i in range(len(hilbert)):
             hilberts_steps = [
-                hilberts_steps[0] * hilbert[len(hilbert) - i - 1]
+                hilberts_steps[0] * hilbert[len(hilbert) - 1 - i]
             ] + hilberts_steps
 
         for i in range(self.N_mode):
             self.step[i] = hilberts_steps[mode[i]]
             self.sizes[i] = hilbert[mode[i]]
 
+        if hilberts_steps[0] <= pre_compute_size:
+            self.np_index = np.zeros(hilberts_steps[0], dtype=np.int32)
+            for i in range(hilberts_steps[0]):
+                self.np_index[i] = self.next()
+            self.indexes = self.np_index[:]
+            self.pre_computed = True
 
     cdef int start(self):
         cdef int i
@@ -181,9 +69,18 @@ cdef class Hilbert_Iterator:
             self.iterator[i] = 0
 
     cdef int next(self):
-        cdef int i
-        old_pos = self.position
-        i = self.N_mode
+        """
+        Get the indexes in order
+        """
+        cdef int i = self.N_mode
+        cdef int old_pos = self.position
+        if self.pre_computed:
+
+
+            old_pos = self.indexes[self.position]
+            self.position += 1
+            return old_pos
+
         while i:
             i = i-1
             self.iterator[i] += 1
@@ -197,11 +94,23 @@ cdef class Hilbert_Iterator:
 
         return old_pos
 
-    cdef int at(int i):
-        \"""return the ith position\"""
-        # same as self.start(); for _ in range(i): self.next(); return self.position
-        # For small hilbert space, precomputing as a vector would be the fastest.
-        # But
+    cdef int at(self, int linear_idx):
+        """
+        Calculates and returns the index for the i-th element
+        of the iteration sequence without changing the iterator's state.
+        """
+        cdef int final_pos = 0
+        cdef int j, current_mode_idx
+
+        if self.pre_computed:
+            return self.indexes[linear_idx]
+
+        for j in range(self.N_mode - 1, -1, -1):
+            current_mode_idx = linear_idx % self.sizes[j]
+            final_pos += current_mode_idx * self.step[j]
+            linear_idx = linear_idx // self.sizes[j]
+
+        return final_pos
 
 
 @cython.boundscheck(False)
@@ -265,7 +174,7 @@ cpdef Dense cy_N_mode_dense(
     return out
 
 
-# TODO merge left and right?
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -322,4 +231,3 @@ cpdef Dense N_mode_data_dense(
                     val * state.data[row_state * state_row_stride + j * state_col_stride]
 
     return out
-"""
