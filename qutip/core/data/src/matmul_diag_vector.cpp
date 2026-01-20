3bb86b1f06fbb7d07acf78158e46a51963b3d128
Ashley Milsted <ammilste@amazon.com>
2026-01-19 17:38:38 -0800

    Special-case scale==1 DIA routines to restore perf

--- qutip/core/data/src/matmul_diag_vector.cpp
+++ qutip/core/data/src/matmul_diag_vector.cpp
@@ -12,20 +12,27 @@ void _matmul_diag_vector(
 ){
     const double * data_dbl = reinterpret_cast<const double *>(data);
     const double * vec_dbl = reinterpret_cast<const double *>(vec);
-    const double * scale_dbl = reinterpret_cast<const double *>(&scale);
     double * out_dbl = reinterpret_cast<double *>(out);
-    double prod_re, prod_im, scaled_re, scaled_im;
-    // Gcc does not vectorize complex automatically?
-    for (IntT i=0; i<length*2; i+=2){
-        // Compute data[i] * vec[i]
-        prod_re = data_dbl[i] * vec_dbl[i] - data_dbl[i+1] * vec_dbl[i+1];
-        prod_im = data_dbl[i] * vec_dbl[i+1] + data_dbl[i+1] * vec_dbl[i];
-        // Multiply by scale
-        scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
-        scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
-        // Accumulate
-        out_dbl[i] += scaled_re;
-        out_dbl[i+1] += scaled_im;
+    double prod_re, prod_im;
+
+    if (scale == std::complex<double>(1.0, 0.0)) {
+        for (IntT i=0; i<length*2; i+=2){
+            prod_re = data_dbl[i] * vec_dbl[i] - data_dbl[i+1] * vec_dbl[i+1];
+            prod_im = data_dbl[i] * vec_dbl[i+1] + data_dbl[i+1] * vec_dbl[i];
+            out_dbl[i] += prod_re;
+            out_dbl[i+1] += prod_im;
+        }
+    } else {
+        const double * scale_dbl = reinterpret_cast<const double *>(&scale);
+        double scaled_re, scaled_im;
+        for (IntT i=0; i<length*2; i+=2){
+            prod_re = data_dbl[i] * vec_dbl[i] - data_dbl[i+1] * vec_dbl[i+1];
+            prod_im = data_dbl[i] * vec_dbl[i+1] + data_dbl[i+1] * vec_dbl[i];
+            scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
+            scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
+            out_dbl[i] += scaled_re;
+            out_dbl[i+1] += scaled_im;
+        }
     }
 }
 
@@ -59,23 +66,32 @@ void _matmul_diag_block(
 ){
     const double * data_dbl = reinterpret_cast<const double *>(data);
     const double * vec_dbl = reinterpret_cast<const double *>(vec);
-    const double * scale_dbl = reinterpret_cast<const double *>(&scale);
     double * out_dbl = reinterpret_cast<double *>(out);
     IntT ptr = 0;
-    double prod_re, prod_im, scaled_re, scaled_im;
-    // Gcc does not vectorize complex automatically?
-    for (IntT i=0; i<length; i++)
-      for (IntT j=0; j<width; j++){
-        // Compute data[ptr] * vec[i]
-        prod_re = data_dbl[ptr] * vec_dbl[2*i] - data_dbl[ptr+1] * vec_dbl[2*i+1];
-        prod_im = data_dbl[ptr] * vec_dbl[2*i+1] + data_dbl[ptr+1] * vec_dbl[2*i];
-        // Multiply by scale
-        scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
-        scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
-        // Accumulate
-        out_dbl[ptr] += scaled_re;
-        out_dbl[ptr+1] += scaled_im;
-        ptr+=2;
+    double prod_re, prod_im;
+
+    if (scale == std::complex<double>(1.0, 0.0)) {
+        for (IntT i=0; i<length; i++)
+          for (IntT j=0; j<width; j++){
+            prod_re = data_dbl[ptr] * vec_dbl[2*i] - data_dbl[ptr+1] * vec_dbl[2*i+1];
+            prod_im = data_dbl[ptr] * vec_dbl[2*i+1] + data_dbl[ptr+1] * vec_dbl[2*i];
+            out_dbl[ptr] += prod_re;
+            out_dbl[ptr+1] += prod_im;
+            ptr+=2;
+        }
+    } else {
+        const double * scale_dbl = reinterpret_cast<const double *>(&scale);
+        double scaled_re, scaled_im;
+        for (IntT i=0; i<length; i++)
+          for (IntT j=0; j<width; j++){
+            prod_re = data_dbl[ptr] * vec_dbl[2*i] - data_dbl[ptr+1] * vec_dbl[2*i+1];
+            prod_im = data_dbl[ptr] * vec_dbl[2*i+1] + data_dbl[ptr+1] * vec_dbl[2*i];
+            scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
+            scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
+            out_dbl[ptr] += scaled_re;
+            out_dbl[ptr+1] += scaled_im;
+            ptr+=2;
+        }
     }
 }
 
@@ -111,22 +127,27 @@ void _matmul_dag_diag_vector(
 ){
     const double * data_dbl = reinterpret_cast<const double *>(data);
     const double * vec_dbl = reinterpret_cast<const double *>(vec);
-    const double * scale_dbl = reinterpret_cast<const double *>(&scale);
     double * out_dbl = reinterpret_cast<double *>(out);
-    double prod_re, prod_im, scaled_re, scaled_im;
-    // Conjugate the diagonal data: conj(data[i]) * vec[i]
-    // conj(a + bi) = a - bi
-    // (a - bi) * (c + di) = ac + bd + i(ad - bc)
-    for (IntT i=0; i<length*2; i+=2){
-        // Compute conj(data[i]) * vec[i]
-        prod_re = data_dbl[i] * vec_dbl[i] + data_dbl[i+1] * vec_dbl[i+1];
-        prod_im = data_dbl[i] * vec_dbl[i+1] - data_dbl[i+1] * vec_dbl[i];
-        // Multiply by scale
-        scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
-        scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
-        // Accumulate
-        out_dbl[i] += scaled_re;
-        out_dbl[i+1] += scaled_im;
+    double prod_re, prod_im;
+
+    if (scale == std::complex<double>(1.0, 0.0)) {
+        for (IntT i=0; i<length*2; i+=2){
+            prod_re = data_dbl[i] * vec_dbl[i] + data_dbl[i+1] * vec_dbl[i+1];
+            prod_im = data_dbl[i] * vec_dbl[i+1] - data_dbl[i+1] * vec_dbl[i];
+            out_dbl[i] += prod_re;
+            out_dbl[i+1] += prod_im;
+        }
+    } else {
+        const double * scale_dbl = reinterpret_cast<const double *>(&scale);
+        double scaled_re, scaled_im;
+        for (IntT i=0; i<length*2; i+=2){
+            prod_re = data_dbl[i] * vec_dbl[i] + data_dbl[i+1] * vec_dbl[i+1];
+            prod_im = data_dbl[i] * vec_dbl[i+1] - data_dbl[i+1] * vec_dbl[i];
+            scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
+            scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
+            out_dbl[i] += scaled_re;
+            out_dbl[i+1] += scaled_im;
+        }
     }
 }
 
@@ -160,25 +181,32 @@ void _matmul_dag_diag_block(
 ){
     const double * data_dbl = reinterpret_cast<const double *>(data);
     const double * vec_dbl = reinterpret_cast<const double *>(vec);
-    const double * scale_dbl = reinterpret_cast<const double *>(&scale);
     double * out_dbl = reinterpret_cast<double *>(out);
     IntT ptr = 0;
-    double prod_re, prod_im, scaled_re, scaled_im;
-    // Conjugate the diagonal data: conj(data[i]) * vec[j]
-    // conj(a + bi) = a - bi
-    // (a - bi) * (c + di) = ac + bd + i(ad - bc)
-    for (IntT i=0; i<length; i++)
-      for (IntT j=0; j<width; j++){
-        // Compute conj(data[i]) * vec[ptr]
-        prod_re = data_dbl[2*i] * vec_dbl[ptr] + data_dbl[2*i+1] * vec_dbl[ptr+1];
-        prod_im = data_dbl[2*i] * vec_dbl[ptr+1] - data_dbl[2*i+1] * vec_dbl[ptr];
-        // Multiply by scale
-        scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
-        scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
-        // Accumulate
-        out_dbl[ptr] += scaled_re;
-        out_dbl[ptr+1] += scaled_im;
-        ptr+=2;
+    double prod_re, prod_im;
+
+    if (scale == std::complex<double>(1.0, 0.0)) {
+        for (IntT i=0; i<length; i++)
+          for (IntT j=0; j<width; j++){
+            prod_re = data_dbl[2*i] * vec_dbl[ptr] + data_dbl[2*i+1] * vec_dbl[ptr+1];
+            prod_im = data_dbl[2*i] * vec_dbl[ptr+1] - data_dbl[2*i+1] * vec_dbl[ptr];
+            out_dbl[ptr] += prod_re;
+            out_dbl[ptr+1] += prod_im;
+            ptr+=2;
+        }
+    } else {
+        const double * scale_dbl = reinterpret_cast<const double *>(&scale);
+        double scaled_re, scaled_im;
+        for (IntT i=0; i<length; i++)
+          for (IntT j=0; j<width; j++){
+            prod_re = data_dbl[2*i] * vec_dbl[ptr] + data_dbl[2*i+1] * vec_dbl[ptr+1];
+            prod_im = data_dbl[2*i] * vec_dbl[ptr+1] - data_dbl[2*i+1] * vec_dbl[ptr];
+            scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
+            scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
+            out_dbl[ptr] += scaled_re;
+            out_dbl[ptr+1] += scaled_im;
+            ptr+=2;
+        }
     }
 }
 
