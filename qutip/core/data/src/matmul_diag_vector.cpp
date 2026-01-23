b9b3d4cc6109ed4dbaddd0a2a08bd562395b5fbf
Ashley Milsted <ammilste@amazon.com>
2026-01-23 15:19:34 -0800

    Make intermediate vars conditional on arm64

--- qutip/core/data/src/matmul_diag_vector.cpp
+++ qutip/core/data/src/matmul_diag_vector.cpp
@@ -2,6 +2,14 @@
 
 #include "matmul_diag_vector.hpp"
 
+// Use intermediate variables on ARM64 (better for Apple Silicon),
+// direct accumulation on x86_64.
+#if defined(__aarch64__) || defined(__arm64__)
+#define USE_INTERMEDIATE_VARS 1
+#else
+#define USE_INTERMEDIATE_VARS 0
+#endif
+
 template <typename IntT>
 void _matmul_diag_vector(
         const std::complex<double> * _RESTRICT data,
@@ -15,25 +23,43 @@ void _matmul_diag_vector(
     const double * data_dbl = reinterpret_cast<const double *>(data);
     const double * vec_dbl = reinterpret_cast<const double *>(vec);
     double * out_dbl = reinterpret_cast<double *>(out);
+#if USE_INTERMEDIATE_VARS
     double prod_re, prod_im;
+#endif
 
     if (scale == std::complex<double>(1.0, 0.0)) {
         for (IntT i=0; i<length*2; i+=2){
+#if USE_INTERMEDIATE_VARS
             prod_re = data_dbl[i] * vec_dbl[i] - data_dbl[i+1] * vec_dbl[i+1];
             prod_im = data_dbl[i] * vec_dbl[i+1] + data_dbl[i+1] * vec_dbl[i];
             out_dbl[i] += prod_re;
             out_dbl[i+1] += prod_im;
+#else
+            out_dbl[i] += data_dbl[i] * vec_dbl[i];
+            out_dbl[i] -= data_dbl[i+1] * vec_dbl[i+1];
+            out_dbl[i+1] += data_dbl[i] * vec_dbl[i+1];
+            out_dbl[i+1] += data_dbl[i+1] * vec_dbl[i];
+#endif
         }
     } else {
         const double * scale_dbl = reinterpret_cast<const double *>(&scale);
+#if USE_INTERMEDIATE_VARS
         double scaled_re, scaled_im;
+#endif
         for (IntT i=0; i<length*2; i+=2){
+#if USE_INTERMEDIATE_VARS
             prod_re = data_dbl[i] * vec_dbl[i] - data_dbl[i+1] * vec_dbl[i+1];
             prod_im = data_dbl[i] * vec_dbl[i+1] + data_dbl[i+1] * vec_dbl[i];
             scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
             scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
             out_dbl[i] += scaled_re;
             out_dbl[i+1] += scaled_im;
+#else
+            out_dbl[i] += scale_dbl[0] * (data_dbl[i] * vec_dbl[i] - data_dbl[i+1] * vec_dbl[i+1])
+                        - scale_dbl[1] * (data_dbl[i] * vec_dbl[i+1] + data_dbl[i+1] * vec_dbl[i]);
+            out_dbl[i+1] += scale_dbl[0] * (data_dbl[i] * vec_dbl[i+1] + data_dbl[i+1] * vec_dbl[i])
+                          + scale_dbl[1] * (data_dbl[i] * vec_dbl[i] - data_dbl[i+1] * vec_dbl[i+1]);
+#endif
         }
     }
 }
@@ -70,28 +96,46 @@ void _matmul_diag_block(
     const double * vec_dbl = reinterpret_cast<const double *>(vec);
     double * out_dbl = reinterpret_cast<double *>(out);
     IntT ptr = 0;
+#if USE_INTERMEDIATE_VARS
     double prod_re, prod_im;
+#endif
 
     if (scale == std::complex<double>(1.0, 0.0)) {
         for (IntT i=0; i<length; i++)
           for (IntT j=0; j<width; j++){
+#if USE_INTERMEDIATE_VARS
             prod_re = data_dbl[ptr] * vec_dbl[2*i] - data_dbl[ptr+1] * vec_dbl[2*i+1];
             prod_im = data_dbl[ptr] * vec_dbl[2*i+1] + data_dbl[ptr+1] * vec_dbl[2*i];
             out_dbl[ptr] += prod_re;
             out_dbl[ptr+1] += prod_im;
+#else
+            out_dbl[ptr] += data_dbl[ptr] * vec_dbl[2*i];
+            out_dbl[ptr] -= data_dbl[ptr+1] * vec_dbl[2*i+1];
+            out_dbl[ptr+1] += data_dbl[ptr] * vec_dbl[2*i+1];
+            out_dbl[ptr+1] += data_dbl[ptr+1] * vec_dbl[2*i];
+#endif
             ptr+=2;
         }
     } else {
         const double * scale_dbl = reinterpret_cast<const double *>(&scale);
+#if USE_INTERMEDIATE_VARS
         double scaled_re, scaled_im;
+#endif
         for (IntT i=0; i<length; i++)
           for (IntT j=0; j<width; j++){
+#if USE_INTERMEDIATE_VARS
             prod_re = data_dbl[ptr] * vec_dbl[2*i] - data_dbl[ptr+1] * vec_dbl[2*i+1];
             prod_im = data_dbl[ptr] * vec_dbl[2*i+1] + data_dbl[ptr+1] * vec_dbl[2*i];
             scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
             scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
             out_dbl[ptr] += scaled_re;
             out_dbl[ptr+1] += scaled_im;
+#else
+            out_dbl[ptr] += scale_dbl[0] * (data_dbl[ptr] * vec_dbl[2*i] - data_dbl[ptr+1] * vec_dbl[2*i+1])
+                          - scale_dbl[1] * (data_dbl[ptr] * vec_dbl[2*i+1] + data_dbl[ptr+1] * vec_dbl[2*i]);
+            out_dbl[ptr+1] += scale_dbl[0] * (data_dbl[ptr] * vec_dbl[2*i+1] + data_dbl[ptr+1] * vec_dbl[2*i])
+                            + scale_dbl[1] * (data_dbl[ptr] * vec_dbl[2*i] - data_dbl[ptr+1] * vec_dbl[2*i+1]);
+#endif
             ptr+=2;
         }
     }
@@ -130,25 +174,43 @@ void _matmul_dag_diag_vector(
     const double * data_dbl = reinterpret_cast<const double *>(data);
     const double * vec_dbl = reinterpret_cast<const double *>(vec);
     double * out_dbl = reinterpret_cast<double *>(out);
+#if USE_INTERMEDIATE_VARS
     double prod_re, prod_im;
+#endif
 
     if (scale == std::complex<double>(1.0, 0.0)) {
         for (IntT i=0; i<length*2; i+=2){
+#if USE_INTERMEDIATE_VARS
             prod_re = data_dbl[i] * vec_dbl[i] + data_dbl[i+1] * vec_dbl[i+1];
             prod_im = data_dbl[i] * vec_dbl[i+1] - data_dbl[i+1] * vec_dbl[i];
             out_dbl[i] += prod_re;
             out_dbl[i+1] += prod_im;
+#else
+            out_dbl[i] += data_dbl[i] * vec_dbl[i];
+            out_dbl[i] += data_dbl[i+1] * vec_dbl[i+1];
+            out_dbl[i+1] += data_dbl[i] * vec_dbl[i+1];
+            out_dbl[i+1] -= data_dbl[i+1] * vec_dbl[i];
+#endif
         }
     } else {
         const double * scale_dbl = reinterpret_cast<const double *>(&scale);
+#if USE_INTERMEDIATE_VARS
         double scaled_re, scaled_im;
+#endif
         for (IntT i=0; i<length*2; i+=2){
+#if USE_INTERMEDIATE_VARS
             prod_re = data_dbl[i] * vec_dbl[i] + data_dbl[i+1] * vec_dbl[i+1];
             prod_im = data_dbl[i] * vec_dbl[i+1] - data_dbl[i+1] * vec_dbl[i];
             scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
             scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
             out_dbl[i] += scaled_re;
             out_dbl[i+1] += scaled_im;
+#else
+            out_dbl[i] += scale_dbl[0] * (data_dbl[i] * vec_dbl[i] + data_dbl[i+1] * vec_dbl[i+1])
+                        - scale_dbl[1] * (data_dbl[i] * vec_dbl[i+1] - data_dbl[i+1] * vec_dbl[i]);
+            out_dbl[i+1] += scale_dbl[0] * (data_dbl[i] * vec_dbl[i+1] - data_dbl[i+1] * vec_dbl[i])
+                          + scale_dbl[1] * (data_dbl[i] * vec_dbl[i] + data_dbl[i+1] * vec_dbl[i+1]);
+#endif
         }
     }
 }
@@ -187,28 +249,46 @@ void _matmul_dag_diag_block(
     const double * vec_dbl = reinterpret_cast<const double *>(vec);
     double * out_dbl = reinterpret_cast<double *>(out);
     IntT ptr = 0;
+#if USE_INTERMEDIATE_VARS
     double prod_re, prod_im;
+#endif
 
     if (scale == std::complex<double>(1.0, 0.0)) {
         for (IntT i=0; i<length; i++)
           for (IntT j=0; j<width; j++){
+#if USE_INTERMEDIATE_VARS
             prod_re = data_dbl[2*i] * vec_dbl[ptr] + data_dbl[2*i+1] * vec_dbl[ptr+1];
             prod_im = data_dbl[2*i] * vec_dbl[ptr+1] - data_dbl[2*i+1] * vec_dbl[ptr];
             out_dbl[ptr] += prod_re;
             out_dbl[ptr+1] += prod_im;
+#else
+            out_dbl[ptr] += data_dbl[2*i] * vec_dbl[ptr];
+            out_dbl[ptr] += data_dbl[2*i+1] * vec_dbl[ptr+1];
+            out_dbl[ptr+1] += data_dbl[2*i] * vec_dbl[ptr+1];
+            out_dbl[ptr+1] -= data_dbl[2*i+1] * vec_dbl[ptr];
+#endif
             ptr+=2;
         }
     } else {
         const double * scale_dbl = reinterpret_cast<const double *>(&scale);
+#if USE_INTERMEDIATE_VARS
         double scaled_re, scaled_im;
+#endif
         for (IntT i=0; i<length; i++)
           for (IntT j=0; j<width; j++){
+#if USE_INTERMEDIATE_VARS
             prod_re = data_dbl[2*i] * vec_dbl[ptr] + data_dbl[2*i+1] * vec_dbl[ptr+1];
             prod_im = data_dbl[2*i] * vec_dbl[ptr+1] - data_dbl[2*i+1] * vec_dbl[ptr];
             scaled_re = scale_dbl[0] * prod_re - scale_dbl[1] * prod_im;
             scaled_im = scale_dbl[0] * prod_im + scale_dbl[1] * prod_re;
             out_dbl[ptr] += scaled_re;
             out_dbl[ptr+1] += scaled_im;
+#else
+            out_dbl[ptr] += scale_dbl[0] * (data_dbl[2*i] * vec_dbl[ptr] + data_dbl[2*i+1] * vec_dbl[ptr+1])
+                          - scale_dbl[1] * (data_dbl[2*i] * vec_dbl[ptr+1] - data_dbl[2*i+1] * vec_dbl[ptr]);
+            out_dbl[ptr+1] += scale_dbl[0] * (data_dbl[2*i] * vec_dbl[ptr+1] - data_dbl[2*i+1] * vec_dbl[ptr])
+                            + scale_dbl[1] * (data_dbl[2*i] * vec_dbl[ptr] + data_dbl[2*i+1] * vec_dbl[ptr+1]);
+#endif
             ptr+=2;
         }
     }
