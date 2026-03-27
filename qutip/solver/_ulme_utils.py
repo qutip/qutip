cbc9e2ad7adbf066392457219707494a89c25009
Eric Giguere <eric.giguere@gmail.com>
2026-03-27 17:30:52 -0400

    Different method match

--- qutip/solver/_ulme_utils.py
+++ qutip/solver/_ulme_utils.py
@@ -57,47 +57,49 @@ def _make_L_prop(H: qt.QobjEvo | qt.Qobj, X: qt.QobjEvo | qt.Qobj, g: callable, 
 def _make_L_eigs(H: qt.Qobj, X: qt.Qobj, g: callable):
     vals, vecs = H.eigenstates(output_type="oper")
     X_H = (vecs.dag() @ X @ vecs).data
-    L_H = qt.data.multiply(X_H, qt.data.Dense(gw(-np.subtract.outer(vals, vals))))
-    L = vecs @ qt.Qobj(L_H, dims=a.dims) @ vecs.dag()
+    L_H = qt.data.multiply(X_H, qt.data.Dense(g(-np.subtract.outer(vals, vals))))
+    L = vecs @ qt.Qobj(L_H, dims=X.dims) @ vecs.dag()
     return L
 
 
 def _make_lambda_eigen(H, X, g):
-
     @functools.lru_cache
     def f(e1, e2, eps=1e-5):
         return integrate.quad(
             lambda w: g(w -e1) * g(w + e2),
-            -30, 30, weight='cauchy', wvar=0
-        )[0] * 2 * np.pi
+            -50, 50, weight='cauchy', wvar=0
+        )[0] / (-2 * np.pi)
 
     vals, vecs = H.eigenstates(output_type="oper")
-    X_diag = (vecs.dag() @ X @ vecs).full()
+    vecs = qt.Qobj(vecs)
+    print(vecs)
+    X_diag = qt.Qobj(vecs.dag() @ qt.Qobj(X @ vecs)).full()
     N = len(vals)
     fs = np.zeros((N, N, N), dtype=float)
     for i, j, k in itertools.product(range(N), repeat=3):
-        fs[i, j, k] = f(vals[i] - vals[k], vals[j] - vals[k])
-    LL = np.einsum("ijk,ik,kj->ij", fs, X_diag, X_diag)
-    return vecs @ qt.Qobj(LL, dims=H.dims) @ vecs.dag()
+        fs[i, j, k] = f(vals[i] - vals[j], vals[j] - vals[k])
+    LL = np.einsum("ijk,ij,jk->ik", fs, X_diag, X_diag)
+    return vecs @ qt.Qobj(qt.Qobj(LL, dims=H.dims) @ vecs.dag())
 
 
 def _make_lambda_prop(H: qt.QobjEvo, X: qt.QobjEvo, g: callable, T, Nt):
     U = qt.Propagator(H)
     if isinstance(X, qt.QobjEvo):
-        def op(t, s, sp):
+        def op(s, sp, t):
             if s == sp:
                 return 0
-            return U(t, s + t) @ X(s + t) @ U(s + t, sp + t) @ X(sp + t) @ U(sp + t, t) * (g(s) * g(-sp) * (-1 + 2 * (s > sp)))
+            return U(t, s + t) @ X(s + t) @ U(s + t, sp + t) @ X(sp + t) @ U(sp + t, t) * (g(s) * g(-sp) * np.sign(s - sp) )
     else:
-        def op(t, s, sp):
+        def op(s, sp, t):
             if s == sp:
                 return 0
-            return U(t, s + t) @ X @ U(s + t, sp + t) @ X @ U(sp + t, t) * (g(s) * g(-sp) * (-1 + 2 * (s > sp)))
+            return U(t+ s, t ) * X * U(sp + t, s + t) * X * U(t, t + sp) * (g(s) * g(-sp) * np.sign(s - sp) )
 
     if isinstance(H,  qt.QobjEvo) or isinstance(X, qt.QobjEvo):
         return lambda t: integrate_2d_flat(op, t, T, Nt) * -0.5j
 
-    return integrate_2d_flat(op, 0, T, Nt)(0) * -0.5j
+    return integrate_2d_flat(op, 0, T, Nt) * -0.5j
+
 
 def integrate_2d_flat(op, t, T, Nt):
     ts = np.linspace(-T, T, Nt)
