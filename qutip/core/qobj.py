03c5a32539ddccf5956ec1bf6cbac817fcd3b777
Veronika Kurth <veronika.kurth@posteo.de>
2026-07-29 11:33:07 +0900

    Fix 'Block quote ends without a blank line' warning detected by docutils; now doc build should work

--- qutip/core/qobj.py
+++ qutip/core/qobj.py
@@ -951,8 +951,7 @@ class Qobj:
 
         Returns
         -------
-        data : numpy.ndarray, scipy.sparse.csr(dia)_array, or
-               scipy.sparse.csr(dia)_matrix, etc.
+        data : numpy.ndarray, scipy.sparse.csr(dia)_array, scipy.sparse.csr(dia)_matrix, etc.
             Matrix in the type of the underlying libraries.
         """
         return _data.extract(self._data, format, copy)
