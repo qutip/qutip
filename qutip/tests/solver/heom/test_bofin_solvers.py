7e8000060eb584a82f4de37b1163113f878c7daf
steps-re <mike@stepsventures.com>
2026-07-29 22:44:26 -0400

    fix(heom): always initialize final_ado_state/ado_states on HEOMResult (#2933)
    * fix(heom): always initialize final_ado_state/ado_states on HEOMResult
    
    Result objects expose all attributes even when the corresponding data
    is not stored (e.g. Result.states is [] when store_states is False),
    but HEOMResult only created _final_ado_state and ado_states when
    store_ados was True, so accessing final_ado_state with store_ados=False
    raised AttributeError instead of returning None.
    
    Follow-up to #2931, as suggested by @pmenczel in review.
    
    Signed-off-by: Mike German <mike@stepsventures.com>
    
    * doc: add towncrier entry for #2933
    
    Signed-off-by: Mike German <mike@stepsventures.com>
    
    * refactor: move change rationale from code comments to changelog per review
    
    Signed-off-by: Mike German <mike@stepsventures.com>
    
    ---------
    
    Signed-off-by: Mike German <mike@stepsventures.com>

--- qutip/tests/solver/heom/test_bofin_solvers.py
+++ qutip/tests/solver/heom/test_bofin_solvers.py
@@ -1794,8 +1794,8 @@ class TestHEOMResult:
     def test_create_ado_states_attribute(self):
         options = fill_options()
         result = HEOMResult(e_ops=[], options=options)
-        assert not hasattr(result, "final_ado_state")
-        assert not hasattr(result, "ado_states")
+        assert result.final_ado_state is None
+        assert result.ado_states == []
         assert result.store_ados is False
 
         options = fill_options(store_ados=True)
@@ -1803,6 +1803,20 @@ class TestHEOMResult:
         assert result.final_ado_state is None
         assert result.ado_states == []
         assert result.store_ados is True
+
+    def test_final_ado_state_none_when_ados_not_stored(self):
+        options = fill_options()
+        result = HEOMResult(e_ops=[], options=options)
+
+        ados = self.mk_ados([2, 3], max_depth=2)
+        rho, ado_soln = self.mk_rho_and_soln(ados, [[2], [2]])
+        ado_state = HierarchyADOsState(rho, ados, ado_soln)
+
+        result.add(0.1, ado_state)
+
+        assert result.final_state is rho
+        assert result.final_ado_state is None
+        assert result.ado_states == []
 
     @pytest.mark.parametrize(['e_op_type'], [
         pytest.param("qobj", id="qobj"),
