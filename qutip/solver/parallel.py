427df077a0c3108ae1396edcff3ce79b7f5bd85c
Veronika Kurth <veronika.kurth@posteo.de>
2026-09-15 12:56:27 +0900

    Since Python 3.14, forkserver is default for POSIX (except on macOS)

--- qutip/solver/parallel.py
+++ qutip/solver/parallel.py
@@ -15,12 +15,7 @@ import warnings
 from qutip.ui.progressbar import progress_bars
 from qutip.settings import available_cpu_count
 
-if sys.platform == 'darwin':
-    mp_context = multiprocessing.get_context('fork')
-elif sys.platform == 'linux':
-    # forkserver would handle threads better, but is much slower at starting
-    # the executor and spawning tasks
-    #mp_context = multiprocessing.get_context('fork')
+if sys.platform == 'linux':
     mp_context = multiprocessing.get_context('forkserver')
 else:
     mp_context = multiprocessing.get_context()
