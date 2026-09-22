5fcd03257894a84499e9a5c48e531ca65dac0133
Mayank Goel <mayank.goel447@gmail.com>
2026-09-23 04:10:53 +0530

    Minor typehinting for distributions

--- qutip/_distributions.pyx
+++ qutip/_distributions.pyx
@@ -42,9 +42,9 @@ cpdef np.ndarray[np.complex128_t, ndim=1] psi_n_single_fock_multiple_position_co
       015402. doi:10.1088/1361-6404/aa9584
     """
     
-    x_size = x.shape[0]
+    cdef const int x_size = x.shape[0]
     cdef np.ndarray[np.complex128_t, ndim=2] result = np.zeros((n + 1, x_size), dtype=np.complex128)
-    pi_025 = pi ** (-0.25)
+    cdef const double pi_025 = pi ** (-0.25)
 
     for j in range(x_size):
         result[0, j] = pi_025 * cexp(-(x[j] ** 2) / 2)
